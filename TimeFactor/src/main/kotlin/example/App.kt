cfdace554cbb13791f819da7d4254aab1bfb85cf
aterai <aterai@outlook.com>
2019-01-25 04:07:05 +0900

    refactor: use safe calls instead of Optional

--- TimeFactor/src/main/kotlin/example/App.kt
+++ TimeFactor/src/main/kotlin/example/App.kt
@@ -1,15 +1,14 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.util.Optional
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 class MainPanel : JPanel(BorderLayout()) {
-  private val spinner = JSpinner()
+  private val spinner: JSpinner? = JSpinner()
 
   init {
     val lv = UIManager.get("Tree.timeFactor") as Number
-    spinner.setModel(SpinnerNumberModel(lv, 0L, 5000L, 500L))
+    spinner?.setModel(SpinnerNumberModel(lv, 0L, 5000L, 500L))
     UIManager.put("List.timeFactor", 5000L)
 
     val model = arrayOf("a", "aa", "b", "bbb", "bbc")
@@ -34,19 +33,27 @@ class MainPanel : JPanel(BorderLayout()) {
 
   override fun updateUI() {
     // Unnecessary safe call on a non-null receiver of type JSpinner
+    // private val spinner = JSpinner()
     // val lv = spinner
     //     ?.let { it.getModel().getValue() }
     //     ?: 1000L
 
     // NullPointerException
+    // private val spinner = JSpinner()
     // val lv = spinner.getModel().getValue()
 
     // Condition 'spinner != null' is always 'true'
+    // private val spinner = JSpinner()
     // val lv = if (spinner != null) spinner.getModel().getValue() else 1000L
 
-    val lv = Optional.ofNullable(spinner)
-        .map { it.getModel().getValue() }
-        .orElse(1000L)
+    // private val spinner = JSpinner()
+    // val lv = Optional.ofNullable(spinner)
+    //     .map { it.getModel().getValue() }
+    //     .orElse(1000L)
+
+    // private val spinner: JSpinner? = JSpinner()
+    val lv = spinner?.getModel()?.getValue() ?: 1000L
+
     UIManager.put("ComboBox.timeFactor", lv)
     UIManager.put("List.timeFactor", lv)
     UIManager.put("Table.timeFactor", lv)
