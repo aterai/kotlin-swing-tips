fa56e8f154e67bca4664190721f533f75d66652c
aterai <aterai@outlook.com>
2019-01-27 01:55:11 +0900

    add: ProgressCircle

---
+++ ProgressCircle/src/main/kotlin/example/App.kt
@@ -0,0 +1,178 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.geom.Arc2D
+import java.awt.geom.Area
+import java.awt.geom.Ellipse2D
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
+import java.util.Random
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.basic.BasicProgressBarUI
+
+class MainPanel : JPanel(BorderLayout()) {
+  protected val progress1: JProgressBar = object : JProgressBar() {
+    override fun updateUI() {
+      super.updateUI()
+      setUI(ProgressCircleUI())
+      setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25))
+    }
+  }
+  protected val progress2: JProgressBar = object : JProgressBar() {
+    override fun updateUI() {
+      super.updateUI()
+      setUI(ProgressCircleUI())
+      setBorder(BorderFactory.createEmptyBorder(25, 25, 25, 25))
+    }
+  }
+
+  init {
+    progress1.setForeground(Color(0xFF_AA_AA))
+    progress2.setStringPainted(true)
+    progress2.setFont(progress2.getFont().deriveFont(24f))
+
+    val slider = JSlider()
+    slider.putClientProperty("Slider.paintThumbArrowShape", java.lang.Boolean.TRUE)
+    progress1.setModel(slider.getModel())
+
+    val button = JButton("start")
+    button.addActionListener({ e ->
+      val b = e.getSource() as JButton
+      b.setEnabled(false)
+      val worker = object : BackgroundTask() {
+        override fun done() {
+          if (b.isDisplayable()) {
+            b.setEnabled(true)
+          }
+        }
+      }
+      worker.addPropertyChangeListener(ProgressListener(progress2))
+      worker.execute()
+    })
+
+    add(slider, BorderLayout.NORTH)
+    add(JPanel(GridLayout(1, 2)).apply {
+      add(progress1)
+      add(progress2)
+    })
+    add(button, BorderLayout.SOUTH)
+    setPreferredSize(Dimension(320, 240))
+  }
+}
+
+internal class ProgressCircleUI : BasicProgressBarUI() {
+  override fun getPreferredSize(c: JComponent): Dimension {
+    val d = super.getPreferredSize(c)
+    val v = Math.max(d.width, d.height)
+    d.setSize(v, v)
+    return d
+  }
+
+  override fun paint(g: Graphics, c: JComponent) {
+    // public void paintDeterminate(Graphics g, JComponent c) {
+    val b = progressBar.getInsets() // area for border
+    val barRectWidth = progressBar.getWidth() - b.right - b.left
+    val barRectHeight = progressBar.getHeight() - b.top - b.bottom
+    if (barRectWidth <= 0 || barRectHeight <= 0) {
+      return
+    }
+
+    val g2 = g.create() as Graphics2D
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+
+    val degree = 360 * progressBar.getPercentComplete()
+    val sz = Math.min(barRectWidth, barRectHeight).toDouble()
+    val cx = b.left + barRectWidth * .5
+    val cy = b.top + barRectHeight * .5
+    val or = sz * .5
+    // double ir = or - 20;
+    val ir = or * .5 // .8;
+    val inner = Ellipse2D.Double(cx - ir, cy - ir, ir * 2, ir * 2)
+    val outer = Ellipse2D.Double(cx - or, cy - or, sz, sz)
+    val sector = Arc2D.Double(cx - or, cy - or, sz, sz, 90 - degree, degree, Arc2D.PIE)
+
+    val foreground = Area(sector)
+    val background = Area(outer)
+    val hole = Area(inner)
+
+    foreground.subtract(hole)
+    background.subtract(hole)
+
+    // draw the track
+    g2.setPaint(Color(0xDD_DD_DD))
+    g2.fill(background)
+
+    // draw the circular sector
+    // AffineTransform at = AffineTransform.getScaleInstance(-1.0, 1.0);
+    // at.translate(-(barRectWidth + b.left * 2), 0);
+    // AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(degree), cx, cy);
+    // g2.fill(at.createTransformedShape(area));
+    g2.setPaint(progressBar.getForeground())
+    g2.fill(foreground)
+    g2.dispose()
+
+    // Deal with possible text painting
+    if (progressBar.isStringPainted()) {
+      paintString(g, b.left, b.top, barRectWidth, barRectHeight, 0, b)
+    }
+  }
+}
+
+open class BackgroundTask : SwingWorker<String, Void>() {
+  private val rnd = Random()
+
+  override fun doInBackground(): String {
+    var current = 0
+    val lengthOfTask = 100
+    while (current <= lengthOfTask && !isCancelled()) {
+      try { // dummy task
+        Thread.sleep((rnd.nextInt(50) + 1).toLong())
+      } catch (ex: InterruptedException) {
+        return "Interrupted"
+      }
+
+      setProgress(100 * current / lengthOfTask)
+      current++
+    }
+    return "Done"
+  }
+}
+
+internal class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
+
+  init {
+    this.progressBar.setValue(0)
+  }
+
+  override fun propertyChange(e: PropertyChangeEvent) {
+    val strPropertyName = e.getPropertyName()
+    if ("progress" == strPropertyName) {
+      progressBar.setIndeterminate(false)
+      val progress = e.getNewValue() as Int
+      progressBar.setValue(progress)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater({
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  })
+}
