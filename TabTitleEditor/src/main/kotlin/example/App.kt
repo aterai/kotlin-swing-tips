e476ebb64a1a75c7b8dcf050505a54176501fedf
aterai <aterai@outlook.com>
2019-02-14 19:42:25 +0900

    add: TabTitleEditor

---
+++ TabTitleEditor/src/main/kotlin/example/App.kt
@@ -0,0 +1,161 @@
+// -*- mode:java; encoding:utf-8 -*-
+// vim:set fileencoding=utf-8:
+// @homepage@
+
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.FocusAdapter
+import java.awt.event.FocusEvent
+import java.awt.event.KeyEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.event.ChangeEvent
+import javax.swing.event.ChangeListener
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    add(JTabbedPane().apply {
+      val l = TabTitleEditListener(this)
+      addChangeListener(l)
+      addMouseListener(l)
+      addTab("Shortcuts", JTextArea(INFO))
+      addTab("badfasdfa", JLabel("bbbbbbbbbbbafasdf"))
+      addTab("cccc", JScrollPane(JTree()))
+      addTab("dddddddd", JLabel("dadfasdfasd"))
+    })
+    setPreferredSize(Dimension(320, 240))
+  }
+
+  companion object {
+    private const val INFO = """ Start editing: Double-Click, Enter-Key
+ Commit rename: field-focusLost, Enter-Key
+Cancel editing: Esc-Key, title.isEmpty
+"""
+  }
+}
+
+class TabTitleEditListener(val tabbedPane: JTabbedPane) : MouseAdapter(), ChangeListener, DocumentListener {
+  protected val editor = JTextField()
+  protected var editingIdx = -1
+  protected var len = -1
+  protected var dim: Dimension? = null
+  protected var tabComponent: Component? = null
+  protected val startEditing: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      editingIdx = tabbedPane.getSelectedIndex()
+      tabComponent = tabbedPane.getTabComponentAt(editingIdx)
+      tabbedPane.setTabComponentAt(editingIdx, editor)
+      editor.setVisible(true)
+      editor.setText(tabbedPane.getTitleAt(editingIdx))
+      editor.selectAll()
+      editor.requestFocusInWindow()
+      len = editor.getText().length
+      dim = editor.getPreferredSize()
+      editor.setMinimumSize(dim)
+    }
+  }
+  protected val renameTabTitle: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      val title = editor.getText().trim { it <= ' ' }
+      if (editingIdx >= 0 && !title.isEmpty()) {
+        tabbedPane.setTitleAt(editingIdx, title)
+      }
+      cancelEditing.actionPerformed(ActionEvent(tabbedPane, ActionEvent.ACTION_PERFORMED, CANCEL))
+    }
+  }
+  protected val cancelEditing: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      if (editingIdx >= 0) {
+        tabbedPane.setTabComponentAt(editingIdx, tabComponent)
+        editor.setVisible(false)
+        editingIdx = -1
+        len = -1
+        tabComponent = null
+        editor.setPreferredSize(null)
+        tabbedPane.requestFocusInWindow()
+      }
+    }
+  }
+
+  init {
+    editor.setBorder(BorderFactory.createEmptyBorder())
+    editor.addFocusListener(object : FocusAdapter() {
+      override fun focusLost(e: FocusEvent?) {
+        renameTabTitle.actionPerformed(ActionEvent(tabbedPane, ActionEvent.ACTION_PERFORMED, RENAME))
+      }
+    })
+    val im = editor.getInputMap(JComponent.WHEN_FOCUSED)
+    val am = editor.getActionMap()
+    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), CANCEL)
+    am.put(CANCEL, cancelEditing)
+    im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), RENAME)
+    am.put(RENAME, renameTabTitle)
+    editor.getDocument().addDocumentListener(this)
+    tabbedPane.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), START)
+    tabbedPane.getActionMap().put(START, startEditing)
+  }
+
+  override fun stateChanged(e: ChangeEvent) {
+    renameTabTitle.actionPerformed(ActionEvent(tabbedPane, ActionEvent.ACTION_PERFORMED, RENAME))
+  }
+
+  override fun insertUpdate(e: DocumentEvent) {
+    updateTabSize()
+  }
+
+  override fun removeUpdate(e: DocumentEvent) {
+    updateTabSize()
+  }
+
+  override fun changedUpdate(e: DocumentEvent) { /* not needed */
+  }
+
+  override fun mouseClicked(e: MouseEvent) {
+    val r = tabbedPane.getBoundsAt(tabbedPane.getSelectedIndex())
+    val isDoubleClick = e.getClickCount() >= 2
+    if (isDoubleClick && r.contains(e.getPoint())) {
+      startEditing.actionPerformed(ActionEvent(tabbedPane, ActionEvent.ACTION_PERFORMED, START))
+    } else {
+      renameTabTitle.actionPerformed(ActionEvent(tabbedPane, ActionEvent.ACTION_PERFORMED, RENAME))
+    }
+  }
+
+  protected fun updateTabSize() {
+    editor.setPreferredSize(if (editor.getText().length > len) null else dim)
+    tabbedPane.revalidate()
+  }
+
+  companion object {
+    protected const val START = "start-editing"
+    protected const val CANCEL = "cancel-editing"
+    protected const val RENAME = "rename-tab-title"
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
