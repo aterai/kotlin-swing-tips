d73e6dbc7b66ad23f4dcfc5f11b6d1b144d56c5d
aterai <aterai@outlook.com>
2019-02-14 19:30:47 +0900

    add: EditTabTitle

---
+++ EditTabTitle/src/main/kotlin/example/App.kt
@@ -0,0 +1,131 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    val a = JTextArea(INFO)
+    a.setEditable(false)
+    val tabbedPane = EditableTabbedPane().apply {
+      addTab("Shortcuts", JScrollPane(a))
+      addTab("badfasdf", JLabel("bbbbbbbbbbbafasdf"))
+      addTab("cccc", JScrollPane(JTree()))
+      addTab("ddddddd", JButton("dadfasdfasd"))
+    }
+    add(tabbedPane)
+    setPreferredSize(Dimension(320, 240))
+  }
+
+  companion object {
+    private const val INFO = """Start editing: Double-Click, Enter-Key
+Commit rename: field-focusLost, Enter-Key
+Cancel editing: Esc-Key, title.isEmpty
+"""
+  }
+}
+
+internal class EditableTabbedPane : JTabbedPane() {
+  protected val glassPane: Container = EditorGlassPane()
+  protected val editor = JTextField()
+  protected val startEditing: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      getRootPane().setGlassPane(glassPane)
+      val rect = getBoundsAt(getSelectedIndex())
+      val p = SwingUtilities.convertPoint(this@EditableTabbedPane, rect.getLocation(), glassPane)
+      rect.setLocation(p)
+      rect.grow(-2, -2)
+      editor.setBounds(rect)
+      editor.setText(getTitleAt(getSelectedIndex()))
+      editor.selectAll()
+      glassPane.add(editor)
+      glassPane.setVisible(true)
+      editor.requestFocusInWindow()
+    }
+  }
+  protected val cancelEditing: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      glassPane.setVisible(false)
+    }
+  }
+  protected val renameTab: Action = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      if (!editor.getText().trim { it <= ' ' }.isEmpty()) {
+        setTitleAt(getSelectedIndex(), editor.getText())
+        val c = getTabComponentAt(getSelectedIndex())
+        if (c is JComponent) {
+          c.revalidate()
+        }
+      }
+      glassPane.setVisible(false)
+    }
+  }
+
+  init {
+    editor.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 3))
+    editor.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "rename-tab")
+    editor.getActionMap().put("rename-tab", renameTab)
+    editor.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "cancel-editing")
+    editor.getActionMap().put("cancel-editing", cancelEditing)
+
+    addMouseListener(object : MouseAdapter() {
+      override fun mouseClicked(e: MouseEvent) {
+        val isDoubleClick = e.getClickCount() >= 2
+        if (isDoubleClick) {
+          startEditing.actionPerformed(ActionEvent(e.getComponent(), ActionEvent.ACTION_PERFORMED, ""))
+        }
+      }
+    })
+    getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "start-editing")
+    getActionMap().put("start-editing", startEditing)
+  }
+
+  private inner class EditorGlassPane : JComponent() {
+    init {
+      setOpaque(false)
+      setFocusTraversalPolicy(object : DefaultFocusTraversalPolicy() {
+        override fun accept(c: Component) = c == editor
+      })
+      addMouseListener(object : MouseAdapter() {
+        override fun mouseClicked(e: MouseEvent) {
+          if (!editor.getBounds().contains(e.getPoint())) {
+            renameTab.actionPerformed(ActionEvent(e.getComponent(), ActionEvent.ACTION_PERFORMED, ""))
+          }
+        }
+      })
+    }
+
+    override fun setVisible(flag: Boolean) {
+      super.setVisible(flag)
+      setFocusTraversalPolicyProvider(flag)
+      setFocusCycleRoot(flag)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
