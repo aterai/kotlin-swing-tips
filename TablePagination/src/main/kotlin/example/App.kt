cb86cb25da22a14fdc138fe00b1a1981323db2f6
aterai <aterai@outlook.com>
2019-02-19 20:17:50 +0900

    refactor: unnecessary safe call on a non-null

--- TablePagination/src/main/kotlin/example/App.kt
+++ TablePagination/src/main/kotlin/example/App.kt
@@ -1,7 +1,6 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.util.Objects
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicHTML
 import javax.swing.plaf.basic.BasicRadioButtonUI
@@ -104,7 +103,7 @@ class MainPanel : JPanel(BorderLayout()) {
   }
 
   private fun makeRadioButton(itemsPerPage: Int, current: Int, target: Int): JRadioButton {
-    val radio = object : JRadioButton(Objects.toString(target)) {
+    val radio = object : JRadioButton(target.toString()) {
       protected override fun fireStateChanged() {
         val bm = getModel()
         if (bm.isEnabled()) {
