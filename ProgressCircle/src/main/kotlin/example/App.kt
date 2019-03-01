a412434b32e4a9852e3c4b518f79084811cf8d0d
aterai <aterai@outlook.com>
2019-03-02 00:53:11 +0900

    detekt: LongMethod

--- ProgressCircle/src/main/kotlin/example/App.kt
+++ ProgressCircle/src/main/kotlin/example/App.kt
@@ -4,6 +4,7 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.geom.Arc2D
 import java.awt.geom.Area
 import java.awt.geom.Ellipse2D
+import java.awt.geom.Point2D
 import java.beans.PropertyChangeEvent
 import java.beans.PropertyChangeListener
 import java.util.Random
@@ -61,55 +62,46 @@ class MainPanel : JPanel(BorderLayout()) {
 }
 
 internal class ProgressCircleUI : BasicProgressBarUI() {
-  override fun getPreferredSize(c: JComponent): Dimension {
-    val d = super.getPreferredSize(c)
-    val v = Math.max(d.width, d.height)
-    d.setSize(v, v)
-    return d
+  override fun getPreferredSize(c: JComponent) = super.getPreferredSize(c).apply {
+    val v = Math.max(width, height)
+    setSize(v, v)
   }
 
   override fun paint(g: Graphics, c: JComponent) {
-    // public void paintDeterminate(Graphics g, JComponent c) {
     val b = progressBar.getInsets() // area for border
     val barRectWidth = progressBar.getWidth() - b.right - b.left
     val barRectHeight = progressBar.getHeight() - b.top - b.bottom
-    if (barRectWidth <= 0 || barRectHeight <= 0) {
-      return
-    }
+    // if (barRectWidth <= 0 || barRectHeight <= 0) {
+    //   return
+    // }
 
-    val g2 = g.create() as Graphics2D
+    val g2 = g as Graphics2D
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 
     val degree = 360 * progressBar.getPercentComplete()
     val sz = Math.min(barRectWidth, barRectHeight).toDouble()
-    val cx = b.left + barRectWidth * .5
-    val cy = b.top + barRectHeight * .5
-    val or = sz * .5
-    // double ir = or - 20;
-    val ir = or * .5 // .8;
-    val inner = Ellipse2D.Double(cx - ir, cy - ir, ir * 2, ir * 2)
-    val outer = Ellipse2D.Double(cx - or, cy - or, sz, sz)
-    val sector = Arc2D.Double(cx - or, cy - or, sz, sz, 90 - degree, degree, Arc2D.PIE)
+    val cp = Point2D.Double(b.left + barRectWidth * .5, b.top + barRectHeight * .5)
+    val r = sz * .5
 
-    val foreground = Area(sector)
-    val background = Area(outer)
-    val hole = Area(inner)
+    val track = Area(Ellipse2D.Double(cp.x - r, cp.y - r, sz, sz))
+    val sector = Area(Arc2D.Double(cp.x - r, cp.y - r, sz, sz, 90 - degree, degree, Arc2D.PIE))
+    val hole = Area(Ellipse2D.Double(cp.x - r * .5, cp.y - r * .5, r, r))
 
-    foreground.subtract(hole)
-    background.subtract(hole)
+    track.subtract(hole)
+    sector.subtract(hole)
 
     // draw the track
     g2.setPaint(Color(0xDD_DD_DD))
-    g2.fill(background)
+    g2.fill(track)
 
     // draw the circular sector
     // AffineTransform at = AffineTransform.getScaleInstance(-1.0, 1.0);
     // at.translate(-(barRectWidth + b.left * 2), 0);
-    // AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(degree), cx, cy);
+    // AffineTransform at = AffineTransform.getRotateInstance(Math.toRadians(degree), cp.x, cp.y);
     // g2.fill(at.createTransformedShape(area));
     g2.setPaint(progressBar.getForeground())
-    g2.fill(foreground)
-    g2.dispose()
+    g2.fill(sector)
+    // g2.dispose()
 
     // Deal with possible text painting
     if (progressBar.isStringPainted()) {
@@ -139,7 +131,6 @@ open class BackgroundTask : SwingWorker<String, Void>() {
 }
 
 internal class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
-
   init {
     this.progressBar.setValue(0)
   }
