948226681be161116d644631edbda4608f4e323d
aterai <aterai@outlook.com>
2019-03-11 17:49:28 +0900

    detekt: LongMethod

--- ComboBoxEditorVerifier/src/main/kotlin/example/App.kt
+++ ComboBoxEditorVerifier/src/main/kotlin/example/App.kt
@@ -124,26 +124,24 @@ internal class LengthInputVerifier : InputVerifier() {
 }
 
 fun main() {
-  EventQueue.invokeLater(object : Runnable {
-    override fun run() {
-      try {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-      } catch (ex: ClassNotFoundException) {
-        ex.printStackTrace()
-      } catch (ex: InstantiationException) {
-        ex.printStackTrace()
-      } catch (ex: IllegalAccessException) {
-        ex.printStackTrace()
-      } catch (ex: UnsupportedLookAndFeelException) {
-        ex.printStackTrace()
-      }
-      JFrame().apply {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-        getContentPane().add(MainPanel())
-        pack()
-        setLocationRelativeTo(null)
-        setVisible(true)
-      }
+  EventQueue.invokeLater {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
     }
-  })
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
 }
