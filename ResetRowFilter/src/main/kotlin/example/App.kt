78451d53752933ac2f8cbbad3c80f3567e1f71fb
aterai <aterai@outlook.com>
2019-03-17 18:22:45 +0900

    add: ResetRowFilter

---
+++ ResetRowFilter/src/main/kotlin/example/App.kt
@@ -0,0 +1,90 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableModel
+import javax.swing.table.TableRowSorter
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    val check1 = JCheckBox("Custom Sorting")
+
+    val columnNames = arrayOf("String", "Integer", "Boolean")
+    val data = arrayOf(
+      arrayOf<Any>("AA", 1, true), arrayOf<Any>("BB", 2, false),
+      arrayOf<Any>("cc", 3, true), arrayOf<Any>("dd", 4, false),
+      arrayOf<Any>("ee", 5, false), arrayOf<Any>("FF", -1, true),
+      arrayOf<Any>("GG", -2, false), arrayOf<Any>("HH", -3, true),
+      arrayOf<Any>("II", -4, false), arrayOf<Any>("JJ", -5, false),
+      arrayOf<Any>("KK", 11, true), arrayOf<Any>("LL", 22, false),
+      arrayOf<Any>("MM", 33, true), arrayOf<Any>("NN", 44, false),
+      arrayOf<Any>("OO", 55, false)
+    )
+    val model = object : DefaultTableModel(data, columnNames) {
+      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+    }
+    val table = JTable(model)
+    table.setFillsViewportHeight(true)
+    // XXX: sorter.setSortsOnUpdates(true)
+
+    val filter = object : RowFilter<TableModel, Int>() {
+      override fun include(entry: Entry<out TableModel, out Int>): Boolean {
+        val vidx = table.convertRowIndexToView(entry.getIdentifier().toInt())
+        return vidx < USER_SPECIFIED_NUMBER_OF_ROWS
+      }
+    }
+    val sorter = object : TableRowSorter<TableModel>(model) {
+      override fun toggleSortOrder(column: Int) {
+        super.toggleSortOrder(column)
+        if (check1.isSelected()) {
+          model.fireTableDataChanged()
+          sort() // allRowsChanged()
+        }
+      }
+    }
+
+    table.setRowSorter(sorter)
+    sorter.setSortKeys(listOf(RowSorter.SortKey(1, SortOrder.DESCENDING)))
+
+    val check2 = JCheckBox("viewRowIndex < $USER_SPECIFIED_NUMBER_OF_ROWS")
+    check2.addActionListener {
+      sorter.setRowFilter((it.getSource() as? JCheckBox)?.isSelected()?.let { filter })
+    }
+
+    val box = Box.createHorizontalBox()
+    box.add(check1)
+    box.add(Box.createHorizontalStrut(5))
+    box.add(check2)
+    add(box, BorderLayout.NORTH)
+    add(JScrollPane(table))
+    setPreferredSize(Dimension(320, 240))
+  }
+
+  companion object {
+    private const val USER_SPECIFIED_NUMBER_OF_ROWS = 5
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
