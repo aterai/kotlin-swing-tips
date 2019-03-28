19c4e8196cfd791d67e2fd57c26182d5ef6350f7
aterai <aterai@outlook.com>
2019-03-28 18:41:36 +0900

    add: MultipleButtonsInTableCell

---
+++ MultipleButtonsInTableCell/src/main/kotlin/example/App.kt
@@ -0,0 +1,151 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellEditor
+import javax.swing.table.TableCellRenderer
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    val empty = ""
+    val columnNames = arrayOf("String", "Button")
+    val data = arrayOf(
+        arrayOf<Any>("AAA", empty),
+        arrayOf<Any>("CCC", empty),
+        arrayOf<Any>("BBB", empty),
+        arrayOf<Any>("ZZZ", empty))
+    val model = object : DefaultTableModel(data, columnNames) {
+      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+    }
+    val table = object : JTable(model) {
+      override fun updateUI() {
+        super.updateUI()
+        setRowHeight(36)
+        setAutoCreateRowSorter(true)
+        getColumnModel().getColumn(1)?.also {
+          it.setCellRenderer(ButtonsRenderer())
+          it.setCellEditor(ButtonsEditor(this))
+        }
+      }
+    }
+
+    add(JScrollPane(table))
+    setBorder(BorderFactory.createTitledBorder("Multiple Buttons in a Table Cell"))
+    setPreferredSize(Dimension(320, 240))
+  }
+}
+
+open class ButtonsPanel : JPanel() {
+  val buttons = listOf(JButton("view"), JButton("edit"))
+
+  init {
+    setOpaque(true)
+    for (b in buttons) {
+      b.setFocusable(false)
+      b.setRolloverEnabled(false)
+      add(b)
+    }
+  }
+}
+
+internal class ButtonsRenderer : TableCellRenderer {
+  private val panel = object : ButtonsPanel() {
+    override fun updateUI() {
+      super.updateUI()
+      setName("Table.cellRenderer")
+    }
+  }
+
+  override fun getTableCellRendererComponent(
+    table: JTable,
+    value: Any,
+    isSelected: Boolean,
+    hasFocus: Boolean,
+    row: Int,
+    column: Int
+  ): Component {
+    panel.setBackground(if (isSelected) table.getSelectionBackground() else table.getBackground())
+    return panel
+  }
+}
+
+internal class ViewAction(private val table: JTable) : AbstractAction("view") {
+  override fun actionPerformed(e: ActionEvent) {
+    JOptionPane.showMessageDialog(table, "Viewing")
+  }
+}
+
+internal class EditAction(private val table: JTable) : AbstractAction("edit") {
+  override fun actionPerformed(e: ActionEvent) {
+    // Object o = table.getModel().getValueAt(table.getSelectedRow(), 0);
+    val row = table.convertRowIndexToModel(table.getEditingRow())
+    val o = table.getModel().getValueAt(row, 0)
+    JOptionPane.showMessageDialog(table, "Editing: $o")
+  }
+}
+
+internal class ButtonsEditor(protected val table: JTable) : AbstractCellEditor(), TableCellEditor {
+  protected val panel = ButtonsPanel()
+
+  private inner class EditingStopHandler : MouseAdapter(), ActionListener {
+    override fun mousePressed(e: MouseEvent) {
+      val o = e.getSource()
+      if (o is TableCellEditor) {
+        actionPerformed(ActionEvent(o, ActionEvent.ACTION_PERFORMED, ""))
+      } else if (o is JButton) {
+        if (o.getModel().isPressed() && table.isRowSelected(table.getEditingRow()) && e.isControlDown()) {
+          panel.setBackground(table.getBackground())
+        }
+      }
+    }
+
+    override fun actionPerformed(e: ActionEvent) {
+      EventQueue.invokeLater { fireEditingStopped() }
+    }
+  }
+
+  init {
+    panel.buttons.get(0).setAction(ViewAction(table))
+    panel.buttons.get(1).setAction(EditAction(table))
+
+    val handler = EditingStopHandler()
+    for (b in panel.buttons) {
+      b.addMouseListener(handler)
+      b.addActionListener(handler)
+    }
+    panel.addMouseListener(handler)
+  }
+
+  override fun getTableCellEditorComponent(tbl: JTable, value: Any, isSelected: Boolean, row: Int, column: Int) =
+      panel.also { it.setBackground(tbl.getSelectionBackground()) }
+
+  override fun getCellEditorValue() = ""
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    } catch (ex: ClassNotFoundException) {
+      ex.printStackTrace()
+    } catch (ex: InstantiationException) {
+      ex.printStackTrace()
+    } catch (ex: IllegalAccessException) {
+      ex.printStackTrace()
+    } catch (ex: UnsupportedLookAndFeelException) {
+      ex.printStackTrace()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
