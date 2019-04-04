1de9ddde8c400152fa1e93a93abfc02abb737ee0
aterai <aterai@outlook.com>
2019-04-04 15:02:06 +0900

    add: WindowOpacity

---
+++ WindowOpacity/src/main/kotlin/example/App.kt
@@ -0,0 +1,118 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ItemEvent
+import java.awt.image.BufferedImage
+import java.io.IOException
+import java.util.Optional
+import javax.imageio.ImageIO
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+class MainPanel : JPanel() {
+  @Transient
+  private val imageTexture = makeImageTexture()
+  @Transient
+  private val checkerTexture = makeCheckerTexture()
+  @Transient
+  private var texture: TexturePaint? = null
+
+  init {
+    setBackground(Color(.5f, .8f, .5f, .5f))
+    add(JLabel("JLabel: "))
+    add(JTextField(10))
+    add(JButton("JButton"))
+
+    val model = arrayOf("Color(.5f, .8f, .5f, .5f)", "ImageTexturePaint", "CheckerTexturePaint")
+    val combo = JComboBox<String>(model)
+    combo.addItemListener { e ->
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        when (e.getItem()) {
+          "ImageTexturePaint" -> {
+            texture = imageTexture
+            setOpaque(false)
+          }
+          "CheckerTexturePaint" -> {
+            texture = checkerTexture
+            setOpaque(false)
+          }
+          else -> {
+            texture = null
+            setOpaque(true)
+          }
+        }
+        getRootPane().getContentPane().repaint()
+      }
+    }
+    add(combo)
+    setPreferredSize(Dimension(320, 240))
+  }
+
+  protected override fun paintComponent(g: Graphics) {
+    texture?.also {
+      val g2 = g.create() as Graphics2D
+      g2.setPaint(it)
+      g2.fillRect(0, 0, getWidth(), getHeight())
+      g2.dispose()
+    }
+    super.paintComponent(g)
+  }
+
+  private fun makeImageTexture(): TexturePaint {
+    // unkaku_w.png http://www.viva-edo.com/komon/edokomon.html
+    val bi = javaClass.getResource("unkaku_w.png")?.let {
+      try {
+        ImageIO.read(it)
+      } catch (ex: IOException) {
+        makeMissingImage()
+      }
+    } ?: makeMissingImage()
+    return TexturePaint(bi, Rectangle(bi.getWidth(), bi.getHeight()))
+  }
+
+  private fun makeMissingImage(): BufferedImage {
+    val missingIcon = UIManager.getIcon("OptionPane.errorIcon")
+    val w = missingIcon.getIconWidth()
+    val h = missingIcon.getIconHeight()
+    val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+    val g2 = bi.createGraphics()
+    missingIcon.paintIcon(null, g2, 0, 0)
+    g2.dispose()
+    return bi
+  }
+
+  private fun makeCheckerTexture(): TexturePaint {
+    val cs = 6
+    val sz = cs * cs
+    val bi = BufferedImage(sz, sz, BufferedImage.TYPE_INT_ARGB)
+    val g2 = bi.createGraphics()
+    g2.setPaint(Color(200, 150, 100, 50))
+    g2.fillRect(0, 0, sz, sz)
+    var i = 0
+    while (i * cs < sz) {
+      var j = 0
+      while (j * cs < sz) {
+        if ((i + j) % 2 == 0) {
+          g2.fillRect(i * cs, j * cs, cs, cs)
+        }
+        j++
+      }
+      i++
+    }
+    g2.dispose()
+    return TexturePaint(bi, Rectangle(sz, sz))
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    JFrame.setDefaultLookAndFeelDecorated(true)
+    JFrame().apply {
+      setBackground(Color(0x0, true))
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
