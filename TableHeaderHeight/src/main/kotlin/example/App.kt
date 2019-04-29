9a97a39ad14305da27f6e1a0fb0ef475318812bf
aterai <aterai@outlook.com>
2019-04-29 17:06:44 +0900

    ktlint: avoid unused imports

--- TableHeaderHeight/src/main/kotlin/example/App.kt
+++ TableHeaderHeight/src/main/kotlin/example/App.kt
@@ -3,7 +3,6 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
-import javax.swing.table.JTableHeader
 import javax.swing.table.TableColumn
 
 class MainPanel : JPanel(BorderLayout()) {
