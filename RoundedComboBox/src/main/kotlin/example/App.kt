3a8fbd1bbf59c1ca091f65456230f7d8a1f062a3
aterai <aterai@outlook.com>
2019-06-27 14:04:22 +0900

    refactor: remove unused function

--- RoundedComboBox/src/main/kotlin/example/App.kt
+++ RoundedComboBox/src/main/kotlin/example/App.kt
@@ -147,40 +147,6 @@ class MainPanel : JPanel(BorderLayout()) {
       model.addElement("6789000000000")
       return model
     }
-
-    @JvmStatic
-    fun main(args: Array<String>) {
-      EventQueue.invokeLater(object : Runnable {
-        override fun run() {
-          createAndShowGui()
-        }
-      })
-    }
-
-    fun createAndShowGui() {
-      try {
-        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-      } catch (ex: ClassNotFoundException) {
-        ex.printStackTrace()
-        Toolkit.getDefaultToolkit().beep()
-      } catch (ex: InstantiationException) {
-        ex.printStackTrace()
-        Toolkit.getDefaultToolkit().beep()
-      } catch (ex: IllegalAccessException) {
-        ex.printStackTrace()
-        Toolkit.getDefaultToolkit().beep()
-      } catch (ex: UnsupportedLookAndFeelException) {
-        ex.printStackTrace()
-        Toolkit.getDefaultToolkit().beep()
-      }
-
-      val frame = JFrame("@title@")
-      frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      frame.getContentPane().add(MainPanel())
-      frame.pack()
-      frame.setLocationRelativeTo(null)
-      frame.setVisible(true)
-    }
   }
 }
 
