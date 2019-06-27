f1f97d18b6bb3ac18dd7f08bece9fe6b3d2c764b
aterai <aterai@outlook.com>
2019-06-27 20:56:59 +0900

    detekt: MatchingDeclarationName

--- AdjustRowHeightFillsViewport/src/main/kotlin/example/App.kt
+++ AdjustRowHeightFillsViewport/src/main/kotlin/example/App.kt
@@ -6,67 +6,67 @@ import java.awt.event.ComponentEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val columnNames = arrayOf("String", "Integer", "Boolean")
-  private val data = arrayOf(
-    arrayOf<Any>("aaa", 12, true),
-    arrayOf<Any>("bbb", 5, false),
-    arrayOf<Any>("CCC", 92, true),
-    arrayOf<Any>("DDD", 0, false))
-  private val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  private val table = object : JTable(model) {
-    private var prevHeight = -1
-    private var prevCount = -1
+private val columnNames = arrayOf("String", "Integer", "Boolean")
+private val data = arrayOf(
+  arrayOf<Any>("aaa", 12, true),
+  arrayOf<Any>("bbb", 5, false),
+  arrayOf<Any>("CCC", 92, true),
+  arrayOf<Any>("DDD", 0, false))
+private val model = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+}
+private val table = object : JTable(model) {
+  private var prevHeight = -1
+  private var prevCount = -1
 
-    private fun updateRowsHeight(vport: JViewport) {
-      val height = vport.getExtentSize().height
-      val rowCount = getModel().getRowCount()
-      val defaultRowHeight = height / rowCount
-      if ((height != prevHeight || rowCount != prevCount) && defaultRowHeight > 0) {
-        // var remainder = height - rowCount * defaultRowHeight
-        var remainder = height % rowCount
-        for (i in 0 until rowCount) {
-          val a = if (remainder > 0) if (i == rowCount - 1) remainder else 1 else 0
-          setRowHeight(i, defaultRowHeight + a)
-          remainder--
-        }
+  private fun updateRowsHeight(vport: JViewport) {
+    val height = vport.getExtentSize().height
+    val rowCount = getModel().getRowCount()
+    val defaultRowHeight = height / rowCount
+    if ((height != prevHeight || rowCount != prevCount) && defaultRowHeight > 0) {
+      // var remainder = height - rowCount * defaultRowHeight
+      var remainder = height % rowCount
+      for (i in 0 until rowCount) {
+        val a = if (remainder > 0) if (i == rowCount - 1) remainder else 1 else 0
+        setRowHeight(i, defaultRowHeight + a)
+        remainder--
       }
-      prevHeight = height
-      prevCount = rowCount
     }
-
-    override fun doLayout() {
-      super.doLayout()
-      val clz = JViewport::class.java
-      (SwingUtilities.getAncestorOfClass(clz, this) as? JViewport)?.also {
-        updateRowsHeight(it)
-      }
-      // Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, this))
-      //   .filter { clz.isInstance(it) }.map { clz.cast(it) }
-      //   .ifPresent { this.updateRowsHeight(it) }
-    }
+    prevHeight = height
+    prevCount = rowCount
   }
 
-  init {
-    val scroll = JScrollPane(table)
-    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)
-    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
-    // TEST: scroll.addComponentListener(new TableRowHeidhtAdjuster())
-    scroll.addComponentListener(object : ComponentAdapter() {
-      override fun componentResized(e: ComponentEvent) {
-        val sp = e.getComponent() as? JScrollPane ?: return
-        sp.getViewport().getView().revalidate()
-      }
-    })
+  override fun doLayout() {
+    super.doLayout()
+    val clz = JViewport::class.java
+    (SwingUtilities.getAncestorOfClass(clz, this) as? JViewport)?.also {
+      updateRowsHeight(it)
+    }
+    // Optional.ofNullable(SwingUtilities.getAncestorOfClass(clz, this))
+    //   .filter { clz.isInstance(it) }.map { clz.cast(it) }
+    //   .ifPresent { this.updateRowsHeight(it) }
+  }
+}
 
-    val button = JButton("add")
-    button.addActionListener { model.addRow(arrayOf<Any>("", 0, false)) }
+fun makeUI(): Component {
+  val scroll = JScrollPane(table)
+  scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)
+  scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
+  // TEST: scroll.addComponentListener(new TableRowHeidhtAdjuster())
+  scroll.addComponentListener(object : ComponentAdapter() {
+    override fun componentResized(e: ComponentEvent) {
+      val sp = e.getComponent() as? JScrollPane ?: return
+      sp.getViewport().getView().revalidate()
+    }
+  })
 
-    add(scroll)
-    add(button, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  val button = JButton("add")
+  button.addActionListener { model.addRow(arrayOf<Any>("", 0, false)) }
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(button, BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -102,7 +102,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
