8841b3c75df090994508ba8829286bcb82909fb0
aterai <aterai@outlook.com>
2019-06-29 17:49:04 +0900

    detekt: MatchingDeclarationName

--- DetectScrollToBottom/src/main/kotlin/example/App.kt
+++ DetectScrollToBottom/src/main/kotlin/example/App.kt
@@ -3,42 +3,42 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(GridLayout(1, 2)) {
-  init {
-    UIManager.put("ScrollBar.minimumThumbSize", Dimension(32, 32))
+fun makeUI(): Component {
+  UIManager.put("ScrollBar.minimumThumbSize", Dimension(32, 32))
 
-    val textArea = JTextArea()
-    val sb = StringBuilder()
-    for (i in 0..2000) {
-      sb.append(String.format("%04d%n", i))
-    }
-    textArea.setText(sb.toString())
-    textArea.setCaretPosition(0)
-
-    add(makePanel(textArea))
-    add(makePanel(JTable(100, 3)))
-    setPreferredSize(Dimension(320, 240))
+  val textArea = JTextArea()
+  val sb = StringBuilder()
+  for (i in 0..2000) {
+    sb.append(String.format("%04d%n", i))
   }
+  textArea.setText(sb.toString())
+  textArea.setCaretPosition(0)
 
-  private fun makePanel(c: Component): Component {
-    val check = JCheckBox("JCheckBox")
-    check.setEnabled(false)
-    val p = JPanel(BorderLayout())
-    val scroll = JScrollPane(c)
-    scroll.getVerticalScrollBar().getModel().addChangeListener { e ->
-      (e.getSource() as? BoundedRangeModel)?.also {
-        val extent = it.getExtent()
-        val maximum = it.getMaximum()
-        val value = it.getValue()
-        if (value + extent >= maximum) {
-          check.setEnabled(true)
-        }
+  return JPanel(GridLayout(1, 2)).also {
+    it.add(makePanel(textArea))
+    it.add(makePanel(JTable(100, 3)))
+    it.setPreferredSize(Dimension(320, 240))
+  }
+}
+
+private fun makePanel(c: Component): Component {
+  val check = JCheckBox("JCheckBox")
+  check.setEnabled(false)
+  val p = JPanel(BorderLayout())
+  val scroll = JScrollPane(c)
+  scroll.getVerticalScrollBar().getModel().addChangeListener { e ->
+    (e.getSource() as? BoundedRangeModel)?.also {
+      val extent = it.getExtent()
+      val maximum = it.getMaximum()
+      val value = it.getValue()
+      if (value + extent >= maximum) {
+        check.setEnabled(true)
       }
     }
-    p.add(scroll)
-    p.add(check, BorderLayout.SOUTH)
-    return p
   }
+  p.add(scroll)
+  p.add(check, BorderLayout.SOUTH)
+  return p
 }
 
 fun main() {
@@ -51,7 +51,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
