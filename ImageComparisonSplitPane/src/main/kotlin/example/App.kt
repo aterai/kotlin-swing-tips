7a9712398e982135af98a41671f9b966733c6e09
aterai <aterai@outlook.com>
2019-07-04 19:19:48 +0900

    detekt: MatchingDeclarationName

--- ImageComparisonSplitPane/src/main/kotlin/example/App.kt
+++ ImageComparisonSplitPane/src/main/kotlin/example/App.kt
@@ -3,50 +3,51 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val check = JCheckBox("setXORMode(Color.BLUE)", true)
-    check.addActionListener { repaint() }
+fun makeUI(): Component {
+  val split = JSplitPane()
+  split.setContinuousLayout(true)
+  split.setResizeWeight(.5)
 
-    val split = JSplitPane()
-    split.setContinuousLayout(true)
-    split.setResizeWeight(.5)
+  val check = JCheckBox("setXORMode(Color.BLUE)", true)
+  check.addActionListener { split.repaint() }
 
-    val icon = ImageIcon(javaClass.getResource("test.png"))
+  val cl = Thread.currentThread().getContextClassLoader()
+  val icon = ImageIcon(cl.getResource("example/test.png"))
 
-    val beforeCanvas = object : JComponent() {
-      protected override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        g.drawImage(icon.getImage(), 0, 0, icon.getIconWidth(), icon.getIconHeight(), this)
-      }
+  val beforeCanvas = object : JComponent() {
+    protected override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      g.drawImage(icon.getImage(), 0, 0, icon.getIconWidth(), icon.getIconHeight(), this)
     }
-    split.setLeftComponent(beforeCanvas)
+  }
+  split.setLeftComponent(beforeCanvas)
 
-    val afterCanvas = object : JComponent() {
-      protected override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        val g2 = g.create() as Graphics2D
-        val iw = icon.getIconWidth()
-        val ih = icon.getIconHeight()
-        if (check.isSelected()) {
-          g2.setColor(getBackground())
-          g2.setXORMode(Color.BLUE)
-        } else {
-          g2.setPaintMode()
-        }
-        val pt = getLocation()
-        val ins = split.getBorder().getBorderInsets(split)
-        g2.translate(-pt.x + ins.left, 0)
-        g2.drawImage(icon.getImage(), 0, 0, iw, ih, this)
-        g2.dispose()
+  val afterCanvas = object : JComponent() {
+    protected override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      val g2 = g.create() as Graphics2D
+      val iw = icon.getIconWidth()
+      val ih = icon.getIconHeight()
+      if (check.isSelected()) {
+        g2.setColor(getBackground())
+        g2.setXORMode(Color.BLUE)
+      } else {
+        g2.setPaintMode()
       }
+      val pt = getLocation()
+      val ins = split.getBorder().getBorderInsets(split)
+      g2.translate(-pt.x + ins.left, 0)
+      g2.drawImage(icon.getImage(), 0, 0, iw, ih, this)
+      g2.dispose()
     }
-    split.setRightComponent(afterCanvas)
+  }
+  split.setRightComponent(afterCanvas)
 
-    add(split)
-    add(check, BorderLayout.SOUTH)
-    setOpaque(false)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(split)
+    it.add(check, BorderLayout.SOUTH)
+    it.setOpaque(false)
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -60,7 +61,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
