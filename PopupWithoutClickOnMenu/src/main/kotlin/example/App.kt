a78a81178d1875de7f908f6079c7c224d431719d
aterai <aterai@outlook.com>
2019-07-05 16:36:36 +0900

    detekt: MatchingDeclarationName

--- PopupWithoutClickOnMenu/src/main/kotlin/example/App.kt
+++ PopupWithoutClickOnMenu/src/main/kotlin/example/App.kt
@@ -5,59 +5,59 @@ import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val check = JCheckBox("JMenu: hover(show popup automatically) on cursor", true)
-    val handler = object : MouseAdapter() {
-      override fun mousePressed(e: MouseEvent) {
-        if (check.isSelected()) {
-          (e.getComponent() as? AbstractButton)?.doClick()
-        }
-      }
-
-      override fun mouseEntered(e: MouseEvent) {
-        if (check.isSelected()) {
-          (e.getComponent() as? AbstractButton)?.doClick()
-        }
+fun makeUI(): Component {
+  val check = JCheckBox("JMenu: hover(show popup automatically) on cursor", true)
+  val handler = object : MouseAdapter() {
+    override fun mousePressed(e: MouseEvent) {
+      if (check.isSelected()) {
+        (e.getComponent() as? AbstractButton)?.doClick()
       }
     }
-    val menuBar = makeMenuBar()
-    menuBar.getSubElements()
-      .filterIsInstance(JMenu::class.java)
-      .forEach { it.addMouseListener(handler) }
 
-    EventQueue.invokeLater { getRootPane().setJMenuBar(menuBar) }
+    override fun mouseEntered(e: MouseEvent) {
+      if (check.isSelected()) {
+        (e.getComponent() as? AbstractButton)?.doClick()
+      }
+    }
+  }
+  val menuBar = makeMenuBar()
+  menuBar.getSubElements()
+    .filterIsInstance(JMenu::class.java)
+    .forEach { it.addMouseListener(handler) }
 
-    add(check, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  val p = JPanel(BorderLayout()).also {
+    it.add(check, BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
+  }
+  EventQueue.invokeLater { p.getRootPane().setJMenuBar(menuBar) }
+  return p
+}
+
+fun makeMenuBar() = JMenuBar().also {
+  it.add(JMenu("File")).also { menu ->
+    menu.add("Open")
+    menu.add("Save")
+    menu.add("Exit")
   }
 
-  private fun makeMenuBar() = JMenuBar().also {
-    it.add(JMenu("File")).also { menu ->
-      menu.add("Open")
-      menu.add("Save")
-      menu.add("Exit")
-    }
+  it.add(JMenu("Edit")).also { menu ->
+    menu.add("Undo")
+    menu.add("Redo")
+    menu.addSeparator()
+    menu.add("Cut")
+    menu.add("Copy")
+    menu.add("Paste")
+    menu.add("Delete")
+  }
 
-    it.add(JMenu("Edit")).also { menu ->
-      menu.add("Undo")
-      menu.add("Redo")
-      menu.addSeparator()
-      menu.add("Cut")
-      menu.add("Copy")
-      menu.add("Paste")
-      menu.add("Delete")
-    }
-
-    it.add(JMenu("Test")).also { menu ->
-      menu.add("JMenuItem1")
-      menu.add("JMenuItem2")
-      menu.add(JMenu("JMenu").also { sub ->
-        sub.add("JMenuItem4")
-        sub.add("JMenuItem5")
-      })
-      menu.add("JMenuItem3")
-    }
+  it.add(JMenu("Test")).also { menu ->
+    menu.add("JMenuItem1")
+    menu.add("JMenuItem2")
+    menu.add(JMenu("JMenu").also { sub ->
+      sub.add("JMenuItem4")
+      sub.add("JMenuItem5")
+    })
+    menu.add("JMenuItem3")
   }
 }
 
@@ -71,7 +71,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
--- PositionInGridLayout/src/main/kotlin/example/App.kt
+++ PositionInGridLayout/src/main/kotlin/example/App.kt
@@ -4,26 +4,26 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.event.ActionListener
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val gl = GridLayout(5, 7, 5, 5)
-    val p = JPanel(gl)
-    p.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
+fun makeUI(): Component {
+  val gl = GridLayout(5, 7, 5, 5)
+  val p = JPanel(gl)
+  p.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
 
-    val log = JTextArea(3, 0)
-    val al = ActionListener { e ->
-      val c = e.getSource() as Component
-      val idx = p.getComponentZOrder(c)
-      val row = idx / gl.getColumns()
-      val col = idx % gl.getColumns()
-      log.append(String.format("Row: %d, Column: %d%n", row + 1, col + 1))
-    }
-    List(gl.getRows() * gl.getColumns()) { JButton() }
-      .forEach { p.add(it.also { it.addActionListener(al) }) }
+  val log = JTextArea(3, 0)
+  val al = ActionListener { e ->
+    val c = e.getSource() as Component
+    val idx = p.getComponentZOrder(c)
+    val row = idx / gl.getColumns()
+    val col = idx % gl.getColumns()
+    log.append(String.format("Row: %d, Column: %d%n", row + 1, col + 1))
+  }
+  List(gl.getRows() * gl.getColumns()) { JButton() }
+    .forEach { p.add(it.also { it.addActionListener(al) }) }
 
-    add(p)
-    add(JScrollPane(log), BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(p)
+    it.add(JScrollPane(log), BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -37,7 +37,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
