b8af69d094794b9411a07a13ab6a88eb0ccb83fa
aterai <aterai@outlook.com>
2019-07-08 18:04:04 +0900

    detekt: MatchingDeclarationName

--- ScrollableTracksViewportWidth/src/main/kotlin/example/App.kt
+++ ScrollableTracksViewportWidth/src/main/kotlin/example/App.kt
@@ -3,30 +3,30 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(GridLayout(0, 1)) {
-  init {
-    val table1 = JTable(1, 3)
-    val scroll1 = JScrollPane(table1)
-    scroll1.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_SUBSEQUENT_COLUMNS(Default)"))
+fun makeUI(): Component {
+  val table1 = JTable(1, 3)
+  val scroll1 = JScrollPane(table1)
+  scroll1.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_SUBSEQUENT_COLUMNS(Default)"))
 
-    val table2 = JTable(1, 3)
-    table2.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
-    val scroll2 = JScrollPane(table2)
-    scroll2.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_OFF"))
+  val table2 = JTable(1, 3)
+  table2.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
+  val scroll2 = JScrollPane(table2)
+  scroll2.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_OFF"))
 
-    val table3 = object : JTable(1, 3) {
-      // java - How to make JTable both AutoResize and horizontall scrollable? - Stack Overflow
-      // https://stackoverflow.com/questions/6104916/how-to-make-jtable-both-autoresize-and-horizontall-scrollable
-      override fun getScrollableTracksViewportWidth() = getPreferredSize().width < getParent().getWidth()
-    }
-    table3.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
-    val scroll3 = JScrollPane(table3)
-    scroll3.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_OFF + getScrollableTracksViewportWidth()"))
+  val table3 = object : JTable(1, 3) {
+    // java - How to make JTable both AutoResize and horizontall scrollable? - Stack Overflow
+    // https://stackoverflow.com/questions/6104916/how-to-make-jtable-both-autoresize-and-horizontall-scrollable
+    override fun getScrollableTracksViewportWidth() = getPreferredSize().width < getParent().getWidth()
+  }
+  table3.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
+  val scroll3 = JScrollPane(table3)
+  scroll3.setBorder(BorderFactory.createTitledBorder("AUTO_RESIZE_OFF + getScrollableTracksViewportWidth()"))
 
-    add(scroll1)
-    add(scroll2)
-    add(scroll3)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(GridLayout(0, 1)).also {
+    it.add(scroll1)
+    it.add(scroll2)
+    it.add(scroll3)
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -40,7 +40,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
