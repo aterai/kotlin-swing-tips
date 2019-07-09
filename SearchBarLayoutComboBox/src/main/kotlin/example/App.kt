1477d37604d4fd658eff54d2b6d6a8463c579e62
aterai <aterai@outlook.com>
2019-07-09 15:07:02 +0900

    detekt: MatchingDeclarationName

--- SearchBarLayoutComboBox/src/main/kotlin/example/App.kt
+++ SearchBarLayoutComboBox/src/main/kotlin/example/App.kt
@@ -3,26 +3,27 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val p = JPanel(GridLayout(4, 1, 5, 5))
-    setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 20))
-    p.add(JLabel("Default JComboBox"))
-    p.add(JComboBox<String>(arrayOf("Google", "Yahoo!", "Bing")))
-    p.add(JLabel("SearchBar JComboBox"))
+fun makeUI(): Component {
+  val p = JPanel(GridLayout(4, 1, 5, 5))
+  p.add(JLabel("Default JComboBox"))
+  p.add(JComboBox<String>(arrayOf("Google", "Yahoo!", "Bing")))
+  p.add(JLabel("SearchBar JComboBox"))
 
-    val m = SearchEngineComboBoxModel<example.SearchEngine>()
-    m.addElement(SearchEngine("Google", "http://www.google.com/", ImageIcon(javaClass.getResource("google.png"))))
-    m.addElement(SearchEngine("Yahoo!", "http://www.yahoo.com/", ImageIcon(javaClass.getResource("yahoo.png"))))
-    m.addElement(SearchEngine("Bing", "http://www.bing.com/", ImageIcon(javaClass.getResource("bing.png"))))
+  val cl = Thread.currentThread().getContextClassLoader()
+  val m = SearchEngineComboBoxModel<example.SearchEngine>()
+  m.addElement(SearchEngine("Google", "http://www.google.com/", ImageIcon(cl.getResource("example/google.png"))))
+  m.addElement(SearchEngine("Yahoo!", "http://www.yahoo.com/", ImageIcon(cl.getResource("example/yahoo.png"))))
+  m.addElement(SearchEngine("Bing", "http://www.bing.com/", ImageIcon(cl.getResource("example/bing.png"))))
 
-    val combo = JSearchBar(m)
-    combo.getEditor().setItem("java swing")
+  val combo = JSearchBar(m)
+  combo.getEditor().setItem("java swing")
 
-    p.add(combo)
-    // p.add(new SearchBarComboBox(makeModel()));
-    add(p, BorderLayout.NORTH)
-    setPreferredSize(Dimension(320, 240))
+  p.add(combo)
+  // p.add(new SearchBarComboBox(makeModel()));
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 20))
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -36,7 +37,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
