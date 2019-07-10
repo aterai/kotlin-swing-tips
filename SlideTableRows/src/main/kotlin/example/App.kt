3b222efe75ab463611d2cd42d44e3701fcc1a902
aterai <aterai@outlook.com>
2019-07-10 17:59:02 +0900

    detekt: MatchingDeclarationName

--- SlideTableRows/src/main/kotlin/example/App.kt
+++ SlideTableRows/src/main/kotlin/example/App.kt
@@ -6,103 +6,101 @@ import java.awt.event.ActionListener
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  protected val columnNames = arrayOf("String", "Integer", "Boolean")
-  protected val data = arrayOf(
-      arrayOf<Any>("aaa", 12, true),
-      arrayOf<Any>("bbb", 5, false),
-      arrayOf<Any>("CCC", 92, true),
-      arrayOf<Any>("DDD", 0, false))
-  protected val model: DefaultTableModel = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = when (column) {
-      0 -> String::class.java
-      1 -> Number::class.java
-      2 -> java.lang.Boolean::class.java
-      else -> super.getColumnClass(column)
-    }
+const val START_HEIGHT = 4
+const val END_HEIGHT = 24
+const val DELAY = 10
+
+val columnNames = arrayOf("String", "Integer", "Boolean")
+val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false))
+val model: DefaultTableModel = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = when (column) {
+    0 -> String::class.java
+    1 -> Number::class.java
+    2 -> java.lang.Boolean::class.java
+    else -> super.getColumnClass(column)
   }
-  protected val table = JTable(model)
-  protected val createAction: Action = object : AbstractAction("add") {
-    override fun actionPerformed(e: ActionEvent) {
-      createActionPerformed()
-    }
+}
+val table = JTable(model)
+val createAction: Action = object : AbstractAction("add") {
+  override fun actionPerformed(e: ActionEvent) {
+    createActionPerformed()
   }
-  protected val deleteAction: Action = object : AbstractAction("delete") {
-    override fun actionPerformed(e: ActionEvent) {
-      deleteActionPerformed()
-    }
+}
+val deleteAction: Action = object : AbstractAction("delete") {
+  override fun actionPerformed(e: ActionEvent) {
+    deleteActionPerformed()
+  }
+}
+
+fun makeUI(): Component {
+  table.setFillsViewportHeight(true)
+  table.setAutoCreateRowSorter(true)
+  table.setRowHeight(START_HEIGHT)
+  for (i in 0 until model.getRowCount()) {
+    table.setRowHeight(i, END_HEIGHT)
   }
 
-  init {
-    table.setFillsViewportHeight(true)
-    table.setAutoCreateRowSorter(true)
-    table.setRowHeight(START_HEIGHT)
-    for (i in 0 until model.getRowCount()) {
-      table.setRowHeight(i, END_HEIGHT)
+  val popup = object : JPopupMenu() {
+    override fun show(c: Component, x: Int, y: Int) {
+      val table = c as? JTable ?: return
+      deleteAction.setEnabled(table.getSelectedRowCount() > 0)
+      super.show(table, x, y)
     }
+  }
+  popup.add(createAction)
+  popup.addSeparator()
+  popup.add(deleteAction)
 
-    val popup = object : JPopupMenu() {
-      override fun show(c: Component, x: Int, y: Int) {
-        val table = c as? JTable ?: return
-        deleteAction.setEnabled(table.getSelectedRowCount() > 0)
-        super.show(table, x, y)
+  val scroll = JScrollPane(table)
+  scroll.setComponentPopupMenu(popup)
+  table.setInheritsPopupMenu(true)
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(JButton(createAction), BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
+  }
+}
+
+fun createActionPerformed() {
+  model.addRow(arrayOf<Any>("New name", model.getRowCount(), false))
+  Timer(DELAY, object : ActionListener {
+    private val index = table.convertRowIndexToView(model.getRowCount() - 1)
+    private var height = START_HEIGHT
+    override fun actionPerformed(e: ActionEvent) {
+      if (height < END_HEIGHT) {
+        table.setRowHeight(index, height++)
+      } else {
+        (e.getSource() as? Timer)?.stop()
       }
     }
-    popup.add(createAction)
-    popup.addSeparator()
-    popup.add(deleteAction)
+  }).start()
+}
 
-    val scroll = JScrollPane(table)
-    scroll.setComponentPopupMenu(popup)
-    table.setInheritsPopupMenu(true)
-    add(scroll)
-    add(JButton(createAction), BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+fun deleteActionPerformed() {
+  val selection = table.getSelectedRows()
+  if (selection.size == 0) {
+    return
   }
-
-  protected fun createActionPerformed() {
-    model.addRow(arrayOf<Any>("New name", model.getRowCount(), false))
-    Timer(DELAY, object : ActionListener {
-      private val index = table.convertRowIndexToView(model.getRowCount() - 1)
-      private var height = START_HEIGHT
-      override fun actionPerformed(e: ActionEvent) {
-        if (height < END_HEIGHT) {
-          table.setRowHeight(index, height++)
-        } else {
-          (e.getSource() as? Timer)?.stop()
+  Timer(DELAY, object : ActionListener {
+    private var height = END_HEIGHT
+    override fun actionPerformed(e: ActionEvent) {
+      height--
+      if (height > START_HEIGHT) {
+        for (i in selection.indices.reversed()) {
+          table.setRowHeight(selection[i], height)
+        }
+      } else {
+        (e.getSource() as? Timer)?.stop()
+        for (i in selection.indices.reversed()) {
+          model.removeRow(table.convertRowIndexToModel(selection[i]))
         }
       }
-    }).start()
-  }
-
-  protected fun deleteActionPerformed() {
-    val selection = table.getSelectedRows()
-    if (selection.size == 0) {
-      return
     }
-    Timer(DELAY, object : ActionListener {
-      private var height = END_HEIGHT
-      override fun actionPerformed(e: ActionEvent) {
-        height--
-        if (height > START_HEIGHT) {
-          for (i in selection.indices.reversed()) {
-            table.setRowHeight(selection[i], height)
-          }
-        } else {
-          (e.getSource() as? Timer)?.stop()
-          for (i in selection.indices.reversed()) {
-            model.removeRow(table.convertRowIndexToModel(selection[i]))
-          }
-        }
-      }
-    }).start()
-  }
-
-  companion object {
-    protected const val START_HEIGHT = 4
-    protected const val END_HEIGHT = 24
-    protected const val DELAY = 10
-  }
+  }).start()
 }
 
 fun main() {
@@ -115,7 +113,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
