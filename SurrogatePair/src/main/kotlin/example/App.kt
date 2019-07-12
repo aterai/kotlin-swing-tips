7e439857ff97b39e25ee06c5a3504bc040c05a26
aterai <aterai@outlook.com>
2019-07-12 09:25:51 +0900

    detekt: MatchingDeclarationName

--- SurrogatePair/src/main/kotlin/example/App.kt
+++ SurrogatePair/src/main/kotlin/example/App.kt
@@ -12,59 +12,60 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.text.html.HTMLEditorKit
 import javax.swing.text.html.StyleSheet
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val styleSheet = StyleSheet()
-    // styleSheet.addRule("body {font-size: 24pt; font-family: IPAexGothic;}")
-    val htmlEditorKit = HTMLEditorKit()
-    htmlEditorKit.setStyleSheet(styleSheet)
-    val editor1 = JEditorPane()
-    editor1.setEditorKit(htmlEditorKit)
+fun makeUI(): Component {
+  val styleSheet = StyleSheet()
+  // styleSheet.addRule("body {font-size: 24pt; font-family: IPAexGothic;}")
+  val htmlEditorKit = HTMLEditorKit()
+  htmlEditorKit.setStyleSheet(styleSheet)
+  val editor1 = JEditorPane()
+  editor1.setEditorKit(htmlEditorKit)
 
-    val url = javaClass.getResource("SurrogatePair.html")
+  val cl = Thread.currentThread().getContextClassLoader()
+  val url = cl.getResource("example/SurrogatePair.html")
+  try {
+    InputStreamReader(url.openStream(), StandardCharsets.UTF_8).use { editor1.read(it, "html") }
+  } catch (ex: IOException) {
+    editor1.setText("<html><p>(&#xD85B;&#xDE40;) (&#x26E40;)<br />(&#xD842;&#xDF9F;) (&#x00020B9F;)</p></html>")
+  }
+
+  val editor2 = JEditorPane()
+  // editor2.setFont(new Font("IPAexGothic", Font.PLAIN, 24))
+  editor2.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
+  editor2.setText("(\uD85B\uDE40) (\u26E40)\n(\uD842\uDF9F) (\u20B9F)")
+  // editor2.setText("(𦹀) (𦹀)\n(𠮟) (𠮟)")
+
+  val p = JPanel(GridLayout(0, 1))
+  p.add(makeTitledPanel("Numeric character reference", editor1))
+  p.add(makeTitledPanel("Unicode escapes", editor2))
+
+  val button = JButton("browse: SurrogatePair.html")
+  button.addActionListener { browseCacheFile(url) }
+
+  return JPanel(BorderLayout()).also {
+    it.add(p)
+    it.add(button, BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
+  }
+}
+
+private fun browseCacheFile(url: URL) {
+  if (Desktop.isDesktopSupported()) {
     try {
-      InputStreamReader(url.openStream(), StandardCharsets.UTF_8).use { editor1.read(it, "html") }
-    } catch (ex: IOException) {
-      editor1.setText("<html><p>(&#xD85B;&#xDE40;) (&#x26E40;)<br />(&#xD842;&#xDF9F;) (&#x00020B9F;)</p></html>")
-    }
-
-    val editor2 = JEditorPane()
-    // editor2.setFont(new Font("IPAexGothic", Font.PLAIN, 24))
-    editor2.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
-    editor2.setText("(\uD85B\uDE40) (\u26E40)\n(\uD842\uDF9F) (\u20B9F)")
-    // editor2.setText("(𦹀) (𦹀)\n(𠮟) (𠮟)")
-
-    val p = JPanel(GridLayout(0, 1))
-    p.add(makeTitledPanel("Numeric character reference", editor1))
-    p.add(makeTitledPanel("Unicode escapes", editor2))
-
-    val button = JButton("browse: SurrogatePair.html")
-    button.addActionListener { browseCacheFile(url) }
-
-    add(p)
-    add(button, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun browseCacheFile(url: URL) {
-    if (Desktop.isDesktopSupported()) {
-      try {
-        BufferedInputStream(url.openStream()).use {
-          val path = Files.createTempFile("_tmp", ".html")
-          path.toFile().deleteOnExit()
-          Files.copy(it, path, StandardCopyOption.REPLACE_EXISTING)
-          Desktop.getDesktop().browse(path.toUri())
-        }
-      } catch (ex: IOException) {
-        ex.printStackTrace()
-        Toolkit.getDefaultToolkit().beep()
+      BufferedInputStream(url.openStream()).use {
+        val path = Files.createTempFile("_tmp", ".html")
+        path.toFile().deleteOnExit()
+        Files.copy(it, path, StandardCopyOption.REPLACE_EXISTING)
+        Desktop.getDesktop().browse(path.toUri())
       }
+    } catch (ex: IOException) {
+      ex.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
     }
   }
+}
 
-  private fun makeTitledPanel(title: String, c: Component) = JScrollPane(c).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-  }
+fun makeTitledPanel(title: String, c: Component) = JScrollPane(c).also {
+  it.setBorder(BorderFactory.createTitledBorder(title))
 }
 
 fun main() {
@@ -77,7 +78,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
