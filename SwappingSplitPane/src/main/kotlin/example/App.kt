de66eaedcf09912aac67585cd6ceb88977251e5e
aterai <aterai@outlook.com>
2019-07-14 13:07:07 +0900

    detekt: MatchingDeclarationName

--- SwappingSplitPane/src/main/kotlin/example/App.kt
+++ SwappingSplitPane/src/main/kotlin/example/App.kt
@@ -3,48 +3,48 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val sp = JSplitPane().also {
-      it.setLeftComponent(JScrollPane(JTree()))
-      it.setRightComponent(JScrollPane(JTable(6, 3)))
-      it.setResizeWeight(.4)
+fun makeUI(): Component {
+  val sp = JSplitPane().also {
+    it.setLeftComponent(JScrollPane(JTree()))
+    it.setRightComponent(JScrollPane(JTable(6, 3)))
+    it.setResizeWeight(.4)
+  }
+
+  val check = JCheckBox("Keep DividerLocation", true)
+
+  val button = JButton("swap")
+  button.setFocusable(false)
+  button.addActionListener {
+    val left = sp.getLeftComponent()
+    val right = sp.getRightComponent()
+
+    // sp.removeAll(); // Divider is also removed
+    sp.remove(left)
+    sp.remove(right)
+    // or:
+    // sp.setLeftComponent(null);
+    // sp.setRightComponent(null);
+
+    sp.setLeftComponent(right)
+    sp.setRightComponent(left)
+
+    sp.setResizeWeight(1.0 - sp.getResizeWeight())
+    if (check.isSelected()) {
+      sp.setDividerLocation(sp.getDividerLocation())
     }
+  }
 
-    val check = JCheckBox("Keep DividerLocation", true)
+  val box = Box.createHorizontalBox().also {
+    it.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5))
+    it.add(check)
+    it.add(Box.createHorizontalGlue())
+    it.add(button)
+  }
 
-    val button = JButton("swap")
-    button.setFocusable(false)
-    button.addActionListener {
-      val left = sp.getLeftComponent()
-      val right = sp.getRightComponent()
-
-      // sp.removeAll(); // Divider is also removed
-      sp.remove(left)
-      sp.remove(right)
-      // or:
-      // sp.setLeftComponent(null);
-      // sp.setRightComponent(null);
-
-      sp.setLeftComponent(right)
-      sp.setRightComponent(left)
-
-      sp.setResizeWeight(1.0 - sp.getResizeWeight())
-      if (check.isSelected()) {
-        sp.setDividerLocation(sp.getDividerLocation())
-      }
-    }
-
-    val box = Box.createHorizontalBox().also {
-      it.setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5))
-      it.add(check)
-      it.add(Box.createHorizontalGlue())
-      it.add(button)
-    }
-
-    add(sp)
-    add(box, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(sp)
+    it.add(box, BorderLayout.SOUTH)
+    it.setPreferredSize(Dimension(320, 240))
   }
 }
 
@@ -58,7 +58,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
