8c328a29ee5dcf45cb4dbebc69d427079813d963
aterai <aterai@outlook.com>
2019-11-10 18:45:46 +0900

    detekt: MatchingDeclarationName

--- FileAlreadyExistsDialog/src/main/kotlin/example/App.kt
+++ FileAlreadyExistsDialog/src/main/kotlin/example/App.kt
@@ -3,36 +3,36 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val fileChooser = object : JFileChooser() {
-      override fun approveSelection() {
-        val f = getSelectedFile()
-        if (f.exists() && getDialogType() == SAVE_DIALOG) {
-          val m = "<html>%s already exists.<br>Do you want to replace it?".format(f.getAbsolutePath())
-          val rv = JOptionPane.showConfirmDialog(this, m, "Save As", JOptionPane.YES_NO_OPTION)
-          if (rv != JOptionPane.YES_OPTION) {
-            return
-          }
+fun makeUI(): Component {
+  val fileChooser = object : JFileChooser() {
+    override fun approveSelection() {
+      val f = getSelectedFile()
+      if (f.exists() && getDialogType() == SAVE_DIALOG) {
+        val m = "<html>%s already exists.<br>Do you want to replace it?".format(f.getAbsolutePath())
+        val rv = JOptionPane.showConfirmDialog(this, m, "Save As", JOptionPane.YES_NO_OPTION)
+        if (rv != JOptionPane.YES_OPTION) {
+          return
         }
-        super.approveSelection()
       }
+      super.approveSelection()
     }
-    val button = JButton("Override JFileChooser#approveSelection()")
-    button.addActionListener {
-      val ret = fileChooser.showSaveDialog(rootPane)
-      if (ret == JFileChooser.APPROVE_OPTION) {
-        val file = fileChooser.getSelectedFile()
-        println(file)
-      }
-    }
-    val p = JPanel(GridBagLayout())
-    p.setBorder(BorderFactory.createTitledBorder("JFileChooser#showSaveDialog(...)"))
-    p.add(button)
-    add(p)
-    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20))
-    setPreferredSize(Dimension(320, 240))
   }
+  val button = JButton("Override JFileChooser#approveSelection()")
+  button.addActionListener {
+    val ret = fileChooser.showSaveDialog(button.getRootPane())
+    if (ret == JFileChooser.APPROVE_OPTION) {
+      val file = fileChooser.getSelectedFile()
+      println(file)
+    }
+  }
+  val p = JPanel(GridBagLayout())
+  p.setBorder(BorderFactory.createCompoundBorder(
+    BorderFactory.createEmptyBorder(20, 20, 20, 20),
+    BorderFactory.createTitledBorder("JFileChooser#showSaveDialog(...)")
+  ))
+  p.add(button)
+  p.setPreferredSize(Dimension(320, 240))
+  return p
 }
 
 fun main() {
@@ -45,7 +45,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
