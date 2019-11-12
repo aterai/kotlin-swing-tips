ce009356b06cb550bcc34369abb9bed5d888e4cc
aterai <aterai@outlook.com>
2019-11-13 02:40:40 +0900

    detekt: MatchingDeclarationName

--- FocusDashedRectGap/src/main/kotlin/example/App.kt
+++ FocusDashedRectGap/src/main/kotlin/example/App.kt
@@ -3,43 +3,41 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel() {
-  companion object {
-    // private static final String PAD = "<html><table><td height='32'>";
-    // private static final String PAD = "<html><table cellpadding='0'>";
-    private const val PAD = "<html><table><td style='padding:1'>"
-  }
+// private const val PAD = "<html><table><td height='32'>"
+// private const val PAD = "<html><table cellpadding='0'>"
+private const val PAD = "<html><table><td style='padding:1'>"
 
-  init {
-    println(UIManager.getInt("Button.dashedRectGapX"))
-    println(UIManager.getInt("Button.dashedRectGapY"))
-    println(UIManager.getInt("Button.dashedRectGapHeight"))
-    println(UIManager.getInt("Button.dashedRectGapWidth"))
-    UIManager.put("Button.dashedRectGapX", 5)
-    UIManager.put("Button.dashedRectGapY", 5)
-    UIManager.put("Button.dashedRectGapHeight", 10)
-    UIManager.put("Button.dashedRectGapWidth", 10)
-    UIManager.put("Button.margin", Insets(8, 8, 8, 8))
-    UIManager.put("ToggleButton.margin", Insets(8, 8, 8, 8))
-    UIManager.put("RadioButton.margin", Insets(8, 8, 8, 8))
-    UIManager.put("CheckBox.margin", Insets(8, 8, 8, 8))
-    add(JButton("JButton"))
-    add(Box.createHorizontalStrut(32))
-    add(JToggleButton("JToggleButton"))
-    add(Box.createHorizontalStrut(32))
-    add(JCheckBox("JCheckBox"))
-    add(object : JCheckBox("JCheckBox+BorderPainted") {
-      override fun updateUI() {
-        super.updateUI()
-        setBorderPainted(true)
-      }
-    })
-    add(JCheckBox(PAD + "JCheckBox+td.padding"))
+fun makeUI(): Component {
+  println(UIManager.getInt("Button.dashedRectGapX"))
+  println(UIManager.getInt("Button.dashedRectGapY"))
+  println(UIManager.getInt("Button.dashedRectGapHeight"))
+  println(UIManager.getInt("Button.dashedRectGapWidth"))
+  UIManager.put("Button.dashedRectGapX", 5)
+  UIManager.put("Button.dashedRectGapY", 5)
+  UIManager.put("Button.dashedRectGapHeight", 10)
+  UIManager.put("Button.dashedRectGapWidth", 10)
+  UIManager.put("Button.margin", Insets(8, 8, 8, 8))
+  UIManager.put("ToggleButton.margin", Insets(8, 8, 8, 8))
+  UIManager.put("RadioButton.margin", Insets(8, 8, 8, 8))
+  UIManager.put("CheckBox.margin", Insets(8, 8, 8, 8))
+  val p = JPanel()
+  p.add(JButton("JButton"))
+  p.add(Box.createHorizontalStrut(32))
+  p.add(JToggleButton("JToggleButton"))
+  p.add(Box.createHorizontalStrut(32))
+  p.add(JCheckBox("JCheckBox"))
+  p.add(object : JCheckBox("JCheckBox+BorderPainted") {
+    override fun updateUI() {
+      super.updateUI()
+      setBorderPainted(true)
+    }
+  })
+  p.add(JCheckBox(PAD + "JCheckBox+td.padding"))
 
-    add(JRadioButton("JRadioButton"))
-    add(JRadioButton(PAD + "JRadioButton+td.padding"))
-    setPreferredSize(Dimension(320, 240))
-  }
+  p.add(JRadioButton("JRadioButton"))
+  p.add(JRadioButton(PAD + "JRadioButton+td.padding"))
+  p.setPreferredSize(Dimension(320, 240))
+  return p
 }
 
 fun main() {
@@ -52,7 +50,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
