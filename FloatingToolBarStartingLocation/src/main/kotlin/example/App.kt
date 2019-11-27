a37dff40ee6a7c2ee4e0b82acba15b0a7a73a935
aterai <aterai@outlook.com>
2019-11-27 15:58:06 +0900

    add: FloatingToolBarStartingLocation

---
+++ FloatingToolBarStartingLocation/src/main/kotlin/example/App.kt
@@ -0,0 +1,45 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.basic.BasicToolBarUI
+
+fun makeUI(): Component {
+  val toolbar = JToolBar("ToolBar")
+  toolbar.add(JCheckBox("JCheckBox"))
+  toolbar.add(JTextField(10))
+  val p = JPanel(BorderLayout())
+  EventQueue.invokeLater {
+    (p.getTopLevelAncestor() as? Window)?.also { w ->
+      (toolbar.getUI() as? BasicToolBarUI)?.also {
+        val pt = w.getLocation()
+        it.setFloatingLocation(pt.x + 120, pt.y + 160)
+        it.setFloating(true, null)
+      }
+    }
+  }
+  p.add(toolbar, BorderLayout.NORTH)
+  p.add(Box.createHorizontalStrut(0), BorderLayout.WEST)
+  p.add(Box.createHorizontalStrut(0), BorderLayout.EAST)
+  p.add(JScrollPane(JTree()))
+  p.setPreferredSize(Dimension(320, 240))
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
