05745e854a76a1408db9d4b5d1f9df34b7378e7c
aterai <aterai@outlook.com>
2019-12-04 18:36:29 +0900

    refactor: remove explicit type arguments

--- AdjustRowHeightFillsViewport/src/main/kotlin/example/App.kt
+++ AdjustRowHeightFillsViewport/src/main/kotlin/example/App.kt
@@ -8,10 +8,10 @@ import javax.swing.table.DefaultTableModel
 
 private val columnNames = arrayOf("String", "Integer", "Boolean")
 private val data = arrayOf(
-  arrayOf<Any>("aaa", 12, true),
-  arrayOf<Any>("bbb", 5, false),
-  arrayOf<Any>("CCC", 92, true),
-  arrayOf<Any>("DDD", 0, false))
+  arrayOf("aaa", 12, true),
+  arrayOf("bbb", 5, false),
+  arrayOf("CCC", 92, true),
+  arrayOf("DDD", 0, false))
 private val model = object : DefaultTableModel(data, columnNames) {
   override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
 }
@@ -61,7 +61,7 @@ fun makeUI(): Component {
   })
 
   val button = JButton("add")
-  button.addActionListener { model.addRow(arrayOf<Any>("", 0, false)) }
+  button.addActionListener { model.addRow(arrayOf("", 0, false)) }
 
   return JPanel(BorderLayout()).also {
     it.add(scroll)
