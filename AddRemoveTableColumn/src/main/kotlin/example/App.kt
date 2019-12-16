40e423633e91229b926eac3de3385afe1c960673
aterai <aterai@outlook.com>
2019-12-16 17:38:10 +0900

    add: AddRemoveTableColumn

---
+++ AddRemoveTableColumn/src/main/kotlin/example/App.kt
@@ -0,0 +1,109 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.util.stream.Stream
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.event.PopupMenuEvent
+import javax.swing.event.PopupMenuListener
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.JTableHeader
+import javax.swing.table.TableColumnModel
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    UIManager.put("CheckBoxMenuItem.doNotCloseOnMouseClick", true)
+    val table = JTable(DefaultTableModel(12, 8))
+    val pop = TableHeaderPopupMenu(table)
+    val header = table.getTableHeader()
+    header.setComponentPopupMenu(pop)
+    pop.addPopupMenuListener(object : PopupMenuListener {
+      override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) {
+        cleanupHeader() // Java 9 doNotCloseOnMouseClick ArrayIndexOutOfBoundsException
+      }
+
+      override fun popupMenuWillBecomeInvisible(e: PopupMenuEvent) {
+        cleanupHeader()
+      }
+
+      override fun popupMenuCanceled(e: PopupMenuEvent) {
+        /* not needed */
+      }
+
+      private fun cleanupHeader() {
+        header.setDraggedColumn(null)
+        header.repaint()
+      }
+    })
+    add(JScrollPane(table))
+    setPreferredSize(Dimension(320, 240))
+  }
+}
+
+class TableHeaderPopupMenu(table: JTable) : JPopupMenu() {
+  override fun show(c: Component, x: Int, y: Int) {
+    (c as? JTableHeader)?.also {
+      updateMenuItems(it.getColumnModel())
+      super.show(c, x, y)
+    }
+  }
+
+  private fun updateMenuItems(columnModel: TableColumnModel) {
+    val isOnlyOneMenu = columnModel.getColumnCount() == 1
+    if (isOnlyOneMenu) {
+      stream(this)
+        .map { it.getComponent() }
+        .forEach { mi -> mi.setEnabled(mi !is AbstractButton || !mi.isSelected()) }
+    } else {
+      stream(this)
+        .forEach { me -> me.component.setEnabled(true) }
+    }
+  }
+
+  companion object {
+    private fun stream(me: MenuElement): Stream<MenuElement> {
+      return Stream.of(*me.subElements)
+        .flatMap { m: MenuElement ->
+          Stream.concat(
+            Stream.of(m),
+            stream(m)
+          )
+        }
+    }
+  }
+
+  init {
+    val columnModel = table.columnModel
+    columnModel.getColumns().toList().forEach { tableColumn ->
+      val name = tableColumn.getHeaderValue()?.toString() ?: ""
+      val item = JCheckBoxMenuItem(name, true)
+      item.addItemListener { e ->
+        val check = e.getItemSelectable() as? AbstractButton ?: return@addItemListener
+        if (check.isSelected()) {
+          columnModel.addColumn(tableColumn)
+        } else {
+          columnModel.removeColumn(tableColumn)
+        }
+        updateMenuItems(columnModel)
+      }
+      add(item)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
