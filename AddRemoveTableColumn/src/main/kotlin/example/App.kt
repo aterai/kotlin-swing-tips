7f50f7b7e97e7398a98e1fc642c8a36b0a882b7c
aterai <aterai@outlook.com>
2019-12-17 14:52:30 +0900

    refactor: override JPopupMenu#show(...) instead of PopupMenuListener

--- AddRemoveTableColumn/src/main/kotlin/example/App.kt
+++ AddRemoveTableColumn/src/main/kotlin/example/App.kt
@@ -2,8 +2,6 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.event.PopupMenuEvent
-import javax.swing.event.PopupMenuListener
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 import javax.swing.table.TableColumnModel
@@ -12,27 +10,7 @@ class MainPanel : JPanel(BorderLayout()) {
   init {
     UIManager.put("CheckBoxMenuItem.doNotCloseOnMouseClick", true)
     val table = JTable(DefaultTableModel(12, 8))
-    val pop = TableHeaderPopupMenu(table)
-    val header = table.getTableHeader()
-    header.setComponentPopupMenu(pop)
-    pop.addPopupMenuListener(object : PopupMenuListener {
-      override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) {
-        cleanupHeader() // Java 9 doNotCloseOnMouseClick ArrayIndexOutOfBoundsException
-      }
-
-      override fun popupMenuWillBecomeInvisible(e: PopupMenuEvent) {
-        cleanupHeader()
-      }
-
-      override fun popupMenuCanceled(e: PopupMenuEvent) {
-        /* not needed */
-      }
-
-      private fun cleanupHeader() {
-        header.setDraggedColumn(null)
-        header.repaint()
-      }
-    })
+    table.getTableHeader().setComponentPopupMenu(TableHeaderPopupMenu(table))
     add(JScrollPane(table))
     setPreferredSize(Dimension(320, 240))
   }
@@ -41,6 +19,9 @@ class MainPanel : JPanel(BorderLayout()) {
 class TableHeaderPopupMenu(table: JTable) : JPopupMenu() {
   override fun show(c: Component, x: Int, y: Int) {
     (c as? JTableHeader)?.also {
+      it.setDraggedColumn(null)
+      it.repaint()
+      it.getTable().repaint()
       updateMenuItems(it.getColumnModel())
       super.show(c, x, y)
     }
