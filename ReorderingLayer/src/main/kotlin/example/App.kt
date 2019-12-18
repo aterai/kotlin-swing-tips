f99775f383d2fe2fc6481025500748b4f112a336
aterai <aterai@outlook.com>
2019-12-18 16:16:43 +0900

    refactor: remove unnecessary processing

--- ReorderingLayer/src/main/kotlin/example/App.kt
+++ ReorderingLayer/src/main/kotlin/example/App.kt
@@ -1,7 +1,3 @@
-// -*- mode:java; encoding:utf-8 -*-
-// vim:set fileencoding=utf-8:
-// @homepage@
-
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
@@ -13,18 +9,17 @@ import javax.swing.plaf.LayerUI
 class MainPanel : JPanel(BorderLayout()) {
   init {
     val box = Box.createVerticalBox()
-    box.setBorder(BorderFactory.createMatteBorder(10, 8, 5, 1, Color.RED))
-    for ((idx, c) in listOf<Component>(
-      JLabel("<html>111<br>11<br>11"), JButton("2"), JCheckBox("3"), JTextField(14)
-    ).withIndex()) {
-      box.add(createToolBarButton(idx, c))
-    }
+    box.setBorder(BorderFactory.createMatteBorder(10, 5, 5, 5, Color.GREEN))
+    listOf<Component>(
+      JLabel("<html>000<br>00<br>00"), JButton("1"), JCheckBox("2"), JTextField("3")
+    ).forEach { addDraggablePanel(box, it) }
     add(JLayer(box, ReorderingLayerUI<JComponent>()), BorderLayout.NORTH)
     setPreferredSize(Dimension(320, 240))
   }
 
-  private fun createToolBarButton(i: Int, c: Component): Component {
-    val l = JLabel(String.format(" %04d ", i))
+  private fun addDraggablePanel(parent: Container, c: Component) {
+    val idx = parent.getComponentCount()
+    val l = JLabel(" %04d ".format(idx))
     l.setOpaque(true)
     l.setBackground(Color.RED)
     val p = JPanel(BorderLayout())
@@ -35,25 +30,22 @@ class MainPanel : JPanel(BorderLayout()) {
     p.add(l, BorderLayout.WEST)
     p.add(c)
     p.setOpaque(false)
-    return p
+    parent.add(p)
   }
 }
 
 class ReorderingLayerUI<V : JComponent> : LayerUI<V>() {
-  private val prevRect = Rectangle()
-  private val draggingRect = Rectangle()
   private val startPt = Point()
   private val dragOffset = Point()
-  private val rubberStamp = JPanel()
+  private val canvas = JPanel()
   private val gestureMotionThreshold = DragSource.getDragThreshold()
   private var draggingComponent: Component? = null
-  private var gap: Component? = null
-  private var index = -1
+  private var fillerComponent: Component? = null
 
   override fun paint(g: Graphics, c: JComponent) {
     super.paint(g, c)
     if (c is JLayer<*> && draggingComponent != null) {
-      SwingUtilities.paintComponent(g, draggingComponent, rubberStamp, draggingRect)
+      SwingUtilities.paintComponent(g, draggingComponent, canvas, DRAGGING_RECT)
     }
   }
 
@@ -74,109 +66,99 @@ class ReorderingLayerUI<V : JComponent> : LayerUI<V>() {
         startPt.setLocation(e.getPoint())
         l.repaint()
       }
-      MouseEvent.MOUSE_RELEASED -> {
-        val pt = e.getPoint()
-        val cmp = draggingComponent
-        draggingComponent = null
+      MouseEvent.MOUSE_RELEASED -> if (draggingComponent != null) {
         // swap the dragging panel and the dummy filler
-        for ((i, c) in parent.getComponents().withIndex()) {
-          if (c == gap) {
-            replaceComponent(parent, gap, cmp, i)
-            return
-          }
-          val tgt = getTargetIndex(c.getBounds(), pt, i)
-          if (tgt >= 0) {
-            replaceComponent(parent, gap, cmp, tgt)
-            return
-          }
-        }
-        val idx = if (parent.getParent().getBounds().contains(pt)) parent.getComponentCount() else index
-        replaceComponent(parent, gap, cmp, idx)
-        l.repaint()
+        val idx = parent.getComponentZOrder(fillerComponent)
+        replaceComponents(parent, fillerComponent, draggingComponent, idx)
+        draggingComponent = null
       }
     }
   }
 
   override fun processMouseMotionEvent(e: MouseEvent, l: JLayer<out V>) {
     if (e.getID() == MouseEvent.MOUSE_DRAGGED) {
-      val pt = e.getPoint()
       val parent = l.getView()
-      if (draggingComponent == null) {
-        // MotionThreshold
+      val pt = e.getPoint()
+      if (draggingComponent == null) { // MotionThreshold
         if (startPt.distance(pt) > gestureMotionThreshold) {
           startDragging(parent, pt)
         }
         return
       }
-
-      // update the cursor window location
-      updateWindowLocation(pt, parent)
+      // update the filler panel location
+      if (!PREV_RECT.contains(pt)) {
+        updateFillerLocation(parent, fillerComponent, pt)
+      }
+      // update the dragging panel location
+      updateDraggingPanelLocation(parent, pt, dragOffset)
       l.repaint()
-      if (prevRect.contains(pt)) {
+    }
+  }
+
+  private fun updateDraggingPanelLocation(parent: JComponent, pt: Point, dragOffset: Point) {
+    val i = parent.getInsets()
+    val r = SwingUtilities.calculateInnerArea(parent, INNER_RECT)
+    val x = r.x
+    val y = pt.y - dragOffset.y
+    val h = DRAGGING_RECT.height
+    val yy: Int
+    yy = if (y < i.top) {
+      i.top
+    } else {
+      if (r.contains(x, y + h)) y else r.height + i.top - h
+    }
+    DRAGGING_RECT.setLocation(x, yy)
+  }
+
+  private fun updateFillerLocation(parent: Container, filler: Component?, pt: Point) {
+    // change the dummy filler location
+    for (i in 0 until parent.getComponentCount()) {
+      val c = parent.getComponent(i)
+      val r = c.getBounds()
+      if (c == filler && r.contains(pt)) {
         return
       }
-
-      // change the dummy filler location
-      for ((i, c) in parent.getComponents().withIndex()) {
-        val r = c.getBounds()
-        if (c == gap && r.contains(pt)) {
-          return
-        }
-        val tgt = getTargetIndex(r, pt, i)
-        if (tgt >= 0) {
-          replaceComponent(parent, gap, gap, tgt)
-          return
-        }
+      val tgt = getTargetIndex(r, pt, i)
+      if (tgt >= 0) {
+        replaceComponents(parent, filler, filler, tgt)
+        return
       }
-      parent.revalidate()
-      l.repaint()
     }
   }
 
   private fun startDragging(parent: JComponent, pt: Point) {
     val c = parent.getComponentAt(pt)
-    index = parent.getComponentZOrder(c)
+    val index = parent.getComponentZOrder(c)
     if (c == parent || index < 0) {
       return
     }
     draggingComponent = c
     val r = c.getBounds()
-    draggingRect.setBounds(r) // save draggingComponent size
-
+    DRAGGING_RECT.bounds = r // save draggingComponent size
     dragOffset.setLocation(pt.x - r.x, pt.y - r.y)
-    gap = Box.createRigidArea(r.getSize())
-    replaceComponent(parent, c, gap, index)
-    updateWindowLocation(pt, parent)
-  }
-
-  private fun updateWindowLocation(pt: Point, parent: JComponent) {
-    val i = parent.getInsets()
-    val r = SwingUtilities.calculateInnerArea(parent, R3)
-    val x = r.x
-    val y = pt.y - dragOffset.y
-    val h = draggingRect.height
-    val yy = if (y < i.top) i.top else if (r.contains(x, y + h)) y else r.height + i.top - h
-    draggingRect.setLocation(x, yy)
+    fillerComponent = Box.createRigidArea(r.getSize())
+    replaceComponents(parent, c, fillerComponent, index)
+    updateDraggingPanelLocation(parent, pt, dragOffset)
   }
 
   private fun getTargetIndex(r: Rectangle, pt: Point, i: Int): Int {
     val ht2 = (.5 + r.height * .5).toInt()
-    R1.setBounds(r.x, r.y, r.width, ht2)
-    R2.setBounds(r.x, r.y + ht2, r.width, ht2)
+    TOP_HALF_RECT.setBounds(r.x, r.y, r.width, ht2)
+    BOTTOM_HALF_RECT.setBounds(r.x, r.y + ht2, r.width, ht2)
     return when {
-      R1.contains(pt) -> {
-        prevRect.setBounds(R1)
+      TOP_HALF_RECT.contains(pt) -> {
+        PREV_RECT.setBounds(TOP_HALF_RECT)
         if (i > 1) i else 0
       }
-      R2.contains(pt) -> {
-        prevRect.setBounds(R2)
+      BOTTOM_HALF_RECT.contains(pt) -> {
+        PREV_RECT.setBounds(BOTTOM_HALF_RECT)
         i
       }
       else -> -1
     }
   }
 
-  private fun replaceComponent(parent: Container, remove: Component?, insert: Component?, idx: Int) {
+  private fun replaceComponents(parent: Container, remove: Component?, insert: Component?, idx: Int) {
     if (insert == null) {
       return
     }
@@ -187,9 +169,11 @@ class ReorderingLayerUI<V : JComponent> : LayerUI<V>() {
   }
 
   companion object {
-    private val R1 = Rectangle()
-    private val R2 = Rectangle()
-    private val R3 = Rectangle()
+    private val TOP_HALF_RECT = Rectangle()
+    private val BOTTOM_HALF_RECT = Rectangle()
+    private val INNER_RECT = Rectangle()
+    private val PREV_RECT = Rectangle()
+    private val DRAGGING_RECT = Rectangle()
   }
 }
 
