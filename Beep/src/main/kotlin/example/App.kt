4e0325b69640f3d5acfe0cee8baa56bd139d8ec4
aterai <aterai@outlook.com>
2020-01-02 04:43:11 +0900

    add: Beep

---
+++ Beep/src/main/kotlin/example/App.kt
@@ -0,0 +1,38 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder("Toolkit.getDefaultToolkit().beep()")
+  val button = JButton("showMessageDialog")
+  button.addActionListener {
+    Toolkit.getDefaultToolkit().beep()
+    JOptionPane.showMessageDialog(p.rootPane, "Error Message", "Title", JOptionPane.ERROR_MESSAGE)
+  }
+  p.add(button)
+  return JPanel(BorderLayout()).also {
+    it.add(p)
+    it.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
