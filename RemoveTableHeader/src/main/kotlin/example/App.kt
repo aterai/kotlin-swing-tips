59067b54dd25636594dcafe2e6ac9e5b7b952a50
aterai <aterai@outlook.com>
2020-01-03 03:09:53 +0900

    add: RemoveTableHeader

---
+++ RemoveTableHeader/src/main/kotlin/example/App.kt
@@ -0,0 +1,63 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellRenderer
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true), arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true), arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = object : JTable(model) {
+    private val evenColor = Color(0xF0_F0_F0)
+    override fun prepareRenderer(tcr: TableCellRenderer, row: Int, column: Int): Component {
+      val c = super.prepareRenderer(tcr, row, column)
+      if (isCellSelected(row, column)) {
+        c.foreground = getSelectionForeground()
+        c.background = getSelectionBackground()
+      } else {
+        c.foreground = foreground
+        c.background = if (row % 2 == 0) evenColor else background
+      }
+      return c
+    }
+  }
+  table.autoCreateRowSorter = true
+
+  val scrollPane = JScrollPane(table)
+  val check = JCheckBox("JTableHeader visible: ", true)
+  check.addActionListener { e ->
+    scrollPane.columnHeader.isVisible = (e.source as? JCheckBox)?.isSelected ?: false
+    scrollPane.revalidate()
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(scrollPane)
+    it.add(check, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
