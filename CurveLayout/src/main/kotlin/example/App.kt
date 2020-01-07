6145c181f7799baaa9e8e397134de6ce821b8a7e
aterai <aterai@outlook.com>
2020-01-07 15:45:59 +0900

    refactor: use 'pow' function instead of Math.pow(...)

--- CurveLayout/src/main/kotlin/example/App.kt
+++ CurveLayout/src/main/kotlin/example/App.kt
@@ -2,13 +2,15 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 fun makeUI(): Component {
   val a2 = 4.0
   val panel1 = JPanel(FlowLayout(FlowLayout.LEFT))
   val panel2 = object : JPanel() {
     // protected val A2 = 4.0
-    protected override fun paintComponent(g: Graphics) {
+    override fun paintComponent(g: Graphics) {
       super.paintComponent(g)
       val i = getInsets()
       val g2 = g.create() as? Graphics2D ?: return
@@ -18,7 +20,7 @@ fun makeUI(): Component {
       var px = 0
       var py = 0
       for (x in 0 until w) {
-        val y = Math.pow(x / a2, 2.0).toInt()
+        val y = (x / a2).pow(2.0).toInt()
         g2.drawLine(px, py, x, y)
         px = x
         py = y
@@ -38,7 +40,7 @@ fun makeUI(): Component {
             val insets = target.getInsets()
             val vgap = getVgap()
             val hgap = getHgap()
-            val rowh = (target.getHeight() - insets.top - insets.bottom - vgap * 2) / nmembers
+            val rh = (target.getHeight() - insets.top - insets.bottom - vgap * 2) / nmembers
             var x = insets.left + hgap
             var y = insets.top + vgap
             for (i in 0 until nmembers) {
@@ -47,8 +49,8 @@ fun makeUI(): Component {
                 val d = m.getPreferredSize()
                 m.setSize(d.width, d.height)
                 m.setLocation(x, y)
-                y += vgap + minOf(rowh, d.height)
-                x = (a2 * Math.sqrt(y.toDouble())).toInt()
+                y += vgap + minOf(rh, d.height)
+                x = (a2 * sqrt(y.toDouble())).toInt()
               }
             }
           }
@@ -66,11 +68,11 @@ fun makeUI(): Component {
 
 private fun initPanel(title: String, p: JComponent): Component {
   p.setBorder(BorderFactory.createTitledBorder(title))
-  p.add(JCheckBox("aaaaaaaaaaaaaaa"))
-  p.add(JCheckBox("bbbbbbbb"))
-  p.add(JCheckBox("ccccccccccc"))
-  p.add(JCheckBox("ddddddd"))
-  p.add(JCheckBox("eeeeeeeeeee"))
+  p.add(JCheckBox("000000000000000000"))
+  p.add(JCheckBox("11111111111"))
+  p.add(JCheckBox("222222"))
+  p.add(JCheckBox("3333333333"))
+  p.add(JCheckBox("444444444444"))
   return p
 }
 
