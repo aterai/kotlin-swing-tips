feb5356685a0affefd827cc320b62a2af943544f
aterai <aterai@outlook.com>
2020-01-11 05:18:03 +0900

    add: ToggleButtonBar

---
+++ ToggleButtonBar/src/main/kotlin/example/App.kt
@@ -0,0 +1,170 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.geom.Area
+import java.awt.geom.Path2D
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+class MainPanel : JPanel() {
+  init {
+    val roundIcon = ToggleButtonBarCellIcon()
+    val rectIcon = CellIcon()
+    add(makeToggleButtonBar(0xFF_74_00, roundIcon))
+    add(makeToggleButtonBar(0x55_55_55, rectIcon))
+    add(makeToggleButtonBar(0x00_64_00, roundIcon))
+    add(makeToggleButtonBar(0x8B_00_00, rectIcon))
+    add(makeToggleButtonBar(0x00_1E_43, roundIcon))
+    preferredSize = Dimension(320, 240)
+  }
+
+  private fun makeButton(title: String): AbstractButton {
+    val b = JRadioButton(title)
+    b.horizontalTextPosition = SwingConstants.CENTER
+    b.border = BorderFactory.createEmptyBorder()
+    b.isContentAreaFilled = false
+    b.isFocusPainted = false
+    b.foreground = Color.WHITE
+    return b
+  }
+
+  private fun makeToggleButtonBar(cc: Int, icon: Icon): Component {
+    val bg = ButtonGroup()
+    val p = JPanel(GridLayout(1, 0, 0, 0))
+    p.border = BorderFactory.createTitledBorder(String.format("Color: #%06X", cc))
+    val color = Color(cc)
+    listOf("left", "center", "right")
+      .map { makeButton(it) }
+      .forEach {
+        it.background = color
+        it.icon = icon
+        bg.add(it)
+        p.add(it)
+      }
+    return p
+  }
+}
+
+class CellIcon : Icon {
+  override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    g2.translate(x, y)
+    var ssc = TL
+    var bgc = BR
+    if (c is AbstractButton) {
+      val m = c.model
+      if (m.isSelected || m.isRollover) {
+        ssc = ST
+        bgc = SB
+      }
+    }
+    val w = c.width
+    val h = c.height
+    g2.paint = c.background
+    g2.fillRect(0, 0, w, h)
+    g2.paint = GradientPaint(0f, 0f, ssc, 0f, h.toFloat(), bgc, true)
+    g2.fillRect(0, 0, w, h)
+    g2.paint = TL
+    g2.fillRect(0, 0, 1, h)
+    g2.paint = BR
+    g2.fillRect(w, 0, 1, h)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 80
+
+  override fun getIconHeight() = 20
+
+  companion object {
+    private val TL = Color(1f, 1f, 1f, .2f)
+    private val BR = Color(0f, 0f, 0f, .2f)
+    private val ST = Color(1f, 1f, 1f, .4f)
+    private val SB = Color(1f, 1f, 1f, .1f)
+  }
+}
+
+class ToggleButtonBarCellIcon : Icon {
+  override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
+    val parent = c.parent ?: return
+    val r = 8f
+    val fx = x.toFloat()
+    val fy = y.toFloat()
+    var fw = c.width.toFloat()
+    val fh = c.height - 1f
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    val p = Path2D.Float()
+    when {
+      c === parent.getComponent(0) -> { // :first-child
+        p.moveTo(fx, y + r)
+        p.quadTo(fx, fy, x + r, fy)
+        p.lineTo(x + fw, fy)
+        p.lineTo(x + fw, y + fh)
+        p.lineTo(x + r, y + fh)
+        p.quadTo(fx, y + fh, fx, y + fh - r)
+      }
+      c === parent.getComponent(parent.componentCount - 1) -> { // :last-child
+        fw--
+        p.moveTo(fx, fy)
+        p.lineTo(x + fw - r, fy)
+        p.quadTo(x + fw, fy, x + fw, y + r)
+        p.lineTo(x + fw, y + fh - r)
+        p.quadTo(x + fw, y + fh, x + fw - r, y + fh)
+        p.lineTo(fx, y + fh)
+      }
+      else -> {
+        p.moveTo(fx, fy)
+        p.lineTo(x + fw, fy)
+        p.lineTo(x + fw, y + fh)
+        p.lineTo(fx, y + fh)
+      }
+    }
+    p.closePath()
+    var ssc = TL
+    var bgc = BR
+    if (c is AbstractButton) {
+      val m = c.model
+      if (m.isSelected || m.isRollover) {
+        ssc = ST
+        bgc = SB
+      }
+    }
+    val area = Area(p)
+    g2.paint = c.background
+    g2.fill(area)
+    g2.paint = GradientPaint(fx, fy, ssc, fx, y + fh, bgc, true)
+    g2.fill(area)
+    g2.paint = BR
+    g2.draw(area)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 80
+
+  override fun getIconHeight() = 20
+
+  companion object {
+    private val TL = Color(1f, 1f, 1f, .2f)
+    private val BR = Color(0f, 0f, 0f, .2f)
+    private val ST = Color(1f, 1f, 1f, .4f)
+    private val SB = Color(1f, 1f, 1f, .1f)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
