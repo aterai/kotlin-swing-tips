943d3afb2877381bb7ea592499f0396db3788869
aterai <aterai@outlook.com>
2020-01-25 00:48:55 +0900

    add: ButtonPainted

---
+++ ButtonPainted/src/main/kotlin/example/App.kt
@@ -0,0 +1,140 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.image.BufferedImage
+import java.awt.image.RescaleOp
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+class MainPanel : JPanel(BorderLayout()) {
+  init {
+    val p = JPanel()
+    val b1 = JButton("button")
+    val b2 = JButton()
+    val rss = ImageIcon(javaClass.getResource("feed-icon-14x14.png")) // http://feedicons.com/
+    b2.icon = rss
+    b2.rolloverIcon = makeRolloverIcon(rss)
+    p.add(b1)
+    p.add(b2)
+    p.border = BorderFactory.createEmptyBorder(20, 10, 20, 10)
+    val list = listOf(b1, b2)
+
+    val focusPainted = JCheckBox("setFocusPainted", true)
+    focusPainted.addActionListener { e ->
+      val flg = (e.source as? JCheckBox)?.isSelected == true
+      list.forEach { it.isFocusPainted = flg }
+      p.revalidate()
+    }
+
+    val borderPainted = JCheckBox("setBorderPainted", true)
+    borderPainted.addActionListener { e ->
+      val flg = (e.source as? JCheckBox)?.isSelected == true
+      list.forEach { it.isBorderPainted = flg }
+      p.revalidate()
+    }
+
+    val contentAreaFilled = JCheckBox("setContentAreaFilled", true)
+    contentAreaFilled.addActionListener { e ->
+      val flg = (e.source as? JCheckBox)?.isSelected == true
+      list.forEach { it.isContentAreaFilled = flg }
+      p.revalidate()
+    }
+
+    val rolloverEnabled = JCheckBox("setRolloverEnabled", true)
+    rolloverEnabled.addActionListener { e ->
+      val flg = (e.source as? JCheckBox)?.isSelected == true
+      list.forEach { it.isRolloverEnabled = flg }
+      p.revalidate()
+    }
+
+    val box = Box.createVerticalBox()
+    listOf(focusPainted, borderPainted, contentAreaFilled, rolloverEnabled).forEach { box.add(it) }
+    add(box, BorderLayout.NORTH)
+    add(p)
+
+    val mb = JMenuBar()
+    mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+    EventQueue.invokeLater { getRootPane().setJMenuBar(mb) }
+
+    border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    preferredSize = Dimension(320, 240)
+  }
+
+  private fun makeRolloverIcon(srcIcon: ImageIcon): ImageIcon {
+    val w = srcIcon.iconWidth
+    val h = srcIcon.iconHeight
+    val img = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+    val g2 = img.createGraphics()
+    srcIcon.paintIcon(null, g2, 0, 0)
+    val scaleFactors = floatArrayOf(1.2f, 1.2f, 1.2f, 1f)
+    val offsets = floatArrayOf(0f, 0f, 0f, 0f)
+    val op = RescaleOp(scaleFactors, offsets, g2.renderingHints)
+    g2.dispose()
+    return ImageIcon(op.filter(img, null))
+  }
+}
+
+object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.getName()
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.getName(), lafInfo.getClassName(), lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(lafName: String, lafClassName: String, lafRadioGroup: ButtonGroup): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.setActionCommand(lafClassName)
+    lafItem.setHideActionText(true)
+    lafItem.addActionListener {
+      val m = lafRadioGroup.getSelection()
+      runCatching {
+        setLookAndFeel(m.getActionCommand())
+      }.onFailure {
+        it.printStackTrace()
+        Toolkit.getDefaultToolkit().beep()
+      }
+    }
+    lafRadioGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(MainPanel())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
