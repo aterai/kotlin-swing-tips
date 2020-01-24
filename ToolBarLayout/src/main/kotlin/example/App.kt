d0c789e1e331f3c2ad6aa780453a6a4ec1c48548
aterai <aterai@outlook.com>
2020-01-25 01:21:20 +0900

    detekt: MatchingDeclarationName

--- ToolBarLayout/src/main/kotlin/example/App.kt
+++ ToolBarLayout/src/main/kotlin/example/App.kt
@@ -4,40 +4,41 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import java.net.URL
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    var url1 = javaClass.getResource("/toolbarButtonGraphics/general/Copy24.gif")
-    var url2 = javaClass.getResource("/toolbarButtonGraphics/general/Cut24.gif")
-    var url3 = javaClass.getResource("/toolbarButtonGraphics/general/Help24.gif")
-    if (url1 == null) {
-      url1 = javaClass.getResource("Copy24.gif")
-      url2 = javaClass.getResource("Cut24.gif")
-      url3 = javaClass.getResource("Help24.gif")
-    }
-
-    val toolbar1 = JToolBar("ToolBarButton")
-    toolbar1.add(JButton(ImageIcon(url1)))
-    toolbar1.add(JButton(ImageIcon(url2)))
-    toolbar1.add(Box.createGlue())
-    toolbar1.add(JButton(ImageIcon(url3)))
-
-    val toolbar2 = JToolBar("JButton")
-    toolbar2.add(createToolBarButton(url1))
-    toolbar2.add(createToolBarButton(url2))
-    toolbar2.add(Box.createGlue())
-    toolbar2.add(createToolBarButton(url3))
-
-    add(toolbar1, BorderLayout.NORTH)
-    add(JScrollPane(JTextArea()))
-    add(toolbar2, BorderLayout.SOUTH)
-    preferredSize = Dimension(320, 240)
+fun makeUI(): Component {
+  val cl = Thread.currentThread().getContextClassLoader()
+  var url1 = cl.getResource("toolbarButtonGraphics/general/Copy24.gif")
+  var url2 = cl.getResource("toolbarButtonGraphics/general/Cut24.gif")
+  var url3 = cl.getResource("toolbarButtonGraphics/general/Help24.gif")
+  if (url1 == null) {
+    url1 = cl.getResource("example/Copy24.gif")
+    url2 = cl.getResource("example/Cut24.gif")
+    url3 = cl.getResource("example/Help24.gif")
   }
 
-  private fun createToolBarButton(url: URL?): JButton {
-    val b = JButton(ImageIcon(url))
-    b.isRequestFocusEnabled = false
-    return b
-  }
+  val toolbar1 = JToolBar("ToolBarButton")
+  toolbar1.add(JButton(ImageIcon(url1)))
+  toolbar1.add(JButton(ImageIcon(url2)))
+  toolbar1.add(Box.createGlue())
+  toolbar1.add(JButton(ImageIcon(url3)))
+
+  val toolbar2 = JToolBar("JButton")
+  toolbar2.add(createToolBarButton(url1))
+  toolbar2.add(createToolBarButton(url2))
+  toolbar2.add(Box.createGlue())
+  toolbar2.add(createToolBarButton(url3))
+
+  val p = JPanel(BorderLayout())
+  p.add(toolbar1, BorderLayout.NORTH)
+  p.add(JScrollPane(JTextArea()))
+  p.add(toolbar2, BorderLayout.SOUTH)
+  p.preferredSize = Dimension(320, 240)
+  return p
+}
+
+private fun createToolBarButton(url: URL?): JButton {
+  val b = JButton(ImageIcon(url))
+  b.isRequestFocusEnabled = false
+  return b
 }
 
 fun main() {
@@ -50,7 +51,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
