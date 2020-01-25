2317a42a202752e1e860b4a386b8fab75cd77067
aterai <aterai@outlook.com>
2020-01-26 00:44:02 +0900

    add: Reordering

---
+++ Reordering/src/main/kotlin/example/App.kt
@@ -0,0 +1,48 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  val header = table.tableHeader
+  header.reorderingAllowed = false
+  val checkBox = JCheckBox("disable column dragging", true)
+  checkBox.addActionListener {
+    header.reorderingAllowed = (it.source as? JCheckBox)?.isSelected != true
+  }
+  val p = JPanel(BorderLayout())
+  p.add(checkBox, BorderLayout.NORTH)
+  p.add(JScrollPane(table))
+  p.preferredSize = Dimension(320, 240)
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
