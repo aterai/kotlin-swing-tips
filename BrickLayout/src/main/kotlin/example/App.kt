29fce9cb38988ffd8c240a2016cc304bfa2ea1ce
aterai <aterai@outlook.com>
2020-02-02 01:48:33 +0900

    add: BrickLayout

---
+++ BrickLayout/src/main/kotlin/example/App.kt
@@ -0,0 +1,56 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private const val X_SIZE = 6
+private const val Y_SIZE = 8
+private const val WIDTH = 2
+
+fun makeUI(): Component {
+  val panel = JPanel(GridBagLayout())
+  panel.border = BorderFactory.createTitledBorder("Brick Layout")
+  val c = GridBagConstraints()
+  c.fill = GridBagConstraints.HORIZONTAL
+  for (y in 0 until Y_SIZE) {
+    c.gridx = y and 1 // start x offset
+    c.gridwidth = WIDTH
+    for (x in 0 until X_SIZE) {
+      panel.add(makeBrick(), c)
+      c.gridx += WIDTH
+    }
+  }
+  // GridBagLayout to create a board
+  // https://community.oracle.com/thread/1357310
+  // <dummy-row>
+  c.gridwidth = 1
+  c.gridx = 0
+  while (c.gridx <= WIDTH * X_SIZE) {
+    panel.add(Box.createHorizontalStrut(24), c)
+    c.gridx++
+  }
+  // </dummy-row>
+
+  panel.preferredSize = Dimension(320, 240)
+  return panel
+}
+
+private fun makeBrick() = JButton(" ")
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
