d693576ac825da391c16aa8b53e198dcc17a3428
aterai <aterai@outlook.com>
2020-02-02 01:51:39 +0900

    detekt: MatchingDeclarationName

--- TraverseAllNodes/src/main/kotlin/example/App.kt
+++ TraverseAllNodes/src/main/kotlin/example/App.kt
@@ -4,48 +4,49 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.tree.DefaultMutableTreeNode
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val tree = JTree()
-    val textArea = JTextArea()
-    val root = tree.model.root as DefaultMutableTreeNode
+fun makeUI(): Component {
+  val tree = JTree()
+  val textArea = JTextArea()
+  val root = tree.model.root as DefaultMutableTreeNode
 
-    val depthFirst = JButton("<html>depthFirst<br>postorder")
-    depthFirst.addActionListener {
-      textArea.text = ""
-      root.depthFirstEnumeration()
-        .toList().forEach { textArea.append("$it\n") }
-    }
+  val depthFirst = JButton("<html>depthFirst<br>postorder")
+  depthFirst.addActionListener {
+    textArea.text = ""
+    root.depthFirstEnumeration()
+      .toList().forEach { textArea.append("$it\n") }
+  }
 
-    val breadthFirst = JButton("breadthFirst")
-    breadthFirst.addActionListener {
-      textArea.text = ""
-      root.breadthFirstEnumeration()
-        .toList().forEach { textArea.append("$it\n") }
-    }
+  val breadthFirst = JButton("breadthFirst")
+  breadthFirst.addActionListener {
+    textArea.text = ""
+    root.breadthFirstEnumeration()
+      .toList().forEach { textArea.append("$it\n") }
+  }
 
-    val preorder = JButton("preorder")
-    preorder.addActionListener {
-      textArea.text = ""
-      root.preorderEnumeration()
-        .toList().forEach { textArea.append("$it\n") }
-    }
+  val preorder = JButton("preorder")
+  preorder.addActionListener {
+    textArea.text = ""
+    root.preorderEnumeration()
+      .toList().forEach { textArea.append("$it\n") }
+  }
 
-    val p = JPanel(GridLayout(0, 1, 5, 5))
-    p.add(depthFirst)
-    p.add(breadthFirst)
-    p.add(preorder)
+  val p = JPanel(GridLayout(0, 1, 5, 5))
+  p.add(depthFirst)
+  p.add(breadthFirst)
+  p.add(preorder)
 
-    val panel = JPanel(BorderLayout())
-    panel.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
-    panel.add(p, BorderLayout.NORTH)
+  val panel = JPanel(BorderLayout())
+  panel.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  panel.add(p, BorderLayout.NORTH)
 
-    val sp = JSplitPane()
-    sp.leftComponent = JScrollPane(tree)
-    sp.rightComponent = JScrollPane(textArea)
-    add(sp)
-    add(panel, BorderLayout.EAST)
-    preferredSize = Dimension(320, 240)
+  val sp = JSplitPane()
+  sp.leftComponent = JScrollPane(tree)
+  sp.rightComponent = JScrollPane(textArea)
+
+  return JPanel(BorderLayout()).also {
+    it.add(sp)
+    it.add(panel, BorderLayout.EAST)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
@@ -59,7 +60,7 @@ fun main() {
     }
     JFrame().apply {
       setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
-      getContentPane().add(MainPanel())
+      getContentPane().add(makeUI())
       pack()
       setLocationRelativeTo(null)
       setVisible(true)
