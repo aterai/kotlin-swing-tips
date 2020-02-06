d7c9ec03f12664029e145160d8ab04449df68c1e
aterai <aterai@outlook.com>
2020-02-06 14:03:00 +0900

    add: SpinnerEditor

---
+++ SpinnerEditor/src/main/kotlin/example/App.kt
@@ -0,0 +1,56 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private fun makeUI(): Component {
+  val model = SpinnerNumberModel(10, 0, 1000, 1)
+
+  val spinner1 = JSpinner(model)
+  spinner1.isEnabled = false
+  // UIManager.put("FormattedTextField.inactiveBackground", Color.RED)
+
+  val spinner2 = JSpinner(model)
+  (spinner2.editor as? JSpinner.NumberEditor)?.textField?.also {
+    it.isEditable = false
+    it.background = UIManager.getColor("FormattedTextField.background")
+  }
+
+  val box = Box.createVerticalBox()
+  box.add(makeTitledPanel("Default", JSpinner(model)))
+  box.add(Box.createVerticalStrut(10))
+  box.add(makeTitledPanel("spinner.setEnabled(false)", spinner1))
+  box.add(Box.createVerticalStrut(10))
+  box.add(makeTitledPanel("editor.setEditable(false)", spinner2))
+
+  val p = JPanel(BorderLayout())
+  p.add(box, BorderLayout.NORTH)
+  p.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  p.preferredSize = Dimension(320, 240)
+  return p
+}
+
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c)
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
