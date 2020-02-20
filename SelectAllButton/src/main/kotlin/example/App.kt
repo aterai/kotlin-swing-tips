f5484daa98f0f5181d650954b0bff1d59f5e733c
aterai <aterai@outlook.com>
2020-02-21 02:02:30 +0900

    add: SelectAllButton

---
+++ SelectAllButton/src/main/kotlin/example/App.kt
@@ -0,0 +1,73 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  val selectAllAction = object : AbstractAction("selectAll") {
+    override fun actionPerformed(e: ActionEvent) {
+      e.source = table
+      table.actionMap["selectAll"].actionPerformed(e)
+    }
+  }
+  val copyAction = object : AbstractAction("copy") {
+    override fun actionPerformed(e: ActionEvent) {
+      e.source = table
+      table.actionMap["copy"].actionPerformed(e)
+    }
+  }
+
+  val menuBar = JMenuBar()
+  val menu = JMenu("Edit")
+  menu.mnemonic = KeyEvent.VK_E
+  menuBar.add(menu)
+  val p = JPanel()
+  listOf(selectAllAction, copyAction).forEach {
+    menu.add(it)
+    p.add(JButton(it))
+  }
+
+  val sp = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+  sp.topComponent = JScrollPane(table)
+  sp.bottomComponent = JScrollPane(JTextArea())
+  sp.resizeWeight = .5
+
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(sp)
+    it.preferredSize = Dimension(320, 240)
+    EventQueue.invokeLater { p.rootPane.jMenuBar = menuBar }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
