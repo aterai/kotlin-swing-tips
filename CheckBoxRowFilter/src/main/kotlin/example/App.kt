144ba4cf359146c64e628ba50086cf2debb81d2f
aterai <aterai@outlook.com>
2020-02-23 05:41:49 +0900

    add: CheckBoxRowFilter

---
+++ CheckBoxRowFilter/src/main/kotlin/example/App.kt
@@ -0,0 +1,66 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.event.TableModelEvent
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableModel
+import javax.swing.table.TableRowSorter
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("#", "String", "Integer")
+  val data = arrayOf(
+    arrayOf(false, "aaa", 12),
+    arrayOf(false, "bbb", 5),
+    arrayOf(false, "CCC", 92),
+    arrayOf(false, "DDD", 0)
+  )
+  val model: TableModel = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val selector = JTable(model)
+  selector.autoCreateRowSorter = true
+  selector.columnModel.getColumn(0).maxWidth = 32
+  val viewer = object : JTable(model) {
+    override fun isCellEditable(row: Int, column: Int) = false
+  }
+  viewer.autoCreateRowSorter = true
+  val cm = viewer.columnModel
+  cm.removeColumn(cm.getColumn(0))
+  val sorter = TableRowSorter(model)
+  viewer.rowSorter = sorter
+  sorter.rowFilter = object : RowFilter<TableModel, Int?>() {
+    override fun include(entry: Entry<out TableModel, out Int?>): Boolean {
+      val i = entry.identifier ?: -1
+      return entry.model.getValueAt(i, 0) == true
+    }
+  }
+  model.addTableModelListener { e ->
+    if (e.type == TableModelEvent.UPDATE) {
+      sorter.allRowsChanged()
+    }
+  }
+  return JPanel(GridLayout(0, 1)).also {
+    it.add(JScrollPane(selector))
+    it.add(JScrollPane(viewer))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
