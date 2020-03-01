093116591f87e984e3763387c4aa22cf5e43a34d
aterai <aterai@outlook.com>
2020-03-01 19:03:01 +0900

    add: DefaultEditorKit

---
+++ DefaultEditorKit/src/main/kotlin/example/App.kt
@@ -0,0 +1,80 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.text.DefaultEditorKit.CopyAction
+import javax.swing.text.DefaultEditorKit.CutAction
+import javax.swing.text.DefaultEditorKit.PasteAction
+import javax.swing.text.JTextComponent
+
+fun makeUI(): Component {
+  val pf1 = JTextField(30)
+  pf1.componentPopupMenu = TextFieldPopupMenu()
+  val pf2 = JTextField(30)
+  pf2.componentPopupMenu = TextFieldPopupMenu()
+  val panel = Box.createVerticalBox()
+  panel.border = BorderFactory.createTitledBorder("E-mail Address")
+  panel.add(pf1)
+  panel.add(Box.createVerticalStrut(5))
+  panel.add(JLabel("Please enter your email address twice for confirmation:"))
+  panel.add(pf2)
+  panel.add(Box.createVerticalStrut(5))
+  return JPanel(BorderLayout()).also {
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.add(panel, BorderLayout.NORTH)
+    it.add(JScrollPane(JTextArea("Dummy")))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private class TextFieldPopupMenu : JPopupMenu() {
+  override fun show(c: Component, x: Int, y: Int) {
+    if (c is JTextComponent) {
+      val hasSelectedText = c.selectedText == null
+      for (menuElement in subElements) {
+        val m = menuElement.component
+        if (m is JMenuItem && m.action is PasteAction) {
+          continue
+        }
+        m.isEnabled = hasSelectedText
+      }
+      super.show(c, x, y)
+    }
+  }
+
+  init {
+    add(CutAction())
+    add(CopyAction())
+    add(PasteAction())
+    add(object : AbstractAction("delete") {
+      override fun actionPerformed(e: ActionEvent) {
+        (invoker as? JTextComponent)?.replaceSelection(null)
+      }
+    })
+    addSeparator()
+    add(object : AbstractAction("cut2") {
+      override fun actionPerformed(e: ActionEvent) {
+        (invoker as? JTextComponent)?.cut()
+      }
+    })
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
