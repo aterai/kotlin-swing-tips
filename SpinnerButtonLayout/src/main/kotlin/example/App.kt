b783795fd9a1aace588eafe3e6d36b3a33b241d7
aterai <aterai@outlook.com>
2020-03-06 15:57:58 +0900

    add: SpinnerButtonLayout

---
+++ SpinnerButtonLayout/src/main/kotlin/example/App.kt
@@ -0,0 +1,70 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.basic.BasicSpinnerUI
+
+fun makeUI(): Component {
+  val model = SpinnerNumberModel(10, 0, 1000, 1)
+  val box = Box.createVerticalBox()
+  box.add(makeTitledPanel("Default", JSpinner(model)))
+  val spinner1 = JSpinner(model)
+  spinner1.componentOrientation = ComponentOrientation.RIGHT_TO_LEFT
+  box.add(makeTitledPanel("RIGHT_TO_LEFT", spinner1))
+  val spinner2 = object : JSpinner(model) {
+    override fun updateUI() {
+      super.updateUI()
+      setUI(object : BasicSpinnerUI() {
+        override fun createLayout() = SpinnerLayout()
+      })
+    }
+  }
+  box.add(makeTitledPanel("L(Prev), R(Next)", spinner2))
+  val spinner3 = object : JSpinner(model) {
+    override fun setLayout(mgr: LayoutManager) {
+      super.setLayout(SpinnerLayout())
+    }
+  }
+  box.add(makeTitledPanel("L(Prev), R(Next)", spinner3))
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+private class SpinnerLayout : BorderLayout() {
+  private val layoutMap = mapOf(
+    "Editor" to "Center",
+    "Next" to "East",
+    "Previous" to "West"
+  )
+
+  override fun addLayoutComponent(comp: Component, constraints: Any) {
+    val cons = layoutMap[constraints] ?: constraints
+    super.addLayoutComponent(comp, cons)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
