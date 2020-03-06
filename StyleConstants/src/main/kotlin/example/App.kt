2fa5a5e4a5e87e0d1aafe278c1fe843c69d9355b
aterai <aterai@outlook.com>
2020-03-06 15:59:24 +0900

    add: StyleConstants

---
+++ StyleConstants/src/main/kotlin/example/App.kt
@@ -0,0 +1,69 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.text.StyleConstants
+import javax.swing.text.StyleContext
+
+private val jtp = JTextPane()
+
+private fun append(str: String, flg: Boolean) {
+  val style = if (flg) StyleContext.DEFAULT_STYLE else "error"
+  val doc = jtp.styledDocument
+  runCatching {
+    doc.insertString(doc.length, str, doc.getStyle(style))
+  }
+}
+
+fun makeUI(): Component {
+  val ok = JButton("Test")
+  ok.addActionListener { append("Test test test test\n", true) }
+
+  val err = JButton("Error")
+  err.addActionListener { append("Error error error error\n", false) }
+
+  val clr = JButton("Clear")
+  clr.addActionListener { jtp.text = "" }
+
+  val box = Box.createHorizontalBox()
+  box.add(Box.createHorizontalGlue())
+  box.add(ok)
+  box.add(err)
+  box.add(Box.createHorizontalStrut(5))
+  box.add(clr)
+
+  jtp.isEditable = false
+  val doc = jtp.styledDocument
+  val def = doc.getStyle(StyleContext.DEFAULT_STYLE)
+  val error = doc.addStyle("error", def)
+  StyleConstants.setForeground(error, Color.RED)
+
+  val scroll = JScrollPane(jtp)
+  scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+  scroll.verticalScrollBar.unitIncrement = 25
+
+  return JPanel(BorderLayout(5, 5)).also {
+    it.add(scroll)
+    it.add(box, BorderLayout.SOUTH)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
