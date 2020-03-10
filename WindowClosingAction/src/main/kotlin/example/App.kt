c00a010868a47b15a7859460a8202eab9d968769
aterai <aterai@outlook.com>
2020-03-10 17:49:14 +0900

    add: WindowClosingAction

---
+++ WindowClosingAction/src/main/kotlin/example/App.kt
@@ -0,0 +1,76 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.WindowEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.basic.BasicToolBarUI
+
+fun makeUI(): Component {
+  val list = listOf<JComponent>(
+    JMenuItem("Open(dummy)"), JMenuItem("Save(dummy)"),
+    JSeparator(), JMenuItem(ExitAction())
+  )
+
+  val popup = JPopupMenu()
+  list.forEach { popup.add(it) }
+
+  val menu = JMenu("File")
+  list.forEach { menu.add(it) }
+
+  val bar = JMenuBar()
+  bar.add(menu)
+
+  val toolBar = JToolBar()
+  toolBar.add(JLabel("Floatable JToolBar:"))
+  toolBar.add(Box.createGlue())
+  toolBar.add(ExitAction())
+
+  val tree = JTree()
+  tree.componentPopupMenu = popup
+
+  return JPanel(BorderLayout()).also {
+    it.add(bar, BorderLayout.NORTH)
+    it.add(JScrollPane(tree))
+    it.add(toolBar, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private class ExitAction : AbstractAction("Exit") {
+  override fun actionPerformed(e: ActionEvent) {
+    val p = SwingUtilities.getUnwrappedParent(e.source as? Component) ?: return
+    val root = when (p) {
+      is JPopupMenu -> SwingUtilities.getRoot(p.invoker)
+      is JToolBar -> getToolBarRoot(p)
+      else -> SwingUtilities.getRoot(p)
+    }
+    (root as? Window)?.also { window ->
+      window.dispatchEvent(WindowEvent(window, WindowEvent.WINDOW_CLOSING))
+    }
+  }
+
+  private fun getToolBarRoot(b: JToolBar) = if ((b.ui as? BasicToolBarUI)?.isFloating == true) {
+    SwingUtilities.getWindowAncestor(b).owner
+  } else {
+    SwingUtilities.getRoot(b)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
