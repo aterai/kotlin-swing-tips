5c841dba42d2d811c40164ef3a97fc5f591cfaed
aterai <aterai@outlook.com>
2020-03-12 16:34:37 +0900

    add: AnimatedGif

---
+++ AnimatedGif/src/main/kotlin/example/App.kt
@@ -0,0 +1,44 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val box = Box.createVerticalBox()
+  val cl = Thread.currentThread().getContextClassLoader()
+  listOf(
+      "no_disposal_specified",
+      "do_not_dispose",
+      "restore_to_background_color",
+      "restore_to_previous"
+    )
+    .forEach {
+      val icon = ImageIcon(cl.getResource("example/$it.gif"))
+      box.add(JLabel(it, icon, SwingConstants.LEFT))
+      box.add(Box.createVerticalStrut(20))
+    }
+  box.add(Box.createVerticalGlue())
+  return JPanel(BorderLayout()).also {
+    it.add(box)
+    it.border = BorderFactory.createEmptyBorder(20, 40, 20, 40)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+      getContentPane().add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      setVisible(true)
+    }
+  }
+}
