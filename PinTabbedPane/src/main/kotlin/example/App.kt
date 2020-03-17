c42d75c8e7e057b6af99799587a974e476d0a9f5
aterai <aterai@outlook.com>
2020-03-17 15:01:32 +0900

    add: PinTabbedPane

---
+++ PinTabbedPane/src/main/kotlin/example/App.kt
@@ -0,0 +1,115 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val icons = listOf(
+    "wi0009-16.png", "wi0054-16.png", "wi0062-16.png",
+    "wi0063-16.png", "wi0124-16.png", "wi0126-16.png"
+  )
+  val tabbedPane = JTabbedPane(SwingConstants.TOP, JTabbedPane.SCROLL_TAB_LAYOUT)
+  val cl = Thread.currentThread().contextClassLoader
+  icons.forEach {
+    tabbedPane.addTab(it, ImageIcon(cl.getResource("example/$it")), JLabel(it), it)
+  }
+  tabbedPane.componentPopupMenu = PinTabPopupMenu()
+  return JPanel(BorderLayout()).also {
+    it.add(tabbedPane)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+internal class PinTabPopupMenu : JPopupMenu() {
+  private val pinTabMenuItem = JCheckBoxMenuItem(object : AbstractAction("pin tab") {
+    override fun actionPerformed(e: ActionEvent) {
+      val t = invoker as JTabbedPane
+      val check = e.source as JCheckBoxMenuItem
+      val idx = t.selectedIndex
+      val cmp = t.getComponentAt(idx)
+      val tab = t.getTabComponentAt(idx)
+      val icon = t.getIconAt(idx)
+      val tip = t.getToolTipTextAt(idx)
+      val flg = t.isEnabledAt(idx)
+      val i = searchNewSelectedIndex(t, idx, check.isSelected)
+      t.remove(idx)
+      t.insertTab(if (check.isSelected) "" else tip, icon, cmp, tip, i)
+      t.setTabComponentAt(i, tab)
+      t.setEnabledAt(i, flg)
+      if (flg) {
+        t.selectedIndex = i
+      }
+    }
+  })
+
+  override fun show(c: Component, x: Int, y: Int) {
+    if (c is JTabbedPane) {
+      val idx = c.indexAtLocation(x, y)
+      pinTabMenuItem.isEnabled = idx >= 0
+      pinTabMenuItem.isSelected = isSelectedPinTab(c, idx)
+      super.show(c, x, y)
+    }
+  }
+
+  private fun searchNewSelectedIndex(t: JTabbedPane, idx: Int, dir: Boolean): Int {
+    var i: Int
+    if (dir) {
+      i = 0
+      while (i < idx) {
+        if (!isSelectedPinTab(t, i)) {
+          break
+        }
+        i++
+      }
+    } else {
+      i = t.tabCount - 1
+      while (i > idx) {
+        if (isSelectedPinTab(t, i)) {
+          break
+        }
+        i--
+      }
+    }
+    return i
+  }
+
+  private fun isSelectedPinTab(t: JTabbedPane, idx: Int): Boolean {
+    return idx >= 0 && idx == t.selectedIndex && isEmpty(t.getTitleAt(idx))
+  }
+
+  private fun isEmpty(s: String?): Boolean {
+    return s == null || s.isEmpty()
+  }
+
+  init {
+    add(pinTabMenuItem)
+    addSeparator()
+    add("close all").addActionListener {
+      val t = invoker as? JTabbedPane ?: return@addActionListener
+      for (i in t.tabCount - 1 downTo 0) {
+        if (!isEmpty(t.getTitleAt(i))) {
+          t.removeTabAt(i)
+        }
+      }
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
