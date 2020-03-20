f5b886c36b208a3223f8d1b67af12edbf062e982
aterai <aterai@outlook.com>
2020-03-21 03:43:36 +0900

    add: BlockingDialog

---
+++ BlockingDialog/src/main/kotlin/example/App.kt
@@ -0,0 +1,65 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val panel = JPanel(BorderLayout())
+  val check = JCheckBox("0x22_FF_00_00")
+  val button = JButton("Stop 5sec")
+  button.addActionListener {
+    val w = SwingUtilities.getWindowAncestor(panel.rootPane)
+    val dialog = JDialog(w, Dialog.ModalityType.APPLICATION_MODAL)
+    dialog.isUndecorated = true
+    dialog.bounds = w.bounds
+    dialog.cursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)
+    val color = if (check.isSelected) 0x22_FF_00_00 else 0x01_00_00_00
+    dialog.background = Color(color, true)
+
+    object : BackgroundTask() {
+      public override fun done() {
+        if (!panel.isDisplayable) {
+          cancel(true)
+          return
+        }
+        dialog.isVisible = false
+      }
+    }.execute()
+    dialog.isVisible = true
+  }
+  val p = JPanel()
+  p.add(check)
+  p.add(JTextField(10))
+  p.add(button)
+
+  panel.add(p, BorderLayout.NORTH)
+  panel.add(JScrollPane(JTextArea(100, 80)))
+  panel.preferredSize = Dimension(320, 240)
+  return panel
+}
+
+open class BackgroundTask : SwingWorker<String, Void?>() {
+  @Throws(InterruptedException::class)
+  public override fun doInBackground(): String {
+    Thread.sleep(5_000)
+    return "Done"
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
