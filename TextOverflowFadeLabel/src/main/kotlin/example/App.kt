7c2666dffe4d06ee36bedbc88f9d8c8d2f5abcf5
aterai <aterai@outlook.com>
2020-03-25 19:21:51 +0900

    refactor: use of setter method instead of property access syntax

--- TextOverflowFadeLabel/src/main/kotlin/example/App.kt
+++ TextOverflowFadeLabel/src/main/kotlin/example/App.kt
@@ -5,44 +5,42 @@ import java.awt.font.TextLayout
 import java.awt.image.BufferedImage
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val text = "012345678901234567890123456789012345678901234567890123456789"
-    val box = Box.createVerticalBox().also {
-      it.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-      it.add(makeTitledPanel("default JLabel ellipsis", JLabel(text)))
-      it.add(Box.createVerticalStrut(5))
-      it.add(makeTitledPanel("html JLabel fade out", FadeOutLabel("<html>$text")))
-      it.add(Box.createVerticalStrut(5))
-      it.add(makeTitledPanel("JLabel TextLayout fade out", TextOverflowFadeLabel(text)))
-      it.add(Box.createVerticalStrut(5))
-      it.add(makeTitledPanel("JLabel BufferedImage fade out", FadingOutLabel(text)))
-      it.add(Box.createVerticalGlue())
-    }
-
-    add(box, BorderLayout.NORTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val text = "012345678901234567890123456789012345678901234567890123456789"
+  val box = Box.createVerticalBox().also {
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.add(makeTitledPanel("default JLabel ellipsis", JLabel(text)))
+    it.add(Box.createVerticalStrut(5))
+    it.add(makeTitledPanel("html JLabel fade out", FadeOutLabel("<html>$text")))
+    it.add(Box.createVerticalStrut(5))
+    it.add(makeTitledPanel("JLabel TextLayout fade out", TextOverflowFadeLabel(text)))
+    it.add(Box.createVerticalStrut(5))
+    it.add(makeTitledPanel("JLabel BufferedImage fade out", FadingOutLabel(text)))
+    it.add(Box.createVerticalGlue())
   }
-
-  private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-    it.add(c)
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class FadeOutLabel(text: String) : JLabel(text) {
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
 
+private class FadeOutLabel(text: String) : JLabel(text) {
   override fun paintComponent(g: Graphics) {
-    val i = getInsets()
-    val w = getWidth() - i.left - i.right
-    val h = getHeight() - i.top - i.bottom
+    val i = insets
+    val w = width - i.left - i.right
+    val h = height - i.top - i.bottom
 
     val rect = Rectangle(i.left, i.top, w - LENGTH, h)
 
-    val g2 = g.create() as Graphics2D
-    g2.setFont(g.getFont())
-    g2.setClip(rect)
-    g2.setComposite(AlphaComposite.SrcOver.derive(.99999f))
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.font = g.font
+    g2.clip = rect
+    g2.composite = AlphaComposite.SrcOver.derive(.99999f)
     super.paintComponent(g2)
 
     rect.width = 1
@@ -50,8 +48,8 @@ internal class FadeOutLabel(text: String) : JLabel(text) {
     (w - LENGTH until w).forEach {
       rect.x = it
       alpha = maxOf(0f, alpha - DIFF)
-      g2.setComposite(AlphaComposite.SrcOver.derive(alpha))
-      g2.setClip(rect)
+      g2.composite = AlphaComposite.SrcOver.derive(alpha)
+      g2.clip = rect
       super.paintComponent(g2)
     }
     g2.dispose()
@@ -63,22 +61,22 @@ internal class FadeOutLabel(text: String) : JLabel(text) {
   }
 }
 
-internal class TextOverflowFadeLabel(text: String) : JLabel(text) {
+private class TextOverflowFadeLabel(text: String) : JLabel(text) {
   override fun paintComponent(g: Graphics) {
-    val i = getInsets()
-    val w = getWidth() - i.left - i.right
-    val h = getHeight() - i.top - i.bottom
+    val i = insets
+    val w = width - i.left - i.right
+    val h = height - i.top - i.bottom
     val rect = Rectangle(i.left, i.top, w - LENGTH, h)
 
-    val g2 = g.create() as Graphics2D
-    g2.setFont(g.getFont())
-    g2.setPaint(getForeground())
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.font = g.font
+    g2.paint = foreground
 
-    val tl = TextLayout(getText(), getFont(), g2.getFontRenderContext())
+    val tl = TextLayout(text, font, g2.fontRenderContext)
     val baseline = getBaseline(w, h).toFloat()
     val fx = i.left.toFloat()
 
-    g2.setClip(rect)
+    g2.clip = rect
     tl.draw(g2, fx, baseline)
 
     rect.width = 1
@@ -86,8 +84,8 @@ internal class TextOverflowFadeLabel(text: String) : JLabel(text) {
     (w - LENGTH until w).forEach {
       rect.x = it
       alpha = maxOf(0f, alpha - DIFF)
-      g2.setComposite(AlphaComposite.SrcOver.derive(alpha))
-      g2.setClip(rect)
+      g2.composite = AlphaComposite.SrcOver.derive(alpha)
+      g2.clip = rect
       tl.draw(g2, fx, baseline)
     }
     g2.dispose()
@@ -99,15 +97,16 @@ internal class TextOverflowFadeLabel(text: String) : JLabel(text) {
   }
 }
 
-internal class FadingOutLabel(text: String) : JLabel(text) {
+private class FadingOutLabel(text: String) : JLabel(text) {
   private val dim = Dimension()
+
   @Transient
   private var buffer: Image? = null
 
   override fun paintComponent(g: Graphics) {
     // super.paintComponent(g);
-    val w = getWidth()
-    val h = getHeight()
+    val w = width
+    val h = height
     if (buffer == null || dim.width != w || dim.height != h) {
       dim.setSize(w, h)
       buffer = updateImage(dim)
@@ -118,11 +117,11 @@ internal class FadingOutLabel(text: String) : JLabel(text) {
   private fun updateImage(d: Dimension): BufferedImage {
     val img = BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB)
     val g2 = img.createGraphics()
-    g2.setFont(getFont())
-    g2.setPaint(getForeground())
-    val tl = TextLayout(getText(), getFont(), g2.getFontRenderContext())
+    g2.font = font
+    g2.paint = foreground
+    val tl = TextLayout(text, font, g2.fontRenderContext)
     val baseline = getBaseline(d.width, d.height).toFloat()
-    tl.draw(g2, getInsets().left.toFloat(), baseline)
+    tl.draw(g2, insets.left.toFloat(), baseline)
     g2.dispose()
 
     val spx = maxOf(0, d.width - LENGTH)
@@ -153,7 +152,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
