a74cdc9c52ef0443670af6574acb9053ff51a5c5
aterai <aterai@outlook.com>
2020-03-26 19:24:07 +0900

    refactor: use of setter method instead of property access syntax

--- AddRemoveTableColumn/src/main/kotlin/example/App.kt
+++ AddRemoveTableColumn/src/main/kotlin/example/App.kt
@@ -6,51 +6,50 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 import javax.swing.table.TableColumnModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    UIManager.put("CheckBoxMenuItem.doNotCloseOnMouseClick", true)
-    val table = JTable(DefaultTableModel(12, 8))
-    table.getTableHeader().setComponentPopupMenu(TableHeaderPopupMenu(table))
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  UIManager.put("CheckBoxMenuItem.doNotCloseOnMouseClick", true)
+  val table = JTable(DefaultTableModel(12, 8))
+  table.tableHeader.componentPopupMenu = TableHeaderPopupMenu(table)
+  return JScrollPane(table).also {
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class TableHeaderPopupMenu(table: JTable) : JPopupMenu() {
+private class TableHeaderPopupMenu(table: JTable) : JPopupMenu() {
   override fun show(c: Component, x: Int, y: Int) {
     (c as? JTableHeader)?.also {
-      it.setDraggedColumn(null)
+      it.draggedColumn = null
       it.repaint()
-      it.getTable().repaint()
-      updateMenuItems(it.getColumnModel())
+      it.table.repaint()
+      updateMenuItems(it.columnModel)
       super.show(c, x, y)
     }
   }
 
   private fun updateMenuItems(columnModel: TableColumnModel) {
-    val isOnlyOneMenu = columnModel.getColumnCount() == 1
+    val isOnlyOneMenu = columnModel.columnCount == 1
     if (isOnlyOneMenu) {
       descendants(this)
-        .map { it.getComponent() }
-        .forEach { it.setEnabled(it !is AbstractButton || !it.isSelected()) }
+        .map { it.component }
+        .forEach { it.isEnabled = it !is AbstractButton || !it.isSelected }
     } else {
       descendants(this)
-        .forEach { it.getComponent().setEnabled(true) }
+        .forEach { it.component.isEnabled = true }
     }
   }
 
   private fun descendants(me: MenuElement): List<MenuElement> =
     // me.getSubElements().map { children(it) }.fold(listOf(me)) { a, b -> a + b }
-    me.getSubElements().flatMap { listOf(it) + descendants(it) }
+    me.subElements.flatMap { listOf(it) + descendants(it) }
 
   init {
-    val columnModel = table.getColumnModel()
-    columnModel.getColumns().toList().forEach { tableColumn ->
-      val name = tableColumn.getHeaderValue()?.toString() ?: ""
+    val columnModel = table.columnModel
+    columnModel.columns.toList().forEach { tableColumn ->
+      val name = tableColumn.headerValue?.toString() ?: ""
       val item = JCheckBoxMenuItem(name, true)
       item.addItemListener { e ->
-        val check = e.getItemSelectable() as? AbstractButton ?: return@addItemListener
-        if (check.isSelected()) {
+        val check = e.itemSelectable as? AbstractButton ?: return@addItemListener
+        if (check.isSelected) {
           columnModel.addColumn(tableColumn)
         } else {
           columnModel.removeColumn(tableColumn)
@@ -72,7 +71,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
