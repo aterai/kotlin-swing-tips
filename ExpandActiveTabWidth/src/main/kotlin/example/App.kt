0bd079e6debb99eb0ad25133f1a777faa0343b05
aterai <aterai@outlook.com>
2020-03-27 15:43:59 +0900

    refactor: use of setter method instead of property access syntax

--- ExpandActiveTabWidth/src/main/kotlin/example/App.kt
+++ ExpandActiveTabWidth/src/main/kotlin/example/App.kt
@@ -3,48 +3,49 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
+fun makeUI(): Component {
+  val tabbedPane = JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT)
+  val cl = Thread.currentThread().contextClassLoader
   // [XP Style Icons - Download](https://xp-style-icons.en.softonic.com/)
-  private val icons = listOf(
+  val icons = listOf(
     "wi0009-16.png", "wi0054-16.png", "wi0062-16.png",
-    "wi0063-16.png", "wi0124-16.png", "wi0126-16.png")
-
-  init {
-    val tabbedPane = JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT)
-    icons.forEach { path ->
-      val icon = ImageIcon(javaClass.getResource(path))
-      val label = ShrinkLabel(path, icon)
-      tabbedPane.addTab(path, icon, JLabel(path), path)
-      tabbedPane.setTabComponentAt(tabbedPane.getTabCount() - 1, label)
-    }
-    updateTabWidth(tabbedPane)
-    tabbedPane.addChangeListener { e ->
-      (e.getSource() as? JTabbedPane)?.also {
-        updateTabWidth(it)
-      }
-    }
-    add(tabbedPane)
-    setPreferredSize(Dimension(320, 240))
+    "wi0063-16.png", "wi0124-16.png", "wi0126-16.png"
+  )
+  icons.forEach { path ->
+    val icon = ImageIcon(cl.getResource("example/$path"))
+    val label = ShrinkLabel(path, icon)
+    tabbedPane.addTab(path, icon, JLabel(path), path)
+    tabbedPane.setTabComponentAt(tabbedPane.tabCount - 1, label)
   }
+  updateTabWidth(tabbedPane)
+  tabbedPane.addChangeListener { e ->
+    (e.source as? JTabbedPane)?.also {
+      updateTabWidth(it)
+    }
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(tabbedPane)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
 
-  private fun updateTabWidth(tabs: JTabbedPane) {
-    when (tabs.getTabPlacement()) {
-      JTabbedPane.TOP, JTabbedPane.BOTTOM -> {
-        val idx = tabs.getSelectedIndex()
-        for (i in 0 until tabs.getTabCount()) {
-          (tabs.getTabComponentAt(i) as? ShrinkLabel)?.isSelected = i == idx
-        }
+private fun updateTabWidth(tabs: JTabbedPane) {
+  when (tabs.tabPlacement) {
+    JTabbedPane.TOP, JTabbedPane.BOTTOM -> {
+      val idx = tabs.selectedIndex
+      for (i in 0 until tabs.tabCount) {
+        (tabs.getTabComponentAt(i) as? ShrinkLabel)?.isSelected = i == idx
       }
     }
   }
 }
 
-class ShrinkLabel(title: String, icon: Icon) : JLabel(title, icon, SwingConstants.LEFT) {
+private class ShrinkLabel(title: String, icon: Icon) : JLabel(title, icon, SwingConstants.LEFT) {
   var isSelected = false
 
   override fun getPreferredSize() = super.getPreferredSize()?.also {
     if (!isSelected) {
-      it.width = getIcon()?.getIconWidth() ?: it.width
+      it.width = icon?.iconWidth ?: it.width
     }
   }
 }
@@ -59,7 +60,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
