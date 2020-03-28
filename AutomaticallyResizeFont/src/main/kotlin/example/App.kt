c9d89f0a04de04d01731c70f9a8e2a509587afb4
aterai <aterai@outlook.com>
2020-03-29 04:55:37 +0900

    add: AutomaticallyResizeFont

---
+++ AutomaticallyResizeFont/src/main/kotlin/example/App.kt
@@ -0,0 +1,56 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import kotlin.math.abs
+
+private const val TEST = "1234567890\nabc def ghi jkl mno"
+
+fun makeUI(): Component {
+  val font = Font(Font.MONOSPACED, Font.PLAIN, 12)
+  val editor1 = JTextPane()
+  editor1.font = font
+  editor1.text = "Default\n$TEST"
+  val editor2: JTextPane = object : JTextPane() {
+    private val rect = Rectangle()
+    private var fontSize = 0f
+    override fun doLayout() {
+      val f = .08f * SwingUtilities.calculateInnerArea(this, rect).width
+      val fontSizeShouldChange = abs(fontSize - f) > 1.0e-1
+      if (fontSizeShouldChange) {
+        setFont(font.deriveFont(f))
+        fontSize = f
+      }
+      super.doLayout()
+    }
+  }
+  editor2.font = font
+  editor2.text = "doLayout + deriveFont\n$TEST"
+  val sp = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+  sp.topComponent = editor1
+  sp.bottomComponent = editor2
+  sp.resizeWeight = .5
+  return JPanel(BorderLayout()).also {
+    it.add(sp)
+    it.preferredSize = Dimension(320, 240)
+    EventQueue.invokeLater { sp.setDividerLocation(.5) }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
--- ColumnSpanningCellRenderer/src/main/kotlin/example/App.kt
+++ ColumnSpanningCellRenderer/src/main/kotlin/example/App.kt
@@ -6,48 +6,48 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val see = "See Also: Constant Field Values"
-    val columnNames = arrayOf("AAA", "BBB")
-    val data = arrayOf(
-      arrayOf(makeOptionPaneDescription("error"), see),
-      arrayOf(makeOptionPaneDescription("information"), see),
-      arrayOf(makeOptionPaneDescription("question"), see),
-      arrayOf(makeOptionPaneDescription("warning"), see)
-    )
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun isCellEditable(row: Int, column: Int) = false
-    }
-    val table = JTable(model)
-    table.setAutoCreateRowSorter(true)
-    table.getTableHeader().setReorderingAllowed(false)
-    table.setRowSelectionAllowed(true)
-    table.setFillsViewportHeight(true)
-    table.setShowVerticalLines(false)
-    table.setIntercellSpacing(Dimension(0, 1))
-    table.setRowHeight(56)
-    val renderer = ColumnSpanningCellRenderer()
-    for (i in 0 until table.getColumnModel().getColumnCount()) {
-      table.getColumnModel().getColumn(i).also {
-        it.setCellRenderer(renderer)
-        it.setMinWidth(50)
-      }
-    }
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val see = "See Also: Constant Field Values"
+  val columnNames = arrayOf("AAA", "BBB")
+  val data = arrayOf(
+    arrayOf(makeOptionPaneDescription("error"), see),
+    arrayOf(makeOptionPaneDescription("information"), see),
+    arrayOf(makeOptionPaneDescription("question"), see),
+    arrayOf(makeOptionPaneDescription("warning"), see)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun isCellEditable(row: Int, column: Int) = false
   }
-
-  private fun makeOptionPaneDescription(type: String): OptionPaneDescription {
-    val key = type + "Icon"
-    val icon = UIManager.getIcon("OptionPane.$key")
-    val fmt = "public static final int %s_MESSAGE%nUsed for %s messages."
-    val msg = fmt.format(type.toUpperCase(Locale.ENGLISH), type)
-    return OptionPaneDescription(key, icon, msg)
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
+  table.tableHeader.reorderingAllowed = false
+  table.rowSelectionAllowed = true
+  table.fillsViewportHeight = true
+  table.showVerticalLines = false
+  table.intercellSpacing = Dimension(0, 1)
+  table.rowHeight = 56
+  val renderer = ColumnSpanningCellRenderer()
+  for (i in 0 until table.columnModel.columnCount) {
+    table.columnModel.getColumn(i).also {
+      it.cellRenderer = renderer
+      it.minWidth = 50
+    }
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class ColumnSpanningCellRenderer : JPanel(BorderLayout()), TableCellRenderer {
+private fun makeOptionPaneDescription(type: String): OptionPaneDescription {
+  val key = type + "Icon"
+  val icon = UIManager.getIcon("OptionPane.$key")
+  val fmt = "public static final int %s_MESSAGE%nUsed for %s messages."
+  val msg = fmt.format(type.toUpperCase(Locale.ENGLISH), type)
+  return OptionPaneDescription(key, icon, msg)
+}
+
+private class ColumnSpanningCellRenderer : JPanel(BorderLayout()), TableCellRenderer {
   private val textArea = JTextArea(2, 999_999)
   private val label = JLabel()
   private val iconLabel = JLabel()
@@ -67,7 +67,7 @@ class ColumnSpanningCellRenderer : JPanel(BorderLayout()), TableCellRenderer {
     } else {
       val title = value?.toString() ?: ""
       val mri = table.convertRowIndexToModel(row)
-      val o = table.getModel().getValueAt(mri, 0)
+      val o = table.model.getValueAt(mri, 0)
       d = if (o is OptionPaneDescription) {
         OptionPaneDescription(title, o.icon, o.text)
       } else {
@@ -75,15 +75,15 @@ class ColumnSpanningCellRenderer : JPanel(BorderLayout()), TableCellRenderer {
       }
       remove(iconLabel)
     }
-    label.setText(d.title)
-    textArea.setText(d.text)
-    iconLabel.setIcon(d.icon)
+    label.text = d.title
+    textArea.text = d.text
+    iconLabel.icon = d.icon
     val cr = table.getCellRect(row, column, false)
     if (column != TARGET_IDX) {
-      cr.x -= iconLabel.getPreferredSize().width
+      cr.x -= iconLabel.preferredSize.width
     }
-    scroll.getViewport().setViewPosition(cr.getLocation())
-    setBackground(if (isSelected) Color.ORANGE else Color.WHITE)
+    scroll.viewport.viewPosition = cr.location
+    background = if (isSelected) Color.ORANGE else Color.WHITE
     return this
   }
 
@@ -92,25 +92,25 @@ class ColumnSpanningCellRenderer : JPanel(BorderLayout()), TableCellRenderer {
   }
 
   init {
-    scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)
-    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
-    scroll.setBorder(BorderFactory.createEmptyBorder())
-    scroll.setViewportBorder(BorderFactory.createEmptyBorder())
-    scroll.setOpaque(false)
-    scroll.getViewport().setOpaque(false)
-    textArea.setBorder(BorderFactory.createEmptyBorder())
-    textArea.setMargin(Insets(0, 0, 0, 0))
-    textArea.setForeground(Color.RED)
-    textArea.setEditable(false)
-    textArea.setFocusable(false)
-    textArea.setOpaque(false)
-    iconLabel.setBorder(BorderFactory.createEmptyBorder(0, 4, 0, 4))
-    iconLabel.setOpaque(false)
+    scroll.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER
+    scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+    scroll.border = BorderFactory.createEmptyBorder()
+    scroll.viewportBorder = BorderFactory.createEmptyBorder()
+    scroll.isOpaque = false
+    scroll.viewport.isOpaque = false
+    textArea.border = BorderFactory.createEmptyBorder()
+    textArea.margin = Insets(0, 0, 0, 0)
+    textArea.foreground = Color.RED
+    textArea.isEditable = false
+    textArea.isFocusable = false
+    textArea.isOpaque = false
+    iconLabel.border = BorderFactory.createEmptyBorder(0, 4, 0, 4)
+    iconLabel.isOpaque = false
     val b1 = BorderFactory.createEmptyBorder(2, 2, 2, 2)
     val b2 = BorderFactory.createMatteBorder(0, 0, 1, 1, Color.GRAY)
-    label.setBorder(BorderFactory.createCompoundBorder(b2, b1))
-    setBackground(textArea.background)
-    setOpaque(true)
+    label.border = BorderFactory.createCompoundBorder(b2, b1)
+    background = textArea.background
+    isOpaque = true
     add(label, BorderLayout.NORTH)
     add(scroll)
   }
@@ -128,7 +128,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
