2def80b8b31cc558f51d2ea879f1681685bf0195
aterai <aterai@outlook.com>
2020-04-01 16:18:20 +0900

    refactor: use of setter method instead of property access syntax

--- VerticalNavigationMenu/src/main/kotlin/example/App.kt
+++ VerticalNavigationMenu/src/main/kotlin/example/App.kt
@@ -11,92 +11,92 @@ import javax.swing.tree.ExpandVetoException
 import javax.swing.tree.TreePath
 import javax.swing.tree.TreeSelectionModel
 
-class MainPanel : JPanel(BorderLayout(2, 2)) {
-  init {
-    val emptyIcon = EmptyIcon()
-    UIManager.put("Tree.openIcon", emptyIcon)
-    UIManager.put("Tree.closedIcon", emptyIcon)
-    UIManager.put("Tree.leafIcon", emptyIcon)
-    UIManager.put("Tree.expandedIcon", emptyIcon)
-    UIManager.put("Tree.collapsedIcon", emptyIcon)
-    UIManager.put("Tree.leftChildIndent", 10)
-    UIManager.put("Tree.rightChildIndent", 0)
-    UIManager.put("Tree.paintLines", false)
-    val model = makeModel()
-    val cardLayout = CardLayout()
-    val p = JPanel(cardLayout)
+fun makeUI(): Component {
+  val emptyIcon = EmptyIcon()
+  UIManager.put("Tree.openIcon", emptyIcon)
+  UIManager.put("Tree.closedIcon", emptyIcon)
+  UIManager.put("Tree.leafIcon", emptyIcon)
+  UIManager.put("Tree.expandedIcon", emptyIcon)
+  UIManager.put("Tree.collapsedIcon", emptyIcon)
+  UIManager.put("Tree.leftChildIndent", 10)
+  UIManager.put("Tree.rightChildIndent", 0)
+  UIManager.put("Tree.paintLines", false)
+  val model = makeModel()
+  val cardLayout = CardLayout()
+  val p = JPanel(cardLayout)
 
-    // https://ateraimemo.com/Swing/TraverseAllNodes.html
-    (model.getRoot() as? DefaultMutableTreeNode)?.also { root ->
-      root.postorderEnumeration().toList()
-        .filterIsInstance<DefaultMutableTreeNode>()
-        .map { it.getUserObject().toString() }
-        .forEach { p.add(JLabel(it), it) }
-    }
-
-    val tree = RowSelectionTree()
-    tree.setModel(model)
-    tree.setRowHeight(32)
-    tree.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
-
-    // https://ateraimemo.com/Swing/ExpandAllNodes.html
-    var row = 0
-    while (row < tree.getRowCount()) {
-      tree.expandRow(row++)
-    }
-    tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION)
-    tree.addTreeSelectionListener { e ->
-      // https://ateraimemo.com/Swing/CardLayoutTabbedPane.html
-      (e.getNewLeadSelectionPath().getLastPathComponent() as? DefaultMutableTreeNode)?.also {
-        cardLayout.show(p, it.getUserObject().toString())
-      }
-    }
-    val sp = JSplitPane()
-    sp.setLeftComponent(JScrollPane(tree))
-    sp.setRightComponent(JScrollPane(p))
-    sp.setResizeWeight(.5)
-    add(sp)
-    setPreferredSize(Dimension(320, 240))
+  // https://ateraimemo.com/Swing/TraverseAllNodes.html
+  (model.root as? DefaultMutableTreeNode)?.also { root ->
+    root.postorderEnumeration().toList()
+      .filterIsInstance<DefaultMutableTreeNode>()
+      .map { it.userObject.toString() }
+      .forEach { p.add(JLabel(it), it) }
   }
 
-  private fun makeModel(): DefaultTreeModel {
-    val root = DefaultMutableTreeNode("root")
-    val c1 = DefaultMutableTreeNode("1. Introduction")
-    root.add(c1)
-    val c2 = DefaultMutableTreeNode("2. Chapter")
-    c2.add(DefaultMutableTreeNode("2.1. Section"))
-    c2.add(DefaultMutableTreeNode("2.2. Section"))
-    c2.add(DefaultMutableTreeNode("2.3. Section"))
-    root.add(c2)
-    val c3 = DefaultMutableTreeNode("3. Chapter")
-    c3.add(DefaultMutableTreeNode("3.1. Section"))
-    c3.add(DefaultMutableTreeNode("3.2. Section"))
-    c3.add(DefaultMutableTreeNode("3.3. Section"))
-    c3.add(DefaultMutableTreeNode("3.4. Section"))
-    root.add(c3)
-    return DefaultTreeModel(root)
+  val tree = RowSelectionTree()
+  tree.model = model
+  tree.rowHeight = 32
+  tree.border = BorderFactory.createEmptyBorder(2, 2, 2, 2)
+
+  // https://ateraimemo.com/Swing/ExpandAllNodes.html
+  var row = 0
+  while (row < tree.rowCount) {
+    tree.expandRow(row++)
+  }
+  tree.selectionModel.selectionMode = TreeSelectionModel.SINGLE_TREE_SELECTION
+  tree.addTreeSelectionListener { e ->
+    // https://ateraimemo.com/Swing/CardLayoutTabbedPane.html
+    (e.newLeadSelectionPath.lastPathComponent as? DefaultMutableTreeNode)?.also {
+      cardLayout.show(p, it.userObject.toString())
+    }
+  }
+  val sp = JSplitPane()
+  sp.leftComponent = JScrollPane(tree)
+  sp.rightComponent = JScrollPane(p)
+  sp.resizeWeight = .5
+  return JPanel(BorderLayout(2, 2)).also {
+    it.add(sp)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
+private fun makeModel(): DefaultTreeModel {
+  val root = DefaultMutableTreeNode("root")
+  val c1 = DefaultMutableTreeNode("1. Introduction")
+  root.add(c1)
+  val c2 = DefaultMutableTreeNode("2. Chapter")
+  c2.add(DefaultMutableTreeNode("2.1. Section"))
+  c2.add(DefaultMutableTreeNode("2.2. Section"))
+  c2.add(DefaultMutableTreeNode("2.3. Section"))
+  root.add(c2)
+  val c3 = DefaultMutableTreeNode("3. Chapter")
+  c3.add(DefaultMutableTreeNode("3.1. Section"))
+  c3.add(DefaultMutableTreeNode("3.2. Section"))
+  c3.add(DefaultMutableTreeNode("3.3. Section"))
+  c3.add(DefaultMutableTreeNode("3.4. Section"))
+  root.add(c3)
+  return DefaultTreeModel(root)
+}
+
 // https://ateraimemo.com/Swing/TreeRowSelection.html
-class RowSelectionTree : JTree() {
+private class RowSelectionTree : JTree() {
   @Transient
   private var listener: TreeWillExpandListener? = null
 
   override fun paintComponent(g: Graphics) {
-    g.setColor(getBackground())
-    g.fillRect(0, 0, getWidth(), getHeight())
+    g.color = background
+    g.fillRect(0, 0, width, height)
     val g2 = g.create() as? Graphics2D ?: return
-    g2.setPaint(SELECTED_COLOR)
-    getSelectionRows()
+    g2.paint = SELECTED_COLOR
+    selectionRows
       ?.map { getRowBounds(it) }
-      ?.forEach { g2.fillRect(0, it.y, getWidth(), it.height) }
+      ?.forEach { g2.fillRect(0, it.y, width, it.height) }
     super.paintComponent(g)
     if (hasFocus()) {
-      getLeadSelectionPath().also {
+      leadSelectionPath.also {
         val r = getRowBounds(getRowForPath(it))
-        g2.setPaint(SELECTED_COLOR.darker())
-        g2.drawRect(0, r.y, getWidth() - 1, r.height - 1)
+        g2.paint = SELECTED_COLOR.darker()
+        g2.drawRect(0, r.y, width - 1, r.height - 1)
       }
     }
     g2.dispose()
@@ -109,7 +109,7 @@ class RowSelectionTree : JTree() {
     setUI(object : BasicTreeUI() {
       override fun getPathBounds(tree: JTree?, path: TreePath?): Rectangle? {
         return if (tree != null && treeState != null) {
-          getPathBounds(path, tree.getInsets(), Rectangle())
+          getPathBounds(path, tree.insets, Rectangle())
         } else null
       }
 
@@ -126,12 +126,12 @@ class RowSelectionTree : JTree() {
     val r = getCellRenderer()
     setCellRenderer { tree, value, selected, expanded, leaf, row, hasFocus ->
       val c = r.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus)
-      c.setBackground(if (selected) SELECTED_COLOR else tree.getBackground())
-      (c as? JComponent)?.setOpaque(true)
+      c.background = if (selected) SELECTED_COLOR else tree.background
+      (c as? JComponent)?.isOpaque = true
       return@setCellRenderer c
     }
-    setOpaque(false)
-    setRootVisible(false)
+    isOpaque = false
+    isRootVisible = false
 
     // https://ateraimemo.com/Swing/TreeNodeCollapseVeto.html
     listener = object : TreeWillExpandListener {
@@ -152,7 +152,7 @@ class RowSelectionTree : JTree() {
   }
 }
 
-class EmptyIcon : Icon {
+private class EmptyIcon : Icon {
   override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
     /* Empty icon */
   }
@@ -172,7 +172,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
