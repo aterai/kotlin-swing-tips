3df02475f710bd6422c92a9606dfe34788ee45ca
aterai <aterai@outlook.com>
2020-04-02 18:31:14 +0900

    refactor: use of setter method instead of property access syntax

--- ViewportBorderBounds/src/main/kotlin/example/App.kt
+++ ViewportBorderBounds/src/main/kotlin/example/App.kt
@@ -13,105 +13,106 @@ import javax.swing.plaf.LayerUI
 import javax.swing.text.html.HTMLEditorKit
 import javax.swing.text.html.StyleSheet
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val editor = JEditorPane()
-  private val engine = createEngine()
+private val editor = JEditorPane()
+private val engine = createEngine()
 
-  init {
-    // https://github.com/google/code-prettify/blob/master/styles/desert.css
-    val styleSheet = StyleSheet().also {
-      it.addRule(".str {color:#ffa0a0}")
-      it.addRule(".kwd {color:#f0e68c;font-weight:bold}")
-      it.addRule(".com {color:#87ceeb}")
-      it.addRule(".typ {color:#98fb98}")
-      it.addRule(".lit {color:#cd5c5c}")
-      it.addRule(".pun {color:#ffffff}")
-      it.addRule(".pln {color:#ffffff}")
-      it.addRule(".tag {color:#f0e68c;font-weight:bold}")
-      it.addRule(".atn {color:#bdb76b;font-weight:bold}")
-      it.addRule(".atv {color:#ffa0a0}")
-      it.addRule(".dec {color:#98fb98}")
-    }
-
-    val htmlEditorKit = HTMLEditorKit()
-    htmlEditorKit.setStyleSheet(styleSheet)
-    editor.setEditorKit(htmlEditorKit)
-    editor.setEditable(false)
-    editor.setSelectedTextColor(null)
-    editor.setSelectionColor(Color(0x64_88_AA_AA, true))
-    editor.setBackground(Color(0x64_64_64)) // 0x33_33_33
-
-    val button = JButton("open")
-    button.addActionListener {
-      val fileChooser = JFileChooser()
-      val ret = fileChooser.showOpenDialog(getRootPane())
-      if (ret == JFileChooser.APPROVE_OPTION) {
-        loadFile(fileChooser.getSelectedFile().getAbsolutePath())
-      }
-    }
-
-    val scroll = JScrollPane(editor)
-    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
-
-    val check = JCheckBox("HORIZONTAL_SCROLLBAR_NEVER", true)
-    check.addActionListener { e ->
-      val flag = (e.getSource() as? JCheckBox)?.isSelected() ?: false
-      scroll.setHorizontalScrollBarPolicy(when {
-        flag -> ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-        else -> ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
-      })
-    }
-
-    val box = Box.createHorizontalBox().also {
-      it.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
-      it.add(check)
-      it.add(Box.createHorizontalGlue())
-      it.add(button)
-    }
-
-    add(JLayer(scroll, ScrollPaneLayerUI()))
-    add(box, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  // https://github.com/google/code-prettify/blob/master/styles/desert.css
+  val styleSheet = StyleSheet().also {
+    it.addRule(".str {color:#ffa0a0}")
+    it.addRule(".kwd {color:#f0e68c;font-weight:bold}")
+    it.addRule(".com {color:#87ceeb}")
+    it.addRule(".typ {color:#98fb98}")
+    it.addRule(".lit {color:#cd5c5c}")
+    it.addRule(".pun {color:#ffffff}")
+    it.addRule(".pln {color:#ffffff}")
+    it.addRule(".tag {color:#f0e68c;font-weight:bold}")
+    it.addRule(".atn {color:#bdb76b;font-weight:bold}")
+    it.addRule(".atv {color:#ffa0a0}")
+    it.addRule(".dec {color:#98fb98}")
   }
 
-  private fun loadFile(path: String) {
-    val html = runCatching {
-      File(path).useLines { it.toList() }.joinToString("\n") {
-        it.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-      }
-    }.fold(
-      onSuccess = { prettify(engine, it) },
-      onFailure = { it.message }
-    )
-    editor.setText("<pre>$html\n</pre>")
+  val htmlEditorKit = HTMLEditorKit()
+  htmlEditorKit.styleSheet = styleSheet
+  editor.editorKit = htmlEditorKit
+  editor.isEditable = false
+  editor.selectedTextColor = null
+  editor.selectionColor = Color(0x64_88_AA_AA, true)
+  editor.background = Color(0x64_64_64) // 0x33_33_33
+
+  val button = JButton("open")
+  button.addActionListener {
+    val fileChooser = JFileChooser()
+    val ret = fileChooser.showOpenDialog(button.rootPane)
+    if (ret == JFileChooser.APPROVE_OPTION) {
+      loadFile(fileChooser.selectedFile.absolutePath)
+    }
   }
 
-  private fun createEngine(): ScriptEngine? {
-    val engine = ScriptEngineManager().getEngineByName("JavaScript")
-    val url = MainPanel::class.java.getResource("prettify.js") ?: return null
-    return runCatching {
-      BufferedReader(InputStreamReader(url.openStream(), StandardCharsets.UTF_8)).use { r ->
-        engine.eval("var window={}, navigator=null;")
-        engine.eval(r)
-      }
-      engine
-    }.onFailure { it.printStackTrace() }.getOrNull()
+  val scroll = JScrollPane(editor)
+  scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+
+  val check = JCheckBox("HORIZONTAL_SCROLLBAR_NEVER", true)
+  check.addActionListener { e ->
+    val flag = (e.source as? JCheckBox)?.isSelected ?: false
+    scroll.horizontalScrollBarPolicy = when {
+      flag -> ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+      else -> ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED
+    }
   }
 
-  private fun prettify(engine: ScriptEngine?, src: String) = runCatching {
-    (engine as? Invocable)?.invokeMethod(engine.get("window"), "prettyPrintOne", src) as? String
-  }.getOrNull() ?: "error"
+  val box = Box.createHorizontalBox().also {
+    it.border = BorderFactory.createEmptyBorder(2, 2, 2, 2)
+    it.add(check)
+    it.add(Box.createHorizontalGlue())
+    it.add(button)
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(JLayer(scroll, ScrollPaneLayerUI()))
+    it.add(box, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
+  }
 }
 
-internal class ScrollPaneLayerUI : LayerUI<JScrollPane>() {
+private fun loadFile(path: String) {
+  val html = runCatching {
+    File(path).useLines { it.toList() }.joinToString("\n") {
+      it.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+    }
+  }.fold(
+    onSuccess = { prettify(engine, it) },
+    onFailure = { it.message }
+  )
+  editor.text = "<pre>$html\n</pre>"
+}
+
+private fun createEngine(): ScriptEngine? {
+  val engine = ScriptEngineManager().getEngineByName("JavaScript")
+  val cl = Thread.currentThread().contextClassLoader
+  val url = cl.getResource("example/prettify.js") ?: return null
+  return runCatching {
+    BufferedReader(InputStreamReader(url.openStream(), StandardCharsets.UTF_8)).use { r ->
+      engine.eval("var window={}, navigator=null;")
+      engine.eval(r)
+    }
+    engine
+  }.onFailure { it.printStackTrace() }.getOrNull()
+}
+
+private fun prettify(engine: ScriptEngine?, src: String) = runCatching {
+  (engine as? Invocable)?.invokeMethod(engine.get("window"), "prettyPrintOne", src) as? String
+}.getOrNull() ?: "error"
+
+private class ScrollPaneLayerUI : LayerUI<JScrollPane>() {
   override fun paint(g: Graphics, c: JComponent) {
     super.paint(g, c)
-    val scroll = (c as? JLayer<*>)?.getView() as? JScrollPane ?: return
-    val rect = scroll.getViewportBorderBounds()
-    val m = scroll.getHorizontalScrollBar().getModel()
-    val extent = m.getExtent()
-    val maximum = m.getMaximum()
-    val value = m.getValue()
+    val scroll = (c as? JLayer<*>)?.view as? JScrollPane ?: return
+    val rect = scroll.viewportBorderBounds
+    val m = scroll.horizontalScrollBar.model
+    val extent = m.extent
+    val maximum = m.maximum
+    val value = m.value
     if (value + extent < maximum) {
       val w = rect.width
       val h = rect.height
@@ -119,7 +120,7 @@ internal class ScrollPaneLayerUI : LayerUI<JScrollPane>() {
       val g2 = g.create() as Graphics2D
       g2.translate(rect.x + w - shd, rect.y)
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g2.setPaint(Color(0x08_00_00_00, true))
+      g2.paint = Color(0x08_00_00_00, true)
       for (i in 0 until shd) {
         g2.fillRect(i, 0, shd - i, h)
       }
@@ -140,7 +141,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
