60933ec74d9d91db37ca4239ba8a21077ba29e6f
aterai <aterai@outlook.com>
2020-04-03 02:55:48 +0900

    refactor: use of setter method instead of property access syntax

--- RemoveButtonInComboItem/src/main/kotlin/example/App.kt
+++ RemoveButtonInComboItem/src/main/kotlin/example/App.kt
@@ -8,57 +8,57 @@ import java.time.ZoneId
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.ComboPopup
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val c0 = makeComboBox(isDefault = true, isEditable = false)
-    val c1 = makeComboBox(isDefault = false, isEditable = false)
-    val c2 = makeComboBox(isDefault = true, isEditable = true)
-    val c3 = makeComboBox(isDefault = false, isEditable = true)
+fun makeUI(): Component {
+  val c0 = makeComboBox(isDefault = true, isEditable = false)
+  val c1 = makeComboBox(isDefault = false, isEditable = false)
+  val c2 = makeComboBox(isDefault = true, isEditable = true)
+  val c3 = makeComboBox(isDefault = false, isEditable = true)
 
-    val button = JButton("add")
-    button.addActionListener {
-      val str = LocalDateTime.now(ZoneId.systemDefault()).toString()
-      listOf(c0, c1, c2, c3)
-        // .map { it.getModel() }.filterIsInstance<MutableComboBoxModel<String>>()
-        .mapNotNull { it.getModel() as? MutableComboBoxModel<String> }
-        .forEach { it.insertElementAt(str, it.getSize()) }
-    }
-
-    val p = JPanel(GridLayout(2, 1))
-    p.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    p.add(makeTitledPanel("setEditable(false)", listOf(c0, c1)))
-    p.add(makeTitledPanel("setEditable(true)", listOf(c2, c3)))
-
-    add(p, BorderLayout.NORTH)
-    add(button, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  val button = JButton("add")
+  button.addActionListener {
+    val str = LocalDateTime.now(ZoneId.systemDefault()).toString()
+    listOf(c0, c1, c2, c3)
+      // .map { it.getModel() }.filterIsInstance<MutableComboBoxModel<String>>()
+      .mapNotNull { it.model as? MutableComboBoxModel<String> }
+      .forEach { it.insertElementAt(str, it.size) }
   }
 
-  private fun makeComboBox(isDefault: Boolean, isEditable: Boolean): JComboBox<String> {
-    val m = DefaultComboBoxModel(arrayOf("aaa", "bbb", "ccc"))
-    val comboBox = if (isDefault) JComboBox(m) else RemoveButtonComboBox(m)
-    comboBox.setEditable(isEditable)
-    return comboBox
-  }
+  val p = JPanel(GridLayout(2, 1))
+  p.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  p.add(makeTitledPanel("setEditable(false)", listOf(c0, c1)))
+  p.add(makeTitledPanel("setEditable(true)", listOf(c2, c3)))
 
-  private fun makeTitledPanel(title: String, list: List<Component>): Component {
-    val p = JPanel(GridBagLayout())
-    p.setBorder(BorderFactory.createTitledBorder(title))
-    val c = GridBagConstraints()
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.insets = Insets(5, 5, 5, 5)
-    c.weightx = 1.0
-    c.gridx = GridBagConstraints.REMAINDER
-    list.forEach { cmp -> p.add(cmp, c) }
-    return p
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(button, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class RemoveButtonComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
+private fun makeComboBox(isDefault: Boolean, isEditable: Boolean): JComboBox<String> {
+  val m = DefaultComboBoxModel(arrayOf("aaa", "bbb", "ccc"))
+  val comboBox = if (isDefault) JComboBox(m) else RemoveButtonComboBox(m)
+  comboBox.isEditable = isEditable
+  return comboBox
+}
+
+private fun makeTitledPanel(title: String, list: List<Component>): Component {
+  val p = JPanel(GridBagLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  val c = GridBagConstraints()
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.insets = Insets(5, 5, 5, 5)
+  c.weightx = 1.0
+  c.gridx = GridBagConstraints.REMAINDER
+  list.forEach { cmp -> p.add(cmp, c) }
+  return p
+}
+
+private class RemoveButtonComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
   @Transient
   private var handler: CellButtonsMouseListener? = null
 
-  private fun getList() = (getAccessibleContext().getAccessibleChild(0) as? ComboPopup)?.getList()
+  private fun getList() = (getAccessibleContext().getAccessibleChild(0) as? ComboPopup)?.list
 
   override fun updateUI() {
     if (handler != null) {
@@ -77,47 +77,47 @@ class RemoveButtonComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
   }
 }
 
-class CellButtonsMouseListener : MouseAdapter() {
+private class CellButtonsMouseListener : MouseAdapter() {
   override fun mouseMoved(e: MouseEvent) {
-    val list = e.getComponent() as? JList<*> ?: return
-    val pt = e.getPoint()
+    val list = e.component as? JList<*> ?: return
+    val pt = e.point
     val idx = list.locationToIndex(pt)
-    (list.getCellRenderer() as? ButtonsRenderer<*>)?.rolloverIndex = getButton(list, pt, idx)?.let { idx } ?: -1
+    (list.cellRenderer as? ButtonsRenderer<*>)?.rolloverIndex = getButton(list, pt, idx)?.let { idx } ?: -1
     list.repaint()
   }
 
   override fun mousePressed(e: MouseEvent) {
-    e.getComponent().repaint()
+    e.component.repaint()
   }
 
   override fun mouseReleased(e: MouseEvent) {
-    val list = e.getComponent() as? JList<*> ?: return
-    val pt = e.getPoint()
+    val list = e.component as? JList<*> ?: return
+    val pt = e.point
     val index = list.locationToIndex(pt)
     if (index >= 0) {
       getButton(list, pt, index)?.doClick()
     }
-    (list.getCellRenderer() as? ButtonsRenderer<*>)?.rolloverIndex = -1
+    (list.cellRenderer as? ButtonsRenderer<*>)?.rolloverIndex = -1
     list.repaint()
   }
 
   override fun mouseExited(e: MouseEvent) {
-    val list = e.getComponent() as? JList<*> ?: return
-    (list.getCellRenderer() as? ButtonsRenderer<*>)?.rolloverIndex = -1
+    val list = e.component as? JList<*> ?: return
+    (list.cellRenderer as? ButtonsRenderer<*>)?.rolloverIndex = -1
   }
 
   private fun <E> getButton(list: JList<E>, pt: Point, index: Int): JButton? {
-    val proto = list.getPrototypeCellValue()
-    val c = list.getCellRenderer().getListCellRendererComponent(list, proto, index, false, false)
+    val proto = list.prototypeCellValue
+    val c = list.cellRenderer.getListCellRendererComponent(list, proto, index, false, false)
     val r = list.getCellBounds(index, index)
-    c.setBounds(r)
+    c.bounds = r
     // c.doLayout(); // may be needed for other layout managers (eg. FlowLayout)
     pt.translate(-r.x, -r.y)
     return SwingUtilities.getDeepestComponentAt(c, pt.x, pt.y) as? JButton
   }
 }
 
-class ButtonsRenderer<E>(comboBox: RemoveButtonComboBox<E>) : ListCellRenderer<E> {
+private class ButtonsRenderer<E>(comboBox: RemoveButtonComboBox<E>) : ListCellRenderer<E> {
   private var targetIndex = 0
   var rolloverIndex = -1
   private val panel = object : JPanel(BorderLayout()) { // *1
@@ -133,24 +133,24 @@ class ButtonsRenderer<E>(comboBox: RemoveButtonComboBox<E>) : ListCellRenderer<E
 
     override fun updateUI() {
       super.updateUI()
-      setBorder(BorderFactory.createEmptyBorder())
-      setFocusable(false)
-      setRolloverEnabled(false)
-      setContentAreaFilled(false)
+      border = BorderFactory.createEmptyBorder()
+      isFocusable = false
+      isRolloverEnabled = false
+      isContentAreaFilled = false
     }
   }
 
   init {
     deleteButton.addActionListener {
-      val m = comboBox.getModel()
-      val isMoreThanOneItem = m.getSize() > 1
+      val m = comboBox.model
+      val isMoreThanOneItem = m.size > 1
       if (isMoreThanOneItem && m is MutableComboBoxModel<*>) {
         m.removeElementAt(targetIndex)
-        comboBox.setSelectedIndex(-1)
+        comboBox.selectedIndex = -1
         comboBox.showPopup()
       }
     }
-    panel.setOpaque(true)
+    panel.isOpaque = true
     panel.add(deleteButton, BorderLayout.EAST)
   }
 
@@ -165,19 +165,19 @@ class ButtonsRenderer<E>(comboBox: RemoveButtonComboBox<E>) : ListCellRenderer<E
     if (index < 0) {
       return c
     }
-    (c as? JComponent)?.setOpaque(false)
+    (c as? JComponent)?.isOpaque = false
     this.targetIndex = index
     if (isSelected) {
-      panel.setBackground(list.getSelectionBackground())
+      panel.setBackground(list.selectionBackground)
     } else {
-      panel.setBackground(if (index % 2 == 0) EVEN_COLOR else list.getBackground())
+      panel.setBackground(if (index % 2 == 0) EVEN_COLOR else list.background)
     }
-    val showDeleteButton = list.getModel().getSize() > 1
-    deleteButton.setVisible(showDeleteButton)
+    val showDeleteButton = list.model.size > 1
+    deleteButton.isVisible = showDeleteButton
     if (showDeleteButton) {
       val isRollover = index == rolloverIndex
-      deleteButton.getModel().setRollover(isRollover)
-      deleteButton.setForeground(if (isRollover) Color.WHITE else list.getForeground())
+      deleteButton.model.isRollover = isRollover
+      deleteButton.foreground = if (isRollover) Color.WHITE else list.foreground
     }
     panel.add(c)
     return panel
@@ -198,7 +198,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
