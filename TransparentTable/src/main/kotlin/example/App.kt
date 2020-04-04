2f3f7a81bfb4c0662c2085bef27c1df756cf9c05
aterai <aterai@outlook.com>
2020-04-05 01:37:16 +0900

    refactor: use of setter method instead of property access syntax

--- TransparentTable/src/main/kotlin/example/App.kt
+++ TransparentTable/src/main/kotlin/example/App.kt
@@ -9,103 +9,105 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val alphaZero = Color(0x0, true)
-  private val color = Color(255, 0, 0, 50)
+private val alphaZero = Color(0x0, true)
+private val color = Color(255, 0, 0, 50)
 
-  init {
-    val columnNames = arrayOf("String", "Integer", "Boolean")
-    val data = arrayOf(
-      arrayOf("aaa", 12, true),
-      arrayOf("bbb", 5, false),
-      arrayOf("CCC", 92, true),
-      arrayOf("DDD", 0, false))
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-    }
-    val table = object : JTable(model) {
-      override fun updateUI() {
-        setSelectionForeground(ColorUIResource(Color.RED))
-        setSelectionBackground(ColorUIResource(Color.RED))
-        super.updateUI()
-        val m = getModel()
-        for (i in 0 until m.getColumnCount()) {
-          (getDefaultRenderer(m.getColumnClass(i)) as? Component)?.also {
-            SwingUtilities.updateComponentTreeUI(it)
-          }
-        }
-      }
-
-      override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
-        val c = super.prepareEditor(editor, row, column)
-        if (c is JTextField) {
-          c.setOpaque(false)
-        } else if (c is JCheckBox) {
-          c.setBackground(getSelectionBackground())
-        }
-        return c
-      }
-    }
-    table.setAutoCreateRowSorter(true)
-    table.setRowSelectionAllowed(true)
-    table.setFillsViewportHeight(true)
-    table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION)
-    table.setDefaultRenderer(java.lang.Boolean::class.java, TranslucentBooleanRenderer())
-    table.setOpaque(false)
-    table.setBackground(alphaZero)
-
-    val texture = makeImageTexture()
-    val scroll = object : JScrollPane(table) {
-      override fun createViewport() = object : JViewport() {
-        override fun paintComponent(g: Graphics) {
-          val g2 = g.create() as Graphics2D
-          g2.setPaint(texture)
-          g2.fillRect(0, 0, getWidth(), getHeight())
-          g2.dispose()
-          super.paintComponent(g)
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = object : JTable(model) {
+    override fun updateUI() {
+      setSelectionForeground(ColorUIResource(Color.RED))
+      setSelectionBackground(ColorUIResource(Color.RED))
+      super.updateUI()
+      val m = getModel()
+      for (i in 0 until m.columnCount) {
+        (getDefaultRenderer(m.getColumnClass(i)) as? Component)?.also {
+          SwingUtilities.updateComponentTreeUI(it)
         }
       }
     }
-    scroll.getViewport().setOpaque(false)
-    scroll.getViewport().setBackground(alphaZero)
 
-    val check = JCheckBox("setBackground(new Color(255, 0, 0, 50))")
-    check.addActionListener { e ->
-      val b = (e.getSource() as? JCheckBox)?.isSelected() ?: false
-      table.setBackground(if (b) color else alphaZero)
+    override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
+      val c = super.prepareEditor(editor, row, column)
+      if (c is JTextField) {
+        c.isOpaque = false
+      } else if (c is JCheckBox) {
+        c.setBackground(getSelectionBackground())
+      }
+      return c
     }
+  }
+  table.autoCreateRowSorter = true
+  table.rowSelectionAllowed = true
+  table.fillsViewportHeight = true
+  table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION)
+  table.setDefaultRenderer(java.lang.Boolean::class.java, TranslucentBooleanRenderer())
+  table.isOpaque = false
+  table.background = alphaZero
 
-    add(check, BorderLayout.NORTH)
-    add(scroll)
-    setPreferredSize(Dimension(320, 240))
+  val texture = makeImageTexture()
+  val scroll = object : JScrollPane(table) {
+    override fun createViewport() = object : JViewport() {
+      override fun paintComponent(g: Graphics) {
+        val g2 = g.create() as? Graphics2D ?: return
+        g2.paint = texture
+        g2.fillRect(0, 0, width, height)
+        g2.dispose()
+        super.paintComponent(g)
+      }
+    }
+  }
+  scroll.viewport.isOpaque = false
+  scroll.viewport.background = alphaZero
+
+  val check = JCheckBox("setBackground(new Color(255, 0, 0, 50))")
+  check.addActionListener { e ->
+    val b = (e.source as? JCheckBox)?.isSelected ?: false
+    table.background = if (b) color else alphaZero
   }
 
-  private fun makeImageTexture(): TexturePaint {
-    // unkaku_w.png http://www.viva-edo.com/komon/edokomon.html
-    val bi = runCatching {
-      ImageIO.read(javaClass.getResource("unkaku_w.png"))
-    }.getOrNull() ?: makeMissingImage()
-    return TexturePaint(bi, Rectangle(bi.getWidth(), bi.getHeight()))
-  }
-
-  private fun makeMissingImage(): BufferedImage {
-    val missingIcon = UIManager.getIcon("OptionPane.errorIcon")
-    val w = missingIcon.getIconWidth()
-    val h = missingIcon.getIconHeight()
-    val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
-    val g2 = bi.createGraphics()
-    missingIcon.paintIcon(null, g2, 0, 0)
-    g2.dispose()
-    return bi
+  return JPanel(BorderLayout()).also {
+    it.add(check, BorderLayout.NORTH)
+    it.add(scroll)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class TranslucentBooleanRenderer : JCheckBox(), TableCellRenderer {
+private fun makeImageTexture(): TexturePaint {
+  val cl = Thread.currentThread().contextClassLoader
+  // unkaku_w.png http://www.viva-edo.com/komon/edokomon.html
+  val bi = runCatching {
+    ImageIO.read(cl.getResource("example/unkaku_w.png"))
+  }.getOrNull() ?: makeMissingImage()
+  return TexturePaint(bi, Rectangle(bi.width, bi.height))
+}
+
+private fun makeMissingImage(): BufferedImage {
+  val missingIcon = UIManager.getIcon("OptionPane.errorIcon")
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class TranslucentBooleanRenderer : JCheckBox(), TableCellRenderer {
   override fun updateUI() {
     super.updateUI()
-    setHorizontalAlignment(SwingConstants.CENTER)
-    setBorderPainted(true)
-    setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1))
+    horizontalAlignment = SwingConstants.CENTER
+    isBorderPainted = true
+    border = BorderFactory.createEmptyBorder(1, 1, 1, 1)
   }
 
   override fun getTableCellRendererComponent(
@@ -116,24 +118,24 @@ class TranslucentBooleanRenderer : JCheckBox(), TableCellRenderer {
     row: Int,
     column: Int
   ): Component {
-    setHorizontalAlignment(SwingConstants.CENTER)
+    horizontalAlignment = SwingConstants.CENTER
     if (isSelected) {
-      setOpaque(true)
-      setForeground(table.getSelectionForeground())
-      super.setBackground(table.getSelectionBackground())
+      isOpaque = true
+      foreground = table.selectionForeground
+      super.setBackground(table.selectionBackground)
     } else {
-      setOpaque(false)
-      setForeground(table.getForeground())
-      setBackground(table.getBackground())
+      isOpaque = false
+      foreground = table.foreground
+      background = table.background
     }
     setSelected(value == true)
     return this
   }
 
   override fun paintComponent(g: Graphics) {
-    if (!isOpaque()) {
-      g.setColor(getBackground())
-      g.fillRect(0, 0, getWidth(), getHeight())
+    if (!isOpaque) {
+      g.color = background
+      g.fillRect(0, 0, width, height)
     }
     super.paintComponent(g)
   }
@@ -149,7 +151,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
