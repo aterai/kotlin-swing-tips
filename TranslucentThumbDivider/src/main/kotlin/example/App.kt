d65c826fa17ad6a2c3c4b73a745eb8e88763adab
aterai <aterai@outlook.com>
2020-04-05 01:36:04 +0900

    refactor: remove explicit type arguments

--- TranslucentThumbDivider/src/main/kotlin/example/App.kt
+++ TranslucentThumbDivider/src/main/kotlin/example/App.kt
@@ -12,68 +12,69 @@ import java.awt.image.ColorConvertOp
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.LayerUI
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val split = JSplitPane()
-    split.setContinuousLayout(true)
-    split.setResizeWeight(.5)
-    split.setDividerSize(0)
+fun makeUI(): Component {
+  val split = JSplitPane()
+  split.isContinuousLayout = true
+  split.resizeWeight = .5
+  split.dividerSize = 0
 
-    val icon = ImageIcon(javaClass.getResource("test.png"))
+  val cl = Thread.currentThread().contextClassLoader
+  val icon = ImageIcon(cl.getResource("example/test.png"))
 
-    val source = BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB)
-    val g = source.createGraphics()
-    g.drawImage(icon.getImage(), 0, 0, null)
-    g.dispose()
-    val colorConvert = ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null)
-    val destination = colorConvert.filter(source, null)
+  val source = BufferedImage(icon.iconWidth, icon.iconHeight, BufferedImage.TYPE_INT_ARGB)
+  val g = source.createGraphics()
+  g.drawImage(icon.image, 0, 0, null)
+  g.dispose()
+  val colorConvert = ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null)
+  val destination = colorConvert.filter(source, null)
 
-    val beforeCanvas = object : JComponent() {
-      protected override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        val iw = icon.getIconWidth()
-        val ih = icon.getIconHeight()
-        val dim = split.getSize()
-        val x = (dim.width - iw) / 2
-        val y = (dim.height - ih) / 2
-        g.drawImage(icon.getImage(), x, y, iw, ih, this)
-      }
+  val beforeCanvas = object : JComponent() {
+    override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      val iw = icon.iconWidth
+      val ih = icon.iconHeight
+      val dim = split.size
+      val x = (dim.width - iw) / 2
+      val y = (dim.height - ih) / 2
+      g.drawImage(icon.image, x, y, iw, ih, this)
     }
-    split.setLeftComponent(beforeCanvas)
+  }
+  split.leftComponent = beforeCanvas
 
-    val afterCanvas = object : JComponent() {
-      protected override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        val g2 = g.create() as Graphics2D
-        val iw = destination.getWidth(this)
-        val ih = destination.getHeight(this)
-        val pt = getLocation()
-        g2.translate(-pt.x + split.getInsets().left, 0)
+  val afterCanvas = object : JComponent() {
+    override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      val g2 = g.create() as? Graphics2D ?: return
+      val iw = destination.getWidth(this)
+      val ih = destination.getHeight(this)
+      val pt = location
+      g2.translate(-pt.x + split.insets.left, 0)
 
-        val dim = split.getSize()
-        val x = (dim.width - iw) / 2
-        val y = (dim.height - ih) / 2
-        g2.drawImage(destination, x, y, iw, ih, this)
-        g2.dispose()
-      }
+      val dim = split.size
+      val x = (dim.width - iw) / 2
+      val y = (dim.height - ih) / 2
+      g2.drawImage(destination, x, y, iw, ih, this)
+      g2.dispose()
     }
-    split.setRightComponent(afterCanvas)
+  }
+  split.rightComponent = afterCanvas
 
-    val layerUI = DividerLocationDragLayerUI()
-    val check = JCheckBox("Paint divider")
-    check.addActionListener { e ->
-      val c = e.getSource() as? JCheckBox ?: return@addActionListener
-      layerUI.setPaintDividerEnabled(c.isSelected())
-    }
+  val layerUI = DividerLocationDragLayerUI()
+  val check = JCheckBox("Paint divider")
+  check.addActionListener { e ->
+    val c = e.source as? JCheckBox ?: return@addActionListener
+    layerUI.setPaintDividerEnabled(c.isSelected)
+  }
 
-    add(JLayer<JSplitPane>(split, layerUI))
-    add(check, BorderLayout.SOUTH)
-    setOpaque(false)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(JLayer(split, layerUI))
+    it.add(check, BorderLayout.SOUTH)
+    it.isOpaque = false
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
+private class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
   private val startPt = Point()
   private val dc = Cursor.getDefaultCursor()
   private val wc = Cursor.getPredefinedCursor(Cursor.W_RESIZE_CURSOR)
@@ -85,21 +86,21 @@ internal class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
 
   override fun installUI(c: JComponent) {
     super.installUI(c)
-    (c as? JLayer<*>)?.setLayerEventMask(AWTEvent.MOUSE_EVENT_MASK or AWTEvent.MOUSE_MOTION_EVENT_MASK)
+    (c as? JLayer<*>)?.layerEventMask = AWTEvent.MOUSE_EVENT_MASK or AWTEvent.MOUSE_MOTION_EVENT_MASK
   }
 
   override fun uninstallUI(c: JComponent) {
-    (c as? JLayer<*>)?.setLayerEventMask(0)
+    (c as? JLayer<*>)?.layerEventMask = 0
     super.uninstallUI(c)
   }
 
   override fun paint(g: Graphics, c: JComponent) {
     super.paint(g, c)
     if ((isEnter || isDragging) && c is JLayer<*>) {
-      updateThumbLocation(c.getView(), thumb)
-      val g2 = g.create() as Graphics2D
+      updateThumbLocation(c.view, thumb)
+      val g2 = g.create() as? Graphics2D ?: return
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g2.setPaint(Color(255, 100, 100, 100))
+      g2.paint = Color(255, 100, 100, 100)
       g2.fill(thumb)
       if (dividerEnabled) {
         paintDivider(g2)
@@ -109,14 +110,14 @@ internal class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
   }
 
   private fun paintDivider(g2: Graphics2D) {
-    g2.setStroke(BasicStroke(5f))
-    g2.setPaint(Color.WHITE)
+    g2.stroke = BasicStroke(5f)
+    g2.paint = Color.WHITE
     g2.draw(thumb)
 
-    val cx = thumb.getCenterX()
-    val cy = thumb.getCenterY()
+    val cx = thumb.centerX
+    val cy = thumb.centerY
 
-    val line = Line2D.Double(cx, 0.0, cx, thumb.getMinY())
+    val line = Line2D.Double(cx, 0.0, cx, thumb.minY)
     g2.draw(line)
 
     val v = 8.0
@@ -138,19 +139,19 @@ internal class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
     this.dividerEnabled = flg
   }
 
-  protected override fun processMouseEvent(e: MouseEvent, l: JLayer<out JSplitPane>) {
-    val splitPane = l.getView()
-    when (e.getID()) {
+  override fun processMouseEvent(e: MouseEvent, l: JLayer<out JSplitPane>) {
+    val splitPane = l.view
+    when (e.id) {
       MouseEvent.MOUSE_ENTERED -> isEnter = true
       MouseEvent.MOUSE_EXITED -> isEnter = false
       MouseEvent.MOUSE_RELEASED -> isDragging = false
       MouseEvent.MOUSE_PRESSED -> {
-        val c = e.getComponent()
+        val c = e.component
         if (isDraggableComponent(splitPane, c)) {
-          val pt = SwingUtilities.convertPoint(c, e.getPoint(), splitPane)
+          val pt = SwingUtilities.convertPoint(c, e.point, splitPane)
           isDragging = thumb.contains(pt)
-          startPt.setLocation(SwingUtilities.convertPoint(c, e.getPoint(), splitPane))
-          dividerLocation = splitPane.getDividerLocation()
+          startPt.location = SwingUtilities.convertPoint(c, e.point, splitPane)
+          dividerLocation = splitPane.dividerLocation
         }
       }
       // else -> {}
@@ -158,31 +159,31 @@ internal class DividerLocationDragLayerUI : LayerUI<JSplitPane>() {
     splitPane.repaint()
   }
 
-  protected override fun processMouseMotionEvent(e: MouseEvent, l: JLayer<out JSplitPane>) {
-    val splitPane = l.getView()
-    val c = e.getComponent()
-    val pt = SwingUtilities.convertPoint(c, e.getPoint(), splitPane)
-    if (e.getID() == MouseEvent.MOUSE_MOVED) {
-      splitPane.setCursor(if (thumb.contains(e.getPoint())) wc else dc)
-    } else if (isDragging && isDraggableComponent(splitPane, c) && e.getID() == MouseEvent.MOUSE_DRAGGED) {
-      val delta = when (splitPane.getOrientation()) {
+  override fun processMouseMotionEvent(e: MouseEvent, l: JLayer<out JSplitPane>) {
+    val splitPane = l.view
+    val c = e.component
+    val pt = SwingUtilities.convertPoint(c, e.point, splitPane)
+    if (e.id == MouseEvent.MOUSE_MOVED) {
+      splitPane.cursor = if (thumb.contains(e.point)) wc else dc
+    } else if (isDragging && isDraggableComponent(splitPane, c) && e.id == MouseEvent.MOUSE_DRAGGED) {
+      val delta = when (splitPane.orientation) {
         JSplitPane.HORIZONTAL_SPLIT -> pt.x - startPt.x
         else -> pt.y - startPt.y
       }
-      splitPane.setDividerLocation(maxOf(0, dividerLocation + delta))
+      splitPane.dividerLocation = maxOf(0, dividerLocation + delta)
     }
   }
 
   private fun isDraggableComponent(splitPane: JSplitPane, c: Component) =
-      splitPane == c || splitPane == SwingUtilities.getUnwrappedParent(c)
+    splitPane == c || splitPane == SwingUtilities.getUnwrappedParent(c)
 
   private fun updateThumbLocation(c: Component, thumb: Ellipse2D) {
     val splitPane = c as? JSplitPane ?: return
-    val pos = splitPane.getDividerLocation()
-    if (splitPane.getOrientation() == JSplitPane.HORIZONTAL_SPLIT) {
-      thumb.setFrame(pos - R, splitPane.getHeight() / 2.0 - R, R + R, R + R)
+    val pos = splitPane.dividerLocation
+    if (splitPane.orientation == JSplitPane.HORIZONTAL_SPLIT) {
+      thumb.setFrame(pos - R, splitPane.height / 2.0 - R, R + R, R + R)
     } else {
-      thumb.setFrame(splitPane.getWidth() / 2.0 - R, pos - R, R + R, R + R)
+      thumb.setFrame(splitPane.width / 2.0 - R, pos - R, R + R, R + R)
     }
   }
 
@@ -201,7 +202,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
