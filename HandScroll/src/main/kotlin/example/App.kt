3472646badebf18212756f46f6794632cae78949
aterai <aterai@outlook.com>
2020-04-07 18:31:51 +0900

    refactor: use of setter method instead of property access syntax

--- HandScroll/src/main/kotlin/example/App.kt
+++ HandScroll/src/main/kotlin/example/App.kt
@@ -6,87 +6,91 @@ import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val weightMixing = false
-    val label = JLabel(ImageIcon(javaClass.getResource("CRW_3857_JFR.jpg"))) // http://sozai-free.com/
-    val vport = object : JViewport() {
-      private var isAdjusting = false
-      override fun revalidate() {
-        if (!weightMixing && isAdjusting) {
-          return
-        }
-        super.revalidate()
+fun makeUI(): Component {
+  // val weightMixing = false
+  val cl = Thread.currentThread().contextClassLoader
+  // CRW_3857_JFR.jpg: http://sozai-free.com/
+  val label = JLabel(ImageIcon(cl.getResource("example/CRW_3857_JFR.jpg")))
+  val vport = object : JViewport() {
+    private var isAdjusting = false
+    override fun revalidate() {
+      // if (!weightMixing && isAdjusting) {
+      if (isAdjusting) {
+        return
       }
-
-      override fun setViewPosition(p: Point) {
-        if (weightMixing) {
-          super.setViewPosition(p)
-        } else {
-          isAdjusting = true
-          super.setViewPosition(p)
-          isAdjusting = false
-        }
-      }
-    }
-    vport.add(label)
-
-    val scroll = JScrollPane() // new JScrollPane(label);
-    scroll.setViewport(vport)
-
-    val hsl1 = HandScrollListener()
-    vport.addMouseMotionListener(hsl1)
-    vport.addMouseListener(hsl1)
-
-    val radio = JRadioButton("scrollRectToVisible", true)
-    radio.addItemListener { e ->
-      hsl1.withinRangeMode = e.getStateChange() == ItemEvent.SELECTED
+      super.revalidate()
     }
 
-    val box = Box.createHorizontalBox()
-    val bg = ButtonGroup()
-    listOf(radio, JRadioButton("setViewPosition")).forEach {
-      box.add(it)
-      bg.add(it)
+    override fun setViewPosition(p: Point) {
+      // if (weightMixing) {
+      //   super.setViewPosition(p)
+      // } else {
+      isAdjusting = true
+      super.setViewPosition(p)
+      isAdjusting = false
+      // }
     }
+  }
+  vport.add(label)
 
-    // // TEST:
-    // MouseAdapter hsl2 = new DragScrollListener()
-    // label.addMouseMotionListener(hsl2)
-    // label.addMouseListener(hsl2)
-    add(scroll)
-    add(box, BorderLayout.NORTH)
-    scroll.setPreferredSize(Dimension(320, 240))
+  val scroll = JScrollPane() // new JScrollPane(label);
+  scroll.viewport = vport
+
+  val hsl1 = HandScrollListener()
+  vport.addMouseMotionListener(hsl1)
+  vport.addMouseListener(hsl1)
+
+  val radio = JRadioButton("scrollRectToVisible", true)
+  radio.addItemListener { e ->
+    hsl1.withinRangeMode = e.stateChange == ItemEvent.SELECTED
+  }
+
+  val box = Box.createHorizontalBox()
+  val bg = ButtonGroup()
+  listOf(radio, JRadioButton("setViewPosition")).forEach {
+    box.add(it)
+    bg.add(it)
+  }
+
+  // // TEST:
+  // MouseAdapter hsl2 = new DragScrollListener()
+  // label.addMouseMotionListener(hsl2)
+  // label.addMouseListener(hsl2)
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(box, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class HandScrollListener : MouseAdapter() {
+private class HandScrollListener : MouseAdapter() {
   private val defCursor = Cursor.getDefaultCursor()
   private val hndCursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
   private val pp = Point()
   var withinRangeMode = true
 
   override fun mouseDragged(e: MouseEvent) {
-    val vport = e.getComponent() as? JViewport ?: return
-    val cp = e.getPoint()
-    val vp = vport.getViewPosition() // = SwingUtilities.convertPoint(vport, 0, 0, label)
+    val vport = e.component as? JViewport ?: return
+    val cp = e.point
+    val vp = vport.viewPosition // = SwingUtilities.convertPoint(vport, 0, 0, label)
     vp.translate(pp.x - cp.x, pp.y - cp.y)
     if (withinRangeMode) {
       (SwingUtilities.getUnwrappedView(vport) as? JComponent)
-        ?.scrollRectToVisible(Rectangle(vp, vport.getSize()))
+        ?.scrollRectToVisible(Rectangle(vp, vport.size))
     } else {
-      vport.setViewPosition(vp)
+      vport.viewPosition = vp
     }
-    pp.setLocation(cp)
+    pp.location = cp
   }
 
   override fun mousePressed(e: MouseEvent) {
-    e.getComponent().setCursor(hndCursor)
-    pp.setLocation(e.getPoint())
+    e.component.cursor = hndCursor
+    pp.location = e.point
   }
 
   override fun mouseReleased(e: MouseEvent) {
-    e.getComponent().setCursor(defCursor)
+    e.component.cursor = defCursor
   }
 }
 
@@ -100,7 +104,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
