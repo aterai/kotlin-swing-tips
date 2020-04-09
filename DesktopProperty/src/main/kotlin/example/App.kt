b04cd82a1a45dccdc715516589a0cf8c8d1ff6f3
aterai <aterai@outlook.com>
2020-04-09 20:14:15 +0900

    add: DesktopProperty

---
+++ DesktopProperty/src/main/kotlin/example/App.kt
@@ -0,0 +1,57 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+
+private val columnNames = arrayOf("Name", "Class", "Value")
+private val model = DefaultTableModel(null, columnNames)
+
+private fun initModel(e: PropertyChangeEvent?) {
+  if (e != null) {
+    println("----\n${e.propertyName}\n")
+    println(Toolkit.getDefaultToolkit().getDesktopProperty(e.propertyName))
+  }
+  model.rowCount = 0
+  val tk = Toolkit.getDefaultToolkit()
+  for (s in tk.getDesktopProperty("win.propNames") as Array<*>) {
+    val o = tk.getDesktopProperty(s.toString())
+    val row = arrayOf(s, o.javaClass, o)
+    model.addRow(row)
+  }
+}
+
+fun makeUI(): Component {
+  val table: JTable = object : JTable(model) {
+    override fun isCellEditable(row: Int, column: Int) = false
+  }
+  table.autoCreateRowSorter = true
+  val l = PropertyChangeListener { e -> initModel(e) }
+  Toolkit.getDefaultToolkit().addPropertyChangeListener("win.xpstyle.colorName", l)
+  Toolkit.getDefaultToolkit().addPropertyChangeListener("awt.multiClickInterval", l)
+  initModel(null)
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
