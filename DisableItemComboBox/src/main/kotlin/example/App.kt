156f45bb2ae31b56ff2a4ea4efe5c45ced7b4384
aterai <aterai@outlook.com>
2020-04-10 19:34:53 +0900

    refactor: use of setter method instead of property access syntax

--- DisableItemComboBox/src/main/kotlin/example/App.kt
+++ DisableItemComboBox/src/main/kotlin/example/App.kt
@@ -5,59 +5,59 @@ import java.awt.event.ActionEvent
 import java.awt.event.KeyEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val field = JTextField("1, 2, 5")
+fun makeUI(): Component {
+  val field = JTextField("1, 2, 5")
 
-    val combo = DisableItemComboBox(makeModel())
-    combo.setDisableIndex(getDisableIndexFromTextField(field))
+  val combo = DisableItemComboBox(makeModel())
+  combo.setDisableIndex(getDisableIndexFromTextField(field))
 
-    val button = JButton("init")
-    button.addActionListener { combo.setDisableIndex(getDisableIndexFromTextField(field)) }
+  val button = JButton("init")
+  button.addActionListener { combo.setDisableIndex(getDisableIndexFromTextField(field)) }
 
-    val box = Box.createHorizontalBox()
-    box.add(JLabel("Disabled Item Index:"))
-    box.add(field)
-    box.add(Box.createHorizontalStrut(2))
-    box.add(button)
-    add(box, BorderLayout.SOUTH)
-    add(combo, BorderLayout.NORTH)
-    setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10))
-    setPreferredSize(Dimension(320, 240))
+  val box = Box.createHorizontalBox()
+  box.add(JLabel("Disabled Item Index:"))
+  box.add(field)
+  box.add(Box.createHorizontalStrut(2))
+  box.add(button)
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.SOUTH)
+    it.add(combo, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+    it.preferredSize = Dimension(320, 240)
   }
-
-  private fun makeModel() = DefaultComboBoxModel<String>().also {
-    it.addElement("0000000000000")
-    it.addElement("111111")
-    it.addElement("222222222222")
-    it.addElement("33")
-    it.addElement("4444444444444444")
-    it.addElement("555555555555555555555555")
-    it.addElement("6666666666")
-  }
-
-  private fun getDisableIndexFromTextField(field: JTextField) = runCatching {
-    field.getText().split(",")
-      .map { it.trim() }
-      .filterNot { it.isEmpty() }
-      .map { it.toInt() }
-      .toSet()
-  }.onFailure {
-    Toolkit.getDefaultToolkit().beep()
-    val root = field.getRootPane()
-    JOptionPane.showMessageDialog(root, "invalid value.\n${it.message}", "Error", JOptionPane.ERROR_MESSAGE)
-  }.getOrNull().orEmpty()
 }
 
-class DisableItemComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
+private fun makeModel() = DefaultComboBoxModel<String>().also {
+  it.addElement("0000000000000")
+  it.addElement("111111")
+  it.addElement("222222222222")
+  it.addElement("33")
+  it.addElement("4444444444444444")
+  it.addElement("555555555555555555555555")
+  it.addElement("6666666666")
+}
+
+private fun getDisableIndexFromTextField(field: JTextField) = runCatching {
+  field.text.split(",")
+    .map { it.trim() }
+    .filterNot { it.isEmpty() }
+    .map { it.toInt() }
+    .toSet()
+}.onFailure {
+  Toolkit.getDefaultToolkit().beep()
+  val root = field.rootPane
+  JOptionPane.showMessageDialog(root, "invalid value.\n${it.message}", "Error", JOptionPane.ERROR_MESSAGE)
+}.getOrNull().orEmpty()
+
+private class DisableItemComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
   private val disableIndexSet = mutableSetOf<Int>()
   private var isDisableIndex = false
   private val up = object : AbstractAction() {
     override fun actionPerformed(e: ActionEvent) {
-      val si = getSelectedIndex()
+      val si = selectedIndex
       for (i in si - 1 downTo 0) {
         if (!disableIndexSet.contains(i)) {
-          setSelectedIndex(i)
+          selectedIndex = i
           break
         }
       }
@@ -65,10 +65,10 @@ class DisableItemComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
   }
   private val down = object : AbstractAction() {
     override fun actionPerformed(e: ActionEvent) {
-      val si = getSelectedIndex()
-      for (i in si + 1 until getModel().getSize()) {
+      val si = selectedIndex
+      for (i in si + 1 until getModel().size) {
         if (!disableIndexSet.contains(i)) {
-          setSelectedIndex(i)
+          selectedIndex = i
           break
         }
       }
@@ -88,18 +88,18 @@ class DisableItemComboBox<E>(model: ComboBoxModel<E>) : JComboBox<E>(model) {
       val c: Component
       if (disableIndexSet.contains(index)) {
         c = renderer.getListCellRendererComponent(list, value, index, false, false)
-        c.setEnabled(false)
+        c.isEnabled = false
       } else {
         c = renderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
-        c.setEnabled(true)
+        c.isEnabled = true
       }
       return@setRenderer c
     }
     EventQueue.invokeLater {
-      val am = getActionMap()
+      val am = actionMap
       am.put("selectPrevious3", up)
       am.put("selectNext3", down)
-      val im = getInputMap()
+      val im = inputMap
       im.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "selectPrevious3")
       im.put(KeyStroke.getKeyStroke(KeyEvent.VK_KP_UP, 0), "selectPrevious3")
       im.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "selectNext3")
@@ -142,7 +142,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
