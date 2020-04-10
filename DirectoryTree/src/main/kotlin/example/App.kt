599d12de26c60086889810de1d278d2aa65e7cb7
aterai <aterai@outlook.com>
2020-04-10 19:31:12 +0900

    refactor: use of setter method instead of property access syntax

--- DirectoryTree/src/main/kotlin/example/App.kt
+++ DirectoryTree/src/main/kotlin/example/App.kt
@@ -1,6 +1,3 @@
-// -*- mode:java; encoding:utf-8 -*-
-// vim:set fileencoding=utf-8:
-// @homepage@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
@@ -13,94 +10,94 @@ import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeCellRenderer
 import javax.swing.tree.DefaultTreeModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val fileSystemView = FileSystemView.getFileSystemView()
-    val root = DefaultMutableTreeNode()
-    val treeModel = DefaultTreeModel(root)
-    fileSystemView.getRoots()
-      .forEach { fileSystemRoot ->
-        val node = DefaultMutableTreeNode(fileSystemRoot)
-        root.add(node)
-        fileSystemView.getFiles(fileSystemRoot, true)
-          .filter { it.isDirectory() }
-          .map { DefaultMutableTreeNode(it) }
-          .forEach { node.add(it) }
-      }
-    val tree: JTree = object : JTree(treeModel) {
-      override fun updateUI() {
-        setCellRenderer(null)
-        super.updateUI()
-        val r = DefaultTreeCellRenderer()
-        setCellRenderer { tree, value, selected, expanded, leaf, row, hasFocus ->
-          val c = r.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus)
-          if (c is JLabel) {
-            if (selected) {
-              c.setOpaque(false)
-              c.setForeground(r.getTextSelectionColor())
-            } else {
-              c.setOpaque(true)
-              c.setForeground(r.getTextNonSelectionColor())
-              c.setBackground(r.getBackgroundNonSelectionColor())
-            }
-            if (value is DefaultMutableTreeNode) {
-              (value.getUserObject() as? File)?.also {
-                c.setIcon(fileSystemView.getSystemIcon(it))
-                c.setText(fileSystemView.getSystemDisplayName(it))
-                c.setToolTipText(it.getPath())
-              }
+fun makeUI(): Component {
+  val fileSystemView = FileSystemView.getFileSystemView()
+  val root = DefaultMutableTreeNode()
+  val treeModel = DefaultTreeModel(root)
+  fileSystemView.roots
+    .forEach { fileSystemRoot ->
+      val node = DefaultMutableTreeNode(fileSystemRoot)
+      root.add(node)
+      fileSystemView.getFiles(fileSystemRoot, true)
+        .filter { it.isDirectory }
+        .map { DefaultMutableTreeNode(it) }
+        .forEach { node.add(it) }
+    }
+  val tree: JTree = object : JTree(treeModel) {
+    override fun updateUI() {
+      setCellRenderer(null)
+      super.updateUI()
+      val r = DefaultTreeCellRenderer()
+      setCellRenderer { tree, value, selected, expanded, leaf, row, hasFocus ->
+        val c = r.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus)
+        if (c is JLabel) {
+          if (selected) {
+            c.isOpaque = false
+            c.setForeground(r.textSelectionColor)
+          } else {
+            c.isOpaque = true
+            c.setForeground(r.textNonSelectionColor)
+            c.setBackground(r.backgroundNonSelectionColor)
+          }
+          if (value is DefaultMutableTreeNode) {
+            (value.userObject as? File)?.also {
+              c.icon = fileSystemView.getSystemIcon(it)
+              c.text = fileSystemView.getSystemDisplayName(it)
+              c.toolTipText = it.path
             }
           }
-          c
         }
+        c
       }
     }
-    tree.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4))
-    tree.setRootVisible(false)
-    tree.addTreeSelectionListener(FolderSelectionListener(fileSystemView))
-    tree.expandRow(0)
-    setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    add(JScrollPane(tree))
-    setPreferredSize(Dimension(320, 240))
+  }
+  tree.border = BorderFactory.createEmptyBorder(4, 4, 4, 4)
+  tree.isRootVisible = false
+  tree.addTreeSelectionListener(FolderSelectionListener(fileSystemView))
+  tree.expandRow(0)
+  return JPanel(BorderLayout()).also {
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.add(JScrollPane(tree))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class FolderSelectionListener(private val fileSystemView: FileSystemView) : TreeSelectionListener {
+private class FolderSelectionListener(private val fileSystemView: FileSystemView) : TreeSelectionListener {
   override fun valueChanged(e: TreeSelectionEvent) {
-    val node = e.getPath().getLastPathComponent()
-    if (node !is DefaultMutableTreeNode || !node.isLeaf()) {
+    val node = e.path.lastPathComponent
+    if (node !is DefaultMutableTreeNode || !node.isLeaf) {
       return
     }
-    val parent = node.getUserObject()
-    if (parent !is File || !parent.isDirectory()) {
+    val parent = node.userObject
+    if (parent !is File || !parent.isDirectory) {
       return
     }
-    val tree = e.getSource() as JTree
-    val model = tree.getModel() as DefaultTreeModel
+    val tree = e.source as JTree
+    val model = tree.model as DefaultTreeModel
     val worker = object : BackgroundTask(fileSystemView, parent) {
       override fun process(chunks: List<File>) {
-        if (isCancelled()) {
+        if (isCancelled) {
           return
         }
-        if (!tree.isDisplayable()) {
+        if (!tree.isDisplayable) {
           cancel(true)
           return
         }
         chunks.map { DefaultMutableTreeNode(it) }
-          .forEach { model.insertNodeInto(it, node, node.getChildCount()) }
+          .forEach { model.insertNodeInto(it, node, node.childCount) }
       }
     }
     worker.execute()
   }
 }
 
-open class BackgroundTask constructor(
+private open class BackgroundTask constructor(
   private val fileSystemView: FileSystemView,
   private val parent: File
 ) : SwingWorker<String, File>() {
   public override fun doInBackground(): String {
     fileSystemView.getFiles(parent, true)
-      .filter { it.isDirectory() }
+      .filter { it.isDirectory }
       .forEach { publish(it) }
     return "done"
   }
@@ -116,7 +113,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
