eb2d7170891a5d68786772a0e885a2796ffa9435
aterai <aterai@outlook.com>
2020-04-12 00:23:16 +0900

    refactor: use of setter method instead of property access syntax

--- KineticScrolling/src/main/kotlin/example/App.kt
+++ KineticScrolling/src/main/kotlin/example/App.kt
@@ -9,85 +9,86 @@ import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import kotlin.math.abs
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val scroll = JScrollPane().also {
-      it.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER)
-      it.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
-    }
+fun makeUI(): Component {
+  val scroll = JScrollPane().also {
+    it.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER
+    it.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+  }
 
-    // val heavyweightLightweightMixing = false
-    // val viewport = scroll.getViewport() // Java 6
-    val viewport = object : JViewport() { // Java 7
-      // private val HEAVYWEIGHT_LIGHTWEIGHT_MIXING = false
-      private var adjusting = false
-      override fun revalidate() {
-        // if (!heavyweightLightweightMixing && adjusting) {
-        if (adjusting) {
-            return
-        }
-        super.revalidate()
+  // val heavyweightLightweightMixing = false
+  // val viewport = scroll.getViewport() // Java 6
+  val viewport = object : JViewport() { // Java 7
+    // private val HEAVYWEIGHT_LIGHTWEIGHT_MIXING = false
+    private var adjusting = false
+    override fun revalidate() {
+      // if (!heavyweightLightweightMixing && adjusting) {
+      if (adjusting) {
+        return
       }
-
-      override fun setViewPosition(p: Point) {
-        // if (heavyweightLightweightMixing) {
-        //   super.setViewPosition(p)
-        // } else {
-        adjusting = true
-        super.setViewPosition(p)
-        adjusting = false
-      }
-    }
-    scroll.setViewport(viewport)
-
-    val label = JLabel(ImageIcon(javaClass.getResource("CRW_3857_JFR.jpg")))
-    viewport.add(label)
-    val l1 = KineticScrollingListener1(label)
-    val l2 = KineticScrollingListener2(label)
-
-    val r1 = JRadioButton("scrollRectToVisible", true)
-    r1.addItemListener { e ->
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        viewport.removeMouseListener(l2)
-        viewport.removeMouseMotionListener(l2)
-        viewport.removeHierarchyListener(l2)
-        viewport.addMouseMotionListener(l1)
-        viewport.addMouseListener(l1)
-        viewport.addHierarchyListener(l1)
-      }
+      super.revalidate()
     }
 
-    val r2 = JRadioButton("setViewPosition")
-    r2.addItemListener { e ->
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        viewport.removeMouseListener(l1)
-        viewport.removeMouseMotionListener(l1)
-        viewport.removeHierarchyListener(l1)
-        viewport.addMouseMotionListener(l2)
-        viewport.addMouseListener(l2)
-        viewport.addHierarchyListener(l2)
-      }
+    override fun setViewPosition(p: Point) {
+      // if (heavyweightLightweightMixing) {
+      //   super.setViewPosition(p)
+      // } else {
+      adjusting = true
+      super.setViewPosition(p)
+      adjusting = false
     }
+  }
+  scroll.viewport = viewport
 
-    val box = Box.createHorizontalBox()
-    val bg = ButtonGroup()
-    listOf(r1, r2).forEach {
-      box.add(it)
-      bg.add(it)
+  val cl = Thread.currentThread().contextClassLoader
+  val label = JLabel(ImageIcon(cl.getResource("example/CRW_3857_JFR.jpg")))
+  viewport.add(label)
+  val l1 = KineticScrollingListener1(label)
+  val l2 = KineticScrollingListener2(label)
+
+  val r1 = JRadioButton("scrollRectToVisible", true)
+  r1.addItemListener { e ->
+    if (e.stateChange == ItemEvent.SELECTED) {
+      viewport.removeMouseListener(l2)
+      viewport.removeMouseMotionListener(l2)
+      viewport.removeHierarchyListener(l2)
+      viewport.addMouseMotionListener(l1)
+      viewport.addMouseListener(l1)
+      viewport.addHierarchyListener(l1)
     }
+  }
 
-    viewport.addMouseMotionListener(l1)
-    viewport.addMouseListener(l1)
-    viewport.addHierarchyListener(l1)
+  val r2 = JRadioButton("setViewPosition")
+  r2.addItemListener { e ->
+    if (e.stateChange == ItemEvent.SELECTED) {
+      viewport.removeMouseListener(l1)
+      viewport.removeMouseMotionListener(l1)
+      viewport.removeHierarchyListener(l1)
+      viewport.addMouseMotionListener(l2)
+      viewport.addMouseListener(l2)
+      viewport.addHierarchyListener(l2)
+    }
+  }
 
-    add(scroll)
-    add(box, BorderLayout.NORTH)
-    scroll.setPreferredSize(Dimension(320, 240))
+  val box = Box.createHorizontalBox()
+  val bg = ButtonGroup()
+  listOf(r1, r2).forEach {
+    box.add(it)
+    bg.add(it)
+  }
+
+  viewport.addMouseMotionListener(l1)
+  viewport.addMouseListener(l1)
+  viewport.addHierarchyListener(l1)
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(box, BorderLayout.NORTH)
+    scroll.preferredSize = Dimension(320, 240)
   }
 }
 
-class KineticScrollingListener1(private val label: JComponent) : MouseAdapter(), HierarchyListener {
-  private val dc = label.getCursor()
+private class KineticScrollingListener1(private val label: JComponent) : MouseAdapter(), HierarchyListener {
+  private val dc = label.cursor
   private val hc = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
   private val scroller: Timer
   private val startPt = Point()
@@ -95,11 +96,11 @@ class KineticScrollingListener1(private val label: JComponent) : MouseAdapter(),
 
   init {
     this.scroller = Timer(DELAY) { e ->
-      val src = e.getSource()
+      val src = e.source
       val vport = SwingUtilities.getUnwrappedParent(label) as? JViewport ?: return@Timer
-      val vp = vport.getViewPosition()
+      val vp = vport.viewPosition
       vp.translate(-delta.x, -delta.y)
-      label.scrollRectToVisible(Rectangle(vp, vport.getSize()))
+      label.scrollRectToVisible(Rectangle(vp, vport.size))
       if (abs(delta.x) > 0 || abs(delta.y) > 0) {
         delta.setLocation((delta.x * D).toInt(), (delta.y * D).toInt())
       } else if (src is Timer) {
@@ -109,29 +110,29 @@ class KineticScrollingListener1(private val label: JComponent) : MouseAdapter(),
   }
 
   override fun mousePressed(e: MouseEvent) {
-    e.getComponent().setCursor(hc)
-    startPt.setLocation(e.getPoint())
+    e.component.cursor = hc
+    startPt.location = e.point
     scroller.stop()
   }
 
   override fun mouseDragged(e: MouseEvent) {
-    val pt = e.getPoint()
-    val vport = e.getComponent() as? JViewport ?: return
-    val vp = vport.getViewPosition() // = SwingUtilities.convertPoint(vport, 0, 0, label)
+    val pt = e.point
+    val vport = e.component as? JViewport ?: return
+    val vp = vport.viewPosition // = SwingUtilities.convertPoint(vport, 0, 0, label)
     vp.translate(startPt.x - pt.x, startPt.y - pt.y)
     delta.setLocation(SPEED * (pt.x - startPt.x), SPEED * (pt.y - startPt.y))
-    label.scrollRectToVisible(Rectangle(vp, vport.getSize()))
-    startPt.setLocation(pt)
+    label.scrollRectToVisible(Rectangle(vp, vport.size))
+    startPt.location = pt
   }
 
   override fun mouseReleased(e: MouseEvent) {
-    e.getComponent().setCursor(dc)
+    e.component.cursor = dc
     scroller.start()
   }
 
   override fun hierarchyChanged(e: HierarchyEvent) {
     val mask = HierarchyEvent.DISPLAYABILITY_CHANGED
-    if (e.getChangeFlags().toInt() and mask != 0 && !e.getComponent().isDisplayable()) {
+    if (e.changeFlags.toInt() and mask != 0 && !e.component.isDisplayable) {
       scroller.stop()
     }
   }
@@ -143,27 +144,27 @@ class KineticScrollingListener1(private val label: JComponent) : MouseAdapter(),
   }
 }
 
-class KineticScrollingListener2(private val label: JComponent) : MouseAdapter(), HierarchyListener {
-  private val dc = label.getCursor()
+private class KineticScrollingListener2(private val label: JComponent) : MouseAdapter(), HierarchyListener {
+  private val dc = label.cursor
   private val hc = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
   private val startPt = Point()
   private val delta = Point()
   private val inside = Timer(DELAY) { e ->
     val vport = SwingUtilities.getUnwrappedParent(label) as? JViewport ?: return@Timer
-    val vp = vport.getViewPosition()
+    val vp = vport.viewPosition
     vp.translate(-delta.x, -delta.y)
-    vport.setViewPosition(vp)
+    vport.viewPosition = vp
     if (abs(delta.x) > 0 || abs(delta.y) > 0) {
       delta.setLocation((delta.x * D).toInt(), (delta.y * D).toInt())
       // Outside
-      if (vp.x < 0 || vp.x + vport.getWidth() - label.getWidth() > 0) {
+      if (vp.x < 0 || vp.x + vport.width - label.width > 0) {
         delta.x = (delta.x * D).toInt()
       }
-      if (vp.y < 0 || vp.y + vport.getHeight() - label.getHeight() > 0) {
+      if (vp.y < 0 || vp.y + vport.height - label.height > 0) {
         delta.y = (delta.y * D).toInt()
       }
     } else {
-      (e.getSource() as? Timer)?.stop() // inside.stop()
+      (e.source as? Timer)?.stop() // inside.stop()
       if (!isInside(vport, label)) {
         outside.start()
       }
@@ -171,44 +172,44 @@ class KineticScrollingListener2(private val label: JComponent) : MouseAdapter(),
   }
   private val outside = Timer(DELAY) { e ->
     val vport = SwingUtilities.getUnwrappedParent(label) as? JViewport ?: return@Timer
-    val vp = vport.getViewPosition()
+    val vp = vport.viewPosition
     if (vp.x < 0) {
       vp.x = (vp.x * D).toInt()
     }
     if (vp.y < 0) {
       vp.y = (vp.y * D).toInt()
     }
-    if (vp.x + vport.getWidth() - label.getWidth() > 0) {
-      vp.x = (vp.x - (vp.x + vport.getWidth() - label.getWidth()) * (1.0 - D)).toInt()
+    if (vp.x + vport.width - label.width > 0) {
+      vp.x = (vp.x - (vp.x + vport.width - label.width) * (1.0 - D)).toInt()
     }
-    if (vp.y + vport.getHeight() > label.getHeight()) {
-      vp.y = (vp.y - (vp.y + vport.getHeight() - label.getHeight()) * (1.0 - D)).toInt()
+    if (vp.y + vport.height > label.height) {
+      vp.y = (vp.y - (vp.y + vport.height - label.height) * (1.0 - D)).toInt()
     }
-    vport.setViewPosition(vp)
+    vport.viewPosition = vp
     if (isInside(vport, label)) {
-      (e.getSource() as? Timer)?.stop() // outside.stop()
+      (e.source as? Timer)?.stop() // outside.stop()
     }
   }
 
   override fun mousePressed(e: MouseEvent) {
-    e.getComponent().setCursor(hc)
-    startPt.setLocation(e.getPoint())
+    e.component.cursor = hc
+    startPt.location = e.point
     inside.stop()
     outside.stop()
   }
 
   override fun mouseDragged(e: MouseEvent) {
-    val pt = e.getPoint()
+    val pt = e.point
     val vport = SwingUtilities.getUnwrappedParent(label) as? JViewport ?: return
-    val vp = vport.getViewPosition()
+    val vp = vport.viewPosition
     vp.translate(startPt.x - pt.x, startPt.y - pt.y)
-    vport.setViewPosition(vp)
+    vport.viewPosition = vp
     delta.setLocation(SPEED * (pt.x - startPt.x), SPEED * (pt.y - startPt.y))
-    startPt.setLocation(pt)
+    startPt.location = pt
   }
 
   override fun mouseReleased(e: MouseEvent) {
-    e.getComponent().setCursor(dc)
+    e.component.cursor = dc
     val vport = SwingUtilities.getUnwrappedParent(label) as? JViewport ?: return
     if (isInside(vport, label)) {
       inside.start()
@@ -219,15 +220,15 @@ class KineticScrollingListener2(private val label: JComponent) : MouseAdapter(),
 
   override fun hierarchyChanged(e: HierarchyEvent) {
     val mask = HierarchyEvent.DISPLAYABILITY_CHANGED
-    if (e.getChangeFlags().toInt() and mask != 0 && !e.getComponent().isDisplayable()) {
+    if (e.changeFlags.toInt() and mask != 0 && !e.component.isDisplayable) {
       inside.stop()
       outside.stop()
     }
   }
 
-  private fun isInside(vport: JViewport, comp: JComponent) = vport.getViewPosition().let {
-    val ww = it.x >= 0 && it.x + vport.getWidth() - comp.getWidth() <= 0
-    val hh = it.y >= 0 && it.y + vport.getHeight() - comp.getHeight() <= 0
+  private fun isInside(vport: JViewport, comp: JComponent) = vport.viewPosition.let {
+    val ww = it.x >= 0 && it.x + vport.width - comp.width <= 0
+    val hh = it.y >= 0 && it.y + vport.height - comp.height <= 0
     ww && hh
   }
 
@@ -248,7 +249,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
