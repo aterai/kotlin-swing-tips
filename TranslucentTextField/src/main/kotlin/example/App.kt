401cb23050f656ca73e2178e77a4efc35dfa5b91
aterai <aterai@outlook.com>
2020-04-11 16:39:53 +0900

    add: TranslucentTextField

---
+++ TranslucentTextField/src/main/kotlin/example/App.kt
@@ -0,0 +1,61 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private const val TEXT = """
+
+Trail: Creating a GUI with JFC/Swing
+Lesson: Learning Swing by Example
+This lesson explains the concepts you need to use Swing components in building a user interface.
+First we examine the simplest Swing application you can write.
+Then we present several progressively complicated examples of creating user interfaces using
+ components in the javax.swing package.
+We cover several Swing components, such as buttons, labels, and text areas. The handling of
+ events is also discussed, as are layout management and accessibility. This lesson ends with
+a set of questions and exercises so you can test yourself on what you've learned.
+https://docs.oracle.com/javase/tutorial/uiswing/learn/index.html
+"""
+
+fun makeUI(): Component {
+  val textArea1 = JTextArea("JTextArea#setMargin(Insets)$TEXT")
+  textArea1.margin = Insets(5, 5, 5, 5)
+  val scroll1 = JScrollPane(textArea1)
+
+  val textArea2 = JTextArea("JScrollPane#setViewportBorder(...)$TEXT")
+  textArea2.margin = Insets(0, 0, 0, 1)
+  val scroll2 = object : JScrollPane(textArea2) {
+    override fun updateUI() {
+      super.updateUI()
+      EventQueue.invokeLater {
+        val c = getViewport().view.background
+        viewportBorder = BorderFactory.createLineBorder(c, 5)
+      }
+    }
+  }
+
+  val splitPane = JSplitPane()
+  splitPane.resizeWeight = .5
+  splitPane.leftComponent = scroll1
+  splitPane.rightComponent = scroll2
+  splitPane.preferredSize = Dimension(320, 240)
+  return splitPane
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
