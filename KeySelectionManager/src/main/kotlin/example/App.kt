4f0b551f7523db9c4745a14851a2f8260e263d72
aterai <aterai@outlook.com>
2020-04-12 00:19:38 +0900

    add: KeySelectionManager

---
+++ KeySelectionManager/src/main/kotlin/example/App.kt
@@ -0,0 +1,59 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.JComboBox.KeySelectionManager
+
+fun makeUI(): Component {
+  val box = Box.createVerticalBox()
+  box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  box.add(makeTitledPanel("BasicComboBoxUI#DefaultKeySelectionManager", JComboBox(makeModel())))
+  box.add(Box.createVerticalStrut(5))
+
+  val combo1 = object : JComboBox<String>(makeModel()) {
+    override fun selectWithKeyChar(keyChar: Char) = false
+  }
+  box.add(makeTitledPanel("disable JComboBox#selectWithKeyChar(...)", combo1))
+  box.add(Box.createVerticalStrut(5))
+
+  val combo2 = JComboBox(makeModel())
+  combo2.keySelectionManager = KeySelectionManager { _, _ -> -1 }
+  box.add(makeTitledPanel("disable KeySelectionManager#selectionForKey(...)", combo2))
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c)
+  return p
+}
+
+private fun makeModel(): ComboBoxModel<String> {
+  val data = arrayOf(
+    "a", "ab", "abc", "b1", "b2", "b3"
+  )
+  return DefaultComboBoxModel(data)
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
