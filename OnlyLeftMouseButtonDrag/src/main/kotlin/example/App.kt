4278811edc1806e9487190eca525695e26016b97
aterai <aterai@outlook.com>
2020-04-11 16:37:41 +0900

    add: OnlyLeftMouseButtonDrag

---
+++ OnlyLeftMouseButtonDrag/src/main/kotlin/example/App.kt
@@ -0,0 +1,117 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val model = DefaultBoundedRangeModel(50, 0, 0, 100)
+  val slider1 = JSlider(SwingConstants.VERTICAL)
+  val slider2 = JSlider(SwingConstants.HORIZONTAL)
+  listOf(slider1, slider2).forEach {
+    it.model = model
+    it.majorTickSpacing = 20
+    it.minorTickSpacing = 10
+    it.paintTicks = true
+    it.paintLabels = true
+  }
+  val key = "Slider.onlyLeftMouseButtonDrag"
+  val check = object : JCheckBox(key) {
+    override fun updateUI() {
+      super.updateUI()
+      isSelected = UIManager.getLookAndFeelDefaults().getBoolean(key)
+    }
+  }
+  check.addActionListener { e ->
+    val f = (e.source as? JCheckBox)?.isSelected == true
+    UIManager.put(key, f)
+  }
+
+  val box1 = Box.createHorizontalBox()
+  box1.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
+  box1.add(slider1)
+  box1.add(Box.createHorizontalGlue())
+
+  val box2 = Box.createVerticalBox()
+  box2.border = BorderFactory.createEmptyBorder(20, 0, 20, 20)
+  box2.add(slider2)
+  box2.add(Box.createVerticalGlue())
+
+  val mb = JMenuBar()
+  mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+  SwingUtilities.invokeLater { box1.rootPane.jMenuBar = mb }
+
+  return JPanel(BorderLayout()).also {
+    it.add(box1, BorderLayout.WEST)
+    it.add(box2)
+    it.add(check, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+// @see https://java.net/projects/swingset3/sources/svn/content/trunk/SwingSet3/src/com/sun/swingset3/SwingSet3.java
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(lafName: String, lafClassName: String, lafRadioGroup: ButtonGroup): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.actionCommand = lafClassName
+    lafItem.hideActionText = true
+    lafItem.addActionListener {
+      val m = lafRadioGroup.selection
+      runCatching {
+        setLookAndFeel(m.actionCommand)
+      }.onFailure {
+        it.printStackTrace()
+        Toolkit.getDefaultToolkit().beep()
+      }
+    }
+    lafRadioGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+      // firePropertyChange("lookAndFeel", oldLookAndFeel, lookAndFeel)
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
