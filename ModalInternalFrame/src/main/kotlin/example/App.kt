afff60bf53417e27c800096197314d594de6111b
aterai <aterai@outlook.com>
2020-04-14 16:54:29 +0900

    refactor: use of setter method instead of property access syntax

--- ModalInternalFrame/src/main/kotlin/example/App.kt
+++ ModalInternalFrame/src/main/kotlin/example/App.kt
@@ -18,21 +18,21 @@ class MainPanel : JPanel(BorderLayout()) {
 
   init {
     val button = JButton(ModalInternalFrameAction3("Show"))
-    button.setMnemonic(KeyEvent.VK_S)
+    button.mnemonic = KeyEvent.VK_S
 
     val internal = JInternalFrame("Button")
-    internal.getContentPane().add(button)
+    internal.contentPane.add(button)
     internal.setSize(100, 100)
     internal.setLocation(20, 20)
-    internal.setVisible(true)
+    internal.isVisible = true
     desktop.add(internal)
 
     dummyBar.add(JMenu("Frame"))
     add(dummyBar, BorderLayout.NORTH)
-    dummyBar.setVisible(false)
+    dummyBar.isVisible = false
 
     val menu = JMenu("Frame")
-    menu.setMnemonic(KeyEvent.VK_F)
+    menu.mnemonic = KeyEvent.VK_F
     menuBar.add(menu)
 
     var menuItem = JMenuItem(object : AbstractAction("New Frame") {
@@ -42,34 +42,34 @@ class MainPanel : JPanel(BorderLayout()) {
         frame.setSize(130, 100)
         frame.setLocation(30 * openFrameCount, 30 * openFrameCount)
         desktop.add(frame)
-        frame.setVisible(true)
+        frame.isVisible = true
         openFrameCount++
       }
     })
-    menuItem.setMnemonic(KeyEvent.VK_N)
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.ALT_DOWN_MASK))
+    menuItem.mnemonic = KeyEvent.VK_N
+    menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.ALT_DOWN_MASK)
     menu.add(menuItem)
 
     menu.addSeparator()
 
     menuItem = menu.add(ModalInternalFrameAction1("InternalMessageDialog(Normal)"))
-    menuItem.setMnemonic(KeyEvent.VK_1)
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.ALT_DOWN_MASK))
+    menuItem.mnemonic = KeyEvent.VK_1
+    menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_1, InputEvent.ALT_DOWN_MASK)
 
     menuItem = menu.add(ModalInternalFrameAction2("InternalMessageDialog"))
-    menuItem.setMnemonic(KeyEvent.VK_2)
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_2, InputEvent.ALT_DOWN_MASK))
+    menuItem.mnemonic = KeyEvent.VK_2
+    menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_2, InputEvent.ALT_DOWN_MASK)
 
     menuItem = menu.add(ModalInternalFrameAction3("InternalMessageDialog(Print)"))
-    menuItem.setMnemonic(KeyEvent.VK_3)
-    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_3, InputEvent.ALT_DOWN_MASK))
+    menuItem.mnemonic = KeyEvent.VK_3
+    menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_3, InputEvent.ALT_DOWN_MASK)
 
     val b = JButton(DefaultEditorKit.BeepAction())
-    b.setMnemonic(KeyEvent.VK_B)
+    b.mnemonic = KeyEvent.VK_B
     add(b, BorderLayout.SOUTH)
     add(desktop)
-    EventQueue.invokeLater { getRootPane().setJMenuBar(menuBar) }
-    setPreferredSize(Dimension(320, 240))
+    EventQueue.invokeLater { rootPane.jMenuBar = menuBar }
+    preferredSize = Dimension(320, 240)
   }
 
   // menuItem = new JMenuItem(new ModalInternalFrameAction1("InternalMessageDialog(Normal)"))
@@ -88,19 +88,19 @@ class MainPanel : JPanel(BorderLayout()) {
     private val glass = MyGlassPane()
 
     init {
-      glass.setOpaque(false)
-      glass.setVisible(false)
+      glass.isOpaque = false
+      glass.isVisible = false
       desktop.add(glass, JLayeredPane.MODAL_LAYER)
     }
 
     override fun actionPerformed(e: ActionEvent) {
       setJMenuEnabled(false)
       val w = SwingUtilities.getWindowAncestor(desktop)
-      val screen = w.getGraphicsConfiguration().getBounds()
+      val screen = w.graphicsConfiguration.bounds
       glass.setSize(screen.width, screen.height)
-      glass.setVisible(true)
+      glass.isVisible = true
       JOptionPane.showInternalMessageDialog(desktop, "information", "modal2", JOptionPane.INFORMATION_MESSAGE)
-      glass.setVisible(false)
+      glass.isVisible = false
       setJMenuEnabled(true)
     }
   }
@@ -113,7 +113,7 @@ class MainPanel : JPanel(BorderLayout()) {
     private val glass = PrintGlassPane()
 
     init {
-      glass.setVisible(false)
+      glass.isVisible = false
     }
 
     override fun actionPerformed(e: ActionEvent) {
@@ -121,13 +121,13 @@ class MainPanel : JPanel(BorderLayout()) {
       // TEST: UIManager.put("InternalFrame.titleButtonToolTipsOn", false)
       val modal = optionPane.createInternalFrame(desktop, "modal3")
       // TEST: UIManager.put("InternalFrame.titleButtonToolTipsOn", true)
-      optionPane.setMessage("Hello, World")
-      optionPane.setMessageType(JOptionPane.INFORMATION_MESSAGE)
+      optionPane.message = "Hello, World"
+      optionPane.messageType = JOptionPane.INFORMATION_MESSAGE
       removeSystemMenuListener(modal)
       modal.addInternalFrameListener(object : InternalFrameAdapter() {
         override fun internalFrameClosed(e: InternalFrameEvent) {
           glass.removeAll()
-          glass.setVisible(false)
+          glass.isVisible = false
         }
       })
       glass.add(modal)
@@ -135,27 +135,27 @@ class MainPanel : JPanel(BorderLayout()) {
       // val screen = desktop.getBounds();
       // modal.setLocation(screen.x + screen.width / 2 - modal.getSize().width / 2,
       //                   screen.y + screen.height / 2 - modal.getSize().height / 2)
-      getRootPane().setGlassPane(glass)
-      glass.setVisible(true)
-      modal.setVisible(true)
+      rootPane.glassPane = glass
+      glass.isVisible = true
+      modal.isVisible = true
     }
   }
 
   private fun setJMenuEnabled(flag: Boolean) {
-    val bar = getRootPane().getJMenuBar()
-    bar.setVisible(flag)
-    dummyBar.setVisible(!flag)
+    val bar = rootPane.jMenuBar
+    bar.isVisible = flag
+    dummyBar.isVisible = !flag
   }
 
   private fun removeSystemMenuListener(modal: JInternalFrame) {
-    val ui = modal.getUI() as? BasicInternalFrameUI ?: return
-    ui.getNorthPane().getComponents()
-      .filter { it is JLabel || "InternalFrameTitlePane.menuButton" == it.getName() }
+    val ui = modal.ui as? BasicInternalFrameUI ?: return
+    ui.northPane.components
+      .filter { it is JLabel || "InternalFrameTitlePane.menuButton" == it.name }
       .forEach { removeComponentMouseListener(it) }
   }
 
   private fun removeComponentMouseListener(c: Component) {
-    for (ml in c.getMouseListeners()) {
+    for (ml in c.mouseListeners) {
       (c as? JComponent)?.removeMouseListener(ml)
     }
   }
@@ -164,15 +164,15 @@ class MainPanel : JPanel(BorderLayout()) {
 class MyGlassPane : JDesktopPane() {
   override fun updateUI() {
     super.updateUI()
-    setFocusTraversalPolicy(object : DefaultFocusTraversalPolicy() {
+    focusTraversalPolicy = object : DefaultFocusTraversalPolicy() {
       override fun accept(c: Component) = false
-    })
+    }
   }
 
   override fun paintComponent(g: Graphics) {
     val g2 = g.create() as? Graphics2D ?: return
-    g2.setPaint(TEXTURE)
-    g2.fillRect(0, 0, getWidth(), getHeight())
+    g2.paint = TEXTURE
+    g2.fillRect(0, 0, width, height)
     g2.dispose()
   }
 
@@ -185,18 +185,18 @@ class PrintGlassPane : JDesktopPane() {
   override fun setVisible(isVisible: Boolean) {
     val oldVisible = isVisible()
     super.setVisible(isVisible)
-    getRootPane()?.takeIf { isVisible() != oldVisible }?.getLayeredPane()?.setVisible(!isVisible)
+    rootPane?.takeIf { isVisible() != oldVisible }?.layeredPane?.isVisible = !isVisible
   }
 
   override fun paintComponent(g: Graphics) {
     // http://weblogs.java.net/blog/alexfromsun/archive/2008/01/disabling_swing.html
     // it is important to call print() instead of paint() here
     // because print() doesn't affect the frame's double buffer
-    getRootPane()?.getLayeredPane()?.print(g)
+    rootPane?.layeredPane?.print(g)
 
     val g2 = g.create() as? Graphics2D ?: return
-    g2.setPaint(TEXTURE)
-    g2.fillRect(0, 0, getWidth(), getHeight())
+    g2.paint = TEXTURE
+    g2.fillRect(0, 0, width, height)
     g2.dispose()
   }
 
@@ -213,7 +213,7 @@ object TextureUtils {
     val size = cs * cs
     val img = BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
     val g2 = img.createGraphics()
-    g2.setPaint(color)
+    g2.paint = color
     g2.fillRect(0, 0, size, size)
     var i = 0
     while (i * cs < size) {
