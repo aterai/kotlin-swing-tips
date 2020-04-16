378f4bda1d6e9a6942c60f1d0d414a42b4fb148f
aterai <aterai@outlook.com>
2020-04-16 22:06:39 +0900

    refactor: use of setter method instead of property access syntax

--- BalloonToolTip/src/main/kotlin/example/App.kt
+++ BalloonToolTip/src/main/kotlin/example/App.kt
@@ -7,95 +7,95 @@ import java.awt.geom.Area
 import java.awt.geom.RoundRectangle2D
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(GridLayout(1, 2)) {
-  init {
-    val model = DefaultListModel<String>()
-    model.addElement("ABC DEF GHI JKL MNO PQR STU VWX YZ")
-    model.addElement("111")
-    model.addElement("111222")
-    model.addElement("111222333")
-    model.addElement("1234567890 abc def ghi jkl mno pqr stu vwx yz")
-    model.addElement("bbb1")
-    model.addElement("bbb12")
-    model.addElement("1234567890-+*/=ABC DEF GHI JKL MNO PQR STU VWX YZ")
-    model.addElement("bbb123")
+fun makeUI(): Component {
+  val model = DefaultListModel<String>()
+  model.addElement("ABC DEF GHI JKL MNO PQR STU VWX YZ")
+  model.addElement("111")
+  model.addElement("111222")
+  model.addElement("111222333")
+  model.addElement("1234567890 abc def ghi jkl mno pqr stu vwx yz")
+  model.addElement("bbb1")
+  model.addElement("bbb12")
+  model.addElement("1234567890-+*/=ABC DEF GHI JKL MNO PQR STU VWX YZ")
+  model.addElement("bbb123")
 
-    val list1 = object : JList<String>(model) {
-      override fun createToolTip(): JToolTip {
-        val tip = BalloonToolTip()
-        tip.setComponent(this)
-        return tip
-      }
-
-      override fun updateUI() {
-        setCellRenderer(null)
-        super.updateUI()
-        val renderer = DefaultListCellRenderer()
-        setCellRenderer { list, value, index, isSelected, cellHasFocus ->
-          val c = renderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
-          (SwingUtilities.getAncestorOfClass(JViewport::class.java, list) as? JViewport)?.also {
-            val rect = SwingUtilities.calculateInnerArea(it, it.getBounds())
-            val fm = c.getFontMetrics(c.getFont())
-            val str = value?.toString() ?: ""
-            (c as? JComponent)?.setToolTipText(if (fm.stringWidth(str) > rect.width) str else null)
-          }
-          return@setCellRenderer c
-        }
-      }
+  val list1 = object : JList<String>(model) {
+    override fun createToolTip(): JToolTip {
+      val tip = BalloonToolTip()
+      tip.component = this
+      return tip
     }
 
-    val list2 = object : JList<String>(model) {
-      override fun updateUI() {
-        setCellRenderer(null)
-        super.updateUI()
-        val renderer = DefaultListCellRenderer()
-        setCellRenderer { list, value, index, isSelected, cellHasFocus ->
-          val c = renderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
-          (SwingUtilities.getAncestorOfClass(JViewport::class.java, list) as? JViewport)?.also {
-            val rect = SwingUtilities.calculateInnerArea(it, it.getBounds())
-            val fm = c.getFontMetrics(c.getFont())
-            val str = value?.toString() ?: ""
-            (c as? JComponent)?.setToolTipText(if (fm.stringWidth(str) > rect.width) str else null)
-          }
-          return@setCellRenderer c
+    override fun updateUI() {
+      cellRenderer = null
+      super.updateUI()
+      val renderer = DefaultListCellRenderer()
+      setCellRenderer { list, value, index, isSelected, cellHasFocus ->
+        val c = renderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+        (SwingUtilities.getAncestorOfClass(JViewport::class.java, list) as? JViewport)?.also {
+          val rect = SwingUtilities.calculateInnerArea(it, it.bounds)
+          val fm = c.getFontMetrics(c.font)
+          val str = value?.toString() ?: ""
+          (c as? JComponent)?.toolTipText = if (fm.stringWidth(str) > rect.width) str else null
         }
+        return@setCellRenderer c
       }
     }
-
-    add(makeTitledPanel("BalloonToolTip", list1))
-    add(makeTitledPanel("Default JToolTip", list2))
-    setPreferredSize(Dimension(320, 240))
   }
 
-  private fun makeTitledPanel(title: String, c: Component): Component {
-    val scroll = JScrollPane(c)
-    scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER)
-    val p = JPanel(BorderLayout())
-    p.setBorder(BorderFactory.createTitledBorder(title))
-    p.add(scroll)
-    return p
+  val list2 = object : JList<String>(model) {
+    override fun updateUI() {
+      cellRenderer = null
+      super.updateUI()
+      val renderer = DefaultListCellRenderer()
+      setCellRenderer { list, value, index, isSelected, cellHasFocus ->
+        val c = renderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+        (SwingUtilities.getAncestorOfClass(JViewport::class.java, list) as? JViewport)?.also {
+          val rect = SwingUtilities.calculateInnerArea(it, it.bounds)
+          val fm = c.getFontMetrics(c.font)
+          val str = value?.toString() ?: ""
+          (c as? JComponent)?.toolTipText = if (fm.stringWidth(str) > rect.width) str else null
+        }
+        return@setCellRenderer c
+      }
+    }
+  }
+
+  return JPanel(GridLayout(1, 2)).also {
+    it.add(makeTitledPanel("BalloonToolTip", list1))
+    it.add(makeTitledPanel("Default JToolTip", list2))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class BalloonToolTip : JToolTip() {
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val scroll = JScrollPane(c)
+  scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(scroll)
+  return p
+}
+
+private class BalloonToolTip : JToolTip() {
   private var listener: HierarchyListener? = null
 
   override fun updateUI() {
     removeHierarchyListener(listener)
     super.updateUI()
     listener = HierarchyListener { e ->
-      val c = e.getComponent()
-      if (e.getChangeFlags().toInt() and HierarchyEvent.SHOWING_CHANGED != 0 && c.isShowing()) {
+      val c = e.component
+      if (e.changeFlags.toInt() and HierarchyEvent.SHOWING_CHANGED != 0 && c.isShowing) {
         // (SwingUtilities.getRoot(c) as? JWindow)?.also {
         //   println("Popup\$HeavyWeightWindow")
         //   it.setBackground(Color(0x0, true))
         // }
-        (SwingUtilities.getRoot(c) as? JWindow)?.setBackground(Color(0x0, true))
+        (SwingUtilities.getRoot(c) as? JWindow)?.background = Color(0x0, true)
       }
     }
     addHierarchyListener(listener)
-    setOpaque(false)
-    setBorder(BorderFactory.createEmptyBorder(8, 5, 0, 5))
+    isOpaque = false
+    border = BorderFactory.createEmptyBorder(8, 5, 0, 5)
     // setForeground(Color.WHITE)
     // setBackground(Color(0xEF_64_64_64.toInt(), true))
   }
@@ -106,27 +106,30 @@ class BalloonToolTip : JToolTip() {
     val s = makeBalloonShape()
     val g2 = g.create() as? Graphics2D ?: return
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-    g2.setColor(getBackground())
+    g2.color = background
     g2.fill(s)
-    g2.setColor(getForeground())
+    g2.color = foreground
     g2.draw(s)
     g2.dispose()
     super.paintComponent(g)
   }
 
   private fun makeBalloonShape(): Shape {
-    val i = getInsets()
-    val w = getWidth() - 1
-    val h = getHeight() - 1
+    val i = insets
+    val w = width - 1
+    val h = height - 1
     val v = i.top / 2
     val triangle = Polygon()
     triangle.addPoint(i.left + v + v, 0)
     triangle.addPoint(i.left + v, v)
     triangle.addPoint(i.left + v + v + v, v)
-    val area = Area(RoundRectangle2D.Float(
-      0f, v.toFloat(),
-      w.toFloat(), (h - i.bottom - v).toFloat(),
-      i.top.toFloat(), i.top.toFloat()))
+    val area = Area(
+      RoundRectangle2D.Float(
+        0f, v.toFloat(),
+        w.toFloat(), (h - i.bottom - v).toFloat(),
+        i.top.toFloat(), i.top.toFloat()
+      )
+    )
     area.add(Area(triangle))
     return area
   }
@@ -142,7 +145,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
