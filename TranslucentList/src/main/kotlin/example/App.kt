18cbe9f4158615bbc3ad73b656295bb44a4a9038
aterai <aterai@outlook.com>
2020-04-19 03:27:20 +0900

    refactor: use of setter method instead of property access syntax

--- TranslucentList/src/main/kotlin/example/App.kt
+++ TranslucentList/src/main/kotlin/example/App.kt
@@ -8,79 +8,77 @@ import java.awt.image.BufferedImage
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.border.Border
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val model = DefaultListModel<ListItem>().also {
-      it.addElement(ListItem("red", ColorIcon(Color.RED)))
-      it.addElement(ListItem("green", ColorIcon(Color.GREEN)))
-      it.addElement(ListItem("blue", ColorIcon(Color.BLUE)))
-      it.addElement(ListItem("cyan", ColorIcon(Color.CYAN)))
-      it.addElement(ListItem("darkGray", ColorIcon(Color.DARK_GRAY)))
-      it.addElement(ListItem("gray", ColorIcon(Color.GRAY)))
-      it.addElement(ListItem("lightGray", ColorIcon(Color.LIGHT_GRAY)))
-      it.addElement(ListItem("magenta", ColorIcon(Color.MAGENTA)))
-      it.addElement(ListItem("orange", ColorIcon(Color.ORANGE)))
-      it.addElement(ListItem("pink", ColorIcon(Color.PINK)))
-      it.addElement(ListItem("yellow", ColorIcon(Color.YELLOW)))
-      it.addElement(ListItem("black", ColorIcon(Color.BLACK)))
-      it.addElement(ListItem("white", ColorIcon(Color.WHITE)))
-    }
+fun makeUI(): Component {
+  val model = DefaultListModel<ListItem>().also {
+    it.addElement(ListItem("red", ColorIcon(Color.RED)))
+    it.addElement(ListItem("green", ColorIcon(Color.GREEN)))
+    it.addElement(ListItem("blue", ColorIcon(Color.BLUE)))
+    it.addElement(ListItem("cyan", ColorIcon(Color.CYAN)))
+    it.addElement(ListItem("darkGray", ColorIcon(Color.DARK_GRAY)))
+    it.addElement(ListItem("gray", ColorIcon(Color.GRAY)))
+    it.addElement(ListItem("lightGray", ColorIcon(Color.LIGHT_GRAY)))
+    it.addElement(ListItem("magenta", ColorIcon(Color.MAGENTA)))
+    it.addElement(ListItem("orange", ColorIcon(Color.ORANGE)))
+    it.addElement(ListItem("pink", ColorIcon(Color.PINK)))
+    it.addElement(ListItem("yellow", ColorIcon(Color.YELLOW)))
+    it.addElement(ListItem("black", ColorIcon(Color.BLACK)))
+    it.addElement(ListItem("white", ColorIcon(Color.WHITE)))
+  }
 
-    val list = RubberBandSelectionList(model).also {
-      it.setOpaque(false)
-      it.setBackground(Color(0x0, true))
-      it.setForeground(Color.WHITE)
-      // it.addListSelectionListener { e -> SwingUtilities.getUnwrappedParent(e.getSource() as Component).repaint() }
-    }
+  val list = RubberBandSelectionList(model).also {
+    it.isOpaque = false
+    it.background = Color(0x0, true)
+    it.foreground = Color.WHITE
+  }
 
-    val scroll = JScrollPane(list).also {
-      it.setBackground(Color(0x0, true))
-      it.setOpaque(false)
-      it.setBorder(BorderFactory.createEmptyBorder())
-      it.setViewportBorder(BorderFactory.createEmptyBorder())
-      // it.getViewport().addChangeListener { e -> (e.getSource() as Component).repaint() }
-      it.getViewport().setOpaque(false)
-    }
+  val scroll = JScrollPane(list).also {
+    it.background = Color(0x0, true)
+    it.isOpaque = false
+    it.border = BorderFactory.createEmptyBorder()
+    it.viewportBorder = BorderFactory.createEmptyBorder()
+    it.viewport.isOpaque = false
+  }
 
-    val panel = object : JPanel(BorderLayout()) {
-      private val texture = TextureUtils.createCheckerTexture(6)
-      override fun paintComponent(g: Graphics) {
-        val g2 = g.create() as Graphics2D
-        g2.setPaint(texture)
-        g2.fillRect(0, 0, getWidth(), getHeight())
-        g2.dispose()
-        super.paintComponent(g)
-      }
+  val panel = object : JPanel(BorderLayout()) {
+    private val texture = TextureUtils.createCheckerTexture(6)
+    override fun paintComponent(g: Graphics) {
+      val g2 = g.create() as? Graphics2D ?: return
+      g2.paint = texture
+      g2.fillRect(0, 0, width, height)
+      g2.dispose()
+      super.paintComponent(g)
     }
-    panel.setOpaque(false)
-    panel.add(scroll)
+  }
+  panel.isOpaque = false
+  panel.add(scroll)
 
-    add(panel)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(panel)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListItem>(model) {
+private class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListItem>(model) {
   private var rbl: RubberBandingListener? = null
   private var rubberBandColor: Color? = null
   private val rubberBand = Path2D.Double()
 
   override fun updateUI() {
-    setSelectionForeground(null) // Nimbus
-    setSelectionBackground(null) // Nimbus
-    setCellRenderer(null)
+    selectionForeground = null // Nimbus
+    selectionBackground = null // Nimbus
+    cellRenderer = null
     removeMouseListener(rbl)
     removeMouseMotionListener(rbl)
     super.updateUI()
 
-    rubberBandColor = makeRubberBandColor(getSelectionBackground())
-    setLayoutOrientation(HORIZONTAL_WRAP)
-    setVisibleRowCount(0)
-    setFixedCellWidth(62)
-    setFixedCellHeight(62)
-    setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10))
+    rubberBandColor = makeRubberBandColor(selectionBackground)
+    layoutOrientation = HORIZONTAL_WRAP
+    visibleRowCount = 0
+    fixedCellWidth = 62
+    fixedCellHeight = 62
+    border = BorderFactory.createEmptyBorder(5, 10, 5, 10)
 
-    setCellRenderer(ListItemListCellRenderer())
+    cellRenderer = ListItemListCellRenderer()
     rbl = RubberBandingListener()
     addMouseMotionListener(rbl)
     addMouseListener(rbl)
@@ -88,11 +86,11 @@ internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListI
 
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
-    val g2 = g.create() as Graphics2D
-    g2.setPaint(getSelectionBackground())
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.paint = selectionBackground
     g2.draw(rubberBand)
-    g2.setComposite(ALPHA)
-    g2.setPaint(rubberBandColor)
+    g2.composite = ALPHA
+    g2.paint = rubberBandColor
     g2.fill(rubberBand)
     g2.dispose()
   }
@@ -101,9 +99,9 @@ internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListI
     private val srcPoint = Point()
 
     override fun mouseDragged(e: MouseEvent) {
-      val l = e.getComponent() as? JList<*> ?: return
-      l.setFocusable(true)
-      val destPoint = e.getPoint()
+      val l = e.component as? JList<*> ?: return
+      l.isFocusable = true
+      val destPoint = e.point
       rubberBand.reset()
       rubberBand.moveTo(srcPoint.getX(), srcPoint.getY())
       rubberBand.lineTo(destPoint.getX(), srcPoint.getY())
@@ -111,32 +109,32 @@ internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListI
       rubberBand.lineTo(srcPoint.getX(), destPoint.getY())
       rubberBand.closePath()
 
-      val indices = (0 until l.getModel().getSize())
+      val indices = (0 until l.model.size)
         .filter { rubberBand.intersects(l.getCellBounds(it, it)) }.toIntArray()
-      l.setSelectedIndices(indices)
+      l.selectedIndices = indices
       l.repaint()
     }
 
     override fun mouseReleased(e: MouseEvent) {
       rubberBand.reset()
-      val c = e.getComponent()
-      c.setFocusable(true)
+      val c = e.component
+      c.isFocusable = true
       c.repaint()
     }
 
     override fun mousePressed(e: MouseEvent) {
-      val l = e.getComponent() as? JList<*> ?: return
-      val index = l.locationToIndex(e.getPoint())
+      val l = e.component as? JList<*> ?: return
+      val index = l.locationToIndex(e.point)
       val rect = l.getCellBounds(index, index)
-      if (rect.contains(e.getPoint())) {
+      if (rect.contains(e.point)) {
         l.setFocusable(true)
       } else {
         l.clearSelection()
-        l.getSelectionModel().setAnchorSelectionIndex(-1)
-        l.getSelectionModel().setLeadSelectionIndex(-1)
+        l.selectionModel.anchorSelectionIndex = -1
+        l.selectionModel.leadSelectionIndex = -1
         l.setFocusable(false)
       }
-      srcPoint.setLocation(e.getPoint())
+      srcPoint.location = e.point
       l.repaint()
     }
   }
@@ -145,9 +143,9 @@ internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListI
     private val ALPHA = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .1f)
 
     private fun makeRubberBandColor(c: Color): Color {
-      val r = c.getRed()
-      val g = c.getGreen()
-      val b = c.getBlue()
+      val r = c.red
+      val g = c.green
+      val b = c.blue
       return when {
         r > g -> if (r > b) Color(r, 0, 0) else Color(0, 0, b)
         else -> if (g > b) Color(0, g, 0) else Color(0, 0, b)
@@ -156,14 +154,14 @@ internal class RubberBandSelectionList(model: ListModel<ListItem>) : JList<ListI
   }
 }
 
-class ListItemListCellRenderer : ListCellRenderer<ListItem> {
+private class ListItemListCellRenderer : ListCellRenderer<ListItem> {
   private val label = object : JLabel("", null, SwingConstants.CENTER) {
     override fun paintComponent(g: Graphics) {
       super.paintComponent(g)
-      if (SELECTED_COLOR == getBackground()) {
-        val g2 = g.create() as Graphics2D
-        g2.setPaint(SELECTED_COLOR)
-        g2.fillRect(0, 0, getWidth(), getHeight())
+      if (SELECTED_COLOR == background) {
+        val g2 = g.create() as? Graphics2D ?: return
+        g2.paint = SELECTED_COLOR
+        g2.fillRect(0, 0, width, height)
         g2.dispose()
       }
     }
@@ -173,15 +171,15 @@ class ListItemListCellRenderer : ListCellRenderer<ListItem> {
   private val noFocusBorder = UIManager.getBorder("List.noFocusBorder") ?: getNimbusNoFocusBorder()
 
   init {
-    label.setVerticalTextPosition(SwingConstants.BOTTOM)
-    label.setHorizontalTextPosition(SwingConstants.CENTER)
-    label.setForeground(renderer.getForeground())
-    label.setBackground(renderer.getBackground())
-    label.setBorder(noFocusBorder)
-    label.setOpaque(false)
-    renderer.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
+    label.verticalTextPosition = SwingConstants.BOTTOM
+    label.horizontalTextPosition = SwingConstants.CENTER
+    label.foreground = renderer.foreground
+    label.background = renderer.background
+    label.border = noFocusBorder
+    label.isOpaque = false
+    renderer.border = BorderFactory.createEmptyBorder(2, 2, 2, 2)
     renderer.add(label)
-    renderer.setOpaque(false)
+    renderer.isOpaque = false
   }
 
   private fun getNimbusNoFocusBorder(): Border {
@@ -196,15 +194,15 @@ class ListItemListCellRenderer : ListCellRenderer<ListItem> {
     isSelected: Boolean,
     cellHasFocus: Boolean
   ): Component {
-    label.setText(value.title)
-    label.setBorder(if (cellHasFocus) focusBorder else noFocusBorder)
-    label.setIcon(value.icon)
+    label.text = value.title
+    label.border = if (cellHasFocus) focusBorder else noFocusBorder
+    label.icon = value.icon
     if (isSelected) {
-      label.setForeground(list.getSelectionForeground())
-      label.setBackground(SELECTED_COLOR)
+      label.foreground = list.selectionForeground
+      label.background = SELECTED_COLOR
     } else {
-      label.setForeground(list.getForeground())
-      label.setBackground(list.getBackground())
+      label.foreground = list.foreground
+      label.background = list.background
     }
     return renderer
   }
@@ -214,9 +212,9 @@ class ListItemListCellRenderer : ListCellRenderer<ListItem> {
   }
 }
 
-data class ListItem(val title: String, val icon: Icon)
+private data class ListItem(val title: String, val icon: Icon)
 
-object TextureUtils {
+private object TextureUtils {
   // private val DEFAULT_COLOR = Color(0xEE_32_32_32.toInt(), true)
   // private val DEFAULT_COLOR = Color(-0x11CDCDCE, true)
   private val DEFAULT_COLOR = Color(0x32, 0x32, 0x32, 0xEE)
@@ -226,7 +224,7 @@ object TextureUtils {
     val size = cs * cs
     val img = BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
     val g2 = img.createGraphics()
-    g2.setPaint(color)
+    g2.paint = color
     g2.fillRect(0, 0, size, size)
     var i = 0
     while (i * cs < size) {
@@ -242,14 +240,14 @@ object TextureUtils {
     g2.dispose()
     return TexturePaint(img, Rectangle(size, size))
   }
-} /* HideUtilityClassConstructor */
+}
 
-class ColorIcon(private val color: Color) : Icon {
+private class ColorIcon(private val color: Color) : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    val g2 = g.create() as Graphics2D
+    val g2 = g.create() as? Graphics2D ?: return
     g2.translate(x, y)
-    g2.setPaint(color)
-    g2.fillRect(0, 0, getIconWidth(), getIconHeight())
+    g2.paint = color
+    g2.fillRect(0, 0, iconWidth, iconHeight)
     g2.dispose()
   }
 
@@ -268,7 +266,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
