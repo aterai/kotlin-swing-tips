5cf7075f97759385c2e912a647972bac29281737
aterai <aterai@outlook.com>
2020-04-18 16:39:44 +0900

    ktlint: Needless blank line

--- CellEditorFocusCycle/src/main/kotlin/example/App.kt
+++ CellEditorFocusCycle/src/main/kotlin/example/App.kt
@@ -11,7 +11,6 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 
-
 fun makeUI(): Component {
   val table = makeTable()
   val am = table.actionMap
