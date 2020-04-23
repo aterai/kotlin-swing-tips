54604a6514d264fdf07c24d6f7f7dfbc4ff00325
aterai <aterai@outlook.com>
2020-04-23 15:17:36 +0900

    refactor: use of setter method instead of property access syntax

--- ScrollBeyondLastLine/src/main/kotlin/example/App.kt
+++ ScrollBeyondLastLine/src/main/kotlin/example/App.kt
@@ -7,33 +7,33 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val textArea = object : JTextArea() {
-      // https://stackoverflow.com/questions/32679335/java-jtextarea-allow-scrolling-beyond-end-of-text
-      override fun getPreferredSize(): Dimension {
-        val d = super.getPreferredSize()
-        val c = SwingUtilities.getAncestorOfClass(JScrollPane::class.java, this)
-        if (c is JScrollPane && isEditable()) {
-          val r = c.getViewportBorderBounds()
-          d.height += r.height - getRowHeight() - getInsets().bottom
-        }
-        return d
+fun makeUI(): Component {
+  val textArea = object : JTextArea() {
+    // https://stackoverflow.com/questions/32679335/java-jtextarea-allow-scrolling-beyond-end-of-text
+    override fun getPreferredSize(): Dimension {
+      val d = super.getPreferredSize()
+      val c = SwingUtilities.getAncestorOfClass(JScrollPane::class.java, this)
+      if (c is JScrollPane && isEditable) {
+        val r = c.viewportBorderBounds
+        d.height += r.height - rowHeight - insets.bottom
       }
+      return d
     }
-    textArea.setText("1111111111\n222222222222\n\n\n\n\n\n\n\n\n\n333333333333333")
-    textArea.setCaretPosition(0)
-    textArea.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0))
+  }
+  textArea.text = "1111111111\n222222222222\n\n\n\n\n\n\n\n\n\n333333333333333"
+  textArea.caretPosition = 0
+  textArea.border = BorderFactory.createEmptyBorder(0, 2, 0, 0)
 
-    val scroll = JScrollPane(textArea)
-    scroll.setRowHeaderView(LineNumberView(textArea))
+  val scroll = JScrollPane(textArea)
+  scroll.setRowHeaderView(LineNumberView(textArea))
 
-    val check = JCheckBox("editable", true)
-    check.addActionListener { textArea.setEditable(check.isSelected()) }
+  val check = JCheckBox("editable", true)
+  check.addActionListener { textArea.isEditable = check.isSelected }
 
-    add(scroll)
-    add(check, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(check, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
@@ -47,14 +47,14 @@ class LineNumberView(private val textArea: JTextArea) : JComponent() {
   private val fontLeading: Int
 
   init {
-    val font = textArea.getFont()
+    val font = textArea.font
     fontMetrics = getFontMetrics(font)
-    fontHeight = fontMetrics.getHeight()
-    fontAscent = fontMetrics.getAscent()
-    fontDescent = fontMetrics.getDescent()
-    fontLeading = fontMetrics.getLeading()
+    fontHeight = fontMetrics.height
+    fontAscent = fontMetrics.ascent
+    fontDescent = fontMetrics.descent
+    fontLeading = fontMetrics.leading
 
-    textArea.getDocument().addDocumentListener(object : DocumentListener {
+    textArea.document.addDocumentListener(object : DocumentListener {
       override fun insertUpdate(e: DocumentEvent) {
         repaint()
       }
@@ -73,35 +73,35 @@ class LineNumberView(private val textArea: JTextArea) : JComponent() {
         repaint()
       }
     })
-    val i = textArea.getInsets()
-    setBorder(BorderFactory.createCompoundBorder(
+    val i = textArea.insets
+    border = BorderFactory.createCompoundBorder(
       BorderFactory.createMatteBorder(0, 0, 0, 1, Color.GRAY),
-      BorderFactory.createEmptyBorder(i.top, MARGIN, i.bottom, MARGIN - 1)))
-    setOpaque(true)
-    setBackground(Color.WHITE)
+      BorderFactory.createEmptyBorder(i.top, MARGIN, i.bottom, MARGIN - 1))
+    isOpaque = true
+    background = Color.WHITE
     setFont(font)
   }
 
   private fun getComponentWidth(): Int {
-    val lineCount = textArea.getLineCount()
+    val lineCount = textArea.lineCount
     val maxDigits = maxOf(3, lineCount.toString().length)
-    val i = getInsets()
+    val i = insets
     return maxDigits * fontMetrics.stringWidth("0") + i.left + i.right
   }
 
-  override fun getPreferredSize() = Dimension(getComponentWidth(), textArea.getHeight())
+  override fun getPreferredSize() = Dimension(getComponentWidth(), textArea.height)
 
   override fun paintComponent(g: Graphics) {
-    g.setColor(getBackground())
-    val clip = g.getClipBounds()
+    g.color = background
+    val clip = g.clipBounds
     g.fillRect(clip.x, clip.y, clip.width, clip.height)
 
-    g.setColor(getForeground())
+    g.color = foreground
     val base = clip.y
     val start = getLineAtPoint(base)
     val end = getLineAtPoint(base + clip.height)
     var y = start * fontHeight
-    val rmg = getInsets().right
+    val rmg = insets.right
     for (i in start..end) {
       val text = (i + 1).toString()
       val x = getComponentWidth() - rmg - fontMetrics.stringWidth(text)
@@ -112,7 +112,7 @@ class LineNumberView(private val textArea: JTextArea) : JComponent() {
   }
 
   private fun getLineAtPoint(y: Int): Int {
-    val root = textArea.getDocument().getDefaultRootElement()
+    val root = textArea.document.defaultRootElement
     val pos = textArea.viewToModel(Point(0, y))
     return root.getElementIndex(pos)
   }
@@ -132,7 +132,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
