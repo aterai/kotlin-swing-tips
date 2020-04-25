33df161204b2471a75513dd6c3ec101a11777d7f
aterai <aterai@outlook.com>
2020-04-25 17:56:49 +0900

    refactor: use of setter method instead of property access syntax

--- WheelOverNestedScrollPane/src/main/kotlin/example/App.kt
+++ WheelOverNestedScrollPane/src/main/kotlin/example/App.kt
@@ -7,69 +7,67 @@ import javax.swing.plaf.LayerUI
 import javax.swing.table.DefaultTableModel
 import javax.swing.text.JTextComponent
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("String", "Integer", "Boolean")
-    val data = arrayOf(
-      arrayOf("aaa", 12, true), arrayOf("zzz", 6, false),
-      arrayOf("bbb", 22, true), arrayOf("nnn", 9, false),
-      arrayOf("ccc", 32, true), arrayOf("ooo", 8, false),
-      arrayOf("ddd", 42, true), arrayOf("ppp", 9, false),
-      arrayOf("eee", 52, true), arrayOf("qqq", 8, false),
-      arrayOf("fff", 62, true), arrayOf("rrr", 7, false),
-      arrayOf("ggg", 51, true), arrayOf("sss", 6, false),
-      arrayOf("hhh", 41, true), arrayOf("ttt", 5, false),
-      arrayOf("iii", 51, true), arrayOf("uuu", 4, false),
-      arrayOf("jjj", 61, true), arrayOf("vvv", 3, false),
-      arrayOf("kkk", 72, true), arrayOf("www", 2, false),
-      arrayOf("lll", 82, true), arrayOf("xxx", 1, false),
-      arrayOf("mmm", 92, true), arrayOf("yyy", 0, false)
-    )
-    val model: DefaultTableModel = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-    }
-    val table = JTable(model)
-    table.autoCreateRowSorter = true
-    val textPane = JTextPane()
-    textPane.isEditable = false
-    textPane.margin = Insets(5, 10, 5, 5)
-    val c: JTextComponent = JTextArea(TEXT)
-    c.isEditable = false
-    val doc = textPane.document
-    runCatching {
-      doc.insertString(doc.length, TEXT, null)
-      doc.insertString(doc.length, TEXT, null)
-      doc.insertString(doc.length, TEXT, null)
-      textPane.insertComponent(createChildScrollPane(c))
-      doc.insertString(doc.length, "\n", null)
-      doc.insertString(doc.length, TEXT, null)
-      textPane.insertComponent(createChildScrollPane(table))
-      doc.insertString(doc.length, "\n", null)
-      doc.insertString(doc.length, TEXT, null)
-      textPane.insertComponent(JScrollPane(JTree()))
-      doc.insertString(doc.length, "\n", null)
-      doc.insertString(doc.length, TEXT, null)
-    }
-    add(JLayer(JScrollPane(textPane), WheelScrollLayerUI()))
-    preferredSize = Dimension(320, 240)
+private const val TEXT = "aaa\na\na\na\na\naaa\na\na\na\naaa\n"
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true), arrayOf("zzz", 6, false),
+    arrayOf("bbb", 22, true), arrayOf("nnn", 9, false),
+    arrayOf("ccc", 32, true), arrayOf("ooo", 8, false),
+    arrayOf("ddd", 42, true), arrayOf("ppp", 9, false),
+    arrayOf("eee", 52, true), arrayOf("qqq", 8, false),
+    arrayOf("fff", 62, true), arrayOf("rrr", 7, false),
+    arrayOf("ggg", 51, true), arrayOf("sss", 6, false),
+    arrayOf("hhh", 41, true), arrayOf("ttt", 5, false),
+    arrayOf("iii", 51, true), arrayOf("uuu", 4, false),
+    arrayOf("jjj", 61, true), arrayOf("vvv", 3, false),
+    arrayOf("kkk", 72, true), arrayOf("www", 2, false),
+    arrayOf("lll", 82, true), arrayOf("xxx", 1, false),
+    arrayOf("mmm", 92, true), arrayOf("yyy", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
   }
-
-  private fun createChildScrollPane(view: Component): JScrollPane {
-    return object : JScrollPane(view) {
-      override fun getPreferredSize() = Dimension(240, 120)
-
-      override fun getMaximumSize() = super.getMaximumSize().also {
-        it.height = preferredSize.height
-      }
-    }
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
+  val textPane = JTextPane()
+  textPane.isEditable = false
+  textPane.margin = Insets(5, 10, 5, 5)
+  val c: JTextComponent = JTextArea(TEXT)
+  c.isEditable = false
+  val doc = textPane.document
+  runCatching {
+    doc.insertString(doc.length, TEXT, null)
+    doc.insertString(doc.length, TEXT, null)
+    doc.insertString(doc.length, TEXT, null)
+    textPane.insertComponent(createChildScrollPane(c))
+    doc.insertString(doc.length, "\n", null)
+    doc.insertString(doc.length, TEXT, null)
+    textPane.insertComponent(createChildScrollPane(table))
+    doc.insertString(doc.length, "\n", null)
+    doc.insertString(doc.length, TEXT, null)
+    textPane.insertComponent(JScrollPane(JTree()))
+    doc.insertString(doc.length, "\n", null)
+    doc.insertString(doc.length, TEXT, null)
   }
-
-  companion object {
-    private const val TEXT = "aaa\na\na\na\na\naaa\na\na\na\naaa\n"
+  return JPanel(BorderLayout()).also {
+    it.add(JLayer(JScrollPane(textPane), WheelScrollLayerUI()))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class WheelScrollLayerUI : LayerUI<JScrollPane>() {
+private fun createChildScrollPane(view: Component): JScrollPane {
+  return object : JScrollPane(view) {
+    override fun getPreferredSize() = Dimension(240, 120)
+
+    override fun getMaximumSize() = super.getMaximumSize().also {
+      it.height = preferredSize.height
+    }
+  }
+}
+
+private class WheelScrollLayerUI : LayerUI<JScrollPane>() {
   override fun installUI(c: JComponent) {
     super.installUI(c)
     if (c is JLayer<*>) {
@@ -113,7 +111,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
--- ZipFile/src/main/kotlin/example/App.kt
+++ ZipFile/src/main/kotlin/example/App.kt
@@ -19,133 +19,129 @@ import java.util.zip.ZipFile
 import java.util.zip.ZipOutputStream
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  private fun makeZipPanel(): Component {
-    val field = JTextField(20)
-    val button = JButton("select directory")
-    button.addActionListener {
-      val fileChooser = JFileChooser()
-      fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
-      val ret = fileChooser.showOpenDialog(button.rootPane)
-      if (ret == JFileChooser.APPROVE_OPTION) {
-        field.setText(fileChooser.getSelectedFile().getAbsolutePath())
-      }
-    }
-    val button1 = JButton("zip")
-    button1.addActionListener {
-      val str = field.getText()
-      val path = Paths.get(str)
-      // if (str.isEmpty() || Files.notExists(path)) { // noticeably poor performance in JDK 8
-      if (str.isEmpty() || !path.toFile().exists()) {
-        return@addActionListener
-      }
-      val name = path.getFileName().toString() + ".zip"
-      val tgt = path.resolveSibling(name)
-      // if (Files.exists(tgt)) { // noticeably poor performance in JDK 8
-      if (tgt.toFile().exists()) {
-        val m = "<html>$tgt already exists.<br>Do you want to overwrite it?"
-        val rv = JOptionPane.showConfirmDialog(getRootPane(), m, "Zip", JOptionPane.YES_NO_OPTION)
-        if (rv != JOptionPane.YES_OPTION) {
-          return@addActionListener
-        }
-      }
-      runCatching {
-        ZipUtil.zip(path, tgt)
-      }.onFailure {
-        LOGGER.info { "Cant zip! : $path" }
-        Toolkit.getDefaultToolkit().beep()
-      }
-    }
-    val p = JPanel(BorderLayout(5, 2))
-    p.setBorder(BorderFactory.createTitledBorder("Zip"))
-    p.add(field)
-    p.add(button, BorderLayout.EAST)
-    p.add(button1, BorderLayout.SOUTH)
-    return p
-  }
+private val logger = Logger.getLogger(MethodHandles.lookup().lookupClass().name)
 
-  private fun makeUnzipPanel(): Component {
-    val field = JTextField(20)
-    val button = JButton("select .zip file")
-    button.addActionListener {
-      val fileChooser = JFileChooser()
-      val ret = fileChooser.showOpenDialog(getRootPane())
-      if (ret == JFileChooser.APPROVE_OPTION) {
-        field.setText(fileChooser.getSelectedFile().getAbsolutePath())
-      }
-    }
-    val button1 = JButton("unzip")
-    button1.addActionListener {
-      val str = field.getText()
-      makeDestDirPath(str)?.also { destDir ->
-        val path = Paths.get(str)
-        runCatching {
-          // if (Files.exists(destDir)) { // noticeably poor performance in JDK 8
-          if (destDir.toFile().exists()) {
-            val m = "<html>$destDir already exists.<br>Do you want to overwrite it?"
-            val rv = JOptionPane.showConfirmDialog(getRootPane(), m, "Unzip", JOptionPane.YES_NO_OPTION)
-            if (rv != JOptionPane.YES_OPTION) {
-              return@addActionListener
-            }
-          } else {
-            LOGGER.info { "mkdir0: $destDir" }
-            Files.createDirectories(destDir)
-          }
-          ZipUtil.unzip(path, destDir)
-        }.onFailure {
-          LOGGER.info { "Cant unzip! : $path" }
-          Toolkit.getDefaultToolkit().beep()
-        }
-      }
-    }
-    val p = JPanel(BorderLayout(5, 2))
-    p.setBorder(BorderFactory.createTitledBorder("Unzip"))
-    p.add(field)
-    p.add(button, BorderLayout.EAST)
-    p.add(button1, BorderLayout.SOUTH)
-    return p
-  }
-
-  init {
-    LOGGER.setUseParentHandlers(false)
-    val textArea = JTextArea()
-    textArea.setEditable(false)
-    LOGGER.addHandler(TextAreaHandler(TextAreaOutputStream(textArea)))
-    val p = JPanel(GridLayout(2, 1, 10, 10))
-    p.add(makeZipPanel())
-    p.add(makeUnzipPanel())
-    add(p, BorderLayout.NORTH)
-    add(JScrollPane(textArea))
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun makeDestDirPath(text: String): Path? {
-    val path = Paths.get(text)
-    // if (str.isEmpty() || Files.notExists(path)) { // noticeably poor performance in JDK 8
-    if (text.isEmpty() || !path.toFile().exists()) {
-      return null
-    }
-    var name = path.getFileName().toString()
-    val lastDotPos = name.lastIndexOf('.')
-    if (lastDotPos > 0) {
-      name = name.substring(0, lastDotPos)
-    }
-    return path.resolveSibling(name)
-  }
-
-  companion object {
-    val LOGGER_NAME: String = MethodHandles.lookup().lookupClass().getName()
-    private val LOGGER = Logger.getLogger(LOGGER_NAME)
+fun makeUI(): Component {
+  logger.useParentHandlers = false
+  val textArea = JTextArea()
+  textArea.isEditable = false
+  logger.addHandler(TextAreaHandler(TextAreaOutputStream(textArea)))
+  val p = JPanel(GridLayout(2, 1, 10, 10))
+  p.add(makeZipPanel())
+  p.add(makeUnzipPanel())
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(textArea))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-object ZipUtil {
-  private val logger = Logger.getLogger(MainPanel.LOGGER_NAME)
+private fun makeZipPanel(): Component {
+  val field = JTextField(20)
+  val button = JButton("select directory")
+  button.addActionListener {
+    val fileChooser = JFileChooser()
+    fileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+    val ret = fileChooser.showOpenDialog(button.rootPane)
+    if (ret == JFileChooser.APPROVE_OPTION) {
+      field.text = fileChooser.selectedFile.absolutePath
+    }
+  }
+  val button1 = JButton("zip")
+  button1.addActionListener {
+    val str = field.text
+    val path = Paths.get(str)
+    // if (str.isEmpty() || Files.notExists(path)) { // noticeably poor performance in JDK 8
+    if (str.isEmpty() || !path.toFile().exists()) {
+      return@addActionListener
+    }
+    val name = path.fileName.toString() + ".zip"
+    val tgt = path.resolveSibling(name)
+    // if (Files.exists(tgt)) { // noticeably poor performance in JDK 8
+    if (tgt.toFile().exists()) {
+      val m = "<html>$tgt already exists.<br>Do you want to overwrite it?"
+      val rv = JOptionPane.showConfirmDialog(button1.rootPane, m, "Zip", JOptionPane.YES_NO_OPTION)
+      if (rv != JOptionPane.YES_OPTION) {
+        return@addActionListener
+      }
+    }
+    runCatching {
+      ZipUtil.zip(path, tgt)
+    }.onFailure {
+      logger.info { "Cant zip! : $path" }
+      Toolkit.getDefaultToolkit().beep()
+    }
+  }
+  val p = JPanel(BorderLayout(5, 2))
+  p.border = BorderFactory.createTitledBorder("Zip")
+  p.add(field)
+  p.add(button, BorderLayout.EAST)
+  p.add(button1, BorderLayout.SOUTH)
+  return p
+}
+
+private fun makeUnzipPanel(): Component {
+  val field = JTextField(20)
+  val button = JButton("select .zip file")
+  button.addActionListener {
+    val fileChooser = JFileChooser()
+    val ret = fileChooser.showOpenDialog(button.rootPane)
+    if (ret == JFileChooser.APPROVE_OPTION) {
+      field.text = fileChooser.selectedFile.absolutePath
+    }
+  }
+  val button1 = JButton("unzip")
+  button1.addActionListener {
+    val str = field.text
+    makeDestDirPath(str)?.also { destDir ->
+      val path = Paths.get(str)
+      runCatching {
+        // if (Files.exists(destDir)) { // noticeably poor performance in JDK 8
+        if (destDir.toFile().exists()) {
+          val m = "<html>$destDir already exists.<br>Do you want to overwrite it?"
+          val rv = JOptionPane.showConfirmDialog(button.rootPane, m, "Unzip", JOptionPane.YES_NO_OPTION)
+          if (rv != JOptionPane.YES_OPTION) {
+            return@addActionListener
+          }
+        } else {
+          logger.info { "mkdir0: $destDir" }
+          Files.createDirectories(destDir)
+        }
+        ZipUtil.unzip(path, destDir)
+      }.onFailure {
+        logger.info { "Cant unzip! : $path" }
+        Toolkit.getDefaultToolkit().beep()
+      }
+    }
+  }
+  val p = JPanel(BorderLayout(5, 2))
+  p.border = BorderFactory.createTitledBorder("Unzip")
+  p.add(field)
+  p.add(button, BorderLayout.EAST)
+  p.add(button1, BorderLayout.SOUTH)
+  return p
+}
+
+private fun makeDestDirPath(text: String): Path? {
+  val path = Paths.get(text)
+  // if (str.isEmpty() || Files.notExists(path)) { // noticeably poor performance in JDK 8
+  if (text.isEmpty() || !path.toFile().exists()) {
+    return null
+  }
+  var name = path.fileName.toString()
+  val lastDotPos = name.lastIndexOf('.')
+  if (lastDotPos > 0) {
+    name = name.substring(0, lastDotPos)
+  }
+  return path.resolveSibling(name)
+}
+
+private object ZipUtil {
   @Throws(IOException::class)
   fun zip(srcDir: Path, zip: Path) {
     // try (Stream<Path> s = Files.walk(srcDir).filter(Files::isRegularFile)) { // noticeably poor performance in JDK 8
     Files.walk(srcDir)
-      .filter { it.toFile().isFile() }
+      .filter { it.toFile().isFile }
       .use {
         val files = it.collect(Collectors.toList())
         ZipOutputStream(Files.newOutputStream(zip)).use { zos ->
@@ -164,14 +160,14 @@ object ZipUtil {
   fun unzip(zipFilePath: Path, destDir: Path) {
     ZipFile(zipFilePath.toString()).use { zipFile ->
       zipFile.entries().toList().forEach { entry ->
-        val name = entry.getName()
+        val name = entry.name
         val path = destDir.resolve(name)
         if (name.endsWith("/")) { // if (Files.isDirectory(path)) {
           logger.info { "mkdir1: $path" }
           Files.createDirectories(path)
         } else {
           // if (Files.notExists(parent)) { // noticeably poor performance in JDK 8
-          path.getParent()?.takeUnless { it.toFile().exists() }?.also {
+          path.parent?.takeUnless { it.toFile().exists() }?.also {
             logger.info { "mkdir2: $it" }
             Files.createDirectories(it)
           }
@@ -183,8 +179,9 @@ object ZipUtil {
   }
 }
 
-class TextAreaOutputStream(private val textArea: JTextArea) : OutputStream() {
+private class TextAreaOutputStream(private val textArea: JTextArea) : OutputStream() {
   private val buffer = ByteArrayOutputStream()
+
   @Throws(IOException::class)
   override fun flush() {
     textArea.append(buffer.toString("UTF-8"))
@@ -200,14 +197,14 @@ class TextAreaOutputStream(private val textArea: JTextArea) : OutputStream() {
   }
 }
 
-class TextAreaHandler(os: OutputStream) : StreamHandler() {
+private class TextAreaHandler(os: OutputStream) : StreamHandler() {
   private fun configure() {
-    setFormatter(SimpleFormatter())
+    formatter = SimpleFormatter()
     runCatching {
-      setEncoding("UTF-8")
+      encoding = "UTF-8"
     }.onFailure {
       // doing a setEncoding with null should always work.
-      setEncoding(null)
+      encoding = null
     }
   }
 
@@ -238,7 +235,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
