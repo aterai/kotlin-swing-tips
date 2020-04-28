a060b617a962ab755f44859484ba0b1b6abaabbc
aterai <aterai@outlook.com>
2020-04-28 19:02:52 +0900

    refactor: use v.coerceIn(min, max) instead of maxOf(min, minOf(v, max))

--- DragLimitedSlider/src/main/kotlin/example/App.kt
+++ DragLimitedSlider/src/main/kotlin/example/App.kt
@@ -6,60 +6,53 @@ import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.metal.MetalSliderUI
 
-class MainPanel : JPanel(GridLayout(2, 1, 5, 5)) {
-  init {
-    val slider1 = makeSlider("ChangeListener")
-    val slider2 = makeSlider("TrackListener")
-    if (slider2.ui is WindowsSliderUI) {
-      slider2.ui = WindowsDragLimitedSliderUI(slider2)
-    } else {
-      slider2.ui = MetalDragLimitedSliderUI()
-    }
-    add(slider1)
-    add(slider2)
-    preferredSize = Dimension(320, 240)
-  }
+private const val MAXI = 80
+private const val MINI = 40
 
-  private fun makeSlider(title: String): JSlider {
-    val slider = JSlider(0, 100, 40)
-    slider.border = BorderFactory.createTitledBorder(title)
-    slider.majorTickSpacing = 10
-    slider.paintTicks = true
-    slider.paintLabels = true
-    slider.labelTable?.elements()
-      ?.toList()
-      ?.filterIsInstance<JLabel>()
-      ?.forEach {
-      val v = it.text.toInt()
-      if (v > MAXI || v < MINI) {
-        it.foreground = Color.RED
-      }
-    }
-    slider.model.addChangeListener { e ->
-      val m = e.source as? BoundedRangeModel ?: return@addChangeListener
-      if (m.value > MAXI) {
-        m.value = MAXI
-      } else if (m.value < MINI) {
-        m.value = MINI
-      }
-    }
-    return slider
+fun makeUI(): Component {
+  val slider1 = makeSlider("ChangeListener")
+  val slider2 = makeSlider("TrackListener")
+  if (slider2.ui is WindowsSliderUI) {
+    slider2.ui = WindowsDragLimitedSliderUI(slider2)
+  } else {
+    slider2.ui = MetalDragLimitedSliderUI()
   }
-
-  companion object {
-    const val MAXI = 80
-    const val MINI = 40
+  return JPanel(GridLayout(2, 1, 5, 5)).also {
+    it.add(slider1)
+    it.add(slider2)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class WindowsDragLimitedSliderUI(slider: JSlider) : WindowsSliderUI(slider) {
+private fun makeSlider(title: String): JSlider {
+  val slider = JSlider(0, 100, 40)
+  slider.border = BorderFactory.createTitledBorder(title)
+  slider.majorTickSpacing = 10
+  slider.paintTicks = true
+  slider.paintLabels = true
+  slider.labelTable?.elements()
+    ?.toList()
+    ?.filterIsInstance<JLabel>()
+    ?.filterNot { it.text.toInt() in MINI..MAXI }
+    ?.forEach {
+      it.foreground = Color.RED
+    }
+  slider.model.addChangeListener { e ->
+    (e.source as? BoundedRangeModel)?.also {
+      it.value = it.value.coerceIn(MINI, MAXI)
+    }
+  }
+  return slider
+}
+
+private class WindowsDragLimitedSliderUI(slider: JSlider) : WindowsSliderUI(slider) {
   override fun createTrackListener(slider: JSlider): TrackListener {
     return object : TrackListener() {
       override fun mouseDragged(e: MouseEvent) { // case HORIZONTAL:
         val halfThumbWidth = thumbRect.width / 2
         val thumbLeft = e.x - offset
-        val maxPos = xPositionForValue(MainPanel.MAXI) - halfThumbWidth
-        val minPos = xPositionForValue(MainPanel.MINI) - halfThumbWidth
+        val maxPos = xPositionForValue(MAXI) - halfThumbWidth
+        val minPos = xPositionForValue(MINI) - halfThumbWidth
         if (thumbLeft > maxPos) {
           e.translatePoint(maxPos + offset - e.x, 0)
         } else if (thumbLeft < minPos) {
@@ -71,14 +64,14 @@ class WindowsDragLimitedSliderUI(slider: JSlider) : WindowsSliderUI(slider) {
   }
 }
 
-class MetalDragLimitedSliderUI : MetalSliderUI() {
+private class MetalDragLimitedSliderUI : MetalSliderUI() {
   override fun createTrackListener(slider: JSlider): TrackListener {
     return object : TrackListener() {
       override fun mouseDragged(e: MouseEvent) { // case HORIZONTAL:
         val halfThumbWidth = thumbRect.width / 2
         val thumbLeft = e.x - offset
-        val maxPos = xPositionForValue(MainPanel.MAXI) - halfThumbWidth
-        val minPos = xPositionForValue(MainPanel.MINI) - halfThumbWidth
+        val maxPos = xPositionForValue(MAXI) - halfThumbWidth
+        val minPos = xPositionForValue(MINI) - halfThumbWidth
         if (thumbLeft > maxPos) {
           e.translatePoint(maxPos + offset - e.x, 0)
         } else if (thumbLeft < minPos) {
@@ -100,7 +93,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
