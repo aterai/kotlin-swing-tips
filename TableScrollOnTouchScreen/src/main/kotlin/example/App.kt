91952d0763c8effbba4b863d55b449065e92bcbf
aterai <aterai@outlook.com>
2020-04-30 16:35:38 +0900

    refactor: use of setter method instead of property access syntax

--- TableScrollOnTouchScreen/src/main/kotlin/example/App.kt
+++ TableScrollOnTouchScreen/src/main/kotlin/example/App.kt
@@ -9,93 +9,88 @@ import javax.swing.event.ListSelectionListener
 import javax.swing.table.DefaultTableModel
 import kotlin.math.abs
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("String", "Integer", "Boolean")
-    val model = object : DefaultTableModel(null, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-    }
-
-    for (i in 0 until 1000) {
-      model.addRow(arrayOf("12345", i, i % 2 == 0))
-    }
-
-    val table = object : JTable(model) {
-      @Transient
-      private var handler: TableTouchScreenHandler? = null
-
-      override fun updateUI() {
-        removeMouseMotionListener(handler)
-        removeMouseListener(handler)
-        getSelectionModel().removeListSelectionListener(handler)
-        super.updateUI()
-        handler = TableTouchScreenHandler(this)
-        addMouseMotionListener(handler)
-        addMouseListener(handler)
-        getSelectionModel().addListSelectionListener(handler)
-        setRowHeight(30)
-      }
-    }
-    UIManager.put("ScrollBar.width", 30)
-    val scroll = JScrollPane(table)
-    add(scroll)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val model = object : DefaultTableModel(null, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
   }
+
+  for (i in 0 until 1000) {
+    model.addRow(arrayOf("12345", i, i % 2 == 0))
+  }
+
+  val table = object : JTable(model) {
+    @Transient
+    private var handler: TableTouchScreenHandler? = null
+
+    override fun updateUI() {
+      removeMouseMotionListener(handler)
+      removeMouseListener(handler)
+      getSelectionModel().removeListSelectionListener(handler)
+      super.updateUI()
+      handler = TableTouchScreenHandler(this)
+      addMouseMotionListener(handler)
+      addMouseListener(handler)
+      getSelectionModel().addListSelectionListener(handler)
+      setRowHeight(30)
+    }
+  }
+  UIManager.put("ScrollBar.width", 30)
+  val scroll = JScrollPane(table)
+  scroll.preferredSize = Dimension(320, 240)
+  return scroll
 }
 
-class TableTouchScreenHandler(table: JTable) : MouseAdapter(), ListSelectionListener {
+private class TableTouchScreenHandler(table: JTable) : MouseAdapter(), ListSelectionListener {
   private val dc = Cursor.getDefaultCursor()
   private val hc = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
-  private val scroller: Timer
-  private val startPt = Point()
-  private val delta = Point()
-
-  init {
-    this.scroller = Timer(DELAY) { e ->
-      print(".")
-      val vport = SwingUtilities.getUnwrappedParent(table) as? JViewport ?: return@Timer
-      val vp = vport.getViewPosition()
+  private val scroller = Timer(DELAY) { e ->
+    print(".")
+    (SwingUtilities.getUnwrappedParent(table) as? JViewport)?.also {
+      val vp = it.viewPosition
       vp.translate(-delta.x, -delta.y)
-      table.scrollRectToVisible(Rectangle(vp, vport.getSize()))
+      table.scrollRectToVisible(Rectangle(vp, it.size))
       if (abs(delta.x) > 0 || abs(delta.y) > 0) {
         delta.setLocation((delta.x * GRAVITY).toInt(), (delta.y * GRAVITY).toInt())
       } else {
-        (e.getSource() as? Timer)?.stop()
+        (e.source as? Timer)?.stop()
       }
     }
   }
+  private val startPt = Point()
+  private val delta = Point()
 
   override fun mousePressed(e: MouseEvent) {
     println("mousePressed: $delta")
-    val c = e.getComponent()
-    c.setCursor(hc)
-    // c.setEnabled(false);
+    val c = e.component
+    c.cursor = hc
+    // table.isEnabled = false
     val p = SwingUtilities.getUnwrappedParent(c)
     if (p is JViewport) {
-      startPt.setLocation(SwingUtilities.convertPoint(c, e.getPoint(), p))
+      startPt.location = SwingUtilities.convertPoint(c, e.point, p)
       scroller.stop()
     }
   }
 
   override fun mouseDragged(e: MouseEvent) {
-    val c = e.getComponent()
+    val c = e.component
     val p = SwingUtilities.getUnwrappedParent(c)
-    (p as? JViewport)?.also { vport ->
-      val cp = SwingUtilities.convertPoint(c, e.getPoint(), vport)
-      val vp = vport.getViewPosition()
+    (p as? JViewport)?.also {
+      val cp = SwingUtilities.convertPoint(c, e.point, it)
+      val vp = it.viewPosition
       vp.translate(startPt.x - cp.x, startPt.y - cp.y)
       delta.setLocation(VELOCITY * (cp.x - startPt.x), VELOCITY * (cp.y - startPt.y))
-      (c as? JComponent)?.scrollRectToVisible(Rectangle(vp, vport.getSize()))
-      startPt.setLocation(cp)
+      (c as? JComponent)?.scrollRectToVisible(Rectangle(vp, it.size))
+      startPt.location = cp
     }
   }
 
   override fun mouseReleased(e: MouseEvent) {
     println("mouseReleased: $delta")
-    val table = e.getComponent() as? JTable ?: return
-    table.setCursor(dc)
-    // table.setEnabled(true);
-    if (table.isEditing()) {
+    val table = e.component as? JTable ?: return
+    table.cursor = dc
+    // table.isEnabled = true
+    if (table.isEditing) {
       delta.setLocation(0, 0)
     } else {
       scroller.start()
@@ -103,8 +98,8 @@ class TableTouchScreenHandler(table: JTable) : MouseAdapter(), ListSelectionList
   }
 
   override fun valueChanged(e: ListSelectionEvent) {
-    println("\nvalueChanged: " + e.getValueIsAdjusting())
-    if (scroller.isRunning()) {
+    println("\nvalueChanged: " + e.valueIsAdjusting)
+    if (scroller.isRunning) {
       println("isRunning")
       delta.setLocation(0, 0)
     }
@@ -128,7 +123,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
