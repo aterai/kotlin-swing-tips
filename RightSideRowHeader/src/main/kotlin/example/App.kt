2f33f67df78ba14a5fc0fb9443c82eb5de07e751
aterai <aterai@outlook.com>
2020-05-01 03:17:34 +0900

    refactor: use of setter method instead of property access syntax

--- RightSideRowHeader/src/main/kotlin/example/App.kt
+++ RightSideRowHeader/src/main/kotlin/example/App.kt
@@ -5,94 +5,89 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableRowSorter
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val data = arrayOf(
-    arrayOf(1, 11, "A", ES, ES, ES, ES, ES),
-    arrayOf(2, 22, ES, "B", ES, ES, ES, ES),
-    arrayOf(3, 33, ES, ES, "C", ES, ES, ES),
-    arrayOf(4, 1, ES, ES, ES, "D", ES, ES),
-    arrayOf(5, 55, ES, ES, ES, ES, "E", ES),
-    arrayOf(6, 66, ES, ES, ES, ES, ES, "F"))
-  private val columnNames = arrayOf("fixed 1", "fixed 2", "A", "B", "C", "D", "E", "F")
-  private val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = if (column < FIXED_RANGE) Number::class.java else Any::class.java
-  }
-  @Transient
-  private val sorter = TableRowSorter<DefaultTableModel>(model)
-  private val addButton = JButton("add")
+private const val FIXED_RANGE = 2
+private const val ES = ""
 
-  init {
-    val fixedTable = JTable(model)
-    val table = JTable(model)
-    fixedTable.setSelectionModel(table.getSelectionModel())
+private val data = arrayOf(
+  arrayOf(1, 11, "A", ES, ES, ES, ES, ES),
+  arrayOf(2, 22, ES, "B", ES, ES, ES, ES),
+  arrayOf(3, 33, ES, ES, "C", ES, ES, ES),
+  arrayOf(4, 1, ES, ES, ES, "D", ES, ES),
+  arrayOf(5, 55, ES, ES, ES, ES, "E", ES),
+  arrayOf(6, 66, ES, ES, ES, ES, ES, "F"))
+private val columnNames = arrayOf("fixed 1", "fixed 2", "A", "B", "C", "D", "E", "F")
+private val model = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = if (column < FIXED_RANGE) Number::class.java else Any::class.java
+}
 
-    for (i in model.getColumnCount() - 1 downTo 0) {
-      if (i < FIXED_RANGE) {
-        table.removeColumn(table.getColumnModel().getColumn(i))
-        fixedTable.getColumnModel().getColumn(i).setResizable(false)
-      } else {
-        fixedTable.removeColumn(fixedTable.getColumnModel().getColumn(i))
-      }
+@Transient
+private val sorter = TableRowSorter<DefaultTableModel>(model)
+private val addButton = JButton("add")
+
+fun makeUI(): Component {
+  val fixedTable = JTable(model)
+  val table = JTable(model)
+  fixedTable.selectionModel = table.selectionModel
+
+  for (i in model.columnCount - 1 downTo 0) {
+    if (i < FIXED_RANGE) {
+      table.removeColumn(table.columnModel.getColumn(i))
+      fixedTable.columnModel.getColumn(i).resizable = false
+    } else {
+      fixedTable.removeColumn(fixedTable.columnModel.getColumn(i))
     }
-
-    fixedTable.setRowSorter(sorter)
-    fixedTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
-    fixedTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-    fixedTable.putClientProperty("terminateEditOnFocusLost", true)
-    fixedTable.setBorder(BorderFactory.createMatteBorder(0, 1, 0, 0, Color.GRAY))
-    fixedTable.getTableHeader().setBorder(BorderFactory.createMatteBorder(0, 1, 0, 0, Color.GRAY))
-
-    table.setRowSorter(sorter)
-    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
-    table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-    table.putClientProperty("terminateEditOnFocusLost", true)
-
-    val scroll = JScrollPane(table)
-    scroll.setLayout(RightFixedScrollPaneLayout())
-
-    fixedTable.setPreferredScrollableViewportSize(fixedTable.getPreferredSize())
-    scroll.setRowHeaderView(fixedTable)
-
-    scroll.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, fixedTable.getTableHeader())
-    // TEST:
-    // table.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-    // // fixedTable.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-    // scroll.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-    // scroll.setCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER, fixedTable.getTableHeader());
-
-    scroll.getViewport().setBackground(Color.WHITE)
-    scroll.getRowHeader().setBackground(Color.WHITE)
-
-    scroll.getRowHeader().addChangeListener { e ->
-      val viewport = e.getSource() as? JViewport ?: return@addChangeListener
-      scroll.getVerticalScrollBar().setValue(viewport.getViewPosition().y)
-    }
-
-    addButton.addActionListener {
-      sorter.setSortKeys(null)
-      for (i in 0 until 100) { model.addRow(arrayOf(i, i + 1, "A$i", "B$i")) }
-    }
-
-    add(scroll)
-    add(addButton, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
   }
 
-  companion object {
-    const val FIXED_RANGE = 2
-    private const val ES = ""
+  fixedTable.rowSorter = sorter
+  fixedTable.autoResizeMode = JTable.AUTO_RESIZE_OFF
+  fixedTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+  fixedTable.putClientProperty("terminateEditOnFocusLost", true)
+  fixedTable.border = BorderFactory.createMatteBorder(0, 1, 0, 0, Color.GRAY)
+  fixedTable.tableHeader.border = BorderFactory.createMatteBorder(0, 1, 0, 0, Color.GRAY)
+
+  table.rowSorter = sorter
+  table.autoResizeMode = JTable.AUTO_RESIZE_OFF
+  table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+  table.putClientProperty("terminateEditOnFocusLost", true)
+
+  val scroll = JScrollPane(table)
+  scroll.layout = RightFixedScrollPaneLayout()
+
+  fixedTable.preferredScrollableViewportSize = fixedTable.preferredSize
+  scroll.setRowHeaderView(fixedTable)
+
+  scroll.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, fixedTable.tableHeader)
+  scroll.viewport.background = Color.WHITE
+  scroll.rowHeader.background = Color.WHITE
+
+  scroll.rowHeader.addChangeListener { e ->
+    val viewport = e.source as? JViewport ?: return@addChangeListener
+    scroll.verticalScrollBar.value = viewport.viewPosition.y
+  }
+
+  addButton.addActionListener {
+    sorter.sortKeys = null
+    for (i in 0 until 100) {
+      model.addRow(arrayOf(i, i + 1, "A$i", "B$i"))
+    }
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.add(addButton, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
 @Suppress("LargeClass")
-internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
+private class RightFixedScrollPaneLayout : ScrollPaneLayout() {
   @Suppress("LongMethod", "ComplexMethod", "NestedBlockDepth")
   override fun layoutContainer(parent: Container) {
     val scrollPane = parent as? JScrollPane ?: return
-    vsbPolicy = scrollPane.getVerticalScrollBarPolicy()
-    hsbPolicy = scrollPane.getHorizontalScrollBarPolicy()
+    vsbPolicy = scrollPane.verticalScrollBarPolicy
+    hsbPolicy = scrollPane.horizontalScrollBarPolicy
 
-    val availR = scrollPane.getBounds()
+    val availR = scrollPane.bounds
     availR.x = 0
     availR.y = 0
 
@@ -102,19 +97,20 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
     availR.width -= insets.left + insets.right
     availR.height -= insets.top + insets.bottom
 
-    val leftToRight = true // SwingUtilities.isLeftToRight(scrollPane);
+    // val leftToRight = SwingUtilities.isLeftToRight(scrollPane)
+    val leftToRight = scrollPane.componentOrientation.isLeftToRight
 
     val colHeadR = Rectangle(0, availR.y, 0, 0)
-    colHead?.takeIf { it.isVisible() }?.also {
-      val colHeadHeight = minOf(availR.height, it.getPreferredSize().height)
+    colHead?.takeIf { it.isVisible }?.also {
+      val colHeadHeight = minOf(availR.height, it.preferredSize.height)
       colHeadR.height = colHeadHeight
       availR.y += colHeadHeight
       availR.height -= colHeadHeight
     }
 
     val rowHeadR = Rectangle(0, 0, 0, 0)
-    rowHead?.takeIf { it.isVisible() }?.also {
-      val rowHeadWidth = minOf(availR.width, it.getPreferredSize().width)
+    rowHead?.takeIf { it.isVisible }?.also {
+      val rowHeadWidth = minOf(availR.width, it.preferredSize.width)
       rowHeadR.width = rowHeadWidth
       availR.width -= rowHeadWidth
       // if (leftToRight) {
@@ -126,7 +122,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
       rowHeadR.x = availR.x + availR.width
     }
 
-    val vpbInsets = scrollPane.getViewportBorder()?.let {
+    val vpbInsets = scrollPane.viewportBorder?.let {
       val ins = it.getBorderInsets(parent)
       availR.x += ins.left
       availR.y += ins.top
@@ -135,20 +131,19 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
       ins
     } ?: Insets(0, 0, 0, 0)
 
-    val view: Component? = viewport?.getView()
-    val viewPrefSize = view?.getPreferredSize() ?: Dimension()
-    var extentSize = viewport?.toViewCoordinates(availR.getSize()) ?: Dimension()
+    val view = viewport?.view
+    val viewPrefSize = view?.preferredSize ?: Dimension()
+    var extentSize = viewport?.toViewCoordinates(availR.size) ?: Dimension()
 
     var viewTracksViewportWidth = false
     var viewTracksViewportHeight = false
     val isEmpty = availR.width < 0 || availR.height < 0
-    val sv: Scrollable?
-    if (!isEmpty && view is Scrollable) {
-      sv = view
-      viewTracksViewportWidth = sv.getScrollableTracksViewportWidth()
-      viewTracksViewportHeight = sv.getScrollableTracksViewportHeight()
+    val sv = if (!isEmpty && view is Scrollable) {
+      viewTracksViewportWidth = view.scrollableTracksViewportWidth
+      viewTracksViewportHeight = view.scrollableTracksViewportHeight
+      view
     } else {
-      sv = null
+      null
     }
 
     val vsbR = Rectangle(0, availR.y - vpbInsets.top, 0, 0)
@@ -160,7 +155,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
 
     if (vsb != null && vsbNeeded) {
       adjustForVsb(true, rowHeadR, vsbR, vpbInsets, leftToRight)
-      extentSize = viewport.toViewCoordinates(availR.getSize())
+      extentSize = viewport.toViewCoordinates(availR.size)
     }
 
     val hsbR = Rectangle(availR.x - vpbInsets.left, 0, 0, 0)
@@ -174,7 +169,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
       adjustForHsb(true, availR, hsbR, vpbInsets)
       if (vsb != null && !vsbNeeded && vsbPolicy != ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER) {
 
-        extentSize = viewport.toViewCoordinates(availR.getSize())
+        extentSize = viewport.toViewCoordinates(availR.size)
         vsbNeeded = viewPrefSize.height > extentSize.height
 
         if (vsbNeeded) {
@@ -185,22 +180,22 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
     }
 
     if (viewport != null) {
-      viewport.setBounds(availR)
+      viewport.bounds = availR
 
       if (sv != null) {
-        extentSize = viewport.toViewCoordinates(availR.getSize())
+        extentSize = viewport.toViewCoordinates(availR.size)
 
         val oldHsbNeeded = hsbNeeded
         val oldVsbNeeded = vsbNeeded
-        viewTracksViewportWidth = sv.getScrollableTracksViewportWidth()
-        viewTracksViewportHeight = sv.getScrollableTracksViewportHeight()
+        viewTracksViewportWidth = sv.scrollableTracksViewportWidth
+        viewTracksViewportHeight = sv.scrollableTracksViewportHeight
         if (vsb != null && vsbPolicy == ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED) {
           val newVsbNeeded = !viewTracksViewportHeight && viewPrefSize.height > extentSize.height
           if (newVsbNeeded != vsbNeeded) {
             vsbNeeded = newVsbNeeded
             // adjustForVsb(vsbNeeded, availR, vsbR, vpbInsets, leftToRight);
             adjustForVsb(vsbNeeded, rowHeadR, vsbR, vpbInsets, leftToRight)
-            extentSize = viewport.toViewCoordinates(availR.getSize())
+            extentSize = viewport.toViewCoordinates(availR.size)
           }
         }
         if (hsb != null && hsbPolicy == ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED) {
@@ -210,7 +205,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
             adjustForHsb(hsbNeeded, availR, hsbR, vpbInsets)
             if (vsb != null && !vsbNeeded && vsbPolicy != ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER) {
 
-              extentSize = viewport.toViewCoordinates(availR.getSize())
+              extentSize = viewport.toViewCoordinates(availR.size)
               vsbNeeded = viewPrefSize.height > extentSize.height
 
               if (vsbNeeded) {
@@ -221,7 +216,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
           }
         }
         if (oldHsbNeeded != hsbNeeded || oldVsbNeeded != vsbNeeded) {
-          viewport.setBounds(availR)
+          viewport.bounds = availR
         }
       }
     }
@@ -233,9 +228,9 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
     colHeadR.width = availR.width + vpbInsets.left + vpbInsets.right
     colHeadR.x = availR.x - vpbInsets.left
 
-    rowHead?.setBounds(rowHeadR)
+    rowHead?.bounds = rowHeadR
 
-    colHead?.setBounds(colHeadR)
+    colHead?.bounds = colHeadR
 
     if (vsb != null) {
       if (vsbNeeded) {
@@ -245,10 +240,10 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
         //     vsbR.height += colHeadR.height;
         //   }
         // }
-        vsb.setVisible(true)
-        vsb.setBounds(vsbR)
+        vsb.isVisible = true
+        vsb.bounds = vsbR
       } else {
-        vsb.setVisible(false)
+        vsb.isVisible = false
       }
     }
 
@@ -262,10 +257,10 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
         //     hsbR.width += rowHeadR.width;
         //   }
         // }
-        hsb.setVisible(true)
-        hsb.setBounds(hsbR)
+        hsb.isVisible = true
+        hsb.bounds = hsbR
       } else {
-        hsb.setVisible(false)
+        hsb.isVisible = false
       }
     }
 
@@ -295,7 +290,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
   ) {
     val oldWidth = vsbR.width
     if (wantsVsb) {
-      val vsbWidth = maxOf(0, minOf(vsb.getPreferredSize().width, available.width))
+      val vsbWidth = maxOf(0, minOf(vsb.preferredSize.width, available.width))
       available.width -= vsbWidth
       vsbR.width = vsbWidth
 
@@ -313,7 +308,7 @@ internal class RightFixedScrollPaneLayout : ScrollPaneLayout() {
   private fun adjustForHsb(wantsHsb: Boolean, available: Rectangle, hsbR: Rectangle, vpbInsets: Insets) {
     val oldHeight = hsbR.height
     if (wantsHsb) {
-      val hsbHeight = maxOf(0, minOf(available.height, hsb.getPreferredSize().height))
+      val hsbHeight = maxOf(0, minOf(available.height, hsb.preferredSize.height))
       available.height -= hsbHeight
       hsbR.y = available.y + available.height + vpbInsets.bottom
       hsbR.height = hsbHeight
@@ -327,7 +322,7 @@ fun main() {
   EventQueue.invokeLater {
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
