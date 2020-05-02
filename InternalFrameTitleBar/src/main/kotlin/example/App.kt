b5deb107c3c2faf5775f4110f3103dfe66770ec3
aterai <aterai@outlook.com>
2020-05-02 15:08:49 +0900

    refactor: use of setter method instead of property access syntax

--- InternalFrameTitleBar/src/main/kotlin/example/App.kt
+++ InternalFrameTitleBar/src/main/kotlin/example/App.kt
@@ -8,68 +8,67 @@ import java.awt.event.WindowEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicInternalFrameUI
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val closeButton = JButton("close")
-    closeButton.addActionListener { e ->
-      val c = e.getSource() as? Component ?: return@addActionListener
-      (SwingUtilities.getRoot(c) as? Window)?.also {
-        it.dispatchEvent(WindowEvent(it, WindowEvent.WINDOW_CLOSING))
-      }
+fun makeUI(): Component {
+  val closeButton = JButton("close")
+  closeButton.addActionListener { e ->
+    (SwingUtilities.getRoot(e.source as? Component) as? Window)?.also {
+      it.dispatchEvent(WindowEvent(it, WindowEvent.WINDOW_CLOSING))
     }
-
-    val p = JPanel(BorderLayout())
-    p.add(JScrollPane(JTree()))
-    p.add(closeButton, BorderLayout.SOUTH)
-
-    val internal = makeInternalFrame()
-    internal.getContentPane().add(p)
-    internal.setVisible(true)
-
-    add(internal)
-    setOpaque(false)
-    setPreferredSize(Dimension(320, 240))
   }
 
-  private fun makeInternalFrame(): JInternalFrame {
-    val internal = JInternalFrame("Title")
-    val ui = internal.getUI() as? BasicInternalFrameUI ?: return internal
-    val title = ui.getNorthPane()
-    for (l in title.getListeners(MouseMotionListener::class.java)) {
-      title.removeMouseMotionListener(l)
-    }
-    val dwl = DragWindowListener()
-    title.addMouseListener(dwl)
-    title.addMouseMotionListener(dwl)
+  val p = JPanel(BorderLayout())
+  p.add(JScrollPane(JTree()))
+  p.add(closeButton, BorderLayout.SOUTH)
 
-    val focusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager()
-    focusManager.addPropertyChangeListener { e ->
-      if ("activeWindow" == e.getPropertyName()) {
-        runCatching {
-          internal.setSelected(e.getNewValue() != null)
-        }.onFailure {
-          UIManager.getLookAndFeel().provideErrorFeedback(internal)
-        }
-      }
-    }
-    return internal
+  val internal = makeInternalFrame()
+  internal.contentPane.add(p)
+  internal.isVisible = true
+
+  return JPanel(BorderLayout()).also {
+    it.add(internal)
+    it.isOpaque = false
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class DragWindowListener : MouseAdapter() {
+private fun makeInternalFrame(): JInternalFrame {
+  val internal = JInternalFrame("Title")
+  val ui = internal.ui as? BasicInternalFrameUI ?: return internal
+  val title = ui.northPane
+  for (l in title.getListeners(MouseMotionListener::class.java)) {
+    title.removeMouseMotionListener(l)
+  }
+  val dwl = DragWindowListener()
+  title.addMouseListener(dwl)
+  title.addMouseMotionListener(dwl)
+
+  val focusManager = KeyboardFocusManager.getCurrentKeyboardFocusManager()
+  focusManager.addPropertyChangeListener { e ->
+    if ("activeWindow" == e.propertyName) {
+      runCatching {
+        internal.isSelected = e.newValue != null
+      }.onFailure {
+        UIManager.getLookAndFeel().provideErrorFeedback(internal)
+      }
+    }
+  }
+  return internal
+}
+
+private class DragWindowListener : MouseAdapter() {
   private val startPt = Point()
 
   override fun mousePressed(e: MouseEvent) {
     if (SwingUtilities.isLeftMouseButton(e)) {
-      startPt.setLocation(e.getPoint())
+      startPt.location = e.point
     }
   }
 
   override fun mouseDragged(e: MouseEvent) {
     if (SwingUtilities.isLeftMouseButton(e)) {
-      (SwingUtilities.getRoot(e.getComponent()) as? Window)?.also {
-        val pt = it.getLocation()
-        it.setLocation(pt.x - startPt.x + e.getX(), pt.y - startPt.y + e.getY())
+      (SwingUtilities.getRoot(e.component) as? Window)?.also {
+        val pt = it.location
+        it.setLocation(pt.x - startPt.x + e.x, pt.y - startPt.y + e.y)
       }
     }
   }
@@ -84,11 +83,11 @@ fun main() {
       Toolkit.getDefaultToolkit().beep()
     }
     JFrame().apply {
-      setUndecorated(true)
-      setMinimumSize(Dimension(300, 120))
+      isUndecorated = true
+      minimumSize = Dimension(300, 120)
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
-      setBackground(Color(0x0, true))
+      contentPane.add(makeUI())
+      background = Color(0x0, true)
       pack()
       setLocationRelativeTo(null)
       isVisible = true
