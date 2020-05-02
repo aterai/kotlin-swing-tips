c39c5e28ca43169c6dc77d21b1f07c535224c09b
aterai <aterai@outlook.com>
2020-05-02 16:11:30 +0900

    detekt: LabeledExpression

--- TableSortIcon/src/main/kotlin/example/App.kt
+++ TableSortIcon/src/main/kotlin/example/App.kt
@@ -6,80 +6,78 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.IconUIResource
 import javax.swing.table.DefaultTableModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("String", "Integer", "Boolean")
-    val data = arrayOf(
-      arrayOf("aaa", 12, true),
-      arrayOf("bbb", 5, false),
-      arrayOf("CCC", 92, true),
-      arrayOf("DDD", 0, false)
-    )
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-    }
-    val table = JTable(model)
-    table.autoCreateRowSorter = true
+private val EMPTY_ICON = EmptyIcon()
 
-    val clearButton = JButton("clear SortKeys")
-    clearButton.addActionListener {
-      table.rowSorter.setSortKeys(null)
-    }
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
 
-    add(makeRadioPane(table), BorderLayout.NORTH)
-    add(clearButton, BorderLayout.SOUTH)
-    add(JScrollPane(table))
-    preferredSize = Dimension(320, 240)
+  val clearButton = JButton("clear SortKeys")
+  clearButton.addActionListener {
+    table.rowSorter.setSortKeys(null)
   }
 
-  private fun makeRadioPane(table: JTable): Box {
-    val customAscendingSortIcon = ImageIcon(javaClass.getResource("ascending.png"))
-    val customDescendingSortIcon = ImageIcon(javaClass.getResource("descending.png"))
-    val r0 = JRadioButton("Default", true)
-    val r1 = JRadioButton("Empty")
-    val r2 = JRadioButton("Custom")
-    val al = ActionListener { e ->
-      val r = e.source as? JRadioButton ?: return@ActionListener
-      val ascending: Icon
-      val descending: Icon
-      when (r) {
-        r0 -> {
-          ascending = UIManager.getLookAndFeelDefaults().getIcon("Table.ascendingSortIcon")
-          descending = UIManager.getLookAndFeelDefaults().getIcon("Table.descendingSortIcon")
-        }
-        r1 -> {
-          ascending = IconUIResource(EMPTY_ICON)
-          descending = IconUIResource(EMPTY_ICON)
-        }
-        else -> {
-          ascending = IconUIResource(customAscendingSortIcon)
-          descending = IconUIResource(customDescendingSortIcon)
-        }
-      }
-      UIManager.put("Table.ascendingSortIcon", ascending)
-      UIManager.put("Table.descendingSortIcon", descending)
-      table.tableHeader.repaint()
-    }
-    val box1 = Box.createHorizontalBox()
-    box1.border = BorderFactory.createEmptyBorder(0, 5, 0, 5)
-    val bg = ButtonGroup()
-    box1.add(JLabel("Table Sort Icon: "))
-    listOf(r0, r1, r2).forEach { rb ->
-      box1.add(rb)
-      box1.add(Box.createHorizontalStrut(5))
-      bg.add(rb)
-      rb.addActionListener(al)
-    }
-    box1.add(Box.createHorizontalGlue())
-    return box1
-  }
-
-  companion object {
-    private val EMPTY_ICON = EmptyIcon()
+  return JPanel(BorderLayout()).also {
+    it.add(makeRadioPane(table), BorderLayout.NORTH)
+    it.add(clearButton, BorderLayout.SOUTH)
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class EmptyIcon : Icon {
+private fun makeRadioPane(table: JTable): Box {
+  val cl = Thread.currentThread().contextClassLoader
+  val customAscendingSortIcon = ImageIcon(cl.getResource("example/ascending.png"))
+  val customDescendingSortIcon = ImageIcon(cl.getResource("example/descending.png"))
+  val r0 = JRadioButton("Default", true)
+  val r1 = JRadioButton("Empty")
+  val r2 = JRadioButton("Custom")
+  val al = ActionListener { e ->
+    val ascending: Icon
+    val descending: Icon
+    when (e.source as? JRadioButton) {
+      r0 -> {
+        ascending = UIManager.getLookAndFeelDefaults().getIcon("Table.ascendingSortIcon")
+        descending = UIManager.getLookAndFeelDefaults().getIcon("Table.descendingSortIcon")
+      }
+      r1 -> {
+        ascending = IconUIResource(EMPTY_ICON)
+        descending = IconUIResource(EMPTY_ICON)
+      }
+      else -> {
+        ascending = IconUIResource(customAscendingSortIcon)
+        descending = IconUIResource(customDescendingSortIcon)
+      }
+    }
+    UIManager.put("Table.ascendingSortIcon", ascending)
+    UIManager.put("Table.descendingSortIcon", descending)
+    table.tableHeader.repaint()
+  }
+  val box1 = Box.createHorizontalBox()
+  box1.border = BorderFactory.createEmptyBorder(0, 5, 0, 5)
+  val bg = ButtonGroup()
+  box1.add(JLabel("Table Sort Icon: "))
+  listOf(r0, r1, r2).forEach { rb ->
+    box1.add(rb)
+    box1.add(Box.createHorizontalStrut(5))
+    bg.add(rb)
+    rb.addActionListener(al)
+  }
+  box1.add(Box.createHorizontalGlue())
+  return box1
+}
+
+private class EmptyIcon : Icon {
   override fun paintIcon(
     c: Component,
     g: Graphics,
@@ -104,7 +102,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
