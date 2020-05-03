4dae8e32ac29e30ebc3f625d5915d40a8fac9bc3
aterai <aterai@outlook.com>
2020-05-03 16:32:48 +0900

    detekt: StringLiteralDuplication

--- CheckBoxMenuItemIcon/src/main/kotlin/example/App.kt
+++ CheckBoxMenuItemIcon/src/main/kotlin/example/App.kt
@@ -3,47 +3,46 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val check = JCheckBox("JCheckBox#setIcon(...)")
-    check.setIcon(CheckIcon())
-    add(check, BorderLayout.SOUTH)
-    add(JScrollPane(JTextArea()))
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  companion object {
-    fun createMenuBar(): JMenuBar {
-      val menuBar = JMenuBar()
-      val menu = JMenu("JMenu")
-      menuBar.add(menu)
-      menu.add(JCheckBoxMenuItem("default"))
-
-      val defIcon = UIManager.getIcon("CheckBoxMenuItem.checkIcon")
-      UIManager.put("CheckBoxMenuItem.checkIcon", CheckIcon())
-      menu.add(JCheckBoxMenuItem("checkIcon test"))
-      UIManager.put("CheckBoxMenuItem.checkIcon", defIcon)
-
-      val menu2 = JMenu("JMenu2")
-      val jcbmi = JCheckBoxMenuItem("setIcon")
-      jcbmi.setIcon(CheckIcon())
-      // jcbmi.setSelectedIcon(new CheckIcon());
-      menu2.add(jcbmi)
-      menuBar.add(menu)
-      menuBar.add(menu2)
-      return menuBar
-    }
+fun makeUI(): Component {
+  val check = JCheckBox("JCheckBox#setIcon(...)")
+  check.icon = CheckIcon()
+  return JPanel(BorderLayout()).also {
+    EventQueue.invokeLater { it.rootPane.jMenuBar = createMenuBar() }
+    it.add(check, BorderLayout.SOUTH)
+    it.add(JScrollPane(JTextArea()))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class CheckIcon : Icon {
+fun createMenuBar(): JMenuBar {
+  val menuBar = JMenuBar()
+  val menu = JMenu("JMenu")
+  menuBar.add(menu)
+  menu.add(JCheckBoxMenuItem("default"))
+
+  val key = "CheckBoxMenuItem.checkIcon"
+  val defIcon = UIManager.getIcon(key)
+  UIManager.put(key, CheckIcon())
+  menu.add(JCheckBoxMenuItem("checkIcon test"))
+  UIManager.put(key, defIcon)
+
+  val menu2 = JMenu("JMenu2")
+  menu2.add(JCheckBoxMenuItem("setIcon").also {
+    it.icon = CheckIcon()
+  })
+  menuBar.add(menu)
+  menuBar.add(menu2)
+  return menuBar
+}
+
+private class CheckIcon : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    val m = (c as? AbstractButton)?.getModel() ?: return
+    val m = (c as? AbstractButton)?.model ?: return
     val g2 = g.create() as? Graphics2D ?: return
     g2.translate(x, y)
-    g2.setPaint(if (m.isSelected()) Color.ORANGE else Color.GRAY)
+    g2.paint = if (m.isSelected) Color.ORANGE else Color.GRAY
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-    g2.fillOval(1, 1, getIconWidth() - 2, getIconHeight() - 2)
+    g2.fillOval(1, 1, iconWidth - 2, iconHeight - 2)
     g2.dispose()
   }
 
@@ -62,8 +61,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
-      setJMenuBar(MainPanel.createMenuBar())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
--- TerminateEdit/src/main/kotlin/example/App.kt
+++ TerminateEdit/src/main/kotlin/example/App.kt
@@ -77,10 +77,11 @@ fun makeUI(): Component {
     }
   }
 
-  val tefCheck = JCheckBox("terminateEditOnFocusLost", true)
+  val key = "terminateEditOnFocusLost"
+  val tefCheck = JCheckBox(key, true)
   tefCheck.addActionListener { e ->
     val isSelected = (e.source as? JCheckBox)?.isSelected == true
-    table.putClientProperty("terminateEditOnFocusLost", isSelected)
+    table.putClientProperty(key, isSelected)
   }
   val box = JPanel(GridLayout(4, 0))
   box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
