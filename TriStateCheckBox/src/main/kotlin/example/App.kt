1c85f21b2e86aa1c139d659cc4b90c5e8c1975e4
aterai <aterai@outlook.com>
2020-05-04 19:16:34 +0900

    refactor: use of setter method instead of property access syntax

--- TriStateCheckBox/src/main/kotlin/example/App.kt
+++ TriStateCheckBox/src/main/kotlin/example/App.kt
@@ -3,83 +3,86 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.event.TableModelEvent
+import javax.swing.event.TableModelListener
 import javax.swing.plaf.ColorUIResource
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.JTableHeader
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
+import javax.swing.table.TableColumn
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val checkBox = TriStateCheckBox("TriState JCheckBox")
-  private val columnNames = arrayOf(Status.INDETERMINATE, "Integer", "String")
-  private val data = arrayOf(
-    arrayOf(true, 1, "BBB"),
-    arrayOf(false, 12, "AAA"),
-    arrayOf(true, 2, "DDD"),
-    arrayOf(false, 5, "CCC"),
-    arrayOf(true, 3, "EEE"),
-    arrayOf(false, 6, "GGG"),
-    arrayOf(true, 4, "FFF"),
-    arrayOf(false, 7, "HHH")
-  )
-  private val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  private val table = object : JTable(model) {
-    @Transient
-    protected var handler: HeaderCheckBoxHandler? = null
+private const val CHECKBOX_COLUMN = 0
+private val checkBox = TriStateCheckBox("TriState JCheckBox")
+private val columnNames = arrayOf(Status.INDETERMINATE, "Integer", "String")
+private val data = arrayOf(
+  arrayOf(true, 1, "BBB"),
+  arrayOf(false, 12, "AAA"),
+  arrayOf(true, 2, "DDD"),
+  arrayOf(false, 5, "CCC"),
+  arrayOf(true, 3, "EEE"),
+  arrayOf(false, 6, "GGG"),
+  arrayOf(true, 4, "FFF"),
+  arrayOf(false, 7, "HHH")
+)
+private val model = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+}
+private val table = object : JTable(model) {
+  @Transient
+  protected var handler: HeaderCheckBoxHandler? = null
 
-    override fun updateUI() {
-      // [JDK-6788475] Changing to Nimbus LAF and back doesn't reset look and feel of JTable completely
-      // https://bugs.openjdk.java.net/browse/JDK-6788475
-      // XXX: set dummy ColorUIResource
-      setSelectionForeground(ColorUIResource(Color.RED))
-      setSelectionBackground(ColorUIResource(Color.RED))
-      getTableHeader()?.removeMouseListener(handler)
-      getModel()?.removeTableModelListener(handler)
-      super.updateUI()
+  override fun updateUI() {
+    // [JDK-6788475] Changing to Nimbus LAF and back doesn't reset look and feel of JTable completely
+    // https://bugs.openjdk.java.net/browse/JDK-6788475
+    // XXX: set dummy ColorUIResource
+    setSelectionForeground(ColorUIResource(Color.RED))
+    setSelectionBackground(ColorUIResource(Color.RED))
+    getTableHeader()?.removeMouseListener(handler)
+    model?.removeTableModelListener(handler)
+    super.updateUI()
 
-      getModel()?.also {
-        for (i in 0 until it.getColumnCount()) {
-          val r = getDefaultRenderer(it.getColumnClass(i)) as? Component ?: continue
-          SwingUtilities.updateComponentTreeUI(r)
-        }
-        handler = HeaderCheckBoxHandler(this, CHECKBOX_COLUMN)
-        it.addTableModelListener(handler)
-        getTableHeader().addMouseListener(handler)
+    model?.also {
+      for (i in 0 until it.columnCount) {
+        val r = getDefaultRenderer(it.getColumnClass(i)) as? Component ?: continue
+        SwingUtilities.updateComponentTreeUI(r)
       }
-      getColumnModel().getColumn(CHECKBOX_COLUMN).also {
-        it.setHeaderRenderer(HeaderRenderer())
-        it.setHeaderValue(Status.INDETERMINATE)
-      }
+      handler = HeaderCheckBoxHandler(this, CHECKBOX_COLUMN)
+      it.addTableModelListener(handler)
+      getTableHeader().addMouseListener(handler)
     }
-
-    override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
-      val c = super.prepareEditor(editor, row, column)
-      (c as? JCheckBox)?.also {
-        it.setBackground(getSelectionBackground())
-        it.setBorderPainted(true)
-      }
-      return c
+    getColumnModel().getColumn(CHECKBOX_COLUMN).also {
+      it.headerRenderer = HeaderRenderer()
+      it.headerValue = Status.INDETERMINATE
     }
   }
 
-  init {
-    add(JTabbedPane().also {
-      val p = JPanel()
-      p.add(checkBox)
-      it.addTab("JCheckBox", p)
-      it.addTab("JTableHeader", JScrollPane(table))
-    })
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  companion object {
-    private const val CHECKBOX_COLUMN = 0
+  override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
+    val c = super.prepareEditor(editor, row, column)
+    (c as? JCheckBox)?.also {
+      it.background = getSelectionBackground()
+      it.isBorderPainted = true
+    }
+    return c
   }
 }
 
-internal class HeaderRenderer : TableCellRenderer {
+fun makeUI() = JTabbedPane().also {
+  val mb = JMenuBar()
+  mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+  EventQueue.invokeLater { it.rootPane.jMenuBar = mb }
+
+  val p = JPanel()
+  p.add(checkBox)
+  it.addTab("JCheckBox", p)
+  it.addTab("JTableHeader", JScrollPane(table))
+  it.preferredSize = Dimension(320, 240)
+}
+
+private class HeaderRenderer : TableCellRenderer {
   private val check = TriStateCheckBox("Check All")
 
   override fun getTableCellRendererComponent(
@@ -90,24 +93,24 @@ internal class HeaderRenderer : TableCellRenderer {
     row: Int,
     column: Int
   ): Component {
-    val r = table.getTableHeader().getDefaultRenderer()
+    val r = table.tableHeader.defaultRenderer
     val c = r.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
 
-    check.setOpaque(false)
+    check.isOpaque = false
     if (value is Status) {
       check.updateStatus(value)
     } else {
-      check.setSelected(true)
+      check.isSelected = true
     }
     (c as? JLabel)?.also {
-      it.setIcon(ComponentIcon(check))
-      it.setText(null) // XXX: Nimbus???
+      it.icon = ComponentIcon(check)
+      it.text = null // XXX: Nimbus???
     }
     return c
   }
 }
 
-internal class TriStateActionListener : ActionListener {
+private class TriStateActionListener : ActionListener {
   private var currentIcon: Icon? = null
 
   fun setIcon(icon: Icon?) {
@@ -115,71 +118,71 @@ internal class TriStateActionListener : ActionListener {
   }
 
   override fun actionPerformed(e: ActionEvent) {
-    val cb = e.getSource() as? JCheckBox ?: return
-    if (cb.isSelected()) {
-      cb.getIcon()?.run {
-        cb.setIcon(null)
-        cb.setSelected(false)
+    val cb = e.source as? JCheckBox ?: return
+    if (cb.isSelected) {
+      cb.icon?.run {
+        cb.icon = null
+        cb.isSelected = false
       }
     } else {
-      cb.setIcon(currentIcon)
+      cb.icon = currentIcon
     }
   }
 }
 
-internal class TriStateCheckBox(title: String) : JCheckBox(title) {
+private class TriStateCheckBox(title: String) : JCheckBox(title) {
   private var listener: TriStateActionListener? = null
   private var currentIcon: Icon? = null
 
   fun updateStatus(s: Status) {
     when (s) {
       Status.SELECTED -> {
-        setSelected(true)
-        setIcon(null)
+        isSelected = true
+        icon = null
       }
       Status.DESELECTED -> {
-        setSelected(false)
-        setIcon(null)
+        isSelected = false
+        icon = null
       }
       Status.INDETERMINATE -> {
-        setSelected(false)
-        setIcon(currentIcon)
+        isSelected = false
+        icon = currentIcon
       }
       // else -> throw AssertionError("Unknown Status")
     }
   }
 
   override fun updateUI() {
-    setIcon(null)
+    icon = null
     removeActionListener(listener)
     super.updateUI()
-    val iicon = IndeterminateIcon()
+    val indeterminateIcon = IndeterminateIcon()
     val al = TriStateActionListener()
-    al.setIcon(iicon)
+    al.setIcon(indeterminateIcon)
     listener = al
-    currentIcon = iicon
+    currentIcon = indeterminateIcon
     addActionListener(listener)
-    getIcon()?.run {
-      setIcon(currentIcon)
+    icon?.run {
+      icon = currentIcon
     }
   }
 }
 
-internal class IndeterminateIcon : Icon {
+private class IndeterminateIcon : Icon {
   private val icon = UIManager.getIcon("CheckBox.icon")
 
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
     val g2 = g.create() as Graphics2D
     g2.translate(x, y)
     icon.paintIcon(c, g2, 0, 0)
-    g2.setPaint(FOREGROUND)
-    g2.fillRect(SIDE_MARGIN, (getIconHeight() - HEIGHT) / 2, getIconWidth() - SIDE_MARGIN - SIDE_MARGIN, HEIGHT)
+    g2.paint = FOREGROUND
+    g2.fillRect(SIDE_MARGIN, (iconHeight - HEIGHT) / 2, iconWidth - SIDE_MARGIN - SIDE_MARGIN, HEIGHT)
     g2.dispose()
   }
 
-  override fun getIconWidth() = icon.getIconWidth()
+  override fun getIconWidth() = icon.iconWidth
 
-  override fun getIconHeight() = icon.getIconHeight()
+  override fun getIconHeight() = icon.iconHeight
 
   companion object {
     private val FOREGROUND = Color.BLACK // TEST: UIManager.getColor("CheckBox.foreground");
@@ -188,41 +191,40 @@ internal class IndeterminateIcon : Icon {
   }
 }
 
-internal class ComponentIcon(private val cmp: Component) : Icon {
+private class ComponentIcon(private val cmp: Component) : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    SwingUtilities.paintComponent(g, cmp, c.getParent(), x, y, getIconWidth(), getIconHeight())
+    SwingUtilities.paintComponent(g, cmp, c.parent, x, y, iconWidth, iconHeight)
   }
 
-  override fun getIconWidth() = cmp.getPreferredSize().width
+  override fun getIconWidth() = cmp.preferredSize.width
 
-  override fun getIconHeight() = cmp.getPreferredSize().height
+  override fun getIconHeight() = cmp.preferredSize.height
 }
 
-internal enum class Status {
+private enum class Status {
   SELECTED, DESELECTED, INDETERMINATE
 }
 
-// @see https://java.net/projects/swingset3/sources/svn/content/trunk/SwingSet3/src/com/sun/swingset3/SwingSet3.java
-internal object LookAndFeelUtil {
-  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.getName()
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
 
   fun createLookAndFeelMenu(): JMenu {
     val menu = JMenu("LookAndFeel")
     val lafGroup = ButtonGroup()
     for (lafInfo in UIManager.getInstalledLookAndFeels()) {
-      menu.add(createLookAndFeelItem(lafInfo.getName(), lafInfo.getClassName(), lafGroup))
+      menu.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafGroup))
     }
     return menu
   }
 
   private fun createLookAndFeelItem(lafName: String, lafClassName: String, lafGroup: ButtonGroup): JMenuItem {
     val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
-    lafItem.setActionCommand(lafClassName)
-    lafItem.setHideActionText(true)
+    lafItem.actionCommand = lafClassName
+    lafItem.hideActionText = true
     lafItem.addActionListener {
-      val m = lafGroup.getSelection()
+      val m = lafGroup.selection
       runCatching {
-        setLookAndFeel(m.getActionCommand())
+        setLookAndFeel(m.actionCommand)
       }.onFailure {
         it.printStackTrace()
         Toolkit.getDefaultToolkit().beep()
@@ -244,7 +246,6 @@ internal object LookAndFeelUtil {
       UIManager.setLookAndFeel(lookAndFeel)
       LookAndFeelUtil.lookAndFeel = lookAndFeel
       updateLookAndFeel()
-      // firePropertyChange("lookAndFeel", oldLookAndFeel, lookAndFeel)
     }
   }
 
@@ -253,7 +254,62 @@ internal object LookAndFeelUtil {
       SwingUtilities.updateComponentTreeUI(window)
     }
   }
-} /* Singleton */
+}
+
+private class HeaderCheckBoxHandler(val table: JTable, val targetColumnIndex: Int) : MouseAdapter(), TableModelListener {
+
+  override fun tableChanged(e: TableModelEvent) {
+    if (e.type == TableModelEvent.UPDATE && e.column == targetColumnIndex) {
+      val vci = table.convertColumnIndexToView(targetColumnIndex)
+      val column = table.columnModel.getColumn(vci)
+      val status = column.headerValue
+      val m = table.model
+      if (m is DefaultTableModel && fireUpdateEvent(m, column, status)) {
+        val h = table.tableHeader
+        h.repaint(h.getHeaderRect(vci))
+      }
+    }
+  }
+
+  private fun fireUpdateEvent(m: DefaultTableModel, column: TableColumn, status: Any): Boolean {
+    return if (Status.INDETERMINATE == status) {
+      // val l = (m.getDataVector() as Vector<*>).stream()
+      //     .map { v -> (v as Vector<*>).get(targetColumnIndex) as Boolean }
+      //     .distinct()
+      //     .collect(Collectors.toList())
+      val l = m.dataVector.mapNotNull { (it as? List<*>)?.get(targetColumnIndex) as? Boolean }.distinct()
+      val isOnlyOneSelected = l.size == 1
+      if (isOnlyOneSelected) {
+        // column.setHeaderValue(if (l.get(0)) Status.SELECTED else Status.DESELECTED)
+        column.headerValue = if (l.first()) Status.SELECTED else Status.DESELECTED
+        true
+      } else {
+        false
+      }
+    } else {
+      column.headerValue = Status.INDETERMINATE
+      true
+    }
+  }
+
+  override fun mouseClicked(e: MouseEvent) {
+    val header = e.component as? JTableHeader ?: return
+    val tbl = header.table
+    val columnModel = tbl.columnModel
+    val m = tbl.model
+    val vci = columnModel.getColumnIndexAtX(e.x)
+    val mci = tbl.convertColumnIndexToModel(vci)
+    if (mci == targetColumnIndex && m.rowCount > 0) {
+      val column = columnModel.getColumn(vci)
+      val b = Status.DESELECTED === column.headerValue
+      for (i in 0 until m.rowCount) {
+        m.setValueAt(b, i, mci)
+      }
+      column.headerValue = if (b) Status.SELECTED else Status.DESELECTED
+      // header.repaint()
+    }
+  }
+}
 
 fun main() {
   EventQueue.invokeLater {
@@ -265,7 +321,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
