14de2bca799c85ed947bdfaa91b6bec3e5713071
aterai <aterai@outlook.com>
2020-05-06 18:03:31 +0900

    add: FileChooserLayoutOrientation

---
+++ FileChooserLayoutOrientation/src/main/kotlin/example/App.kt
@@ -0,0 +1,122 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.HierarchyEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val log = JTextArea()
+
+  val button1 = JButton("Default")
+  button1.addActionListener {
+    val chooser = JFileChooser()
+    val retValue = chooser.showOpenDialog(log.rootPane)
+    if (retValue == JFileChooser.APPROVE_OPTION) {
+      log.text = chooser.selectedFile.absolutePath
+    }
+  }
+
+  val button2 = JButton("LayoutOrientation: VERTICAL")
+  button2.addActionListener {
+    val chooser = JFileChooser()
+    descendants(chooser)
+      .filterIsInstance<JList<*>>()
+      .first().also {
+        it.addHierarchyListener { e ->
+          if (e.changeFlags and HierarchyEvent.SHOWING_CHANGED.toLong() != 0L && e.component.isShowing) {
+            it.putClientProperty("List.isFileList", false)
+            it.layoutOrientation = JList.VERTICAL
+          }
+        }
+      }
+    val retValue = chooser.showOpenDialog(log.rootPane)
+    if (retValue == JFileChooser.APPROVE_OPTION) {
+      log.text = chooser.selectedFile.absolutePath
+    }
+  }
+
+  val p = JPanel()
+  p.border = BorderFactory.createTitledBorder("JFileChooser")
+  p.add(button1)
+  p.add(button2)
+
+  return JPanel(BorderLayout()).also {
+    val mb = JMenuBar()
+    mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+    SwingUtilities.invokeLater { it.rootPane.jMenuBar = mb }
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(log))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun descendants(parent: Container): List<Component> = parent.components
+  .filterIsInstance<Container>()
+  .flatMap { listOf(it) + descendants(it) }
+
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(lafName: String, lafClassName: String, lafRadioGroup: ButtonGroup): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.actionCommand = lafClassName
+    lafItem.mnemonic = lafName.codePointAt(0)
+    lafItem.hideActionText = true
+    lafItem.addActionListener {
+      val m = lafRadioGroup.selection
+      runCatching {
+        setLookAndFeel(m.actionCommand)
+      }.onFailure {
+        it.printStackTrace()
+        Toolkit.getDefaultToolkit().beep()
+      }
+    }
+    lafRadioGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
