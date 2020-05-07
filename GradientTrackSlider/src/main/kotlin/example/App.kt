cf49f55ce3b41a22f1e879a238638cd627890f5b
aterai <aterai@outlook.com>
2020-05-07 20:00:05 +0900

    refactor: use of setter method instead of property access syntax

--- GradientTrackSlider/src/main/kotlin/example/App.kt
+++ GradientTrackSlider/src/main/kotlin/example/App.kt
@@ -10,77 +10,76 @@ import java.awt.image.PixelGrabber
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.metal.MetalSliderUI
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    UIManager.put("Slider.horizontalThumbIcon", object : Icon {
-      override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) { /* Empty icon */ }
+private val TEXTURE = TextureUtils.createCheckerTexture(6, Color(200, 150, 100, 50))
 
-      override fun getIconWidth() = 15
+fun makeUI(): Component {
+  UIManager.put("Slider.horizontalThumbIcon", object : Icon {
+    override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) { /* Empty icon */ }
 
-      override fun getIconHeight() = 64
-    })
-    println(UIManager.get("Slider.trackWidth"))
-    println(UIManager.get("Slider.majorTickLength"))
-    println(UIManager.getInt("Slider.trackWidth"))
-    println(UIManager.getInt("Slider.majorTickLength"))
-    UIManager.put("Slider.trackWidth", 64)
-    UIManager.put("Slider.majorTickLength", 6)
+    override fun getIconWidth() = 15
 
-    val slider0 = makeSlider()
-    val slider1 = makeSlider()
-    slider1.setUI(GradientPalletSliderUI())
-    slider1.setModel(slider0.getModel())
+    override fun getIconHeight() = 64
+  })
+  // println(UIManager.get("Slider.trackWidth"))
+  // println(UIManager.get("Slider.majorTickLength"))
+  // println(UIManager.getInt("Slider.trackWidth"))
+  // println(UIManager.getInt("Slider.majorTickLength"))
+  UIManager.put("Slider.trackWidth", 64)
+  UIManager.put("Slider.majorTickLength", 6)
 
-    val box = Box.createVerticalBox()
-    box.add(makeTitledPanel("Default:", slider0))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("Gradient translucent track JSlider:", slider1))
-    box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    add(box, BorderLayout.NORTH)
-    setOpaque(false)
-    setPreferredSize(Dimension(320, 240))
-  }
+  val slider0 = makeSlider()
+  val slider1 = makeSlider()
+  slider1.ui = GradientPalletSliderUI()
+  slider1.model = slider0.model
 
-  override fun paintComponent(g: Graphics) {
-    val g2 = g.create() as? Graphics2D ?: return
-    g2.setPaint(TEXTURE)
-    g2.fillRect(0, 0, getWidth(), getHeight())
-    g2.dispose()
-    super.paintComponent(g)
-  }
+  val box = Box.createVerticalBox()
+  box.add(makeTitledPanel("Default:", slider0))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("Gradient translucent track JSlider:", slider1))
+  box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
 
-  private fun makeSlider(): JSlider {
-    val slider = JSlider(SwingConstants.HORIZONTAL, 0, 100, 50)
-    slider.setBackground(Color.GRAY)
-    slider.setOpaque(false)
-    val ma = object : MouseAdapter() {
-      override fun mouseDragged(e: MouseEvent) {
-        e.getComponent().repaint()
-      }
-
-      override fun mouseWheelMoved(e: MouseWheelEvent) {
-        (e.getComponent() as? JSlider)?.getModel()?.also {
-          it.setValue(it.getValue() - e.getWheelRotation())
-        }
-      }
+  val p = object : JPanel(BorderLayout()) {
+    override fun paintComponent(g: Graphics) {
+      val g2 = g.create() as? Graphics2D ?: return
+      g2.paint = TEXTURE
+      g2.fillRect(0, 0, width, height)
+      g2.dispose()
+      super.paintComponent(g)
     }
-    slider.addMouseMotionListener(ma)
-    slider.addMouseWheelListener(ma)
-    return slider
   }
-
-  private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-    it.setOpaque(false)
-    it.add(c)
-  }
-
-  companion object {
-    private val TEXTURE = TextureUtils.createCheckerTexture(6, Color(200, 150, 100, 50))
-  }
+  p.add(box, BorderLayout.NORTH)
+  p.isOpaque = false
+  p.preferredSize = Dimension(320, 240)
+  return p
 }
 
-class GradientPalletSliderUI : MetalSliderUI() {
+private fun makeSlider(): JSlider {
+  val slider = JSlider(SwingConstants.HORIZONTAL, 0, 100, 50)
+  slider.background = Color.GRAY
+  slider.isOpaque = false
+  val ma = object : MouseAdapter() {
+    override fun mouseDragged(e: MouseEvent) {
+      e.component.repaint()
+    }
+
+    override fun mouseWheelMoved(e: MouseWheelEvent) {
+      (e.component as? JSlider)?.model?.also {
+        it.value = it.value - e.wheelRotation
+      }
+    }
+  }
+  slider.addMouseMotionListener(ma)
+  slider.addMouseWheelListener(ma)
+  return slider
+}
+
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.isOpaque = false
+  it.add(c)
+}
+
+private class GradientPalletSliderUI : MetalSliderUI() {
   private var controlDarkShadow = Color(0x64_64_64) // MetalLookAndFeel.getControlDarkShadow()
   private var controlHighlight = Color(0xC8_FF_C8) // MetalLookAndFeel.getControlHighlight()
   private var controlShadow = Color(0x00_64_00) // MetalLookAndFeel.getControlShadow()
@@ -95,14 +94,14 @@ class GradientPalletSliderUI : MetalSliderUI() {
     var trackTop = 0
     val trackRight: Int
     val trackBottom: Int
-    if (slider.getOrientation() == SwingConstants.HORIZONTAL) {
-      trackBottom = trackRect.height - 1 - getThumbOverhang()
-      trackTop = trackBottom - getTrackWidth() + 1
+    if (slider.orientation == SwingConstants.HORIZONTAL) {
+      trackBottom = trackRect.height - 1 - thumbOverhang
+      trackTop = trackBottom - trackWidth + 1
       trackRight = trackRect.width - 1
     } else {
       // if (leftToRight) {
-      trackLeft = trackRect.width - getThumbOverhang() - getTrackWidth()
-      trackRight = trackRect.width - getThumbOverhang() - 1
+      trackLeft = trackRect.width - thumbOverhang - trackWidth
+      trackRight = trackRect.width - thumbOverhang - 1
       // } else {
       //   trackLeft = getThumbOverhang()
       //   trackRight = getThumbOverhang() + getTrackWidth() - 1
@@ -123,19 +122,19 @@ class GradientPalletSliderUI : MetalSliderUI() {
   }
 
   private fun paintTrackBase(g: Graphics, trackTop: Int, trackLeft: Int, trackBottom: Int, trackRight: Int) {
-    if (slider.isEnabled()) {
-      g.setColor(controlDarkShadow)
+    if (slider.isEnabled) {
+      g.color = controlDarkShadow
       g.drawRect(trackLeft, trackTop, trackRight - trackLeft - 1, trackBottom - trackTop - 1)
 
-      g.setColor(controlHighlight)
+      g.color = controlHighlight
       g.drawLine(trackLeft + 1, trackBottom, trackRight, trackBottom)
       g.drawLine(trackRight, trackTop + 1, trackRight, trackBottom)
 
-      g.setColor(controlShadow)
+      g.color = controlShadow
       g.drawLine(trackLeft + 1, trackTop + 1, trackRight - 2, trackTop + 1)
       g.drawLine(trackLeft + 1, trackTop + 1, trackLeft + 1, trackBottom - 2)
     } else {
-      g.setColor(controlShadow)
+      g.color = controlShadow
       g.drawRect(trackLeft, trackTop, trackRight - trackLeft - 1, trackBottom - trackTop - 1)
     }
   }
@@ -147,7 +146,7 @@ class GradientPalletSliderUI : MetalSliderUI() {
     val fillBottom: Int
     val fillRight: Int
 
-    if (slider.getOrientation() == SwingConstants.HORIZONTAL) {
+    if (slider.orientation == SwingConstants.HORIZONTAL) {
       middleOfThumb = thumbRect.x + thumbRect.width / 2
       middleOfThumb -= trackRect.x // To compensate for the g.translate()
       fillTop = trackTop + 1
@@ -163,12 +162,12 @@ class GradientPalletSliderUI : MetalSliderUI() {
       fillBottom = trackBottom - 1
     }
 
-    if (slider.isEnabled()) {
+    if (slider.isEnabled) {
       val x = (fillRight - fillLeft) / (trackRight - trackLeft).toFloat()
-      g.setColor(GradientPalletUtils.getColorFromPallet(GRADIENT_PALLET, x, 0x64 shl 24))
+      g.color = GradientPalletUtils.getColorFromPallet(GRADIENT_PALLET, x, 0x64 shl 24)
       g.fillRect(fillLeft + 1, fillTop + 1, fillRight - fillLeft, fillBottom - fillTop)
     } else {
-      g.setColor(controlShadow)
+      g.color = controlShadow
       g.fillRect(fillLeft, fillTop, fillRight - fillLeft, trackBottom - trackTop)
     }
   }
@@ -176,7 +175,7 @@ class GradientPalletSliderUI : MetalSliderUI() {
   private fun paintTrackHighlight(g: Graphics, trackTop: Int, trackLeft: Int, trackBottom: Int, trackRight: Int) {
     var yy = trackTop + (trackBottom - trackTop) / 2
     for (i in 10 downTo 0) {
-      g.setColor(makeColor(i * .07f))
+      g.color = makeColor(i * .07f)
       g.drawLine(trackLeft + 2, yy, trackRight - trackLeft - 2, yy)
       yy--
     }
@@ -189,7 +188,7 @@ class GradientPalletSliderUI : MetalSliderUI() {
   }
 }
 
-object GradientPalletUtils {
+private object GradientPalletUtils {
   fun makeGradientPallet(): IntArray {
     val image = BufferedImage(100, 1, BufferedImage.TYPE_INT_RGB)
     val g2 = image.createGraphics()
@@ -197,7 +196,7 @@ object GradientPalletUtils {
     val end = Point2D.Float(99f, 0f)
     val dist = floatArrayOf(.0f, .5f, 1f)
     val colors = arrayOf(Color.RED, Color.YELLOW, Color.GREEN)
-    g2.setPaint(LinearGradientPaint(start, end, dist, colors))
+    g2.paint = LinearGradientPaint(start, end, dist, colors)
     g2.fillRect(0, 0, 100, 1)
     g2.dispose()
 
@@ -220,14 +219,14 @@ object GradientPalletUtils {
     // int alpha = 0x64 << 24;
     return Color(alpha or pix, true)
   }
-} /* HideUtilityClassConstructor */
+}
 
-object TextureUtils {
+private object TextureUtils {
   fun createCheckerTexture(cs: Int, color: Color): TexturePaint {
     val size = cs * cs
     val img = BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB)
     val g2 = img.createGraphics()
-    g2.setPaint(color)
+    g2.paint = color
     g2.fillRect(0, 0, size, size)
     var i = 0
     while (i * cs < size) {
@@ -243,7 +242,7 @@ object TextureUtils {
     g2.dispose()
     return TexturePaint(img, Rectangle(size, size))
   }
-} /* HideUtilityClassConstructor */
+}
 
 fun main() {
   EventQueue.invokeLater {
@@ -255,7 +254,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
