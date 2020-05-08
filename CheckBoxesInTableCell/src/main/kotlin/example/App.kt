3d00e6079229f438cefdf3280edd6e80e23abe91
aterai <aterai@outlook.com>
2020-05-08 20:33:30 +0900

    refactor: use of setter method instead of property access syntax

--- CheckBoxesInTableCell/src/main/kotlin/example/App.kt
+++ CheckBoxesInTableCell/src/main/kotlin/example/App.kt
@@ -8,27 +8,27 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("user", "rwx")
-    val data = arrayOf(
-      arrayOf("owner", 7),
-      arrayOf("group", 6),
-      arrayOf("other", 5))
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+fun makeUI(): Component {
+  val columnNames = arrayOf("user", "rwx")
+  val data = arrayOf(
+    arrayOf("owner", 7),
+    arrayOf("group", 6),
+    arrayOf("other", 5))
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = object : JTable(model) {
+    override fun updateUI() {
+      super.updateUI()
+      getColumnModel().getColumn(1).cellRenderer = CheckBoxesRenderer()
+      getColumnModel().getColumn(1).cellEditor = CheckBoxesEditor()
     }
-    val table = object : JTable(model) {
-      override fun updateUI() {
-        super.updateUI()
-        getColumnModel().getColumn(1).setCellRenderer(CheckBoxesRenderer())
-        getColumnModel().getColumn(1).setCellEditor(CheckBoxesEditor())
-      }
-    }
-    table.putClientProperty("terminateEditOnFocusLost", true)
+  }
+  table.putClientProperty("terminateEditOnFocusLost", true)
 
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
@@ -39,9 +39,9 @@ open class CheckBoxesPanel : JPanel() {
 
   override fun updateUI() {
     super.updateUI()
-    setOpaque(false)
-    setBackground(bgc)
-    setLayout(BoxLayout(this, BoxLayout.X_AXIS))
+    isOpaque = false
+    background = bgc
+    layout = BoxLayout(this, BoxLayout.X_AXIS)
     EventQueue.invokeLater { initButtons() }
   }
 
@@ -59,23 +59,23 @@ open class CheckBoxesPanel : JPanel() {
   fun updateButtons(value: Any?) {
     initButtons()
     val i = value as? Int ?: 0
-    buttons[0].setSelected(i and (1 shl 2) != 0)
-    buttons[1].setSelected(i and (1 shl 1) != 0)
-    buttons[2].setSelected(i and (1 shl 0) != 0)
+    buttons[0].isSelected = i and (1 shl 2) != 0
+    buttons[1].isSelected = i and (1 shl 1) != 0
+    buttons[2].isSelected = i and (1 shl 0) != 0
   }
 
   private fun makeCheckBox(title: String) = JCheckBox(title).also {
-    it.setOpaque(false)
-    it.setFocusable(false)
-    it.setRolloverEnabled(false)
-    it.setBackground(bgc)
+    it.isOpaque = false
+    it.isFocusable = false
+    it.isRolloverEnabled = false
+    it.background = bgc
   }
 }
 
 class CheckBoxesRenderer : CheckBoxesPanel(), TableCellRenderer {
   override fun updateUI() {
     super.updateUI()
-    setName("Table.cellRenderer")
+    name = "Table.cellRenderer"
   }
 
   override fun getTableCellRendererComponent(
@@ -97,12 +97,12 @@ class CheckBoxesEditor : AbstractCellEditor(), TableCellEditor {
     override fun updateUI() {
       super.updateUI()
       EventQueue.invokeLater {
-        val am = getActionMap()
+        val am = actionMap
         for (i in buttons.indices) {
           val t = titles[i]
           am.put(t, object : AbstractAction(t) {
             override fun actionPerformed(e: ActionEvent) {
-              buttons.firstOrNull { it.getText() == t }?.doClick()
+              buttons.firstOrNull { it.text == t }?.doClick()
               fireEditingStopped()
             }
           })
@@ -128,9 +128,9 @@ class CheckBoxesEditor : AbstractCellEditor(), TableCellEditor {
 
   override fun getCellEditorValue(): Any {
     var i = 0
-    i = if (panel.buttons[0].isSelected()) 1 shl 2 or i else i
-    i = if (panel.buttons[1].isSelected()) 1 shl 1 or i else i
-    i = if (panel.buttons[2].isSelected()) 1 shl 0 or i else i
+    i = if (panel.buttons[0].isSelected) 1 shl 2 or i else i
+    i = if (panel.buttons[1].isSelected) 1 shl 1 or i else i
+    i = if (panel.buttons[2].isSelected) 1 shl 0 or i else i
     return i
   }
 }
@@ -145,7 +145,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
