a06ca0bcf07d88eba621c3208e68717c836edfe2
aterai <aterai@outlook.com>
2020-05-10 03:14:43 +0900

    refactor: use of setter method instead of property access syntax

--- AllowsAbsolutePositioning/src/main/kotlin/example/App.kt
+++ AllowsAbsolutePositioning/src/main/kotlin/example/App.kt
@@ -7,61 +7,60 @@ import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicScrollBarUI
 
-class MainPanel : JPanel(BorderLayout(5, 5)) {
-  init {
-    // https://docs.oracle.com/javase/8/docs/api/javax/swing/plaf/synth/doc-files/componentProperties.html
-    UIManager.put("ScrollBar.allowsAbsolutePositioning", true)
+fun makeUI(): Component {
+  UIManager.put("ScrollBar.allowsAbsolutePositioning", true)
 
-    val help = "middle mouse click in the track will set the position of the track to where the mouse is.\n"
-    val txt = help.repeat(100)
+  val help = "middle mouse click in the track will set the position of the track to where the mouse is.\n"
+  val txt = help.repeat(100)
 
-    val scroll = JScrollPane(JTextArea("override TrackListener#mousePressed(...)\n$txt"))
-    scroll.setVerticalScrollBar(object : JScrollBar(Adjustable.VERTICAL) {
-      override fun updateUI() {
-        super.updateUI()
-        if (getUI() is WindowsScrollBarUI) {
-          setUI(AbsolutePositioningWindowsScrollBarUI())
-        } else {
-          setUI(AbsolutePositioningBasicScrollBarUI())
-        }
-        putClientProperty("JScrollBar.fastWheelScrolling", true)
+  val scroll = JScrollPane(JTextArea("override TrackListener#mousePressed(...)\n$txt"))
+  scroll.verticalScrollBar = object : JScrollBar(Adjustable.VERTICAL) {
+    override fun updateUI() {
+      super.updateUI()
+      if (getUI() is WindowsScrollBarUI) {
+        setUI(AbsolutePositioningWindowsScrollBarUI())
+      } else {
+        setUI(AbsolutePositioningBasicScrollBarUI())
       }
-    })
+      putClientProperty("JScrollBar.fastWheelScrolling", true)
+    }
+  }
 
-    scroll.setHorizontalScrollBar(object : JScrollBar(Adjustable.HORIZONTAL) {
-      override fun updateUI() {
-        super.updateUI()
-        if (getUI() is WindowsScrollBarUI) {
-          setUI(AbsolutePositioningWindowsScrollBarUI())
-        } else {
-          setUI(AbsolutePositioningBasicScrollBarUI())
-        }
-        putClientProperty("JScrollBar.fastWheelScrolling", true)
+  scroll.horizontalScrollBar = object : JScrollBar(Adjustable.HORIZONTAL) {
+    override fun updateUI() {
+      super.updateUI()
+      if (getUI() is WindowsScrollBarUI) {
+        setUI(AbsolutePositioningWindowsScrollBarUI())
+      } else {
+        setUI(AbsolutePositioningBasicScrollBarUI())
       }
-    })
+      putClientProperty("JScrollBar.fastWheelScrolling", true)
+    }
+  }
 
-    val p = JPanel(GridLayout(1, 2))
-    p.add(JScrollPane(JTextArea(txt)))
-    p.add(scroll)
+  val p = JPanel(GridLayout(1, 2))
+  p.add(JScrollPane(JTextArea(txt)))
+  p.add(scroll)
 
-    add(JLabel("ScrollBar.allowsAbsolutePositioning: true"), BorderLayout.NORTH)
-    add(p)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout(5, 5)).also {
+    it.add(JLabel("ScrollBar.allowsAbsolutePositioning: true"), BorderLayout.NORTH)
+    it.add(p)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class AbsolutePositioningWindowsScrollBarUI : WindowsScrollBarUI() {
+private class AbsolutePositioningWindowsScrollBarUI : WindowsScrollBarUI() {
   override fun createTrackListener(): TrackListener {
     return object : BasicScrollBarUI.TrackListener() {
       override fun mousePressed(e: MouseEvent) {
         if (SwingUtilities.isLeftMouseButton(e)) {
           super.mousePressed(MouseEvent(
-            e.getComponent(), e.getID(), e.getWhen(),
+            e.component, e.id, e.getWhen(),
             InputEvent.BUTTON2_DOWN_MASK xor InputEvent.BUTTON2_MASK, // e.getModifiers(),
-            e.getX(), e.getY(),
-            e.getXOnScreen(), e.getYOnScreen(),
-            e.getClickCount(),
-            e.isPopupTrigger(),
+            e.x, e.y,
+            e.xOnScreen, e.yOnScreen,
+            e.clickCount,
+            e.isPopupTrigger,
             MouseEvent.BUTTON2))
         } else {
           super.mousePressed(e)
@@ -71,18 +70,18 @@ class AbsolutePositioningWindowsScrollBarUI : WindowsScrollBarUI() {
   }
 }
 
-class AbsolutePositioningBasicScrollBarUI : BasicScrollBarUI() {
+private class AbsolutePositioningBasicScrollBarUI : BasicScrollBarUI() {
   override fun createTrackListener(): TrackListener {
     return object : BasicScrollBarUI.TrackListener() {
       override fun mousePressed(e: MouseEvent) {
         if (SwingUtilities.isLeftMouseButton(e)) {
           super.mousePressed(MouseEvent(
-            e.getComponent(), e.getID(), e.getWhen(),
+            e.component, e.id, e.getWhen(),
             InputEvent.BUTTON2_DOWN_MASK xor InputEvent.BUTTON2_MASK,
-            e.getX(), e.getY(),
-            e.getXOnScreen(), e.getYOnScreen(),
-            e.getClickCount(),
-            e.isPopupTrigger(),
+            e.x, e.y,
+            e.xOnScreen, e.yOnScreen,
+            e.clickCount,
+            e.isPopupTrigger,
             MouseEvent.BUTTON2))
         } else {
           super.mousePressed(e)
@@ -102,7 +101,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
