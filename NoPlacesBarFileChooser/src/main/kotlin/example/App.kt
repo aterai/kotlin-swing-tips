2b3a1ac5eb61de25c5b80eef152f2b69e996a51d
aterai <aterai@outlook.com>
2020-05-12 14:27:36 +0900

    add: NoPlacesBarFileChooser

---
+++ NoPlacesBarFileChooser/src/main/kotlin/example/App.kt
@@ -0,0 +1,57 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val log = JTextArea()
+
+  val button1 = JButton("noPlacesBar")
+  button1.addActionListener {
+    UIManager.put("FileChooser.noPlacesBar", true)
+    val fileChooser = JFileChooser()
+    val retValue = fileChooser.showOpenDialog(log.rootPane)
+    if (retValue == JFileChooser.APPROVE_OPTION) {
+      log.text = fileChooser.selectedFile.absolutePath
+    }
+  }
+
+  val button2 = JButton("Default")
+  button2.addActionListener {
+    UIManager.put("FileChooser.noPlacesBar", false)
+    val fileChooser = JFileChooser()
+    val retValue = fileChooser.showOpenDialog(log.rootPane)
+    if (retValue == JFileChooser.APPROVE_OPTION) {
+      log.text = fileChooser.selectedFile.absolutePath
+    }
+  }
+
+  val p = JPanel()
+  p.border = BorderFactory.createTitledBorder("JFileChooser")
+  p.add(button1)
+  p.add(button2)
+
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(log))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
