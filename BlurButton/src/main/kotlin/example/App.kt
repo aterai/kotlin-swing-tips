f8258f151da8696d087f3830347b551f83d43767
aterai <aterai@outlook.com>
2020-05-12 14:40:23 +0900

    refactor: use of setter method instead of property access syntax

--- BlurButton/src/main/kotlin/example/App.kt
+++ BlurButton/src/main/kotlin/example/App.kt
@@ -6,58 +6,58 @@ import java.awt.image.ConvolveOp
 import java.awt.image.Kernel
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val p0 = JPanel()
-    p0.setBorder(BorderFactory.createTitledBorder("Default JButton"))
-    val b0 = JButton("Default JButton")
-    p0.add(b0)
+fun makeUI(): Component {
+  val p0 = JPanel()
+  p0.border = BorderFactory.createTitledBorder("Default JButton")
+  val b0 = JButton("Default JButton")
+  p0.add(b0)
 
-    val p1 = JPanel()
-    p1.setBorder(BorderFactory.createTitledBorder("Blurred JButton1"))
-    val b1 = BlurJButton("Blurred JButton1")
-    p1.add(b1)
+  val p1 = JPanel()
+  p1.border = BorderFactory.createTitledBorder("Blurred JButton1")
+  val b1 = BlurJButton("Blurred JButton1")
+  p1.add(b1)
 
-    val p2 = JPanel()
-    p2.setBorder(BorderFactory.createTitledBorder("Blurred JButton(ConvolveOp.EDGE_NO_OP)"))
-    val b2 = BlurButton("Blurred JButton2")
-    p2.add(b2)
+  val p2 = JPanel()
+  p2.border = BorderFactory.createTitledBorder("Blurred JButton(ConvolveOp.EDGE_NO_OP)")
+  val b2 = BlurButton("Blurred JButton2")
+  p2.add(b2)
 
-    val box = Box.createVerticalBox()
-    listOf(p0, p1, p2).forEach {
-      box.add(it)
-      box.add(Box.createVerticalStrut(10))
-    }
+  val box = Box.createVerticalBox()
+  listOf(p0, p1, p2).forEach {
+    box.add(it)
+    box.add(Box.createVerticalStrut(10))
+  }
 
-    val button = JToggleButton("setEnabled(false)")
-    button.addActionListener { e ->
-      val f = (e.getSource() as? AbstractButton)?.isSelected() ?: false
-      listOf(b0, b1, b2).forEach { it.setEnabled(!f) }
-    }
+  val button = JToggleButton("setEnabled(false)")
+  button.addActionListener { e ->
+    val f = (e.source as? AbstractButton)?.isSelected ?: false
+    listOf(b0, b1, b2).forEach { it.isEnabled = !f }
+  }
 
-    add(box, BorderLayout.NORTH)
-    add(button, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.add(button, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
 // http://shop.oreilly.com/product/9780596009076.do
 // 9. Blur Disabled Components
 // http://code.google.com/p/filthy-rich-clients/source/browse/trunk/swing-hacks-examples-20060109/Ch01-JComponents/09/swinghacks/ch01/JComponents/hack09/BlurJButton.java?r=11
-class BlurJButton(label: String) : JButton(label) {
+private class BlurJButton(label: String) : JButton(label) {
   @Transient
   private var buf: BufferedImage? = null
 
   override fun paintComponent(g: Graphics) {
-    if (isEnabled()) {
+    if (isEnabled) {
       super.paintComponent(g)
     } else {
-      val w = getWidth()
-      val h = getHeight()
-      val img = buf?.takeIf { it.getWidth() == w && it.getHeight() == h }
+      val w = width
+      val h = height
+      val img = buf?.takeIf { it.width == w && it.height == h }
         ?: BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
       val g2 = img.createGraphics()
-      g2.setFont(g.getFont())
+      g2.font = g.font
       super.paintComponent(g2)
       g2.dispose()
       g.drawImage(CONVOLVE_OP.filter(img, null), 0, 0, this)
@@ -73,20 +73,20 @@ class BlurJButton(label: String) : JButton(label) {
   }
 }
 
-class BlurButton(label: String) : JButton(label) {
+private class BlurButton(label: String) : JButton(label) {
   @Transient
   private var buf: BufferedImage? = null
 
   override fun paintComponent(g: Graphics) {
-    if (isEnabled()) {
+    if (isEnabled) {
       super.paintComponent(g)
     } else {
-      val w = getWidth()
-      val h = getHeight()
-      val img = buf?.takeIf { it.getWidth() == w && it.getHeight() == h }
+      val w = width
+      val h = height
+      val img = buf?.takeIf { it.width == w && it.height == h }
         ?: BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
       val g2 = img.createGraphics()
-      g2.setFont(g.getFont())
+      g2.font = g.font
       super.paintComponent(g2)
       g2.dispose()
       g.drawImage(CONVOLVE_OP.filter(img, null), 0, 0, this)
@@ -112,7 +112,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
