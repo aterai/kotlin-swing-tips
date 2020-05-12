546e3c696fb3adde8b6eb026c3a8ee9b9dc7df76
aterai <aterai@outlook.com>
2020-05-12 16:25:43 +0900

    refactor: use of setter method instead of property access syntax

--- BoldMetal/src/main/kotlin/example/App.kt
+++ BoldMetal/src/main/kotlin/example/App.kt
@@ -9,54 +9,56 @@ import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeModel
 import javax.swing.tree.TreePath
 
-class MainPanel : JPanel(BorderLayout()) {
-  companion object {
-    private const val TAG = "<html><b>"
-    private const val BOLD_METAL = "swing.boldMetal"
-  }
+private const val TAG = "<html><b>"
+private const val BOLD_METAL = "swing.boldMetal"
 
-  init {
-    val check = JCheckBox(BOLD_METAL)
-    check.addActionListener { e ->
-      val c = e.getSource() as? JCheckBox ?: return@addActionListener
-      UIManager.put(BOLD_METAL, c.isSelected())
+fun makeUI(): Component {
+  val check = JCheckBox(BOLD_METAL)
+  check.addActionListener { e ->
+    (e.source as? JCheckBox)?.also {
+      UIManager.put(BOLD_METAL, it.isSelected)
       runCatching {
         UIManager.setLookAndFeel(MetalLookAndFeel())
       }
-      SwingUtilities.updateComponentTreeUI(c.getTopLevelAncestor())
+      SwingUtilities.updateComponentTreeUI(it.topLevelAncestor)
     }
-    val tree = JTree()
-    tree.setComponentPopupMenu(TreePopupMenu())
-    val tabbedPane = JTabbedPane()
-    tabbedPane.setBorder(BorderFactory.createTitledBorder("TitledBorder"))
-    tabbedPane.addTab(TAG + "JTree", JScrollPane(tree))
-    tabbedPane.addTab("JLabel", JLabel("JLabel"))
-    tabbedPane.addTab("JTextArea", JScrollPane(JTextArea("JTextArea")))
-    tabbedPane.addTab("JButton", JScrollPane(JButton("JButton")))
-    tabbedPane.addChangeListener { e ->
-      val t = e.getSource() as? JTabbedPane ?: return@addChangeListener
-      for (i in 0 until t.getTabCount()) {
-        val title = t.getTitleAt(i)
-        if (i == t.getSelectedIndex()) {
-          t.setTitleAt(i, TAG + title)
+  }
+
+  val tree = JTree()
+  tree.componentPopupMenu = TreePopupMenu()
+
+  val tabbedPane = JTabbedPane()
+  tabbedPane.border = BorderFactory.createTitledBorder("TitledBorder")
+  tabbedPane.addTab(TAG + "JTree", JScrollPane(tree))
+  tabbedPane.addTab("JLabel", JLabel("JLabel"))
+  tabbedPane.addTab("JTextArea", JScrollPane(JTextArea("JTextArea")))
+  tabbedPane.addTab("JButton", JScrollPane(JButton("JButton")))
+  tabbedPane.addChangeListener { e ->
+    (e.source as? JTabbedPane)?.also {
+      for (i in 0 until it.tabCount) {
+        val title = it.getTitleAt(i)
+        if (i == it.selectedIndex) {
+          it.setTitleAt(i, TAG + title)
         } else if (title.startsWith(TAG)) {
-          t.setTitleAt(i, title.substring(TAG.length))
+          it.setTitleAt(i, title.substring(TAG.length))
         }
       }
     }
-    add(check, BorderLayout.NORTH)
-    add(tabbedPane)
-    setPreferredSize(Dimension(320, 240))
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(check, BorderLayout.NORTH)
+    it.add(tabbedPane)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class TreePopupMenu : JPopupMenu() {
+private class TreePopupMenu : JPopupMenu() {
   private var path: TreePath? = null
   override fun show(c: Component, x: Int, y: Int) {
     (c as? JTree)?.also { tree ->
       path = tree.getPathForLocation(x, y)
       path?.also {
-        tree.setSelectionPath(it)
+        tree.selectionPath = it
         super.show(c, x, y)
       }
     }
@@ -75,42 +77,43 @@ class TreePopupMenu : JPopupMenu() {
       }
     }
     add("add").addActionListener {
-      (getInvoker() as? JTree)?.also { tree ->
-        (tree.getModel() as? DefaultTreeModel)?.also { model ->
-          (path?.getLastPathComponent() as? DefaultMutableTreeNode)?.also { parent ->
+      (invoker as? JTree)?.also { tree ->
+        (tree.model as? DefaultTreeModel)?.also { model ->
+          (path?.lastPathComponent as? DefaultMutableTreeNode)?.also { parent ->
             val child = DefaultMutableTreeNode("New node")
-            model.insertNodeInto(child, parent, parent.getChildCount())
-            tree.scrollPathToVisible(TreePath(child.getPath()))
+            model.insertNodeInto(child, parent, parent.childCount)
+            tree.scrollPathToVisible(TreePath(child.path))
           }
         }
       }
     }
     add("edit").addActionListener {
-      val node = path?.getLastPathComponent() as? DefaultMutableTreeNode ?: return@addActionListener
-      textField.setText(node.getUserObject().toString())
-      val tree = getInvoker() as? JTree ?: return@addActionListener
-      val ret = JOptionPane.showConfirmDialog(
-        tree, textField, "edit", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE
-      )
-      if (ret == JOptionPane.OK_OPTION) {
-        tree.getModel().valueForPathChanged(path, textField.getText())
+      val tree = invoker
+      val node = path?.lastPathComponent
+      if (tree is JTree && node is DefaultMutableTreeNode) {
+        textField.text = node.userObject.toString()
+        val ret = JOptionPane.showConfirmDialog(
+          tree, textField, "edit", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE
+        )
+        if (ret == JOptionPane.OK_OPTION) {
+          tree.model.valueForPathChanged(path, textField.text)
+        }
       }
     }
     addSeparator()
     add("remove").addActionListener {
-      val node = path?.getLastPathComponent() as? DefaultMutableTreeNode ?: return@addActionListener
-      if (!node.isRoot()) {
-        (getInvoker() as? JTree)?.also { tree ->
-          (tree.getModel() as? DefaultTreeModel)?.removeNodeFromParent(node)
-        }
+      val tree = invoker
+      val node = path?.lastPathComponent
+      if (tree is JTree && node is DefaultMutableTreeNode && !node.isRoot) {
+        (tree.model as? DefaultTreeModel)?.removeNodeFromParent(node)
       }
     }
   }
 }
 
-class FocusAncestorListener : AncestorListener {
+private class FocusAncestorListener : AncestorListener {
   override fun ancestorAdded(e: AncestorEvent) {
-    e.getComponent().requestFocusInWindow()
+    e.component.requestFocusInWindow()
   }
 
   override fun ancestorMoved(e: AncestorEvent) {
@@ -127,7 +130,7 @@ fun main() {
   EventQueue.invokeLater {
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
