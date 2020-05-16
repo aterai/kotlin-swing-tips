5fd61397ccd1a23c4355dbe495dcdec9887133f6
aterai <aterai@outlook.com>
2020-05-16 18:27:08 +0900

    detekt: LabeledExpression

--- PlaceholderForEmptyTable/src/main/kotlin/example/App.kt
+++ PlaceholderForEmptyTable/src/main/kotlin/example/App.kt
@@ -5,74 +5,75 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.event.HyperlinkEvent
 import javax.swing.table.DefaultTableModel
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val editor = JEditorPane("text/html", PLACEHOLDER)
-  private val columnNames = arrayOf("Integer", "String", "Boolean")
-  private val model = object : DefaultTableModel(null, columnNames) {
+private const val PLACEHOLDER = "<html>No data! <a href='dummy'>Input hint(beep)</a></html>"
+
+fun makeUI(): Component {
+  val editor = JEditorPane("text/html", PLACEHOLDER)
+  editor.isOpaque = false
+  editor.isEditable = false
+  editor.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
+  editor.addHyperlinkListener { e ->
+    if (e.eventType == HyperlinkEvent.EventType.ACTIVATED) {
+      Toolkit.getDefaultToolkit().beep()
+    }
+  }
+
+  val columnNames = arrayOf("Integer", "String", "Boolean")
+  val model = object : DefaultTableModel(null, columnNames) {
     override fun getColumnClass(column: Int) = when (column) {
       0 -> Number::class.java
       2 -> java.lang.Boolean::class.java
       else -> String::class.java
     }
   }
-  private val table = JTable(model)
-
-  init {
-    editor.setOpaque(false)
-    editor.setEditable(false)
-    editor.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
-    editor.addHyperlinkListener { e ->
-      if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-        Toolkit.getDefaultToolkit().beep()
-      }
+  model.addTableModelListener { e ->
+    (e.source as? DefaultTableModel)?.also {
+      editor.isVisible = it.rowCount == 0
     }
-
-    model.addTableModelListener { e ->
-      (e.getSource() as? DefaultTableModel)?.also {
-        editor.setVisible(it.getRowCount() == 0)
-      }
-    }
-
-    table.setAutoCreateRowSorter(true)
-    table.setFillsViewportHeight(true)
-    table.setComponentPopupMenu(TablePopupMenu())
-    table.setLayout(GridBagLayout())
-    table.add(editor)
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
   }
 
-  companion object {
-    private const val PLACEHOLDER = "<html>No data! <a href='dummy'>Input hint(beep)</a></html>"
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
+  table.fillsViewportHeight = true
+  table.componentPopupMenu = TablePopupMenu()
+  table.layout = GridBagLayout()
+  table.add(editor)
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class TablePopupMenu : JPopupMenu() {
+private class TablePopupMenu : JPopupMenu() {
   private val delete: JMenuItem
 
   init {
     add("add").addActionListener {
-      val table = getInvoker() as? JTable ?: return@addActionListener
-      val model = table.getModel() as? DefaultTableModel ?: return@addActionListener
-      model.addRow(arrayOf(model.getRowCount(), "New row", false))
-      val r = table.getCellRect(model.getRowCount() - 1, 0, true)
-      table.scrollRectToVisible(r)
+      val table = invoker as? JTable
+      val model = table?.model
+      if (table is JTable && model is DefaultTableModel) {
+        model.addRow(arrayOf(model.rowCount, "New row", false))
+        val r = table.getCellRect(model.rowCount - 1, 0, true)
+        table.scrollRectToVisible(r)
+      }
     }
     addSeparator()
     delete = add("delete")
     delete.addActionListener {
-      val table = getInvoker() as? JTable ?: return@addActionListener
-      val model = table.getModel() as? DefaultTableModel ?: return@addActionListener
-      val selection = table.getSelectedRows()
-      for (i in selection.indices.reversed()) {
-        model.removeRow(table.convertRowIndexToModel(selection[i]))
+      val table = invoker as? JTable
+      val model = table?.model
+      if (table is JTable && model is DefaultTableModel) {
+        val selection = table.selectedRows
+        for (i in selection.indices.reversed()) {
+          model.removeRow(table.convertRowIndexToModel(selection[i]))
+        }
       }
     }
   }
 
   override fun show(c: Component, x: Int, y: Int) {
     val table = c as? JTable ?: return
-    delete.setEnabled(table.getSelectedRowCount() > 0)
+    delete.isEnabled = table.selectedRowCount > 0
     super.show(table, x, y)
   }
 }
@@ -87,7 +88,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
