52ecb1b3fe6e8a03b5d573e2806f21d05aacc089
aterai <aterai@outlook.com>
2020-05-16 17:57:03 +0900

    refactor: use of setter method instead of property access syntax

--- AffineTransformOp/src/main/kotlin/example/App.kt
+++ AffineTransformOp/src/main/kotlin/example/App.kt
@@ -7,17 +7,22 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  var mode = Flip.NONE
-  private val image = runCatching { ImageIO.read(javaClass.getResource("test.jpg")) }
+private var mode = Flip.NONE
+
+fun makeUI(): Component {
+  val box = Box.createHorizontalBox()
+  box.add(Box.createHorizontalGlue())
+  box.add(JLabel("Flip: "))
+
+  val cl = Thread.currentThread().contextClassLoader
+  val image = runCatching { ImageIO.read(cl.getResource("example/test.jpg")) }
     .onFailure { it.printStackTrace() }
     .getOrNull() ?: makeMissingImage()
-  private val bg = ButtonGroup()
 
-  private val panel = object : JPanel() {
+  val p = object : JPanel() {
     override fun paintComponent(g: Graphics) {
-      g.setColor(getBackground())
-      g.fillRect(0, 0, getWidth(), getHeight())
+      g.color = background
+      g.fillRect(0, 0, width, height)
       val w = image.getWidth(this)
       val h = image.getHeight(this)
       when (mode) {
@@ -39,57 +44,56 @@ class MainPanel : JPanel(BorderLayout()) {
     }
   }
 
-  init {
-    val box = Box.createHorizontalBox()
-    box.add(Box.createHorizontalGlue())
-    box.add(JLabel("Flip: "))
-    Flip.values().map { makeRadioButton(it) }
-      .forEach {
-        box.add(it)
-        bg.add(it)
-        box.add(Box.createHorizontalStrut(5))
-      }
-    add(panel)
-    add(box, BorderLayout.SOUTH)
-    setOpaque(false)
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun makeRadioButton(f: Flip): JRadioButton {
-    val rb = JRadioButton(f.toString(), f === Flip.NONE)
-    rb.addActionListener {
-      mode = f
-      panel.repaint()
+  val bg = ButtonGroup()
+  Flip.values().map { makeRadioButton(it) }
+    .forEach {
+      box.add(it)
+      bg.add(it)
+      box.add(Box.createHorizontalStrut(5))
     }
-    return rb
-  }
 
-  private fun makeMissingImage(): BufferedImage {
-    val missingIcon = MissingIcon()
-    val w = missingIcon.getIconWidth()
-    val h = missingIcon.getIconHeight()
-    val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
-    val g2 = bi.createGraphics()
-    missingIcon.paintIcon(null, g2, 0, 0)
-    g2.dispose()
-    return bi
+  return JPanel(BorderLayout()).also {
+    it.add(p)
+    it.add(box, BorderLayout.SOUTH)
+    it.isOpaque = false
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-enum class Flip {
+private fun makeRadioButton(f: Flip): JRadioButton {
+  val rb = JRadioButton(f.toString(), f === Flip.NONE)
+  rb.addActionListener {
+    mode = f
+    rb.rootPane.repaint()
+  }
+  return rb
+}
+
+private fun makeMissingImage(): BufferedImage {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private enum class Flip {
   NONE, VERTICAL, HORIZONTAL
 }
 
-class MissingIcon : Icon {
+private class MissingIcon : Icon {
   override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
     val g2 = g.create() as? Graphics2D ?: return
-    val w = getIconWidth()
-    val h = getIconHeight()
+    val w = iconWidth
+    val h = iconHeight
     val gap = w / 5
-    g2.setColor(Color.WHITE)
+    g2.color = Color.WHITE
     g2.fillRect(x, y, w, h)
-    g2.setColor(Color.RED)
-    g2.setStroke(BasicStroke(w / 8f))
+    g2.color = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
     g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
     g2.drawLine(x + gap, y + gap, x + w - gap, y + gap)
     g2.dispose()
@@ -110,7 +114,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
