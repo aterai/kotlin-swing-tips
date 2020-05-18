81ea124b4e49ab32924a413bb4733d99887ae5eb
aterai <aterai@outlook.com>
2020-05-18 19:44:36 +0900

    refactor: use of setter method instead of property access syntax

--- HexFormatterSpinner/src/main/kotlin/example/App.kt
+++ HexFormatterSpinner/src/main/kotlin/example/App.kt
@@ -11,132 +11,132 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.text.DefaultFormatter
 import javax.swing.text.DefaultFormatterFactory
 
-class MainPanel : JPanel(BorderLayout()) {
-  // Character.MIN_CODE_POINT: 0x0, Character.MAX_CODE_POINT: 0x10FFFF
-  private val nm = SpinnerNumberModel(0x51DE, 0x0, Character.MAX_CODE_POINT, 1)
-  private val spinner = JSpinner(nm)
-  private val fontPanel = GlyphPaintPanel()
-  var fontPaintFlag: Set<FontPaint> = EnumSet.allOf(FontPaint::class.java)
+// Character.MIN_CODE_POINT: 0x0, Character.MAX_CODE_POINT: 0x10FFFF
+private val nm = SpinnerNumberModel(0x51DE, 0x0, Character.MAX_CODE_POINT, 1)
+private val spinner = JSpinner(nm)
+private val fontPanel = GlyphPaintPanel()
+private var fontPaintFlag: Set<FontPaint> = EnumSet.allOf(FontPaint::class.java)
 
-  init {
-    nm.addChangeListener { fontPanel.repaint() }
-    val editor = spinner.getEditor() as? JSpinner.NumberEditor
-    editor?.getTextField()?.also {
-      it.setFont(Font(Font.MONOSPACED, Font.PLAIN, it.getFont().getSize()))
-      it.setFormatterFactory(makeFFactory())
-    }
-
-    val exMi = JRadioButton("IPAexMincho")
-    exMi.addItemListener { e ->
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        setFontPaintFlag(EnumSet.of(FontPaint.IPA_EX_MINCHO))
-      }
-    }
-
-    val mjMi = JRadioButton("IPAmjMincho")
-    mjMi.addItemListener { e ->
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        setFontPaintFlag(EnumSet.of(FontPaint.IPA_MJ_MINCHO))
-      }
-    }
-
-    val both = JRadioButton("Both", true)
-    both.addItemListener { e ->
-      if (e.getStateChange() == ItemEvent.SELECTED) {
-        setFontPaintFlag(EnumSet.allOf(FontPaint::class.java))
-      }
-    }
-
-    val p = JPanel()
-    val bg = ButtonGroup()
-    listOf(exMi, mjMi, both).forEach {
-      p.add(it)
-      bg.add(it)
-    }
-
-    add(spinner, BorderLayout.NORTH)
-    add(fontPanel)
-    add(p, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  nm.addChangeListener { fontPanel.repaint() }
+  val editor = spinner.editor as? JSpinner.NumberEditor
+  editor?.textField?.also {
+    it.font = Font(Font.MONOSPACED, Font.PLAIN, it.font.size)
+    it.formatterFactory = makeFFactory()
   }
 
-  private fun getCharacterString() = String(Character.toChars(nm.getNumber().toInt()))
-
-  private fun setFontPaintFlag(fp: EnumSet<FontPaint>) {
-    fontPaintFlag = fp
-    fontPanel.repaint()
-  }
-
-  private inner class GlyphPaintPanel : JPanel() {
-    private val ipaEx = Font("IPAexMincho", Font.PLAIN, 200)
-    private val ipaMj = Font("IPAmjMincho", Font.PLAIN, 200)
-
-    override fun paintComponent(g: Graphics) {
-      val g2 = g.create() as? Graphics2D ?: return
-      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g2.setPaint(Color.WHITE)
-      g2.fillRect(0, 0, getWidth(), getHeight())
-
-      val str = getCharacterString()
-
-      val frc = g2.getFontRenderContext()
-      val exShape = TextLayout(str, ipaEx, frc).getOutline(null)
-      val mjShape = TextLayout(str, ipaMj, frc).getOutline(null)
-
-      val b = exShape.getBounds2D()
-      val cx = getWidth() / 2.0 - b.getCenterX()
-      val cy = getHeight() / 2.0 - b.getCenterY()
-      val toCenterAtf = AffineTransform.getTranslateInstance(cx, cy)
-
-      g2.setPaint(Color.YELLOW)
-      g2.draw(toCenterAtf.createTransformedShape(b))
-
-      val s1 = toCenterAtf.createTransformedShape(exShape)
-      val s2 = toCenterAtf.createTransformedShape(mjShape)
-
-      if (fontPaintFlag.contains(FontPaint.IPA_EX_MINCHO)) {
-        g2.setPaint(Color.CYAN)
-        g2.fill(s1)
-      }
-      if (fontPaintFlag.contains(FontPaint.IPA_MJ_MINCHO)) {
-        g2.setPaint(Color.MAGENTA)
-        g2.fill(s2)
-      }
-      if (fontPaintFlag.containsAll(EnumSet.allOf(FontPaint::class.java))) {
-        g2.setClip(s1)
-        g2.setPaint(Color.BLACK)
-        g2.fill(s2)
-      }
-      g2.dispose()
+  val exMi = JRadioButton("IPAexMincho")
+  exMi.addItemListener { e ->
+    if (e.stateChange == ItemEvent.SELECTED) {
+      setFontPaintFlag(EnumSet.of(FontPaint.IPA_EX_MINCHO))
     }
   }
 
-  private fun makeFFactory(): DefaultFormatterFactory {
-    val formatter = object : DefaultFormatter() {
-      @Throws(ParseException::class)
-      override fun stringToValue(text: String): Any {
-        val pattern = Pattern.compile("^\\s*(\\p{XDigit}{1,6})\\s*$")
-        val matcher = pattern.matcher(text)
-        if (matcher.find()) {
-          val iv = Integer.valueOf(text, 16)
-          if (iv <= Character.MAX_CODE_POINT) {
-            return iv
-          }
-        }
-        Toolkit.getDefaultToolkit().beep()
-        throw ParseException(text, 0)
-      }
-
-      @Throws(ParseException::class)
-      override fun valueToString(value: Any?) = "%06X".format(value as? Int)
+  val mjMi = JRadioButton("IPAmjMincho")
+  mjMi.addItemListener { e ->
+    if (e.stateChange == ItemEvent.SELECTED) {
+      setFontPaintFlag(EnumSet.of(FontPaint.IPA_MJ_MINCHO))
     }
-    formatter.setValueClass(Int::class.java)
-    formatter.setOverwriteMode(true)
-    return DefaultFormatterFactory(formatter)
+  }
+
+  val both = JRadioButton("Both", true)
+  both.addItemListener { e ->
+    if (e.stateChange == ItemEvent.SELECTED) {
+      setFontPaintFlag(EnumSet.allOf(FontPaint::class.java))
+    }
+  }
+
+  val p = JPanel()
+  val bg = ButtonGroup()
+  listOf(exMi, mjMi, both).forEach {
+    p.add(it)
+    bg.add(it)
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(spinner, BorderLayout.NORTH)
+    it.add(fontPanel)
+    it.add(p, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-enum class FontPaint {
+private fun getCharacterString() = String(Character.toChars(nm.number.toInt()))
+
+private fun setFontPaintFlag(fp: EnumSet<FontPaint>) {
+  fontPaintFlag = fp
+  fontPanel.repaint()
+}
+
+private class GlyphPaintPanel : JPanel() {
+  private val ipaEx = Font("IPAexMincho", Font.PLAIN, 200)
+  private val ipaMj = Font("IPAmjMincho", Font.PLAIN, 200)
+
+  override fun paintComponent(g: Graphics) {
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    g2.paint = Color.WHITE
+    g2.fillRect(0, 0, width, height)
+
+    val str = getCharacterString()
+
+    val frc = g2.fontRenderContext
+    val exShape = TextLayout(str, ipaEx, frc).getOutline(null)
+    val mjShape = TextLayout(str, ipaMj, frc).getOutline(null)
+
+    val b = exShape.bounds2D
+    val cx = width / 2.0 - b.centerX
+    val cy = height / 2.0 - b.centerY
+    val toCenterAtf = AffineTransform.getTranslateInstance(cx, cy)
+
+    g2.paint = Color.YELLOW
+    g2.draw(toCenterAtf.createTransformedShape(b))
+
+    val s1 = toCenterAtf.createTransformedShape(exShape)
+    val s2 = toCenterAtf.createTransformedShape(mjShape)
+
+    if (fontPaintFlag.contains(FontPaint.IPA_EX_MINCHO)) {
+      g2.paint = Color.CYAN
+      g2.fill(s1)
+    }
+    if (fontPaintFlag.contains(FontPaint.IPA_MJ_MINCHO)) {
+      g2.paint = Color.MAGENTA
+      g2.fill(s2)
+    }
+    if (fontPaintFlag.containsAll(EnumSet.allOf(FontPaint::class.java))) {
+      g2.clip = s1
+      g2.paint = Color.BLACK
+      g2.fill(s2)
+    }
+    g2.dispose()
+  }
+}
+
+private fun makeFFactory(): DefaultFormatterFactory {
+  val formatter = object : DefaultFormatter() {
+    @Throws(ParseException::class)
+    override fun stringToValue(text: String): Any {
+      val pattern = Pattern.compile("^\\s*(\\p{XDigit}{1,6})\\s*$")
+      val matcher = pattern.matcher(text)
+      if (matcher.find()) {
+        val iv = Integer.valueOf(text, 16)
+        if (iv <= Character.MAX_CODE_POINT) {
+          return iv
+        }
+      }
+      Toolkit.getDefaultToolkit().beep()
+      throw ParseException(text, 0)
+    }
+
+    @Throws(ParseException::class)
+    override fun valueToString(value: Any?) = "%06X".format(value as? Int)
+  }
+  formatter.valueClass = Int::class.java
+  formatter.overwriteMode = true
+  return DefaultFormatterFactory(formatter)
+}
+
+private enum class FontPaint {
   IPA_EX_MINCHO, IPA_MJ_MINCHO
 }
 
@@ -150,7 +150,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
