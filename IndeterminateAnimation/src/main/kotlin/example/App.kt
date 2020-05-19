112bb28b919752a856418f1ed9ecf16a21f024fd
aterai <aterai@outlook.com>
2020-05-20 08:21:35 +0900

    refactor: use of setter method instead of property access syntax

--- IndeterminateAnimation/src/main/kotlin/example/App.kt
+++ IndeterminateAnimation/src/main/kotlin/example/App.kt
@@ -6,65 +6,63 @@ import java.beans.PropertyChangeListener
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicProgressBarUI
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val progress = object : JProgressBar() {
-      override fun updateUI() {
-        super.updateUI()
-        setUI(OneDirectionIndeterminateProgressBarUI())
-      }
+fun makeUI(): Component {
+  val progress = object : JProgressBar() {
+    override fun updateUI() {
+      super.updateUI()
+      setUI(OneDirectionIndeterminateProgressBarUI())
     }
-    // TEST: progress.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-
-    val list = listOf(JProgressBar(), progress)
-
-    val p = JPanel(GridLayout(2, 1))
-    list.forEach { p.add(makePanel(it)) }
-
-    val button = JButton("Start")
-    button.addActionListener {
-      val worker = BackgroundTask()
-      list.forEach {
-        it.setIndeterminate(true)
-        worker.addPropertyChangeListener(ProgressListener(it))
-      }
-      worker.execute()
-    }
-
-    val box = Box.createHorizontalBox()
-    box.add(Box.createHorizontalGlue())
-    box.add(button)
-    box.add(Box.createHorizontalStrut(5))
-
-    add(p)
-    add(box, BorderLayout.SOUTH)
-    setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    setPreferredSize(Dimension(320, 240))
   }
 
-  private fun makePanel(cmp: Component) = JPanel(GridBagLayout()).also {
-    val c = GridBagConstraints()
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.insets = Insets(5, 5, 5, 5)
-    c.weightx = 1.0
-    it.add(cmp, c)
+  val list = listOf(JProgressBar(), progress)
+  val p = JPanel(GridLayout(2, 1))
+  list.forEach { p.add(makePanel(it)) }
+
+  val button = JButton("Start")
+  button.addActionListener {
+    val worker = BackgroundTask()
+    list.forEach {
+      it.isIndeterminate = true
+      worker.addPropertyChangeListener(ProgressListener(it))
+    }
+    worker.execute()
+  }
+
+  val box = Box.createHorizontalBox()
+  box.add(Box.createHorizontalGlue())
+  box.add(button)
+  box.add(Box.createHorizontalStrut(5))
+
+  return JPanel(BorderLayout()).also {
+    it.add(p)
+    it.add(box, BorderLayout.SOUTH)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class OneDirectionIndeterminateProgressBarUI : BasicProgressBarUI() {
+private fun makePanel(cmp: Component) = JPanel(GridBagLayout()).also {
+  val c = GridBagConstraints()
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.insets = Insets(5, 5, 5, 5)
+  c.weightx = 1.0
+  it.add(cmp, c)
+}
+
+private class OneDirectionIndeterminateProgressBarUI : BasicProgressBarUI() {
   // @see com/sun/java/swing/plaf/windows/WindowsProgressBarUI.java
   override fun getBox(r: Rectangle): Rectangle {
     val rect = super.getBox(r)
-    val frameCount = getFrameCount() / 2
-    val currentFrame = getAnimationIndex() % frameCount
+    val frameCount = frameCount / 2
+    val currentFrame = animationIndex % frameCount
 
-    if (progressBar.getOrientation() == JProgressBar.VERTICAL) {
-      var len = progressBar.getHeight()
+    if (progressBar.orientation == JProgressBar.VERTICAL) {
+      var len = progressBar.height
       len += rect.height * 2 // add 2x for the trails
       val delta = len / frameCount.toDouble()
       rect.y = (delta * currentFrame).toInt()
     } else {
-      var len = progressBar.getWidth()
+      var len = progressBar.width
       len += rect.width * 2 // add 2x for the trails
       val delta = len / frameCount.toDouble()
       rect.x = (delta * currentFrame).toInt()
@@ -73,31 +71,31 @@ class OneDirectionIndeterminateProgressBarUI : BasicProgressBarUI() {
   }
 }
 
-class BackgroundTask : SwingWorker<String, Unit>() {
+private class BackgroundTask : SwingWorker<String, Unit>() {
   @Throws(InterruptedException::class)
   override fun doInBackground(): String {
     Thread.sleep(5000)
     var current = 0
     val lengthOfTask = 100
-    while (current <= lengthOfTask && !isCancelled()) {
+    while (current <= lengthOfTask && !isCancelled) {
       Thread.sleep(50)
-      setProgress(100 * current / lengthOfTask)
+      progress = 100 * current / lengthOfTask
       current++
     }
     return "Done"
   }
 }
 
-class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
+private class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
   init {
-    this.progressBar.setValue(0)
+    this.progressBar.value = 0
   }
 
   override fun propertyChange(e: PropertyChangeEvent) {
-    val nv = e.getNewValue()
-    if (e.getPropertyName() == "progress" && nv is Int) {
-      progressBar.setIndeterminate(false)
-      progressBar.setValue(nv)
+    val nv = e.newValue
+    if (e.propertyName == "progress" && nv is Int) {
+      progressBar.isIndeterminate = false
+      progressBar.value = nv
     }
   }
 }
@@ -106,7 +104,7 @@ fun main() {
   EventQueue.invokeLater {
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
