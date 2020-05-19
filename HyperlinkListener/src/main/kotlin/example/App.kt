a6cb5a390f6111520a7bb2e1cffd4b79d65115f1
aterai <aterai@outlook.com>
2020-05-20 08:19:09 +0900

    refactor: use of setter method instead of property access syntax

--- HyperlinkListener/src/main/kotlin/example/App.kt
+++ HyperlinkListener/src/main/kotlin/example/App.kt
@@ -9,85 +9,81 @@ import javax.swing.text.StyleConstants
 import javax.swing.text.View
 import javax.swing.text.html.HTMLDocument
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val link = "https://ateraimemo.com/"
-  private val htmlText = """
+private const val link = "https://ateraimemo.com/"
+private const val htmlText = """
       <html><body>
         html tag: <br /><a href='$link'>$link</a>
       </body></html>
     """
-  private var tooltip: String? = null
+private var tooltip: String? = null
 
-  init {
-    add(JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
-      it.setResizeWeight(.5)
-      it.setTopComponent(JScrollPane(makeEditorPane(false)))
-      it.setBottomComponent(JScrollPane(makeEditorPane(true)))
-    })
-    setPreferredSize(Dimension(320, 240))
+fun makeUI() = JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
+  it.resizeWeight = .5
+  it.topComponent = JScrollPane(makeEditorPane(false))
+  it.bottomComponent = JScrollPane(makeEditorPane(true))
+  it.preferredSize = Dimension(320, 240)
+}
+
+private fun makeEditorPane(editable: Boolean) = JEditorPane().also {
+  it.isEditable = editable
+  it.contentType = "text/html"
+  it.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
+  it.text = htmlText
+  it.addHyperlinkListener { e ->
+    when (e.eventType) {
+      HyperlinkEvent.EventType.ACTIVATED ->
+        JOptionPane.showMessageDialog(it, "You click the link with the URL " + e.url)
+      HyperlinkEvent.EventType.ENTERED -> {
+        tooltip = it.toolTipText
+        it.toolTipText = e.url?.toExternalForm()
+      }
+      HyperlinkEvent.EventType.EXITED -> it.toolTipText = tooltip
+    }
   }
 
-  private fun makeEditorPane(editable: Boolean) = JEditorPane().also {
-    it.setEditable(editable)
-    it.setContentType("text/html")
-    it.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
-    it.setText(htmlText)
-    it.addHyperlinkListener { e ->
-      when (e.getEventType()) {
-        HyperlinkEvent.EventType.ACTIVATED ->
-          JOptionPane.showMessageDialog(it, "You click the link with the URL " + e.getURL())
-        HyperlinkEvent.EventType.ENTERED -> {
-          tooltip = it.getToolTipText()
-          it.setToolTipText(e.getURL()?.toExternalForm())
-        }
-        HyperlinkEvent.EventType.EXITED -> it.setToolTipText(tooltip)
-      }
+  val doc = it.document
+  if (doc is HTMLDocument) {
+    val s = doc.addStyle("button", null)
+    StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER)
+    val button = HyperlinkButton(link)
+    button.addActionListener { e ->
+      val b = (e.source as? AbstractButton)?.isSelected ?: false
+      it.background = if (b) Color.RED else Color.WHITE
+      JOptionPane.showMessageDialog(it, "You click the link with the URL $link")
     }
-
-    val doc = it.getDocument()
-    if (doc is HTMLDocument) {
-      val s = doc.addStyle("button", null)
-      StyleConstants.setAlignment(s, StyleConstants.ALIGN_CENTER)
-      val button = HyperlinkButton(link)
-      button.addActionListener { e ->
-        val b = (e.getSource() as? AbstractButton)?.isSelected() ?: false
-        it.setBackground(if (b) Color.RED else Color.WHITE)
-        JOptionPane.showMessageDialog(it, "You click the link with the URL $link")
-      }
-      button.setToolTipText("button: $link")
-      button.setOpaque(false)
-      StyleConstants.setComponent(s, button)
-      runCatching {
-        doc.insertString(doc.getLength(), "\n----\nJButton:\n", null)
-        doc.insertString(doc.getLength(), link + "\n", doc.getStyle("button"))
-      }
+    button.toolTipText = "button: $link"
+    button.isOpaque = false
+    StyleConstants.setComponent(s, button)
+    runCatching {
+      doc.insertString(doc.getLength(), "\n----\nJButton:\n", null)
+      doc.insertString(doc.getLength(), link + "\n", doc.getStyle("button"))
     }
   }
 }
 
-class HyperlinkButton : JButton {
+private class HyperlinkButton : JButton {
   override fun updateUI() {
     super.updateUI()
     setUI(BasicLinkViewButtonUI())
-    setForeground(Color.BLUE)
-    setBorder(BorderFactory.createEmptyBorder(0, 0, 2, 0))
-    setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR))
+    foreground = Color.BLUE
+    border = BorderFactory.createEmptyBorder(0, 0, 2, 0)
+    cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
   }
 
   constructor(text: String?, icon: Icon?) : super(text, icon)
 
   constructor(text: String?) : this(text, null)
 
-  constructor(icon: Icon) : this(null, icon)
+  // constructor(icon: Icon) : this(null, icon)
 
-  constructor(a: Action) : this(null, null) {
-    super.setAction(a)
-  }
+  // constructor(a: Action) : this(null, null) {
+  //   super.setAction(a)
+  // }
 }
 
-open class LinkViewButtonUI : BasicButtonUI() /* ButtonUI */
+private open class LinkViewButtonUI : BasicButtonUI()
 
-class BasicLinkViewButtonUI : LinkViewButtonUI() {
+private class BasicLinkViewButtonUI : LinkViewButtonUI() {
   private val viewRect = Rectangle()
   private val iconRect = Rectangle()
   private val textRect = Rectangle()
@@ -95,25 +91,25 @@ class BasicLinkViewButtonUI : LinkViewButtonUI() {
   override fun paint(g: Graphics, c: JComponent) {
     val b = c as? AbstractButton ?: return
     val f = c.getFont()
-    g.setFont(f)
+    g.font = f
 
     SwingUtilities.calculateInnerArea(c, viewRect)
     iconRect.setBounds(0, 0, 0, 0)
     textRect.setBounds(0, 0, 0, 0)
 
     val text = SwingUtilities.layoutCompoundLabel(
-      c, c.getFontMetrics(f), b.getText(), null, // altIcon != null ? altIcon : getDefaultIcon(),
-      b.getVerticalAlignment(), b.getHorizontalAlignment(),
-      b.getVerticalTextPosition(), b.getHorizontalTextPosition(),
+      c, c.getFontMetrics(f), b.text, null, // altIcon != null ? altIcon : getDefaultIcon(),
+      b.verticalAlignment, b.horizontalAlignment,
+      b.verticalTextPosition, b.horizontalTextPosition,
       viewRect, iconRect, textRect, 0) // b.getText() == null ? 0 : b.getIconTextGap())
 
     if (c.isOpaque()) {
-      g.setColor(b.getBackground())
+      g.color = b.background
       g.fillRect(0, 0, c.getWidth(), c.getHeight())
     }
 
-    if (b.isRolloverEnabled() && b.getModel().isRollover()) {
-      g.setColor(Color.BLUE)
+    if (b.isRolloverEnabled && b.model.isRollover) {
+      g.color = Color.BLUE
       g.drawLine(
         viewRect.x, viewRect.y + viewRect.height,
         viewRect.x + viewRect.width, viewRect.y + viewRect.height)
@@ -135,7 +131,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
