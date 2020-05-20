f987bb6fe4e8f20461d86566ce7599587caebefd
aterai <aterai@outlook.com>
2020-05-20 09:06:35 +0900

    refactor: use of setter method instead of property access syntax

--- JumpToClickedPositionSlider/src/main/kotlin/example/App.kt
+++ JumpToClickedPositionSlider/src/main/kotlin/example/App.kt
@@ -6,59 +6,59 @@ import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.metal.MetalSliderUI
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val slider1 = JSlider(SwingConstants.VERTICAL, 0, 1000, 500)
-    setSliderUI(slider1)
+fun makeUI(): Component {
+  val slider1 = JSlider(SwingConstants.VERTICAL, 0, 1000, 500)
+  setSliderUI(slider1)
 
-    val slider2 = JSlider(0, 1000, 500)
-    setSliderUI(slider2)
+  val slider2 = JSlider(0, 1000, 500)
+  setSliderUI(slider2)
 
-    add(Box.createHorizontalBox().also {
-      it.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20))
-      it.add(JSlider(SwingConstants.VERTICAL, 0, 1000, 100))
-      it.add(Box.createHorizontalStrut(20))
-      it.add(slider1)
-      it.add(Box.createHorizontalGlue())
-    }, BorderLayout.WEST)
+  val p = JPanel(BorderLayout())
+  p.add(Box.createHorizontalBox().also {
+    it.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
+    it.add(JSlider(SwingConstants.VERTICAL, 0, 1000, 100))
+    it.add(Box.createHorizontalStrut(20))
+    it.add(slider1)
+    it.add(Box.createHorizontalGlue())
+  }, BorderLayout.WEST)
 
-    add(Box.createVerticalBox().also {
-      it.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 20))
-      it.add(makeTitledPanel("Default", JSlider(0, 100, 100)))
-      it.add(Box.createVerticalStrut(20))
-      it.add(makeTitledPanel("Jump to clicked position", slider2))
-      it.add(Box.createVerticalGlue())
-    })
-    // setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 10));
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun setSliderUI(slider: JSlider) {
-    if (slider.getUI() is WindowsSliderUI) {
-      slider.setUI(WindowsJumpToClickedPositionSliderUI(slider))
-    } else {
-      slider.setUI(MetalJumpToClickedPositionSliderUI())
-    }
-    // slider.setSnapToTicks(false);
-    // slider.setPaintTicks(true);
-    // slider.setPaintLabels(true);
-  }
-
-  private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-    it.add(c)
-  }
+  p.add(Box.createVerticalBox().also {
+    it.border = BorderFactory.createEmptyBorder(20, 0, 20, 20)
+    it.add(makeTitledPanel("Default", JSlider(0, 100, 100)))
+    it.add(Box.createVerticalStrut(20))
+    it.add(makeTitledPanel("Jump to clicked position", slider2))
+    it.add(Box.createVerticalGlue())
+  })
+  // setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 10));
+  p.preferredSize = Dimension(320, 240)
+  return p
 }
 
-class WindowsJumpToClickedPositionSliderUI(slider: JSlider) : WindowsSliderUI(slider) {
+private fun setSliderUI(slider: JSlider) {
+  if (slider.ui is WindowsSliderUI) {
+    slider.ui = WindowsJumpToClickedPositionSliderUI(slider)
+  } else {
+    slider.ui = MetalJumpToClickedPositionSliderUI()
+  }
+  // slider.setSnapToTicks(false);
+  // slider.setPaintTicks(true);
+  // slider.setPaintLabels(true);
+}
+
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+private class WindowsJumpToClickedPositionSliderUI(slider: JSlider) : WindowsSliderUI(slider) {
   override fun createTrackListener(slider: JSlider?): TrackListener {
     return object : TrackListener() {
       override fun mousePressed(e: MouseEvent) {
         if (UIManager.getBoolean("Slider.onlyLeftMouseButtonDrag") && SwingUtilities.isLeftMouseButton(e)) {
-          val s = e.getComponent() as? JSlider ?: return
-          when (s.getOrientation()) {
-            SwingConstants.VERTICAL -> s.setValue(valueForYPosition(e.getY()))
-            SwingConstants.HORIZONTAL -> s.setValue(valueForXPosition(e.getX()))
+          val s = e.component as? JSlider ?: return
+          when (s.orientation) {
+            SwingConstants.VERTICAL -> s.value = valueForYPosition(e.y)
+            SwingConstants.HORIZONTAL -> s.value = valueForXPosition(e.x)
             else -> error("orientation must be one of: VERTICAL, HORIZONTAL")
           }
           super.mousePressed(e) // isDragging = true;
@@ -73,15 +73,15 @@ class WindowsJumpToClickedPositionSliderUI(slider: JSlider) : WindowsSliderUI(sl
   }
 }
 
-class MetalJumpToClickedPositionSliderUI : MetalSliderUI() {
+private class MetalJumpToClickedPositionSliderUI : MetalSliderUI() {
   override fun createTrackListener(slider: JSlider?): TrackListener {
     return object : TrackListener() {
       override fun mousePressed(e: MouseEvent) {
         if (UIManager.getBoolean("Slider.onlyLeftMouseButtonDrag") && SwingUtilities.isLeftMouseButton(e)) {
-          val s = e.getComponent() as? JSlider ?: return
-          when (s.getOrientation()) {
-            SwingConstants.VERTICAL -> s.setValue(valueForYPosition(e.getY()))
-            SwingConstants.HORIZONTAL -> s.setValue(valueForXPosition(e.getX()))
+          val s = e.component as? JSlider ?: return
+          when (s.orientation) {
+            SwingConstants.VERTICAL -> s.value = valueForYPosition(e.y)
+            SwingConstants.HORIZONTAL -> s.value = valueForXPosition(e.x)
             else -> error("orientation must be one of: VERTICAL, HORIZONTAL")
           }
           super.mousePressed(e) // isDragging = true;
@@ -106,7 +106,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
