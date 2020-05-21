28f4148709b5e5292de70a97507a44a39672ce50
aterai <aterai@outlook.com>
2020-05-21 18:34:27 +0900

    add: ClearComboBoxItemSelection

---
+++ ClearComboBoxItemSelection/src/main/kotlin/example/App.kt
@@ -0,0 +1,45 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val combo = JComboBox(arrayOf("A", "BB", "CCC", "DD", "E"))
+  combo.selectedIndex = -1
+
+  val b1 = JButton("setSelectedIndex(0)")
+  b1.addActionListener { combo.setSelectedIndex(0) }
+
+  val b2 = JButton("setSelectedIndex(-1)")
+  b2.addActionListener { combo.setSelectedIndex(-1) }
+
+  val b3 = JButton("setSelectedItem(null)")
+  b3.addActionListener { combo.setSelectedItem(null) }
+
+  val box = JPanel(GridLayout(0, 1, 10, 10))
+  listOf<JComponent>(b1, b2, b3, combo).forEach { box.add(it) }
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
