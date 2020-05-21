a1be62f825cf9bbedb3e7a01d9ea62619e5ee465
aterai <aterai@outlook.com>
2020-05-21 18:40:27 +0900

    refactor: remove redundant qualifier name

--- CellEditorInputVerifier/src/main/kotlin/example/App.kt
+++ CellEditorInputVerifier/src/main/kotlin/example/App.kt
@@ -14,79 +14,79 @@ import javax.swing.text.DocumentFilter
 import javax.swing.text.JTextComponent
 import javax.swing.text.NumberFormatter
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val textField1 = JTextField()
-    initBorderAndAlignment(textField1)
-    (textField1.getDocument() as? AbstractDocument)?.setDocumentFilter(IntegerDocumentFilter())
+fun makeUI(): Component {
+  val textField1 = JTextField()
+  initBorderAndAlignment(textField1)
+  (textField1.document as? AbstractDocument)?.documentFilter = IntegerDocumentFilter()
 
-    val textField2 = JTextField()
-    initBorderAndAlignment(textField2)
-    textField2.setInputVerifier(IntegerInputVerifier())
+  val textField2 = JTextField()
+  initBorderAndAlignment(textField2)
+  textField2.inputVerifier = IntegerInputVerifier()
 
-    val textField3 = JFormattedTextField()
-    initBorderAndAlignment(textField3)
-    textField3.setFormatterFactory(NumberFormatterFactory())
+  val textField3 = JFormattedTextField()
+  initBorderAndAlignment(textField3)
+  textField3.formatterFactory = NumberFormatterFactory()
 
-    val columnNames = arrayOf("Default", "DocumentFilter", "InputVerifier", "JFormattedTextField")
-    val model = object : DefaultTableModel(columnNames, 10) {
-      override fun getColumnClass(column: Int) = Integer::class.java
+  val columnNames = arrayOf("Default", "DocumentFilter", "InputVerifier", "JFormattedTextField")
+  val model = object : DefaultTableModel(columnNames, 10) {
+    override fun getColumnClass(column: Int) = Integer::class.java
+  }
+  val table = object : JTable(model) {
+    override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
+      val c = super.prepareEditor(editor, row, column)
+      (c as? JComponent)?.border = BorderFactory.createEmptyBorder(1, 1, 1, 1)
+      return c
     }
-    val table = object : JTable(model) {
-      override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
-        val c = super.prepareEditor(editor, row, column)
-        (c as? JComponent)?.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1))
-        return c
+  }
+  table.columnModel.getColumn(1).cellEditor = DefaultCellEditor(textField1)
+  table.columnModel.getColumn(2).cellEditor = object : DefaultCellEditor(textField2) {
+    override fun stopCellEditing(): Boolean {
+      val editor = component as? JComponent ?: return super.stopCellEditing()
+      val isEditValid = editor.inputVerifier.verify(editor)
+      editor.border = when {
+        isEditValid -> BorderFactory.createEmptyBorder(1, 1, 1, 1)
+        else -> BorderFactory.createLineBorder(Color.RED)
       }
+      return isEditValid && super.stopCellEditing()
     }
-    table.getColumnModel().getColumn(1).setCellEditor(DefaultCellEditor(textField1))
-    table.getColumnModel().getColumn(2).setCellEditor(object : DefaultCellEditor(textField2) {
-      override fun stopCellEditing(): Boolean {
-        val editor = getComponent() as? JComponent ?: return super.stopCellEditing()
-        val isEditValid = editor.getInputVerifier().verify(editor)
-        editor.setBorder(when {
-          isEditValid -> BorderFactory.createEmptyBorder(1, 1, 1, 1)
-          else -> BorderFactory.createLineBorder(Color.RED)
-        })
-        return isEditValid && super.stopCellEditing()
+  }
+  table.columnModel.getColumn(3).cellEditor = object : DefaultCellEditor(textField3) {
+    override fun stopCellEditing(): Boolean {
+      val editor = component as? JFormattedTextField ?: return super.stopCellEditing()
+      val isEditValid = editor.isEditValid
+      editor.border = when {
+        isEditValid -> BorderFactory.createEmptyBorder(1, 1, 1, 1)
+        else -> BorderFactory.createLineBorder(Color.RED)
       }
-    })
-    table.getColumnModel().getColumn(3).setCellEditor(object : DefaultCellEditor(textField3) {
-      override fun stopCellEditing(): Boolean {
-        val editor = getComponent() as? JFormattedTextField ?: return super.stopCellEditing()
-        val isEditValid = editor.isEditValid()
-        editor.setBorder(when {
-          isEditValid -> BorderFactory.createEmptyBorder(1, 1, 1, 1)
-          else -> BorderFactory.createLineBorder(Color.RED)
-        })
-        return isEditValid && super.stopCellEditing()
-      }
-    })
-
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
+      return isEditValid && super.stopCellEditing()
+    }
   }
 
-  private fun initBorderAndAlignment(textField: JTextField) {
-    textField.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1))
-    textField.setHorizontalAlignment(SwingConstants.RIGHT)
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
+}
+
+private fun initBorderAndAlignment(textField: JTextField) {
+  textField.border = BorderFactory.createEmptyBorder(1, 1, 1, 1)
+  textField.horizontalAlignment = SwingConstants.RIGHT
 }
 
 // Validating Text and Filtering Documents and Accessibility and the Java Access Bridge Tech Tips
 // http://java.sun.com/developer/JDCTechTips/2005/tt0518.html
 // Validating with Input Verifiers
-internal class IntegerInputVerifier : InputVerifier() {
+private class IntegerInputVerifier : InputVerifier() {
   override fun verify(c: JComponent): Boolean {
     var verified = false
     if (c is JTextComponent) {
-      val txt = c.getText()
+      val txt = c.text
       if (txt.isEmpty()) {
         return true
       }
       val iv = runCatching { Integer.parseInt(txt) }
-          .onFailure { UIManager.getLookAndFeel().provideErrorFeedback(c) }
-          .getOrNull() ?: -1
+        .onFailure { UIManager.getLookAndFeel().provideErrorFeedback(c) }
+        .getOrNull() ?: -1
       verified = iv >= 0
       // try {
       //   val iv = Integer.parseInt(txt)
@@ -102,23 +102,23 @@ internal class IntegerInputVerifier : InputVerifier() {
 // Validating Text and Filtering Documents and Accessibility and the Java Access Bridge Tech Tips
 // http://java.sun.com/developer/JDCTechTips/2005/tt0518.html
 // Validating with a Document Filter
-internal class IntegerDocumentFilter : DocumentFilter() {
+private class IntegerDocumentFilter : DocumentFilter() {
   @Throws(BadLocationException::class)
-  override fun insertString(fb: DocumentFilter.FilterBypass, offset: Int, text: String?, attr: AttributeSet) {
+  override fun insertString(fb: FilterBypass, offset: Int, text: String?, attr: AttributeSet) {
     if (text != null) {
       replace(fb, offset, 0, text, attr)
     }
   }
 
   @Throws(BadLocationException::class)
-  override fun remove(fb: DocumentFilter.FilterBypass, offset: Int, length: Int) {
+  override fun remove(fb: FilterBypass, offset: Int, length: Int) {
     replace(fb, offset, length, "", null)
   }
 
   @Throws(BadLocationException::class)
-  override fun replace(fb: DocumentFilter.FilterBypass, offset: Int, length: Int, text: String, attrs: AttributeSet?) {
-    val doc = fb.getDocument()
-    val currentLength = doc.getLength()
+  override fun replace(fb: FilterBypass, offset: Int, length: Int, text: String, attrs: AttributeSet?) {
+    val doc = fb.document
+    val currentLength = doc.length
     val currentContent = doc.getText(0, currentLength)
     val before = currentContent.substring(0, offset)
     val after = currentContent.substring(length + offset, currentLength)
@@ -136,13 +136,13 @@ internal class IntegerDocumentFilter : DocumentFilter() {
 
 // How to Use Formatted Text Fields (The Java™ Tutorials > Creating a GUI With JFC/Swing > Using Swing Components)
 // https://docs.oracle.com/javase/tutorial/uiswing/components/formattedtextfield.html
-class NumberFormatterFactory : DefaultFormatterFactory(numberFormatter, numberFormatter, numberFormatter) {
+private class NumberFormatterFactory : DefaultFormatterFactory(numberFormatter, numberFormatter, numberFormatter) {
   companion object {
     private val numberFormatter = NumberFormatter()
 
     init {
-      numberFormatter.setValueClass(Integer::class.java)
-      (numberFormatter.getFormat() as? NumberFormat)?.setGroupingUsed(false)
+      numberFormatter.valueClass = Integer::class.java
+      (numberFormatter.format as? NumberFormat)?.isGroupingUsed = false
     }
   }
 }
@@ -157,7 +157,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
