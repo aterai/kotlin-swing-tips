2d55ff13fed8bc24c371c83a0b1c35f059dd4520
aterai <aterai@outlook.com>
2020-05-26 17:23:26 +0900

    refactor: use of setter method instead of property access syntax

--- ColumnWidthTooltip/src/main/kotlin/example/App.kt
+++ ColumnWidthTooltip/src/main/kotlin/example/App.kt
@@ -8,74 +8,74 @@ import javax.swing.event.MouseInputListener
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("String", "Integer", "Boolean")
-    val data = arrayOf(
-      arrayOf("aaa", 12, true),
-      arrayOf("bbb", 5, false),
-      arrayOf("CCC", 92, true),
-      arrayOf("DDD", 0, false))
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-    }
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false))
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
 
-    val table = object : JTable(model) {
-      @Transient
-      private var handler: MouseInputListener? = null
+  val table = object : JTable(model) {
+    @Transient
+    private var handler: MouseInputListener? = null
 
-      override fun updateUI() {
-        getTableHeader().removeMouseListener(handler)
-        getTableHeader().removeMouseMotionListener(handler)
-        super.updateUI()
-        handler = ColumnWidthResizeHandler()
-        getTableHeader().addMouseListener(handler)
-        getTableHeader().addMouseMotionListener(handler)
-      }
+    override fun updateUI() {
+      getTableHeader().removeMouseListener(handler)
+      getTableHeader().removeMouseMotionListener(handler)
+      super.updateUI()
+      handler = ColumnWidthResizeHandler()
+      getTableHeader().addMouseListener(handler)
+      getTableHeader().addMouseMotionListener(handler)
     }
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class ColumnWidthResizeHandler : MouseInputAdapter() {
+private class ColumnWidthResizeHandler : MouseInputAdapter() {
   private val window = JWindow()
   private val tip = JToolTip()
   private var prev = ""
 
   private fun getToolTipLocation(e: MouseEvent): Point {
-    val p = e.getPoint()
-    val c = e.getComponent()
+    val p = e.point
+    val c = e.component
     SwingUtilities.convertPointToScreen(p, c)
-    p.translate(0, -tip.getPreferredSize().height)
+    p.translate(0, -tip.preferredSize.height)
     return p
   }
 
-  private fun getResizingColumn(e: MouseEvent) = (e.getComponent() as? JTableHeader)?.getResizingColumn()
+  private fun getResizingColumn(e: MouseEvent) = (e.component as? JTableHeader)?.resizingColumn
 
   private fun updateTooltipText(e: MouseEvent) {
     getResizingColumn(e)?.also {
-      val txt = "Width: ${it.getWidth()}px"
-      tip.setTipText(txt)
+      val txt = "Width: ${it.width}px"
+      tip.tipText = txt
       if (prev.length != txt.length) {
         window.pack()
       }
-      window.setLocation(getToolTipLocation(e))
+      window.location = getToolTipLocation(e)
       prev = txt
     }
   }
 
   override fun mouseDragged(e: MouseEvent) {
-    if (!window.isVisible() && getResizingColumn(e) != null) {
+    if (!window.isVisible && getResizingColumn(e) != null) {
       window.add(tip)
-      window.setAlwaysOnTop(true)
-      window.setVisible(true)
+      window.isAlwaysOnTop = true
+      window.isVisible = true
     }
     updateTooltipText(e)
   }
 
   override fun mouseReleased(e: MouseEvent) {
-    window.setVisible(false)
+    window.isVisible = false
   }
 }
 
@@ -89,7 +89,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
