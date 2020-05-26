4752cf0cef508ebffa1bd7eccc198e84e8ca5eb4
aterai <aterai@outlook.com>
2020-05-27 02:17:20 +0900

    add: TreeToolTips

---
+++ TreeToolTips/src/main/kotlin/example/App.kt
@@ -0,0 +1,60 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.MouseEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val tree1 = object : JTree() {
+    override fun getToolTipText(e: MouseEvent) =
+      getPathForLocation(e.x, e.y)?.lastPathComponent?.let { "getToolTipText: $it" }
+  }
+  ToolTipManager.sharedInstance().registerComponent(tree1)
+
+  val tree2 = object : JTree() {
+    override fun updateUI() {
+      setCellRenderer(null)
+      super.updateUI()
+      val r = getCellRenderer()
+      setCellRenderer { tree, value, selected, expanded, leaf, row, hasFocus ->
+        r.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus).also {
+          if (it is JComponent) {
+            it.toolTipText = value?.let { v -> "TreeCellRenderer: $v" }
+          }
+        }
+      }
+    }
+  }
+  ToolTipManager.sharedInstance().registerComponent(tree2)
+
+  return JPanel(GridLayout(2, 1)).also {
+    it.add(makeTitledPanel("Override getToolTipText", JScrollPane(tree1)))
+    it.add(makeTitledPanel("Use TreeCellRenderer", JScrollPane(tree2)))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c)
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
