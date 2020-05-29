74da001b42dfe0d5391b70ad2d1015d4546bb02c
aterai <aterai@outlook.com>
2020-05-30 01:46:21 +0900

    refactor: use of setter method instead of property access syntax

--- SingleThreadExecutor/src/main/kotlin/example/App.kt
+++ SingleThreadExecutor/src/main/kotlin/example/App.kt
@@ -12,139 +12,138 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableModel
 import javax.swing.table.TableRowSorter
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val model = WorkerModel()
-  private val table = JTable(model)
-  private val sorter = TableRowSorter(model)
-  private val deleteRowSet: MutableSet<Int> = TreeSet()
-  // private final ExecutorService executor = Executors.newFixedThreadPool(1);
-  private val executor: ExecutorService = Executors.newSingleThreadExecutor()
+private val model = WorkerModel()
+private val table = JTable(model)
+private val sorter = TableRowSorter(model)
+private val deleteRowSet: MutableSet<Int> = TreeSet()
+private val executor: ExecutorService = Executors.newSingleThreadExecutor()
 
-  init {
-    table.setRowSorter(sorter)
-    model.addProgressValue("Name 1", 100, null)
+fun makeUI(): Component {
+  table.rowSorter = sorter
+  model.addProgressValue("Name 1", 100, null)
 
-    val scrollPane = JScrollPane(table)
-    scrollPane.getViewport().setBackground(Color.WHITE)
-    table.setComponentPopupMenu(TablePopupMenu())
-    table.setFillsViewportHeight(true)
-    table.setIntercellSpacing(Dimension())
-    table.setShowGrid(false)
-    table.putClientProperty("terminateEditOnFocusLost", true)
+  val scrollPane = JScrollPane(table)
+  scrollPane.viewport.background = Color.WHITE
+  table.componentPopupMenu = TablePopupMenu()
+  table.fillsViewportHeight = true
+  table.intercellSpacing = Dimension()
+  table.setShowGrid(false)
+  table.putClientProperty("terminateEditOnFocusLost", true)
 
-    table.getColumnModel().getColumn(0).also {
-      it.setMaxWidth(60)
-      it.setMinWidth(60)
-      it.setResizable(false)
-    }
-    table.getColumnModel().getColumn(2).setCellRenderer(ProgressRenderer())
+  table.columnModel.getColumn(0).also {
+    it.maxWidth = 60
+    it.minWidth = 60
+    it.resizable = false
+  }
+  table.columnModel.getColumn(2).cellRenderer = ProgressRenderer()
 
-    addHierarchyListener { e ->
-      val cf = e.getChangeFlags().toInt() and HierarchyEvent.DISPLAYABILITY_CHANGED
-      if (cf != 0 && !e.getComponent().isDisplayable()) {
+  return JPanel(BorderLayout()).also {
+    it.addHierarchyListener { e ->
+      val cf = e.changeFlags.toInt() and HierarchyEvent.DISPLAYABILITY_CHANGED
+      if (cf != 0 && !e.component.isDisplayable) {
         executor.shutdownNow()
       }
     }
 
     val button = JButton("add")
     button.addActionListener { addActionPerformed() }
-    add(button, BorderLayout.SOUTH)
-    add(scrollPane)
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun addActionPerformed() {
-    val key = model.getRowCount()
-    val worker = object : BackgroundTask() {
-      override fun process(c: List<Int>) {
-        if (isCancelled()) {
-          return
-        }
-        if (!isDisplayable()) {
-          println("process: DISPOSE_ON_CLOSE")
-          cancel(true)
-          executor.shutdown()
-          return
-        }
-        c.forEach { model.setValueAt(it, key, 2) }
-      }
-
-      override fun done() {
-        if (!isDisplayable()) {
-          println("done: DISPOSE_ON_CLOSE")
-          cancel(true)
-          executor.shutdown()
-          return
-        }
-        var i = -1
-        val message = runCatching {
-          i = get()
-          if (i >= 0) "Done" else "Disposed"
-        }.getOrNull() ?: "Interrupted"
-        println("$key:$message(${i}ms)")
-        // executor.remove(this)
-      }
-    }
-    model.addProgressValue("example", 0, worker)
-    executor.execute(worker)
-  }
-
-  private fun cancelActionPerformed() {
-    for (i in table.getSelectedRows()) {
-      val mi = table.convertRowIndexToModel(i)
-      model.getSwingWorker(mi)?.takeUnless { it.isDone() }?.cancel(true)
-    }
-    table.repaint()
-  }
-
-  private fun deleteActionPerformed() {
-    val selection = table.getSelectedRows()
-    if (selection.isEmpty()) {
-      return
-    }
-    for (i in selection) {
-      val mi = table.convertRowIndexToModel(i)
-      deleteRowSet.add(mi)
-      model.getSwingWorker(mi)?.takeUnless { it.isDone() }?.cancel(true)
-    }
-    sorter.setRowFilter(object : RowFilter<TableModel, Int>() {
-      override fun include(entry: Entry<out TableModel, out Int>) = !deleteRowSet.contains(entry.getIdentifier())
-    })
-    table.clearSelection()
-    table.repaint()
-  }
-
-  private inner class TablePopupMenu : JPopupMenu() {
-    private val cancelMenuItem: JMenuItem
-    private val deleteMenuItem: JMenuItem
-
-    init {
-      add("add").addActionListener { addActionPerformed() }
-      addSeparator()
-      cancelMenuItem = add("cancel")
-      cancelMenuItem.addActionListener { cancelActionPerformed() }
-      deleteMenuItem = add("delete")
-      deleteMenuItem.addActionListener { deleteActionPerformed() }
-    }
-
-    override fun show(c: Component, x: Int, y: Int) {
-      val table = c as? JTable ?: return
-      val flag = table.getSelectedRowCount() > 0
-      cancelMenuItem.setEnabled(flag)
-      deleteMenuItem.setEnabled(flag)
-      super.show(table, x, y)
-    }
+    it.add(button, BorderLayout.SOUTH)
+    it.add(scrollPane)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-open class BackgroundTask : SwingWorker<Int, Int>() {
+private fun addActionPerformed() {
+  val key = model.rowCount
+  val worker = object : BackgroundTask() {
+    override fun process(c: List<Int>) {
+      if (isCancelled) {
+        return
+      }
+      if (!table.isDisplayable) {
+        println("process: DISPOSE_ON_CLOSE")
+        cancel(true)
+        executor.shutdown()
+        return
+      }
+      c.forEach { model.setValueAt(it, key, 2) }
+    }
+
+    override fun done() {
+      if (!table.isDisplayable) {
+        println("done: DISPOSE_ON_CLOSE")
+        cancel(true)
+        executor.shutdown()
+        return
+      }
+      var i = -1
+      val message = runCatching {
+        i = get()
+        if (i >= 0) "Done" else "Disposed"
+      }.getOrNull() ?: "Interrupted"
+      println("$key:$message(${i}ms)")
+      // executor.remove(this)
+    }
+  }
+  model.addProgressValue("example", 0, worker)
+  executor.execute(worker)
+}
+
+private fun cancelActionPerformed() {
+  for (i in table.selectedRows) {
+    val mi = table.convertRowIndexToModel(i)
+    model.getSwingWorker(mi)?.takeUnless { it.isDone }?.cancel(true)
+  }
+  table.repaint()
+}
+
+private fun deleteActionPerformed() {
+  val selection = table.selectedRows
+  if (selection.isEmpty()) {
+    return
+  }
+  for (i in selection) {
+    val mi = table.convertRowIndexToModel(i)
+    deleteRowSet.add(mi)
+    model.getSwingWorker(mi)?.takeUnless { it.isDone }?.cancel(true)
+  }
+  sorter.rowFilter = object : RowFilter<TableModel, Int>() {
+    override fun include(entry: Entry<out TableModel, out Int>) = !deleteRowSet.contains(entry.identifier)
+  }
+  table.clearSelection()
+  table.repaint()
+}
+
+private class TablePopupMenu : JPopupMenu() {
+  private val cancelMenuItem: JMenuItem
+  private val deleteMenuItem: JMenuItem
+
+  init {
+    add("add").addActionListener { addActionPerformed() }
+    addSeparator()
+    cancelMenuItem = add("cancel")
+    cancelMenuItem.addActionListener { cancelActionPerformed() }
+    deleteMenuItem = add("delete")
+    deleteMenuItem.addActionListener { deleteActionPerformed() }
+  }
+
+  override fun show(c: Component, x: Int, y: Int) {
+    val table = c as? JTable ?: return
+    val flag = table.selectedRowCount > 0
+    cancelMenuItem.isEnabled = flag
+    deleteMenuItem.isEnabled = flag
+    super.show(table, x, y)
+  }
+}
+
+private open class BackgroundTask : SwingWorker<Int, Int>() {
   private val sleepDummy = (1..50).random()
 
   @Throws(InterruptedException::class)
   override fun doInBackground(): Int? {
     val lengthOfTask = 120
     var current = 0
-    while (current <= lengthOfTask && !isCancelled()) {
+    while (current <= lengthOfTask && !isCancelled) {
       publish(100 * current / lengthOfTask)
       Thread.sleep(sleepDummy.toLong())
       current++
@@ -153,7 +152,7 @@ open class BackgroundTask : SwingWorker<Int, Int>() {
   }
 }
 
-open class WorkerModel : DefaultTableModel() {
+private open class WorkerModel : DefaultTableModel() {
   private val workerMap = ConcurrentHashMap<Int, SwingWorker<Int, Int>>()
   private var number = 0
 
@@ -186,7 +185,7 @@ open class WorkerModel : DefaultTableModel() {
   }
 }
 
-internal class ProgressRenderer : DefaultTableCellRenderer() {
+private class ProgressRenderer : DefaultTableCellRenderer() {
   private val progress = JProgressBar()
   private val renderer: JPanel? = JPanel(BorderLayout())
 
@@ -202,11 +201,11 @@ internal class ProgressRenderer : DefaultTableCellRenderer() {
     var text = "Done"
     if (i < 0) {
       text = "Canceled"
-    } else if (i < progress.getMaximum() && renderer != null) { // < 100
-      progress.setValue(i)
+    } else if (i < progress.maximum && renderer != null) { // < 100
+      progress.value = i
       renderer.add(progress)
-      renderer.setOpaque(false)
-      renderer.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
+      renderer.isOpaque = false
+      renderer.border = BorderFactory.createEmptyBorder(2, 2, 2, 2)
       return renderer
     }
     super.getTableCellRendererComponent(table, text, isSelected, hasFocus, row, column)
@@ -215,7 +214,7 @@ internal class ProgressRenderer : DefaultTableCellRenderer() {
 
   override fun updateUI() {
     super.updateUI()
-    setOpaque(false)
+    isOpaque = false
     renderer?.also { SwingUtilities.updateComponentTreeUI(it) }
   }
 }
@@ -230,7 +229,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
