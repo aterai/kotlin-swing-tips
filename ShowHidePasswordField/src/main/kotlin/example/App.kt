48f7a3cce3777e3dc87d1454684bc39e588d0eca
aterai <aterai@outlook.com>
2020-05-29 18:49:04 +0900

    refactor: use of setter method instead of property access syntax

--- ShowHidePasswordField/src/main/kotlin/example/App.kt
+++ ShowHidePasswordField/src/main/kotlin/example/App.kt
@@ -5,121 +5,125 @@ import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(GridLayout(4, 1, 0, 2)) {
-  init {
-    val pf1 = makePasswordField()
-    val b1 = JCheckBox("show passwords")
-    b1.addActionListener { e ->
-      val c = e.getSource() as? AbstractButton ?: return@addActionListener
-      pf1.setEchoChar(if (c.isSelected()) '\u0000' else getUIEchoChar())
-    }
-    val p1 = JPanel(BorderLayout())
-    p1.add(pf1)
-    p1.add(b1, BorderLayout.SOUTH)
-    add(makeTitledPanel("BorderLayout + JCheckBox", p1))
-    val pf2 = makePasswordField()
-    val b2 = JToggleButton()
-    b2.addActionListener { e ->
-      val c = e.getSource() as? AbstractButton ?: return@addActionListener
-      pf2.setEchoChar(if (c.isSelected()) '\u0000' else getUIEchoChar())
-    }
-    initEyeButton(b2)
-    val p2 = makeOverlayLayoutPanel()
-    p2.add(b2)
-    p2.add(pf2)
-    add(makeTitledPanel("OverlayLayout + JToggleButton", p2))
-    val pf3 = makePasswordField()
-    val tf3 = JTextField(24)
-    tf3.setFont(Font(Font.MONOSPACED, Font.PLAIN, 12))
-    tf3.enableInputMethods(false)
-    tf3.setDocument(pf3.getDocument())
-    val cardLayout = CardLayout()
-    val p3 = object : JPanel(cardLayout) {
-      override fun updateUI() {
-        super.updateUI()
-        setAlignmentX(Component.RIGHT_ALIGNMENT)
-      }
-    }
-    p3.add(pf3, PasswordField.HIDE.toString())
-    p3.add(tf3, PasswordField.SHOW.toString())
-    val b3 = JToggleButton()
-    b3.addActionListener { e ->
-      val c = e.getSource() as? AbstractButton ?: return@addActionListener
-      val s = if (c.isSelected()) PasswordField.SHOW else PasswordField.HIDE
-      cardLayout.show(p3, s.toString())
-    }
-    initEyeButton(b3)
-    val pp3 = makeOverlayLayoutPanel()
-    pp3.add(b3)
-    pp3.add(p3)
-    add(makeTitledPanel("CardLayout + JTextField(can copy) + ...", pp3))
-    val pf4 = makePasswordField()
-    val b4 = JButton()
-    b4.addMouseListener(object : MouseAdapter() {
-      override fun mousePressed(e: MouseEvent) {
-        pf4.setEchoChar('\u0000')
-      }
-
-      override fun mouseReleased(e: MouseEvent) {
-        pf4.setEchoChar(getUIEchoChar())
-      }
-    })
-    initEyeButton(b4)
-    val p4 = makeOverlayLayoutPanel()
-    p4.add(b4)
-    p4.add(pf4)
-    add(makeTitledPanel("press and hold down the mouse button", p4))
-    setBorder(BorderFactory.createEmptyBorder(2, 5, 2, 5))
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val pf1 = makePasswordField()
+  val b1 = JCheckBox("show passwords")
+  b1.addActionListener { e ->
+    val c = e.source as? AbstractButton ?: return@addActionListener
+    pf1.echoChar = if (c.isSelected) '\u0000' else getUIEchoChar()
   }
+  val p1 = JPanel(BorderLayout())
+  p1.add(pf1)
+  p1.add(b1, BorderLayout.SOUTH)
 
-  private fun getUIEchoChar() = UIManager.get("PasswordField.echoChar") as? Char ?: '*'
-
-  private fun initEyeButton(b: AbstractButton) {
-    b.setFocusable(false)
-    b.setOpaque(false)
-    b.setContentAreaFilled(false)
-    b.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 4))
-    b.setAlignmentX(Component.RIGHT_ALIGNMENT)
-    b.setAlignmentY(Component.CENTER_ALIGNMENT)
-    b.setIcon(ColorIcon(Color.GREEN))
-    b.setRolloverIcon(ColorIcon(Color.BLUE))
-    b.setSelectedIcon(ColorIcon(Color.RED))
-    b.setRolloverSelectedIcon(ColorIcon(Color.ORANGE))
-    b.setToolTipText("show/hide passwords")
+  val pf2 = makePasswordField()
+  val b2 = JToggleButton()
+  b2.addActionListener { e ->
+    val c = e.source as? AbstractButton ?: return@addActionListener
+    pf2.echoChar = if (c.isSelected) '\u0000' else getUIEchoChar()
   }
+  initEyeButton(b2)
+  val p2 = makeOverlayLayoutPanel()
+  p2.add(b2)
+  p2.add(pf2)
 
-  private fun makeOverlayLayoutPanel() = object : JPanel() {
-    override fun isOptimizedDrawingEnabled() = false
-  }.also {
-    it.setLayout(OverlayLayout(it))
+  val pf3 = makePasswordField()
+  val tf3 = JTextField(24)
+  tf3.font = Font(Font.MONOSPACED, Font.PLAIN, 12)
+  tf3.enableInputMethods(false)
+  tf3.document = pf3.document
+  val cardLayout = CardLayout()
+  val p3 = object : JPanel(cardLayout) {
+    override fun updateUI() {
+      super.updateUI()
+      alignmentX = Component.RIGHT_ALIGNMENT
+    }
   }
-
-  private fun makePasswordField() = JPasswordField(24).also {
-    it.setText("1234567890")
-    it.setAlignmentX(Component.RIGHT_ALIGNMENT)
+  p3.add(pf3, PasswordField.HIDE.toString())
+  p3.add(tf3, PasswordField.SHOW.toString())
+  val b3 = JToggleButton()
+  b3.addActionListener { e ->
+    val c = e.source as? AbstractButton ?: return@addActionListener
+    val s = if (c.isSelected) PasswordField.SHOW else PasswordField.HIDE
+    cardLayout.show(p3, s.toString())
   }
+  initEyeButton(b3)
+  val pp3 = makeOverlayLayoutPanel()
+  pp3.add(b3)
+  pp3.add(p3)
 
-  private fun makeTitledPanel(title: String, cmp: Component): Component {
-    val p = JPanel(GridBagLayout())
-    p.setBorder(BorderFactory.createTitledBorder(title))
-    val c = GridBagConstraints()
-    c.weightx = 1.0
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.insets = Insets(5, 5, 5, 5)
-    p.add(cmp, c)
-    return p
+  val pf4 = makePasswordField()
+  val b4 = JButton()
+  b4.addMouseListener(object : MouseAdapter() {
+    override fun mousePressed(e: MouseEvent) {
+      pf4.echoChar = '\u0000'
+    }
+
+    override fun mouseReleased(e: MouseEvent) {
+      pf4.echoChar = getUIEchoChar()
+    }
+  })
+  initEyeButton(b4)
+  val p4 = makeOverlayLayoutPanel()
+  p4.add(b4)
+  p4.add(pf4)
+
+  return JPanel(GridLayout(4, 1, 0, 2)).also {
+    it.add(makeTitledPanel("BorderLayout + JCheckBox", p1))
+    it.add(makeTitledPanel("OverlayLayout + JToggleButton", p2))
+    it.add(makeTitledPanel("CardLayout + JTextField(can copy) + ...", pp3))
+    it.add(makeTitledPanel("press and hold down the mouse button", p4))
+    it.border = BorderFactory.createEmptyBorder(2, 5, 2, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-enum class PasswordField { SHOW, HIDE }
+private fun getUIEchoChar() = UIManager.get("PasswordField.echoChar") as? Char ?: '*'
 
-class ColorIcon(private val color: Color) : Icon {
+private fun initEyeButton(b: AbstractButton) {
+  b.isFocusable = false
+  b.isOpaque = false
+  b.isContentAreaFilled = false
+  b.border = BorderFactory.createEmptyBorder(0, 0, 0, 4)
+  b.alignmentX = Component.RIGHT_ALIGNMENT
+  b.alignmentY = Component.CENTER_ALIGNMENT
+  b.icon = ColorIcon(Color.GREEN)
+  b.rolloverIcon = ColorIcon(Color.BLUE)
+  b.selectedIcon = ColorIcon(Color.RED)
+  b.rolloverSelectedIcon = ColorIcon(Color.ORANGE)
+  b.toolTipText = "show/hide passwords"
+}
+
+private fun makeOverlayLayoutPanel() = object : JPanel() {
+  override fun isOptimizedDrawingEnabled() = false
+}.also {
+  it.layout = OverlayLayout(it)
+}
+
+private fun makePasswordField() = JPasswordField(24).also {
+  it.text = "1234567890"
+  it.alignmentX = Component.RIGHT_ALIGNMENT
+}
+
+private fun makeTitledPanel(title: String, cmp: Component): Component {
+  val p = JPanel(GridBagLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  val c = GridBagConstraints()
+  c.weightx = 1.0
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.insets = Insets(5, 5, 5, 5)
+  p.add(cmp, c)
+  return p
+}
+
+private enum class PasswordField { SHOW, HIDE }
+
+private class ColorIcon(private val color: Color) : Icon {
   override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
     val g2 = g.create() as Graphics2D
     g2.translate(x, y)
-    g2.setPaint(color)
-    g2.fillRect(1, 1, getIconWidth() - 2, getIconHeight() - 2)
+    g2.paint = color
+    g2.fillRect(1, 1, iconWidth - 2, iconHeight - 2)
     g2.dispose()
   }
 
@@ -138,7 +142,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
