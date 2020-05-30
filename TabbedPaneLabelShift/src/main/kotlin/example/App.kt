7c4e060b37fb4cf57168725823f8f5d6a543dbdd
aterai <aterai@outlook.com>
2020-05-31 02:10:52 +0900

    SonarLint: String literals should not be duplicated

--- TabbedPaneLabelShift/src/main/kotlin/example/App.kt
+++ TabbedPaneLabelShift/src/main/kotlin/example/App.kt
@@ -3,65 +3,65 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout(5, 5)) {
-  init {
-    println(UIManager.getLookAndFeelDefaults().get("TabbedPane.selectedLabelShift"))
-    println(UIManager.getLookAndFeelDefaults().get("TabbedPane.labelShift"))
+fun makeUI() = JPanel(BorderLayout(5, 5)).also { panel ->
+  val selectedLabelShift = "TabbedPane.selectedLabelShift"
+  val labelShift = "TabbedPane.labelShift"
 
-    val slsiv = UIManager.getLookAndFeelDefaults().getInt("TabbedPane.selectedLabelShift")
-    val slsModel = SpinnerNumberModel(slsiv, -5, 5, 1)
-    slsModel.addChangeListener { e ->
-      val source = e.getSource() as? SpinnerNumberModel ?: return@addChangeListener
-      val offset = source.getNumber().toInt()
-      UIManager.put("TabbedPane.selectedLabelShift", offset)
-      SwingUtilities.updateComponentTreeUI(getTopLevelAncestor())
+  println(UIManager.getLookAndFeelDefaults()[selectedLabelShift])
+  println(UIManager.getLookAndFeelDefaults()[labelShift])
+
+  val sls = UIManager.getLookAndFeelDefaults().getInt(selectedLabelShift)
+  val slsModel = SpinnerNumberModel(sls, -5, 5, 1)
+  slsModel.addChangeListener { e ->
+    (e.source as? SpinnerNumberModel)?.also {
+      UIManager.put(selectedLabelShift, it.number.toInt())
+      SwingUtilities.updateComponentTreeUI(panel.topLevelAncestor)
     }
-
-    val lsiv = UIManager.getLookAndFeelDefaults().getInt("TabbedPane.labelShift")
-    val lsModel = SpinnerNumberModel(lsiv, -5, 5, 1)
-    lsModel.addChangeListener { e ->
-      val source = e.getSource() as? SpinnerNumberModel ?: return@addChangeListener
-      val offset = source.getNumber().toInt()
-      UIManager.put("TabbedPane.labelShift", offset)
-      SwingUtilities.updateComponentTreeUI(getTopLevelAncestor())
-    }
-
-    val box1 = Box.createHorizontalBox()
-    box1.setBorder(BorderFactory.createTitledBorder("UIManager.put(\"TabbedPane.selectedLabelShift\", offset)"))
-    box1.add(JLabel("offset = "))
-    box1.add(JSpinner(slsModel))
-    box1.add(Box.createHorizontalGlue())
-
-    val box2 = Box.createHorizontalBox()
-    box2.setBorder(BorderFactory.createTitledBorder("UIManager.put(\"TabbedPane.labelShift\", offset)"))
-    box2.add(JLabel("offset = "))
-    box2.add(JSpinner(lsModel))
-    box2.add(Box.createHorizontalGlue())
-
-    val p = JPanel(GridLayout(2, 1))
-    p.add(box1)
-    p.add(box2)
-
-    val tabbedPane = JTabbedPane()
-    tabbedPane.addTab("title 0", ColorIcon(Color.RED), JScrollPane(JTree()))
-    tabbedPane.addTab("title 1", ColorIcon(Color.GREEN), JButton("button"))
-    tabbedPane.addTab("title 2", ColorIcon(Color.BLUE), JLabel("label"))
-    tabbedPane.addTab("title 3", JPanel())
-    tabbedPane.setTabComponentAt(3, JLabel("label", ColorIcon(Color.ORANGE), SwingConstants.LEFT))
-
-    add(p, BorderLayout.NORTH)
-    add(tabbedPane)
-    setPreferredSize(Dimension(320, 240))
   }
+
+  val ls = UIManager.getLookAndFeelDefaults().getInt(labelShift)
+  val lsModel = SpinnerNumberModel(ls, -5, 5, 1)
+  lsModel.addChangeListener { e ->
+    (e.source as? SpinnerNumberModel)?.also {
+      UIManager.put(labelShift, it.number.toInt())
+      SwingUtilities.updateComponentTreeUI(panel.topLevelAncestor)
+    }
+  }
+
+  val box1 = Box.createHorizontalBox()
+  box1.border = BorderFactory.createTitledBorder("""UIManager.put("$selectedLabelShift", offset)""")
+  box1.add(JLabel("offset = "))
+  box1.add(JSpinner(slsModel))
+  box1.add(Box.createHorizontalGlue())
+
+  val box2 = Box.createHorizontalBox()
+  box2.border = BorderFactory.createTitledBorder("""UIManager.put("$labelShift", offset)""")
+  box2.add(JLabel("offset = "))
+  box2.add(JSpinner(lsModel))
+  box2.add(Box.createHorizontalGlue())
+
+  val p = JPanel(GridLayout(2, 1))
+  p.add(box1)
+  p.add(box2)
+
+  val tabbedPane = JTabbedPane()
+  tabbedPane.addTab("title 0", ColorIcon(Color.RED), JScrollPane(JTree()))
+  tabbedPane.addTab("title 1", ColorIcon(Color.GREEN), JButton("button"))
+  tabbedPane.addTab("title 2", ColorIcon(Color.BLUE), JLabel("label"))
+  tabbedPane.addTab("title 3", JPanel())
+  tabbedPane.setTabComponentAt(3, JLabel("label", ColorIcon(Color.ORANGE), SwingConstants.LEFT))
+
+  panel.add(p, BorderLayout.NORTH)
+  panel.add(tabbedPane)
+  panel.preferredSize = Dimension(320, 240)
 }
 
-internal class ColorIcon(private val color: Color) : Icon {
-
+private class ColorIcon(private val color: Color) : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    val g2 = g.create() as Graphics2D
+    val g2 = g.create() as? Graphics2D ?: return
     g2.translate(x, y)
-    g2.setPaint(color)
-    g2.fillRect(0, 0, getIconWidth(), getIconHeight())
+    g2.paint = color
+    g2.fillRect(0, 0, iconWidth, iconHeight)
     g2.dispose()
   }
 
@@ -80,7 +80,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
