bee781b813fd80098b652d63b3f50b4fc90ab219
aterai <aterai@outlook.com>
2020-06-03 18:42:27 +0900

    refactor: use of setter method instead of property access syntax

--- GotoLine/src/main/kotlin/example/App.kt
+++ GotoLine/src/main/kotlin/example/App.kt
@@ -7,40 +7,40 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val model = SpinnerNumberModel(100, 1, 2000, 1)
-    val textArea = JTextArea("1111111111111111\n".repeat(2000))
-    textArea.setBorder(BorderFactory.createEmptyBorder(0, 2, 0, 0))
-    val scroll = JScrollPane(textArea)
-    scroll.setRowHeaderView(LineNumberView(textArea))
+fun makeUI(): Component {
+  val model = SpinnerNumberModel(100, 1, 2000, 1)
+  val textArea = JTextArea("1111111111111111\n".repeat(2000))
+  textArea.border = BorderFactory.createEmptyBorder(0, 2, 0, 0)
+  val scroll = JScrollPane(textArea)
+  scroll.setRowHeaderView(LineNumberView(textArea))
 
-    val button = JButton("Goto Line")
-    button.addActionListener {
-      val doc = textArea.getDocument()
-      val root = doc.getDefaultRootElement()
-      val i = model.getNumber().toInt()
-      runCatching {
-        val elem = root.getElement(i - 1)
-        val rect = textArea.modelToView(elem.getStartOffset())
-        val vr = scroll.getViewport().getViewRect()
-        rect.setSize(10, vr.height)
-        textArea.scrollRectToVisible(rect)
-        textArea.setCaretPosition(elem.getStartOffset())
-        // textArea.requestFocus();
-      }.onFailure {
-        UIManager.getLookAndFeel().provideErrorFeedback(textArea)
-      }
+  val button = JButton("Goto Line")
+  button.addActionListener {
+    val doc = textArea.document
+    val root = doc.defaultRootElement
+    val i = model.number.toInt()
+    runCatching {
+      val elem = root.getElement(i - 1)
+      val rect = textArea.modelToView(elem.startOffset)
+      val vr = scroll.viewport.viewRect
+      rect.setSize(10, vr.height)
+      textArea.scrollRectToVisible(rect)
+      textArea.caretPosition = elem.startOffset
+      // textArea.requestFocus();
+    }.onFailure {
+      UIManager.getLookAndFeel().provideErrorFeedback(textArea)
     }
-    // frame.getRootPane().setDefaultButton(button);
-    EventQueue.invokeLater { getRootPane().setDefaultButton(button) }
+  }
+  EventQueue.invokeLater { button.rootPane.defaultButton = button }
 
-    val p = JPanel(BorderLayout())
-    p.add(JSpinner(model))
-    p.add(button, BorderLayout.EAST)
-    add(p, BorderLayout.NORTH)
-    add(scroll)
-    setPreferredSize(Dimension(320, 240))
+  val p = JPanel(BorderLayout())
+  p.add(JSpinner(model))
+  p.add(button, BorderLayout.EAST)
+
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(scroll)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
@@ -52,22 +52,22 @@ internal class LineNumberView(private val textArea: JTextArea) : JComponent() {
   private val fontLeading: Int
   private val componentWidth: Int
     get() {
-      val lineCount = textArea.getLineCount()
+      val lineCount = textArea.lineCount
       val maxDigits = maxOf(3, lineCount.toString().length)
-      val i = getInsets()
+      val i = insets
       return maxDigits * fontMetrics.stringWidth("0") + i.left + i.right
     }
 
   init {
-    val font = textArea.getFont()
+    val font = textArea.font
     fontMetrics = getFontMetrics(font)
-    fontHeight = fontMetrics.getHeight()
-    fontAscent = fontMetrics.getAscent()
-    fontDescent = fontMetrics.getDescent()
-    fontLeading = fontMetrics.getLeading()
+    fontHeight = fontMetrics.height
+    fontAscent = fontMetrics.ascent
+    fontDescent = fontMetrics.descent
+    fontLeading = fontMetrics.leading
     // topInset = textArea.getInsets().top;
 
-    textArea.getDocument().addDocumentListener(object : DocumentListener {
+    textArea.document.addDocumentListener(object : DocumentListener {
       override fun insertUpdate(e: DocumentEvent) {
         repaint()
       }
@@ -76,7 +76,8 @@ internal class LineNumberView(private val textArea: JTextArea) : JComponent() {
         repaint()
       }
 
-      override fun changedUpdate(e: DocumentEvent) { /* not needed */ }
+      override fun changedUpdate(e: DocumentEvent) { /* not needed */
+      }
     })
     textArea.addComponentListener(object : ComponentAdapter() {
       override fun componentResized(e: ComponentEvent) {
@@ -84,34 +85,35 @@ internal class LineNumberView(private val textArea: JTextArea) : JComponent() {
         repaint()
       }
     })
-    val i = textArea.getInsets()
-    setBorder(BorderFactory.createCompoundBorder(
+    val i = textArea.insets
+    border = BorderFactory.createCompoundBorder(
       BorderFactory.createMatteBorder(0, 0, 0, 1, Color.GRAY),
-      BorderFactory.createEmptyBorder(i.top, MARGIN, i.bottom, MARGIN - 1)))
-    setOpaque(true)
-    setBackground(Color.WHITE)
+      BorderFactory.createEmptyBorder(i.top, MARGIN, i.bottom, MARGIN - 1)
+    )
+    isOpaque = true
+    background = Color.WHITE
     setFont(font)
   }
 
   private fun getLineAtPoint(y: Int): Int {
-    val root = textArea.getDocument().getDefaultRootElement()
+    val root = textArea.document.defaultRootElement
     val pos = textArea.viewToModel(Point(0, y))
     return root.getElementIndex(pos)
   }
 
-  override fun getPreferredSize() = Dimension(componentWidth, textArea.getHeight())
+  override fun getPreferredSize() = Dimension(componentWidth, textArea.height)
 
   override fun paintComponent(g: Graphics) {
-    g.setColor(getBackground())
-    val clip = g.getClipBounds()
+    g.color = background
+    val clip = g.clipBounds
     g.fillRect(clip.x, clip.y, clip.width, clip.height)
 
-    g.setColor(getForeground())
+    g.color = foreground
     val base = clip.y
     val start = getLineAtPoint(base)
     val end = getLineAtPoint(base + clip.height)
     var y = start * fontHeight
-    val rmg = getInsets().right
+    val rmg = insets.right
     for (i in start..end) {
       val text = (i + 1).toString()
       val x = componentWidth - rmg - fontMetrics.stringWidth(text)
@@ -136,7 +138,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
