1aee3541090286b0c864f20f4c6dd3ebae1b4fd5
aterai <aterai@outlook.com>
2020-06-05 13:39:03 +0900

    refactor: use of setter method instead of property access syntax

--- TableHeaderCheckBox/src/main/kotlin/example/App.kt
+++ TableHeaderCheckBox/src/main/kotlin/example/App.kt
@@ -13,72 +13,67 @@ import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 import javax.swing.table.TableColumn
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val columnNames = arrayOf(Status.INDETERMINATE, "Integer", "String")
-  private val data = arrayOf(
-    arrayOf(true, 1, "BBB"),
-    arrayOf(false, 12, "AAA"),
-    arrayOf(true, 2, "DDD"),
-    arrayOf(false, 5, "CCC"),
-    arrayOf(true, 3, "EEE"),
-    arrayOf(false, 6, "GGG"),
-    arrayOf(true, 4, "FFF"),
-    arrayOf(false, 7, "HHH")
-  )
-  private val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  private val table = object : JTable(model) {
-    @Transient
-    private var handler: HeaderCheckBoxHandler? = null
+private const val CHECKBOX_COLUMN = 0
+private val columnNames = arrayOf(Status.INDETERMINATE, "Integer", "String")
+private val data = arrayOf(
+  arrayOf(true, 1, "BBB"),
+  arrayOf(false, 12, "AAA"),
+  arrayOf(true, 2, "DDD"),
+  arrayOf(false, 5, "CCC"),
+  arrayOf(true, 3, "EEE"),
+  arrayOf(false, 6, "GGG"),
+  arrayOf(true, 4, "FFF"),
+  arrayOf(false, 7, "HHH")
+)
+private val model = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+}
+private val table = object : JTable(model) {
+  @Transient
+  private var handler: HeaderCheckBoxHandler? = null
 
-    override fun updateUI() {
-      // [JDK-6788475] Changing to Nimbus LAF and back doesn't reset look and feel of JTable completely
-      // https://bugs.openjdk.java.net/browse/JDK-6788475
-      // XXX: set dummy ColorUIResource
-      setSelectionForeground(ColorUIResource(Color.RED))
-      setSelectionBackground(ColorUIResource(Color.RED))
-      getTableHeader()?.removeMouseListener(handler)
-      getModel()?.removeTableModelListener(handler)
-      super.updateUI()
+  override fun updateUI() {
+    // [JDK-6788475] Changing to Nimbus LAF and back doesn't reset look and feel of JTable completely
+    // https://bugs.openjdk.java.net/browse/JDK-6788475
+    // XXX: set dummy ColorUIResource
+    setSelectionForeground(ColorUIResource(Color.RED))
+    setSelectionBackground(ColorUIResource(Color.RED))
+    getTableHeader()?.removeMouseListener(handler)
+    model?.removeTableModelListener(handler)
+    super.updateUI()
 
-      getModel()?.also {
-        for (i in 0 until it.getColumnCount()) {
-          val r = getDefaultRenderer(it.getColumnClass(i)) as? Component ?: continue
-          SwingUtilities.updateComponentTreeUI(r)
-        }
-        handler = HeaderCheckBoxHandler(this, CHECKBOX_COLUMN)
-        it.addTableModelListener(handler)
-        getTableHeader().addMouseListener(handler)
+    model?.also {
+      for (i in 0 until it.columnCount) {
+        val r = getDefaultRenderer(it.getColumnClass(i)) as? Component ?: continue
+        SwingUtilities.updateComponentTreeUI(r)
       }
-      getColumnModel().getColumn(CHECKBOX_COLUMN).also {
-        it.setHeaderRenderer(HeaderRenderer())
-        it.setHeaderValue(Status.INDETERMINATE)
-      }
+      handler = HeaderCheckBoxHandler(this, CHECKBOX_COLUMN)
+      it.addTableModelListener(handler)
+      getTableHeader().addMouseListener(handler)
     }
-
-    override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
-      val c = super.prepareEditor(editor, row, column)
-      (c as? JCheckBox)?.also {
-        it.setBackground(getSelectionBackground())
-        it.setBorderPainted(true)
-      }
-      return c
+    getColumnModel().getColumn(CHECKBOX_COLUMN).also {
+      it.headerRenderer = HeaderRenderer()
+      it.headerValue = Status.INDETERMINATE
     }
   }
 
-  init {
-    table.setFillsViewportHeight(true)
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  companion object {
-    private const val CHECKBOX_COLUMN = 0
+  override fun prepareEditor(editor: TableCellEditor, row: Int, column: Int): Component {
+    val c = super.prepareEditor(editor, row, column)
+    (c as? JCheckBox)?.also {
+      it.background = getSelectionBackground()
+      it.isBorderPainted = true
+    }
+    return c
   }
 }
 
-class HeaderRenderer : TableCellRenderer {
+fun makeUI() = JPanel(BorderLayout()).also {
+  table.fillsViewportHeight = true
+  it.add(JScrollPane(table))
+  it.preferredSize = Dimension(320, 240)
+}
+
+private class HeaderRenderer : TableCellRenderer {
   private val check = JCheckBox("")
   private val label = JLabel("Check All")
 
@@ -97,36 +92,36 @@ class HeaderRenderer : TableCellRenderer {
       Status.INDETERMINATE -> updateCheckBox(isSelected = true, isEnabled = false)
       // else -> throw AssertionError("Unknown Status")
     }
-    check.setOpaque(false)
-    check.setFont(table.getFont())
-    val r = table.getTableHeader().getDefaultRenderer()
+    check.isOpaque = false
+    check.font = table.font
+    val r = table.tableHeader.defaultRenderer
     val c = r.getTableCellRendererComponent(table, status, isSelected, hasFocus, row, column)
     (c as? JLabel)?.also {
-      label.setIcon(ComponentIcon(check))
-      it.setIcon(ComponentIcon(label))
-      it.setText(null) // XXX: Nimbus???
+      label.icon = ComponentIcon(check)
+      it.icon = ComponentIcon(label)
+      it.text = null // XXX: Nimbus???
     }
     return c
   }
 
   private fun updateCheckBox(isSelected: Boolean, isEnabled: Boolean) {
-    check.setSelected(isSelected)
-    check.setEnabled(isEnabled)
+    check.isSelected = isSelected
+    check.isEnabled = isEnabled
   }
 }
 
-class HeaderCheckBoxHandler(
+private class HeaderCheckBoxHandler(
   private val table: JTable,
   private val targetColumnIndex: Int
 ) : MouseAdapter(), TableModelListener {
   override fun tableChanged(e: TableModelEvent) {
-    if (e.getType() == TableModelEvent.UPDATE && e.getColumn() == targetColumnIndex) {
+    if (e.type == TableModelEvent.UPDATE && e.column == targetColumnIndex) {
       val vci = table.convertColumnIndexToView(targetColumnIndex)
-      val column = table.getColumnModel().getColumn(vci)
-      val status = column.getHeaderValue()
-      val m = table.getModel()
+      val column = table.columnModel.getColumn(vci)
+      val status = column.headerValue
+      val m = table.model
       if (m is DefaultTableModel && fireUpdateEvent(m, column, status)) {
-        val h = table.getTableHeader()
+        val h = table.tableHeader
         h.repaint(h.getHeaderRect(vci))
       }
     }
@@ -134,49 +129,49 @@ class HeaderCheckBoxHandler(
 
   private fun fireUpdateEvent(m: DefaultTableModel, column: TableColumn, status: Any): Boolean {
     return if (Status.INDETERMINATE == status) {
-      val l = m.getDataVector().mapNotNull { (it as? List<*>)?.get(targetColumnIndex) as? Boolean }.distinct()
+      val l = m.dataVector.mapNotNull { (it as? List<*>)?.get(targetColumnIndex) as? Boolean }.distinct()
       val isOnlyOneSelected = l.size == 1
       if (isOnlyOneSelected) {
         // column.setHeaderValue(if (l.get(0)) Status.SELECTED else Status.DESELECTED)
-        column.setHeaderValue(if (l.first()) Status.SELECTED else Status.DESELECTED)
+        column.headerValue = if (l.first()) Status.SELECTED else Status.DESELECTED
         true
       } else {
         false
       }
     } else {
-      column.setHeaderValue(Status.INDETERMINATE)
+      column.headerValue = Status.INDETERMINATE
       true
     }
   }
 
   override fun mouseClicked(e: MouseEvent) {
-    val tbl = (e.getComponent() as? JTableHeader)?.getTable() ?: return
-    val columnModel = tbl.getColumnModel()
-    val m = tbl.getModel()
-    val vci = columnModel.getColumnIndexAtX(e.getX())
+    val tbl = (e.component as? JTableHeader)?.table ?: return
+    val columnModel = tbl.columnModel
+    val m = tbl.model
+    val vci = columnModel.getColumnIndexAtX(e.x)
     val mci = tbl.convertColumnIndexToModel(vci)
-    if (mci == targetColumnIndex && m.getRowCount() > 0) {
+    if (mci == targetColumnIndex && m.rowCount > 0) {
       val column = columnModel.getColumn(vci)
-      val b = Status.DESELECTED === column.getHeaderValue()
-      for (i in 0 until m.getRowCount()) {
+      val b = Status.DESELECTED === column.headerValue
+      for (i in 0 until m.rowCount) {
         m.setValueAt(b, i, mci)
       }
-      column.setHeaderValue(if (b) Status.SELECTED else Status.DESELECTED)
+      column.headerValue = if (b) Status.SELECTED else Status.DESELECTED
     }
   }
 }
 
-class ComponentIcon(private val cmp: Component) : Icon {
+private class ComponentIcon(private val cmp: Component) : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    SwingUtilities.paintComponent(g, cmp, c.getParent(), x, y, getIconWidth(), getIconHeight())
+    SwingUtilities.paintComponent(g, cmp, c.parent, x, y, iconWidth, iconHeight)
   }
 
-  override fun getIconWidth() = cmp.getPreferredSize().width
+  override fun getIconWidth() = cmp.preferredSize.width
 
-  override fun getIconHeight() = cmp.getPreferredSize().height
+  override fun getIconHeight() = cmp.preferredSize.height
 }
 
-enum class Status {
+private enum class Status {
   SELECTED, DESELECTED, INDETERMINATE
 }
 
@@ -190,7 +185,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
