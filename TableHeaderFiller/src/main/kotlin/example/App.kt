d66f3659b2147b57d99c124684b2c6e34c8665f0
aterai <aterai@outlook.com>
2020-06-06 17:02:00 +0900

    refactor: remove explicit type arguments

--- TableHeaderFiller/src/main/kotlin/example/App.kt
+++ TableHeaderFiller/src/main/kotlin/example/App.kt
@@ -7,60 +7,56 @@ import javax.swing.plaf.LayerUI
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    add(JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
-      it.setTopComponent(JScrollPane(makeJTable()))
-      it.setBottomComponent(JLayer<JScrollPane>(JScrollPane(makeJTable()), TableHeaderFillerLayerUI()))
-      it.setResizeWeight(.5)
-    })
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun makeJTable() = JTable(4, 3).also {
-    it.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
-    it.setAutoCreateRowSorter(true)
-  }
+fun makeUI() = JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
+  it.topComponent = JScrollPane(makeJTable())
+  it.bottomComponent = JLayer(JScrollPane(makeJTable()), TableHeaderFillerLayerUI())
+  it.resizeWeight = .5
+  it.preferredSize = Dimension(320, 240)
 }
 
-internal class TableHeaderFillerLayerUI : LayerUI<JScrollPane>() {
+private fun makeJTable() = JTable(4, 3).also {
+  it.autoResizeMode = JTable.AUTO_RESIZE_OFF
+  it.autoCreateRowSorter = true
+}
+
+private class TableHeaderFillerLayerUI : LayerUI<JScrollPane>() {
   private val tempTable = JTable(DefaultTableModel(arrayOf(""), 0))
-  private val filler = tempTable.getTableHeader()
-  private val fillerColumn = tempTable.getColumnModel().getColumn(0)
+  private val filler = tempTable.tableHeader
+  private val fillerColumn = tempTable.columnModel.getColumn(0)
 
   override fun paint(g: Graphics?, c: JComponent) {
     super.paint(g, c)
-    val scroll = (c as? JLayer<*>)?.getView() as? JScrollPane ?: return
-    val table = scroll.getViewport().getView() as? JTable ?: return
-    val header = table.getTableHeader()
+    val scroll = (c as? JLayer<*>)?.view as? JScrollPane ?: return
+    val table = scroll.viewport.view as? JTable ?: return
+    val header = table.tableHeader
 
-    var width = header.getWidth()
-    val cm = header.getColumnModel()
-    for (i in 0 until cm.getColumnCount()) {
-      width -= cm.getColumn(i).getWidth()
+    var width = header.width
+    val cm = header.columnModel
+    for (i in 0 until cm.columnCount) {
+      width -= cm.getColumn(i).width
     }
 
     val pt = SwingUtilities.convertPoint(header, 0, 0, c)
-    filler.setLocation(pt.x + header.getWidth() - width, pt.y)
-    filler.setSize(width, header.getHeight())
-    fillerColumn.setWidth(width)
+    filler.setLocation(pt.x + header.width - width, pt.y)
+    filler.setSize(width, header.height)
+    fillerColumn.width = width
 
-    SwingUtilities.paintComponent(g, filler, tempTable, filler.getBounds())
+    SwingUtilities.paintComponent(g, filler, tempTable, filler.bounds)
   }
 
   override fun installUI(c: JComponent?) {
     super.installUI(c)
-    (c as? JLayer<*>)?.setLayerEventMask(AWTEvent.COMPONENT_EVENT_MASK)
+    (c as? JLayer<*>)?.layerEventMask = AWTEvent.COMPONENT_EVENT_MASK
   }
 
   override fun uninstallUI(c: JComponent?) {
-    (c as? JLayer<*>)?.setLayerEventMask(0)
+    (c as? JLayer<*>)?.layerEventMask = 0
     super.uninstallUI(c)
   }
 
   override fun processComponentEvent(e: ComponentEvent, l: JLayer<out JScrollPane>) {
-    val c = e.getComponent() as? JTableHeader ?: return
-    l.repaint(c.getBounds())
+    val c = e.component as? JTableHeader ?: return
+    l.repaint(c.bounds)
   }
 }
 
@@ -74,7 +70,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
