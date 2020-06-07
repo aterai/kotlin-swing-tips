c191138a7d51fd6a65975af67934abb096577108
aterai <aterai@outlook.com>
2020-06-07 15:53:10 +0900

    add: TreeRootVisible

---
+++ TreeRootVisible/src/main/kotlin/example/App.kt
@@ -0,0 +1,39 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val tree = JTree()
+  tree.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  tree.isRootVisible = false
+
+  val check = JCheckBox("JTree#setRootVisible(...)")
+  check.addActionListener { e ->
+    tree.isRootVisible = (e.source as? JCheckBox)?.isSelected == true
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(check, BorderLayout.NORTH)
+    it.add(JScrollPane(tree))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
