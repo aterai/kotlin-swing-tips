30403c311db6f8de2b7d15bfa0a1b06690d57c57
aterai <aterai@outlook.com>
2020-06-08 19:52:56 +0900

    detekt: UnsafeCast

--- ToolButtonPopup/src/main/kotlin/example/App.kt
+++ ToolButtonPopup/src/main/kotlin/example/App.kt
@@ -6,44 +6,45 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.event.PopupMenuEvent
 import javax.swing.event.PopupMenuListener
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val pop1 = JPopupMenu().also {
-      it.add("000")
-      it.add("11111")
-      it.addSeparator()
-      it.add("2222222")
-    }
-    val pop2 = JPopupMenu().also {
-      it.add("33333333333333")
-      it.addSeparator()
-      it.add("4444")
-      it.add("5555555555")
-    }
-    val url = javaClass.getResource("ei0021-16.png")
-    val rigid = Box.createRigidArea(Dimension(5, 5))
-
-    val toolbar = JToolBar("toolbar").also {
-      it.add(makeButton(pop1, "Text", null))
-      it.add(rigid)
-      it.add(makeButton(pop2, "", ImageIcon(url)))
-      it.add(rigid)
-      it.add(makeButton(pop2, "Icon+Text", ImageIcon(url)))
-      it.add(Box.createGlue())
-    }
-    add(toolbar, BorderLayout.NORTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val pop1 = JPopupMenu().also {
+    it.add("000")
+    it.add("11111")
+    it.addSeparator()
+    it.add("2222222")
   }
+  val pop2 = JPopupMenu().also {
+    it.add("33333333333333")
+    it.addSeparator()
+    it.add("4444")
+    it.add("5555555555")
+  }
+  val cl = Thread.currentThread().contextClassLoader
+  val url = cl.getResource("example/ei0021-16.png")
+  val rigid = Box.createRigidArea(Dimension(5, 5))
 
-  private fun makeButton(popup: JPopupMenu, title: String, icon: ImageIcon?) =
-    MenuToggleButton(title, icon).also { it.setPopupMenu(popup) }
+  val toolbar = JToolBar("toolbar").also {
+    it.add(makeButton(pop1, "Text", null))
+    it.add(rigid)
+    it.add(makeButton(pop2, "", ImageIcon(url)))
+    it.add(rigid)
+    it.add(makeButton(pop2, "Icon+Text", ImageIcon(url)))
+    it.add(Box.createGlue())
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(toolbar, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
+  }
 }
 
-class MenuArrowIcon : Icon {
+private fun makeButton(popup: JPopupMenu, title: String, icon: ImageIcon?) =
+  MenuToggleButton(title, icon).also { it.setPopupMenu(popup) }
+
+private class MenuArrowIcon : Icon {
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
-    val g2 = g.create() as Graphics2D
+    val g2 = g.create() as? Graphics2D ?: return
     g2.translate(x, y)
-    g2.setPaint(Color.BLACK)
+    g2.paint = Color.BLACK
     g2.drawLine(2, 3, 6, 3)
     g2.drawLine(3, 4, 5, 4)
     g2.drawLine(4, 5, 4, 5)
@@ -55,43 +56,43 @@ class MenuArrowIcon : Icon {
   override fun getIconHeight() = 9
 }
 
-class MenuToggleButton(text: String = "", icon: Icon? = null) : JToggleButton() {
+private class MenuToggleButton(text: String = "", icon: Icon? = null) : JToggleButton() {
   private var popup: JPopupMenu? = null
-
-  // constructor(icon: Icon) : this("", icon)
 
   init {
     val action = object : AbstractAction(text) {
       override fun actionPerformed(e: ActionEvent) {
-        val b = e.getSource() as? Component ?: return
-        popup?.show(b, 0, b.getHeight())
+        val b = e.source as? Component ?: return
+        popup?.show(b, 0, b.height)
       }
     }
     action.putValue(Action.SMALL_ICON, icon)
     setAction(action)
-    setFocusable(false)
-    setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4 + ARROW_ICON.getIconWidth()))
+    isFocusable = false
+    border = BorderFactory.createEmptyBorder(4, 4, 4, 4 + ARROW_ICON.iconWidth)
   }
 
   fun setPopupMenu(pop: JPopupMenu) {
     this.popup = pop
     pop.addPopupMenuListener(object : PopupMenuListener {
-      override fun popupMenuCanceled(e: PopupMenuEvent) { /* not needed */ }
+      override fun popupMenuCanceled(e: PopupMenuEvent) { /* not needed */
+      }
 
-      override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) { /* not needed */ }
+      override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) { /* not needed */
+      }
 
       override fun popupMenuWillBecomeInvisible(e: PopupMenuEvent) {
-        setSelected(false)
+        isSelected = false
       }
     })
   }
 
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
-    val dim = getSize()
-    val ins = getInsets()
+    val dim = size
+    val ins = insets
     val x = dim.width - ins.right
-    val y = ins.top + (dim.height - ins.top - ins.bottom - ARROW_ICON.getIconHeight()) / 2
+    val y = ins.top + (dim.height - ins.top - ins.bottom - ARROW_ICON.iconHeight) / 2
     ARROW_ICON.paintIcon(this, g, x, y)
   }
 
@@ -110,7 +111,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
