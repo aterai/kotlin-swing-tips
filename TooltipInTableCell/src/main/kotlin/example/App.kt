3cc1946e8bde38f5580e147faf0d7c669c2c96f9
aterai <aterai@outlook.com>
2020-06-08 19:55:38 +0900

    refactor: use of setter method instead of property access syntax

--- TooltipInTableCell/src/main/kotlin/example/App.kt
+++ TooltipInTableCell/src/main/kotlin/example/App.kt
@@ -6,79 +6,65 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val columnNames = arrayOf("String", "List<Icon>")
-  private val informationIcon = getOptionPaneIcon("OptionPane.informationIcon")
-  private val errorIcon = getOptionPaneIcon("OptionPane.errorIcon")
-  private val questionIcon = getOptionPaneIcon("OptionPane.questionIcon")
-  private val warningIcon = getOptionPaneIcon("OptionPane.warningIcon")
-  private val data = arrayOf(
-    arrayOf("aa", listOf(informationIcon, errorIcon)),
-    arrayOf("bb", listOf(errorIcon, informationIcon, warningIcon, questionIcon)),
-    arrayOf("cc", listOf(questionIcon, errorIcon, warningIcon)),
-    arrayOf("dd", listOf(informationIcon)),
-    arrayOf("ee", listOf(warningIcon, questionIcon)))
-  private val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = when (column) {
-      1 -> List::class.java
-      else -> String::class.java
-    }
+private const val LIST_ICON_COLUMN = 1
+private val columnNames = arrayOf("String", "List<Icon>")
+private val informationIcon = getOptionPaneIcon("OptionPane.informationIcon")
+private val errorIcon = getOptionPaneIcon("OptionPane.errorIcon")
+private val questionIcon = getOptionPaneIcon("OptionPane.questionIcon")
+private val warningIcon = getOptionPaneIcon("OptionPane.warningIcon")
+private val data = arrayOf(
+  arrayOf("aa", listOf(informationIcon, errorIcon)),
+  arrayOf("bb", listOf(errorIcon, informationIcon, warningIcon, questionIcon)),
+  arrayOf("cc", listOf(questionIcon, errorIcon, warningIcon)),
+  arrayOf("dd", listOf(informationIcon)),
+  arrayOf("ee", listOf(warningIcon, questionIcon))
+)
+private val model = object : DefaultTableModel(data, columnNames) {
+  override fun getColumnClass(column: Int) = when (column) {
+    1 -> List::class.java
+    else -> String::class.java
   }
-  private val table = object : JTable(model) {
-    override fun getToolTipText(e: MouseEvent): String? {
-      val pt = e.getPoint()
-      val row = rowAtPoint(pt)
-      val column = columnAtPoint(pt)
-      val modelColumnIndex = convertColumnIndexToModel(column)
-      if (modelColumnIndex == LIST_ICON_COLUMN) {
-        val c = prepareRenderer(getCellRenderer(row, column), row, column)
-        if (c is JPanel) {
-          val r = getCellRect(row, column, true)
-          c.setBounds(r)
-          // @see https://stackoverflow.com/questions/10854831/tool-tip-in-jpanel-in-jtable-not-working
-          c.doLayout()
-          pt.translate(-r.x, -r.y)
-          return SwingUtilities.getDeepestComponentAt(c, pt.x, pt.y)
-            ?.let { it as? JLabel }
-            ?.let { (it.getIcon() as? ImageIcon)?.getDescription() }
-            ?: super.getToolTipText(e)
-        }
+}
+private val table = object : JTable(model) {
+  override fun getToolTipText(e: MouseEvent): String? {
+    val pt = e.point
+    val row = rowAtPoint(pt)
+    val column = columnAtPoint(pt)
+    val modelColumnIndex = convertColumnIndexToModel(column)
+    if (modelColumnIndex == LIST_ICON_COLUMN) {
+      val c = prepareRenderer(getCellRenderer(row, column), row, column)
+      if (c is JPanel) {
+        val r = getCellRect(row, column, true)
+        c.setBounds(r)
+        // @see https://stackoverflow.com/questions/10854831/tool-tip-in-jpanel-in-jtable-not-working
+        c.doLayout()
+        pt.translate(-r.x, -r.y)
+        return SwingUtilities.getDeepestComponentAt(c, pt.x, pt.y)
+          ?.let { it as? JLabel }
+          ?.let { (it.icon as? ImageIcon)?.description }
+          ?: super.getToolTipText(e)
       }
-      return super.getToolTipText(e)
     }
-
-//    override fun updateUI() {
-//      // [JDK-6788475] Changing to Nimbus LAF and back doesn't reset look and feel of JTable completely
-//      // https://bugs.openjdk.java.net/browse/JDK-6788475
-//      // XXX: set dummy ColorUIResource
-//      setSelectionForeground(ColorUIResource(Color.RED))
-//      setSelectionBackground(ColorUIResource(Color.RED))
-//      super.updateUI()
-//      // getColumnModel().getColumn(0).setCellRenderer(DefaultTableCellRenderer())
-//      getColumnModel().getColumn(LIST_ICON_COLUMN).setCellRenderer(ListIconRenderer())
-//      setRowHeight(40)
-//    }
-  }
-
-  init {
-    table.setAutoCreateRowSorter(true)
-    table.setRowHeight(40)
-    table.getColumnModel().getColumn(LIST_ICON_COLUMN).setCellRenderer(ListIconRenderer())
-
-    add(JScrollPane(table))
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun getOptionPaneIcon(key: String) = (UIManager.getIcon(key) as? ImageIcon)?.also {
-    it.setDescription(key)
-  }
-
-  companion object {
-    private const val LIST_ICON_COLUMN = 1
+    return super.getToolTipText(e)
   }
 }
 
-class ListIconRenderer : TableCellRenderer {
+fun makeUI(): Component {
+  table.autoCreateRowSorter = true
+  table.rowHeight = 40
+  table.columnModel.getColumn(LIST_ICON_COLUMN).cellRenderer = ListIconRenderer()
+
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun getOptionPaneIcon(key: String) = (UIManager.getIcon(key) as? ImageIcon)?.also {
+  it.description = key
+}
+
+private class ListIconRenderer : TableCellRenderer {
   private val renderer = JPanel(FlowLayout(FlowLayout.LEFT))
 
   override fun getTableCellRendererComponent(
@@ -91,10 +77,10 @@ class ListIconRenderer : TableCellRenderer {
   ): Component {
     renderer.removeAll()
     if (isSelected) {
-      renderer.setOpaque(true)
-      renderer.setBackground(table.getSelectionBackground())
+      renderer.isOpaque = true
+      renderer.background = table.selectionBackground
     } else {
-      renderer.setOpaque(false)
+      renderer.isOpaque = false
       // renderer.setBackground(table.getBackground())
     }
     if (value is List<*>) {
@@ -106,7 +92,7 @@ class ListIconRenderer : TableCellRenderer {
   }
 
   private fun makeLabel(icon: Icon) = JLabel(icon).also {
-    it.setToolTipText(icon.toString())
+    it.toolTipText = icon.toString()
   }
 }
 
@@ -120,7 +106,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
