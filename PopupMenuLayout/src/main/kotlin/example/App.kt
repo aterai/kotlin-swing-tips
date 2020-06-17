aff67b773458905e00931c6b26b6e7dfe4f9b44d
aterai <aterai@outlook.com>
2020-06-17 21:03:07 +0900

    refactor: use of setter method instead of property access syntax

--- PopupMenuLayout/src/main/kotlin/example/App.kt
+++ PopupMenuLayout/src/main/kotlin/example/App.kt
@@ -6,70 +6,68 @@ import java.awt.geom.AffineTransform
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import kotlin.math.roundToInt
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val textArea = JTextArea("★ ☆ ⤾ ⤿ ⥀ ⥁ ⇐ ⇒ ⇦ ⇨ ↺ ↻ ↶ ↷")
-    textArea.componentPopupMenu = makePopup()
-    add(JScrollPane(textArea))
-    preferredSize = Dimension(320, 240)
-  }
-
-  private fun makePopup(): JPopupMenu {
-    val c = GridBagConstraints()
-    c.weightx = 1.0
-    c.weighty = 0.0
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.anchor = GridBagConstraints.CENTER
-    c.gridy = 0
-
-    val popup = JPopupMenu()
-    popup.layout = GridBagLayout()
-    popup.add(makeButton("⇦"), c)
-    popup.add(makeButton("⇨"), c)
-    popup.add(makeButton("↻"), c)
-    popup.add(makeButton("✩"), c)
-
-    c.insets = Insets(2, 0, 2, 0)
-    c.gridwidth = 4
-    c.gridx = 0
-    c.gridy = GridBagConstraints.RELATIVE
-    popup.add(JSeparator(), c)
-
-    c.insets = Insets(0, 0, 0, 0)
-    popup.add(JMenuItem("000000000"), c)
-    popup.add(JPopupMenu.Separator(), c)
-    popup.add(JMenuItem("1111"), c)
-    popup.add(JMenuItem("222222222222"), c)
-    popup.add(JMenuItem("333333333"), c)
-
-    return popup
-  }
-
-  private fun makeButton(symbol: String): AbstractButton {
-    val icon = SymbolIcon(symbol)
-    val b = object : JMenuItem() {
-      private val dim = Dimension(icon.getIconWidth(), icon.getIconHeight())
-      override fun getPreferredSize() = dim
-
-      override fun paintComponent(g: Graphics) {
-        super.paintComponent(g)
-        val cd = size
-        val pd = preferredSize
-        val ix = (.5 * (cd.width - pd.width)).roundToInt()
-        val iy = (.5 * (cd.height - pd.height)).roundToInt()
-        icon.paintIcon(this, g, ix, iy)
-      }
-    }
-    b.isOpaque = true
-    if ("\u21E8" == symbol) {
-      b.isEnabled = false
-      b.toolTipText = "forward"
-    }
-    return b
-  }
+fun makeUI() = JPanel(BorderLayout()).also {
+  val textArea = JTextArea("★ ☆ ⤾ ⤿ ⥀ ⥁ ⇐ ⇒ ⇦ ⇨ ↺ ↻ ↶ ↷")
+  textArea.componentPopupMenu = makePopup()
+  it.add(JScrollPane(textArea))
+  it.preferredSize = Dimension(320, 240)
 }
 
-class SymbolIcon(private val str: String) : Icon {
+private fun makePopup(): JPopupMenu {
+  val c = GridBagConstraints()
+  c.weightx = 1.0
+  c.weighty = 0.0
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.anchor = GridBagConstraints.CENTER
+  c.gridy = 0
+
+  val popup = JPopupMenu()
+  popup.layout = GridBagLayout()
+  popup.add(makeButton("⇦"), c)
+  popup.add(makeButton("⇨"), c)
+  popup.add(makeButton("↻"), c)
+  popup.add(makeButton("✩"), c)
+
+  c.insets = Insets(2, 0, 2, 0)
+  c.gridwidth = 4
+  c.gridx = 0
+  c.gridy = GridBagConstraints.RELATIVE
+  popup.add(JSeparator(), c)
+
+  c.insets = Insets(0, 0, 0, 0)
+  popup.add(JMenuItem("000000000"), c)
+  popup.add(JPopupMenu.Separator(), c)
+  popup.add(JMenuItem("1111"), c)
+  popup.add(JMenuItem("222222222222"), c)
+  popup.add(JMenuItem("333333333"), c)
+
+  return popup
+}
+
+private fun makeButton(symbol: String): AbstractButton {
+  val icon = SymbolIcon(symbol)
+  val b = object : JMenuItem() {
+    private val dim = Dimension(icon.iconWidth, icon.iconHeight)
+    override fun getPreferredSize() = dim
+
+    override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      val cd = size
+      val pd = preferredSize
+      val ix = (.5 * (cd.width - pd.width)).roundToInt()
+      val iy = (.5 * (cd.height - pd.height)).roundToInt()
+      icon.paintIcon(this, g, ix, iy)
+    }
+  }
+  b.isOpaque = true
+  if ("\u21E8" == symbol) {
+    b.isEnabled = false
+    b.toolTipText = "forward"
+  }
+  return b
+}
+
+private class SymbolIcon(private val str: String) : Icon {
   private val font = Font(Font.MONOSPACED, Font.BOLD, ICON_SIZE)
 
   override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
@@ -106,7 +104,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
