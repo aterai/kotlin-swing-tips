04bd295ed2e74c6bfe8059110211dc1ad7235bdd
aterai <aterai@outlook.com>
2020-06-20 02:01:38 +0900

    refactor: use of setter method instead of property access syntax

--- ComboBoxEditorVerifier/src/main/kotlin/example/App.kt
+++ ComboBoxEditorVerifier/src/main/kotlin/example/App.kt
@@ -9,75 +9,75 @@ import javax.swing.plaf.LayerUI
 import javax.swing.plaf.basic.BasicComboBoxEditor
 import javax.swing.text.JTextComponent
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val model = arrayOf("123456", "7890")
-    val combo = JComboBox(model)
-    combo.setEditable(true)
+private const val MAX_HISTORY = 10
+private const val ENTER_PRESSED = "enterPressed"
 
-    val comboBox = object : JComboBox<String>(model) {
-      override fun updateUI() {
-        getActionMap().put(ENTER_PRESSED, null)
-        super.updateUI()
-        val cb = this
-        val defaultEnterPressedAction = getActionMap().get(ENTER_PRESSED)
-        getActionMap().put(ENTER_PRESSED, object : AbstractAction() {
-          override fun actionPerformed(e: ActionEvent) {
-            val isPopupVisible = isPopupVisible()
-            setPopupVisible(false)
-            val m = getModel() as? DefaultComboBoxModel<String> ?: return
-            val str = getEditor().getItem()?.toString() ?: ""
-            if (m.getIndexOf(str) < 0 && getInputVerifier()?.verify(cb) == true) {
-              m.removeElement(str)
-              m.insertElementAt(str, 0)
-              if (m.getSize() > MAX_HISTORY) {
-                m.removeElementAt(MAX_HISTORY)
-              }
-              setSelectedIndex(0)
-              setPopupVisible(isPopupVisible)
-            } else {
-              defaultEnterPressedAction?.actionPerformed(e)
+fun makeUI(): Component {
+  val model = arrayOf("123456", "7890")
+  val combo = JComboBox(model)
+  combo.isEditable = true
+
+  val comboBox = object : JComboBox<String>(model) {
+    override fun updateUI() {
+      actionMap.put(ENTER_PRESSED, null)
+      super.updateUI()
+      val cb = this
+      val defaultEnterPressedAction = actionMap.get(ENTER_PRESSED)
+      val action = object : AbstractAction() {
+        override fun actionPerformed(e: ActionEvent) {
+          val isPopupVisible = isPopupVisible
+          setPopupVisible(false)
+          val m = getModel() as? DefaultComboBoxModel<String> ?: return
+          val str = getEditor().item?.toString() ?: ""
+          if (m.getIndexOf(str) < 0 && inputVerifier?.verify(cb) == true) {
+            m.removeElement(str)
+            m.insertElementAt(str, 0)
+            if (m.size > MAX_HISTORY) {
+              m.removeElementAt(MAX_HISTORY)
             }
+            selectedIndex = 0
+            setPopupVisible(isPopupVisible)
+          } else {
+            defaultEnterPressedAction?.actionPerformed(e)
           }
-        })
-      }
-    }
-    comboBox.setEditable(true)
-    comboBox.setInputVerifier(LengthInputVerifier())
-    comboBox.setEditor(object : BasicComboBoxEditor() {
-      private var validationEditor: Component? = null
-
-      override fun getEditorComponent(): Component? {
-        (super.getEditorComponent() as? JTextComponent)?.also {
-          validationEditor = validationEditor ?: JLayer(it, ValidationLayerUI())
         }
-        return validationEditor
       }
-    })
-    comboBox.addPopupMenuListener(SelectItemMenuListener())
-
-    val p = JPanel(GridLayout(5, 1)).also {
-      it.add(JLabel("Default:", SwingConstants.LEFT))
-      it.add(combo)
-      it.add(Box.createVerticalStrut(15))
-      it.add(JLabel("6 >= str.length()", SwingConstants.LEFT))
-      it.add(comboBox)
+      actionMap.put(ENTER_PRESSED, action)
     }
-    add(p, BorderLayout.NORTH)
-    setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20))
-    setPreferredSize(Dimension(320, 240))
+  }
+  comboBox.isEditable = true
+  comboBox.inputVerifier = LengthInputVerifier()
+  comboBox.editor = object : BasicComboBoxEditor() {
+    private var validationEditor: Component? = null
+
+    override fun getEditorComponent(): Component? {
+      (super.getEditorComponent() as? JTextComponent)?.also {
+        validationEditor = validationEditor ?: JLayer(it, ValidationLayerUI())
+      }
+      return validationEditor
+    }
+  }
+  comboBox.addPopupMenuListener(SelectItemMenuListener())
+
+  val p = JPanel(GridLayout(5, 1)).also {
+    it.add(JLabel("Default:", SwingConstants.LEFT))
+    it.add(combo)
+    it.add(Box.createVerticalStrut(15))
+    it.add(JLabel("6 >= str.length()", SwingConstants.LEFT))
+    it.add(comboBox)
   }
 
-  companion object {
-    private const val MAX_HISTORY = 10
-    private const val ENTER_PRESSED = "enterPressed"
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class SelectItemMenuListener : PopupMenuListener {
+private class SelectItemMenuListener : PopupMenuListener {
   override fun popupMenuWillBecomeVisible(e: PopupMenuEvent) {
-    val c = e.getSource() as? JComboBox<*> ?: return
-    c.setSelectedItem(c.getEditor().getItem())
+    val c = e.source as? JComboBox<*> ?: return
+    c.selectedItem = c.editor.item
   }
 
   override fun popupMenuWillBecomeInvisible(e: PopupMenuEvent) { /* not needed */ }
@@ -86,13 +86,13 @@ class SelectItemMenuListener : PopupMenuListener {
 }
 
 // @see https://docs.oracle.com/javase/tutorial/uiswing/examples/misc/FieldValidatorProject/src/FieldValidator.java
-class ValidationLayerUI<V : JTextComponent> : LayerUI<V>() {
+private class ValidationLayerUI<V : JTextComponent> : LayerUI<V>() {
   override fun paint(g: Graphics, c: JComponent) {
     super.paint(g, c)
     val cb = SwingUtilities.getAncestorOfClass(JComboBox::class.java, c) as? JComboBox<*> ?: return
-    cb.getInputVerifier()?.takeUnless { it.verify(cb) }?.also {
-      val w = c.getWidth()
-      val h = c.getHeight()
+    cb.inputVerifier?.takeUnless { it.verify(cb) }?.also {
+      val w = c.width
+      val h = c.height
       val s = 8
       val pad = 5
       val x = w - pad - s
@@ -100,9 +100,9 @@ class ValidationLayerUI<V : JTextComponent> : LayerUI<V>() {
       val g2 = g.create() as? Graphics2D ?: return
       g2.translate(x, y)
       g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-      g2.setPaint(Color.RED)
+      g2.paint = Color.RED
       g2.fillRect(0, 0, s + 1, s + 1)
-      g2.setPaint(Color.WHITE)
+      g2.paint = Color.WHITE
       g2.drawLine(0, 0, s, s)
       g2.drawLine(0, s, s, 0)
       g2.dispose()
@@ -110,9 +110,9 @@ class ValidationLayerUI<V : JTextComponent> : LayerUI<V>() {
   }
 }
 
-class LengthInputVerifier : InputVerifier() {
+private class LengthInputVerifier : InputVerifier() {
   override fun verify(c: JComponent) = (c as? JComboBox<*>)?.let {
-    MAX_LEN - (it.getEditor().getItem()?.toString()?.length ?: 0) >= 0
+    MAX_LEN - (it.editor.item?.toString()?.length ?: 0) >= 0
   } ?: false
 
   companion object {
@@ -130,7 +130,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
