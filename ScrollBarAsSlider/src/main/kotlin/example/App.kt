442816c126dd974a9ff20f26f8eb13270e06c36e
aterai <aterai@outlook.com>
2020-06-25 18:37:22 +0900

    detekt: LabeledExpression

--- ScrollBarAsSlider/src/main/kotlin/example/App.kt
+++ ScrollBarAsSlider/src/main/kotlin/example/App.kt
@@ -13,28 +13,30 @@ fun makeUI(): Component {
   val scrollbar = JScrollBar(Adjustable.HORIZONTAL, VALUE, EXTENT, MIN, MAX + EXTENT)
   val model = SpinnerNumberModel(VALUE, MIN, MAX, STEP)
 
-  scrollbar.setUnitIncrement(STEP)
-  scrollbar.getModel().addChangeListener { e ->
-    val m = e.getSource() as? BoundedRangeModel ?: return@addChangeListener
-    model.setValue(m.getValue())
+  scrollbar.unitIncrement = STEP
+  scrollbar.model.addChangeListener { e ->
+    (e.source as? BoundedRangeModel)?.also {
+      model.value = it.value
+    }
   }
 
   model.addChangeListener { e ->
-    val source = e.getSource() as? SpinnerNumberModel ?: return@addChangeListener
-    scrollbar.setValue(source.getNumber().toInt())
+    (e.source as? SpinnerNumberModel)?.also {
+      scrollbar.value = it.number.toInt()
+    }
   }
 
   return JPanel(GridLayout(2, 1)).also {
     it.add(makeTitledPanel("JSpinner", JSpinner(model)))
     it.add(makeTitledPanel("JScrollBar", scrollbar))
-    it.setBorder(BorderFactory.createEmptyBorder(10, 5, 10, 5))
-    it.setPreferredSize(Dimension(320, 240))
+    it.border = BorderFactory.createEmptyBorder(10, 5, 10, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-fun makeTitledPanel(title: String, cmp: Component) = JPanel(GridBagLayout()).also {
-  it.setBorder(BorderFactory.createTitledBorder(title))
-  it.setBackground(Color.WHITE)
+private fun makeTitledPanel(title: String, cmp: Component) = JPanel(GridBagLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.background = Color.WHITE
   val c = GridBagConstraints()
   c.weightx = 1.0
   c.fill = GridBagConstraints.HORIZONTAL
