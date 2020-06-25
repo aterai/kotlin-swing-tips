85b84f8bd83be5666b5442a2d070aaddcf58e90c
aterai <aterai@outlook.com>
2020-06-25 18:32:54 +0900

    refactor: use of setter method instead of property access syntax

--- SynthLookAndFeel/src/main/kotlin/example/App.kt
+++ SynthLookAndFeel/src/main/kotlin/example/App.kt
@@ -4,36 +4,35 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.synth.SynthLookAndFeel
 
-class MainPanel : JPanel() {
-  init {
-    add(JButton("JButton1"))
-    add(JButton("JButton2"))
-    add(MyButton("MyButton"))
+fun makeUI() = JPanel().also {
+  it.add(JButton("JButton1"))
+  it.add(JButton("JButton2"))
+  it.add(MyButton("MyButton"))
 
-    val button3 = JButton("JButton3")
-    button3.setName("green:3")
-    add(button3)
+  val button3 = JButton("JButton3")
+  button3.name = "green:3"
+  it.add(button3)
 
-    val button4 = JButton("JButton4")
-    button4.setName("green:4")
-    add(button4)
+  val button4 = JButton("JButton4")
+  button4.name = "green:4"
+  it.add(button4)
 
-    setPreferredSize(Dimension(320, 240))
-  }
+  it.preferredSize = Dimension(320, 240)
 }
 
-class MyButton(title: String) : JButton("$title: class")
+private class MyButton(title: String) : JButton("$title: class")
 
 fun main() {
   EventQueue.invokeLater {
-    MainPanel::class.java.getResourceAsStream("button.xml").use { input ->
+    val cl = Thread.currentThread().contextClassLoader
+    cl.getResourceAsStream("example/button.xml").use { input ->
       val synth = SynthLookAndFeel()
-      synth.load(input, MainPanel::class.java)
+      synth.load(input, MyButton::class.java)
       UIManager.setLookAndFeel(synth)
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
