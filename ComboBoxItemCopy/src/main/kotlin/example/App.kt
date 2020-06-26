129742e889cd818d4d7ecb917ed1d00f020611a6
aterai <aterai@outlook.com>
2020-06-26 13:37:28 +0900

    refactor: use of setter method instead of property access syntax

--- ComboBoxItemCopy/src/main/kotlin/example/App.kt
+++ ComboBoxItemCopy/src/main/kotlin/example/App.kt
@@ -9,80 +9,81 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.text.DefaultEditorKit
 import javax.swing.text.JTextComponent
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val copyKey = "copy"
+fun makeUI(): Component {
+  val copyKey = "copy"
 
-    val combo1 = JComboBox(makeModel(5))
-    val copy = object : AbstractAction() {
-      override fun actionPerformed(e: ActionEvent) {
-        (e.getSource() as? JComboBox<*>)?.getSelectedItem()?.also {
-          val contents = StringSelection(it.toString())
-          val clipboard = Toolkit.getDefaultToolkit().getSystemClipboard()
-          clipboard.setContents(contents, null)
-          println(it)
-        }
+  val combo1 = JComboBox(makeModel(5))
+  val copy = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      (e.source as? JComboBox<*>)?.selectedItem?.also {
+        val contents = StringSelection(it.toString())
+        val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        clipboard.setContents(contents, null)
+        println(it)
       }
     }
-    val am = combo1.getActionMap()
-    am.put(copyKey, copy)
-    val modifiers = InputEvent.CTRL_DOWN_MASK
-    val keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_C, modifiers)
-    val im = combo1.getInputMap(JComponent.WHEN_FOCUSED)
-    im.put(keyStroke, copyKey)
-    val popup = JPopupMenu()
-    popup.add(copyKey).addActionListener { e ->
-      val o = popup.getInvoker()
-      val c = o as? JComboBox<*> ?: SwingUtilities.getAncestorOfClass(JComboBox::class.java, o)
-      (c as? JComboBox<*>)?.also {
-        val a = it.getActionMap().get(copyKey)
-        a.actionPerformed(ActionEvent(it, e.getID(), e.getActionCommand()))
-        // KeyEvent keyEvent = new KeyEvent(c, 0, 0, 0, 0, 'C');
-        // SwingUtilities.notifyAction(a, keyStroke, keyEvent, it, modifiers);
-      }
-    }
-    combo1.setComponentPopupMenu(popup)
-
-    val combo2 = JComboBox(makeModel(10))
-    combo2.setEditable(true)
-    (combo2.getEditor().getEditorComponent() as? JTextField)
-      ?.setComponentPopupMenu(TextFieldPopupMenu())
-
-    val box = Box.createVerticalBox()
-    box.add(makeTitledPanel("Default:", JComboBox(makeModel(0))))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("Editable: false, JPopupMenu, Ctrl+C", combo1))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("Editable: true, JPopupMenu, Ctrl+C", combo2))
-    box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    add(box, BorderLayout.NORTH)
-
-    val textArea = JTextArea()
-    textArea.setComponentPopupMenu(TextFieldPopupMenu())
-    add(JScrollPane(textArea))
-    setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    setPreferredSize(Dimension(320, 240))
   }
+  val am = combo1.actionMap
+  am.put(copyKey, copy)
+  val modifiers = InputEvent.CTRL_DOWN_MASK
+  val keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_C, modifiers)
 
-  private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-    it.add(c)
-  }
-
-  private fun makeModel(start: Int) = DefaultComboBoxModel<String>().also {
-    for (i in start until start + 5) {
-      it.addElement("item: $i")
+  val im = combo1.getInputMap(JComponent.WHEN_FOCUSED)
+  im.put(keyStroke, copyKey)
+  val popup = JPopupMenu()
+  popup.add(copyKey).addActionListener { e ->
+    val o = popup.invoker
+    val c = o as? JComboBox<*> ?: SwingUtilities.getAncestorOfClass(JComboBox::class.java, o)
+    (c as? JComboBox<*>)?.also {
+      val a = it.actionMap.get(copyKey)
+      a.actionPerformed(ActionEvent(it, e.id, e.actionCommand))
+      // KeyEvent keyEvent = new KeyEvent(c, 0, 0, 0, 0, 'C');
+      // SwingUtilities.notifyAction(a, keyStroke, keyEvent, it, modifiers);
     }
+  }
+  combo1.componentPopupMenu = popup
+
+  val combo2 = JComboBox(makeModel(10))
+  combo2.isEditable = true
+  (combo2.editor.editorComponent as? JTextField)?.componentPopupMenu = TextFieldPopupMenu()
+
+  val box = Box.createVerticalBox()
+  box.add(makeTitledPanel("Default:", JComboBox(makeModel(0))))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("Editable: false, JPopupMenu, Ctrl+C", combo1))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("Editable: true, JPopupMenu, Ctrl+C", combo2))
+  box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+
+  val textArea = JTextArea()
+  textArea.componentPopupMenu = TextFieldPopupMenu()
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.add(JScrollPane(textArea))
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-internal class TextFieldPopupMenu : JPopupMenu() {
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+private fun makeModel(start: Int) = DefaultComboBoxModel<String>().also {
+  for (i in start until start + 5) {
+    it.addElement("item: $i")
+  }
+}
+
+private class TextFieldPopupMenu : JPopupMenu() {
   private val cutAction = DefaultEditorKit.CutAction()
   private val copyAction = DefaultEditorKit.CopyAction()
   private val pasteAction = DefaultEditorKit.PasteAction()
   private val deleteAction = object : AbstractAction("delete") {
     override fun actionPerformed(e: ActionEvent) {
-      (getInvoker() as? JTextComponent)?.replaceSelection(null)
+      (invoker as? JTextComponent)?.replaceSelection(null)
     }
   }
 
@@ -95,10 +96,10 @@ internal class TextFieldPopupMenu : JPopupMenu() {
 
   override fun show(c: Component, x: Int, y: Int) {
     if (c is JTextComponent) {
-      val hasSelectedText = c.getSelectedText() != null
-      cutAction.setEnabled(hasSelectedText)
-      copyAction.setEnabled(hasSelectedText)
-      deleteAction.setEnabled(hasSelectedText)
+      val hasSelectedText = c.selectedText != null
+      cutAction.isEnabled = hasSelectedText
+      copyAction.isEnabled = hasSelectedText
+      deleteAction.isEnabled = hasSelectedText
       super.show(c, x, y)
     }
   }
@@ -114,7 +115,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
