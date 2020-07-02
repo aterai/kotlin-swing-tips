e35e3d6dd8a179e25d04cb688e42fb8728f6995e
aterai <aterai@outlook.com>
2020-07-03 02:32:20 +0900

    refactor: use of setter method instead of property access syntax

--- ReplaceUndoableEdit/src/main/kotlin/example/App.kt
+++ ReplaceUndoableEdit/src/main/kotlin/example/App.kt
@@ -14,72 +14,70 @@ import javax.swing.text.PlainDocument
 import javax.swing.undo.CompoundEdit
 import javax.swing.undo.UndoManager
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val undoManager0 = UndoManager()
-    val textField0 = JTextField("default")
-    textField0.getDocument().addUndoableEditListener(undoManager0)
-    val undoManager1 = UndoManager()
-    val textField1 = JTextField()
-    textField1.setDocument(CustomUndoPlainDocument())
-    textField1.setText("111111111111111111")
-    textField1.getDocument().addUndoableEditListener(undoManager1)
-    val undoManager2 = DocumentFilterUndoManager()
-    val textField2 = JTextField()
-    textField2.setText("2222222222222222")
-    val doc = textField2.getDocument()
-    if (doc is AbstractDocument) {
-      doc.addUndoableEditListener(undoManager2)
-      doc.setDocumentFilter(undoManager2.documentFilter)
-    }
-    val button = JButton("setText(LocalDateTime.now(...))")
-    button.addActionListener {
-      val str = LocalDateTime.now(ZoneId.systemDefault()).toString()
-      listOf(textField0, textField1, textField2).forEach { it.setText(str) }
-    }
-    val undoAction: Action = object : AbstractAction("undo") {
-      override fun actionPerformed(e: ActionEvent) {
-        listOf(undoManager0, undoManager1, undoManager2)
-          .filter { it.canUndo() }
-          .forEach { it.undo() }
-      }
-    }
-    val redoAction: Action = object : AbstractAction("redo") {
-      override fun actionPerformed(e: ActionEvent) {
-        listOf(undoManager0, undoManager1, undoManager2)
-          .filter { it.canRedo() }
-          .forEach { it.redo() }
-      }
-    }
-    val p = JPanel()
-    p.add(JButton(undoAction))
-    p.add(JButton(redoAction))
-    p.add(button)
-    val box = Box.createVerticalBox()
-    box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    box.add(makeTitledPanel("Default", textField0))
-    box.add(Box.createVerticalStrut(10))
-    box.add(makeTitledPanel("Document#replace()+AbstractDocument#fireUndoableEditUpdate()", textField1))
-    box.add(Box.createVerticalStrut(10))
-    box.add(makeTitledPanel("DocumentFilter#replace()+UndoableEditListener#undoableEditHappened()", textField2))
-    add(box, BorderLayout.NORTH)
-    add(p, BorderLayout.SOUTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val undoManager0 = UndoManager()
+  val textField0 = JTextField("default")
+  textField0.document.addUndoableEditListener(undoManager0)
+  val undoManager1 = UndoManager()
+  val textField1 = JTextField()
+  textField1.document = CustomUndoPlainDocument()
+  textField1.text = "111111111111111111"
+  textField1.document.addUndoableEditListener(undoManager1)
+  val undoManager2 = DocumentFilterUndoManager()
+  val textField2 = JTextField()
+  textField2.text = "2222222222222222"
+  val doc = textField2.document
+  if (doc is AbstractDocument) {
+    doc.addUndoableEditListener(undoManager2)
+    doc.documentFilter = undoManager2.documentFilter
   }
-
-  private fun makeTitledPanel(title: String, c: Component): Component {
-    val p = JPanel(BorderLayout())
-    p.setBorder(BorderFactory.createTitledBorder(title))
-    p.add(c)
-    return p
+  val button = JButton("setText(LocalDateTime.now(...))")
+  button.addActionListener {
+    val str = LocalDateTime.now(ZoneId.systemDefault()).toString()
+    listOf(textField0, textField1, textField2).forEach { it.text = str }
+  }
+  val undoAction: Action = object : AbstractAction("undo") {
+    override fun actionPerformed(e: ActionEvent) {
+      listOf(undoManager0, undoManager1, undoManager2)
+        .filter { it.canUndo() }
+        .forEach { it.undo() }
+    }
+  }
+  val redoAction: Action = object : AbstractAction("redo") {
+    override fun actionPerformed(e: ActionEvent) {
+      listOf(undoManager0, undoManager1, undoManager2)
+        .filter { it.canRedo() }
+        .forEach { it.redo() }
+    }
+  }
+  val p = JPanel()
+  p.add(JButton(undoAction))
+  p.add(JButton(redoAction))
+  p.add(button)
+  val box = Box.createVerticalBox()
+  box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  box.add(makeTitledPanel("Default", textField0))
+  box.add(Box.createVerticalStrut(10))
+  box.add(makeTitledPanel("Document#replace()+AbstractDocument#fireUndoableEditUpdate()", textField1))
+  box.add(Box.createVerticalStrut(10))
+  box.add(makeTitledPanel("DocumentFilter#replace()+UndoableEditListener#undoableEditHappened()", textField2))
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.add(p, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class CustomUndoPlainDocument : PlainDocument() {
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+private class CustomUndoPlainDocument : PlainDocument() {
   private var compoundEdit: CompoundEdit? = null
 
   override fun fireUndoableEditUpdate(e: UndoableEditEvent) {
-    compoundEdit?.addEdit(e.getEdit()) ?: super.fireUndoableEditUpdate(e)
+    compoundEdit?.addEdit(e.edit) ?: super.fireUndoableEditUpdate(e)
   }
 
   @Throws(BadLocationException::class)
@@ -102,10 +100,9 @@ class CustomUndoPlainDocument : PlainDocument() {
   }
 }
 
-class DocumentFilterUndoManager : UndoManager() {
+private class DocumentFilterUndoManager : UndoManager() {
   var compoundEdit: CompoundEdit? = null
-  @Transient
-  val documentFilter: DocumentFilter = object : DocumentFilter() {
+  @Transient val documentFilter: DocumentFilter = object : DocumentFilter() {
     @Throws(BadLocationException::class)
     override fun replace(
       fb: FilterBypass,
@@ -128,7 +125,7 @@ class DocumentFilterUndoManager : UndoManager() {
   }
 
   override fun undoableEditHappened(e: UndoableEditEvent) {
-    (compoundEdit ?: this).addEdit(e.getEdit())
+    (compoundEdit ?: this).addEdit(e.edit)
   }
 }
 
@@ -142,7 +139,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
