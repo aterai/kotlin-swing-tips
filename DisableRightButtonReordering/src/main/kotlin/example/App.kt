b8bf372cdc38289454a5461fbcd4de59f08aaeea
aterai <aterai@outlook.com>
2020-07-06 02:21:43 +0900

    refactor: use of setter method instead of property access syntax

--- DisableRightButtonReordering/src/main/kotlin/example/App.kt
+++ DisableRightButtonReordering/src/main/kotlin/example/App.kt
@@ -6,58 +6,56 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.LayerUI
 import javax.swing.table.JTableHeader
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val c = JLayer(JScrollPane(makeJTable()), DisableRightButtonSwapLayerUI())
-    val p = JPanel(GridLayout(2, 1))
-    p.add(makeTitledPanel("Default", JScrollPane(makeJTable())))
-    p.add(makeTitledPanel("Disable right mouse button reordering", c))
-    add(p)
-    setPreferredSize(Dimension(320, 240))
-  }
-
-  private fun makeJTable(): JTable {
-    val table = JTable(4, 3)
-    table.setAutoCreateRowSorter(true)
-    val popup = object : JPopupMenu() {
-      override fun show(c: Component, x: Int, y: Int) {
-        (c as? JTableHeader)?.also { header ->
-          header.setDraggedColumn(null)
-          header.repaint()
-          header.getTable().repaint()
-          super.show(c, x, y)
-        }
-      }
-    }
-    popup.add("Item 1")
-    popup.add("Item 2")
-    popup.add("Item 3")
-    table.getTableHeader().setComponentPopupMenu(popup)
-    return table
-  }
-
-  private fun makeTitledPanel(title: String, c: Component): Component {
-    val p = JPanel(BorderLayout())
-    p.setBorder(BorderFactory.createTitledBorder(title))
-    p.add(c)
-    return p
-  }
+fun makeUI() = JPanel(BorderLayout()).also {
+  val c = JLayer(JScrollPane(makeJTable()), DisableRightButtonSwapLayerUI())
+  val p = JPanel(GridLayout(2, 1))
+  p.add(makeTitledPanel("Default", JScrollPane(makeJTable())))
+  p.add(makeTitledPanel("Disable right mouse button reordering", c))
+  it.add(p)
+  it.preferredSize = Dimension(320, 240)
 }
 
-class DisableRightButtonSwapLayerUI : LayerUI<JScrollPane?>() {
+private fun makeJTable(): JTable {
+  val table = JTable(4, 3)
+  table.autoCreateRowSorter = true
+  val popup = object : JPopupMenu() {
+    override fun show(c: Component, x: Int, y: Int) {
+      (c as? JTableHeader)?.also { header ->
+        header.draggedColumn = null
+        header.repaint()
+        header.table.repaint()
+        super.show(c, x, y)
+      }
+    }
+  }
+  popup.add("Item 1")
+  popup.add("Item 2")
+  popup.add("Item 3")
+  table.tableHeader.componentPopupMenu = popup
+  return table
+}
+
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c)
+  return p
+}
+
+private class DisableRightButtonSwapLayerUI : LayerUI<JScrollPane?>() {
   override fun installUI(c: JComponent) {
     super.installUI(c)
-    (c as? JLayer<*>)?.setLayerEventMask(AWTEvent.MOUSE_MOTION_EVENT_MASK)
+    (c as? JLayer<*>)?.layerEventMask = AWTEvent.MOUSE_MOTION_EVENT_MASK
   }
 
   override fun uninstallUI(c: JComponent) {
-    (c as? JLayer<*>)?.setLayerEventMask(0)
+    (c as? JLayer<*>)?.layerEventMask = 0
     super.uninstallUI(c)
   }
 
   override fun processMouseMotionEvent(e: MouseEvent, l: JLayer<out JScrollPane?>) {
-    val id = e.getID()
-    val c = e.getComponent()
+    val id = e.id
+    val c = e.component
     if (c is JTableHeader && id == MouseEvent.MOUSE_DRAGGED && SwingUtilities.isRightMouseButton(e)) {
       e.consume()
     }
@@ -74,7 +72,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
