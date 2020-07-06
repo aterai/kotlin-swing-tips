17f529122509804760326f8894d54fa673ade258
aterai <aterai@outlook.com>
2020-07-07 03:06:14 +0900

    refactor: use of setter method instead of property access syntax

--- DisableRightClick/src/main/kotlin/example/App.kt
+++ DisableRightClick/src/main/kotlin/example/App.kt
@@ -10,60 +10,59 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicComboBoxUI
 import javax.swing.plaf.basic.BasicComboPopup
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val combo1 = makeComboBox(5)
-    if (combo1.getUI() is WindowsComboBoxUI) {
-      combo1.setUI(object : WindowsComboBoxUI() {
-        override fun createPopup() = BasicComboPopup2(comboBox)
-      })
-    } else {
-      combo1.setUI(object : BasicComboBoxUI() {
-        override fun createPopup() = BasicComboPopup2(comboBox)
-      })
-    }
-
-    val combo2 = makeComboBox(20)
-    if (combo2.getUI() is WindowsComboBoxUI) {
-      combo2.setUI(object : WindowsComboBoxUI() {
-        override fun createPopup() = BasicComboPopup3(comboBox)
-      })
-    } else {
-      combo2.setUI(object : BasicComboBoxUI() {
-        override fun createPopup() = BasicComboPopup3(comboBox)
-      })
-    }
-
-    val box = Box.createVerticalBox()
-    box.add(makeTitledPanel("default:", makeComboBox(5)))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("default:", makeComboBox(20)))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("disable right click in drop-down list:", combo1))
-    box.add(Box.createVerticalStrut(5))
-    box.add(makeTitledPanel("disable right click and scroll in drop-down list:", combo2))
-    box.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    add(box, BorderLayout.NORTH)
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val combo1 = makeComboBox(5)
+  if (combo1.ui is WindowsComboBoxUI) {
+    combo1.setUI(object : WindowsComboBoxUI() {
+      override fun createPopup() = BasicComboPopup2(comboBox)
+    })
+  } else {
+    combo1.setUI(object : BasicComboBoxUI() {
+      override fun createPopup() = BasicComboPopup2(comboBox)
+    })
   }
 
-  private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-    it.setBorder(BorderFactory.createTitledBorder(title))
-    it.add(c)
+  val combo2 = makeComboBox(20)
+  if (combo2.ui is WindowsComboBoxUI) {
+    combo2.setUI(object : WindowsComboBoxUI() {
+      override fun createPopup() = BasicComboPopup3(comboBox)
+    })
+  } else {
+    combo2.setUI(object : BasicComboBoxUI() {
+      override fun createPopup() = BasicComboPopup3(comboBox)
+    })
   }
 
-  private fun makeComboBox(size: Int): JComboBox<String> {
-    val model = DefaultComboBoxModel<String>()
-    for (i in 0 until size) {
-      model.addElement("No.$i")
-    }
-    return JComboBox(model)
+  val box = Box.createVerticalBox()
+  box.add(makeTitledPanel("default:", makeComboBox(5)))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("default:", makeComboBox(20)))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("disable right click in drop-down list:", combo1))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("disable right click and scroll in drop-down list:", combo2))
+  box.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class BasicComboPopup2(combo: JComboBox<*>) : BasicComboPopup(combo) {
-  @Transient
-  private var handler2: MouseListener? = null
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+private fun makeComboBox(size: Int): JComboBox<String> {
+  val model = DefaultComboBoxModel<String>()
+  for (i in 0 until size) {
+    model.addElement("No.$i")
+  }
+  return JComboBox(model)
+}
+
+private class BasicComboPopup2(combo: JComboBox<*>) : BasicComboPopup(combo) {
+  @Transient private var handler2: MouseListener? = null
 
   override fun uninstallingUI() {
     super.uninstallingUI()
@@ -74,50 +73,50 @@ class BasicComboPopup2(combo: JComboBox<*>) : BasicComboPopup(combo) {
 
   private inner class Handler2 : MouseAdapter() {
     override fun mouseReleased(e: MouseEvent) {
-      if (e.getSource() != list) {
+      if (e.source != list) {
         return
       }
-      if (list.getModel().getSize() > 0) {
+      if (list.model.size > 0) {
         // <ins>
-        if (!SwingUtilities.isLeftMouseButton(e) || !comboBox.isEnabled()) {
+        if (!SwingUtilities.isLeftMouseButton(e) || !comboBox.isEnabled) {
           return
         }
         // </ins>
         // JList mouse listener
-        if (comboBox.getSelectedIndex() == list.getSelectedIndex()) {
-          comboBox.getEditor().setItem(list.getSelectedValue())
+        if (comboBox.selectedIndex == list.selectedIndex) {
+          comboBox.editor.item = list.selectedValue
         }
-        comboBox.setSelectedIndex(list.getSelectedIndex())
+        comboBox.selectedIndex = list.selectedIndex
       }
-      comboBox.setPopupVisible(false)
+      comboBox.isPopupVisible = false
       // workaround for cancelling an edited item (bug 4530953)
-      if (comboBox.isEditable() && comboBox.getEditor() != null) {
-        comboBox.configureEditor(comboBox.getEditor(), comboBox.getSelectedItem())
+      if (comboBox.isEditable && comboBox.editor != null) {
+        comboBox.configureEditor(comboBox.editor, comboBox.selectedItem)
       }
     }
   }
 }
 
-class BasicComboPopup3(combo: JComboBox<*>) : BasicComboPopup(combo) {
+private class BasicComboPopup3(combo: JComboBox<*>) : BasicComboPopup(combo) {
   override fun createList(): JList<*> {
-    return object : JList<Any>(comboBox.getModel()) {
+    return object : JList<Any>(comboBox.model) {
       override fun processMouseEvent(e: MouseEvent) {
         if (SwingUtilities.isRightMouseButton(e)) {
           return
         }
         var ev = e
-        if (e.isControlDown()) {
+        if (e.isControlDown) {
           // Fix for 4234053. Filter out the Control Key from the list.
           // ie., don't allow CTRL key deselection.
           ev = MouseEvent(
-            e.getComponent(), e.getID(), e.getWhen(),
+            e.component, e.id, e.getWhen(),
             // e.getModifiers() ^ InputEvent.CTRL_MASK,
             // Java 10: e.getModifiersEx() ^ Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx(),
-            e.getModifiersEx() xor InputEvent.CTRL_DOWN_MASK,
-            e.getX(), e.getY(),
-            e.getXOnScreen(), e.getYOnScreen(),
-            e.getClickCount(),
-            e.isPopupTrigger(),
+            e.modifiersEx xor InputEvent.CTRL_DOWN_MASK,
+            e.x, e.y,
+            e.xOnScreen, e.yOnScreen,
+            e.clickCount,
+            e.isPopupTrigger,
             MouseEvent.NOBUTTON
           )
         }
@@ -137,7 +136,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
