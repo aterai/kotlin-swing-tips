84631a67178ea63bff1e664060674b2aed6a4947
aterai <aterai@outlook.com>
2020-07-12 02:59:27 +0900

    refactor: use of setter method instead of property access syntax

--- ResizableComponents/src/main/kotlin/example/App.kt
+++ ResizableComponents/src/main/kotlin/example/App.kt
@@ -10,69 +10,70 @@ import javax.swing.border.Border
 import javax.swing.event.MouseInputAdapter
 import javax.swing.event.MouseInputListener
 
-class MainPanel : JPanel(BorderLayout()) {
-  private val layeredPane = object : JLayeredPane() {
-    override fun isOptimizedDrawingEnabled() = false
-  }
-  private val toolbar = JToolBar("Resizable Components")
-  private val pt = Point()
-  private fun createTree() {
-    val tree = JTree()
-    tree.setVisibleRowCount(8)
-    val c = JScrollPane(tree)
-    val d = c.getPreferredSize()
-    val resizer = JResizer(BorderLayout())
-    resizer.add(c)
-    resizer.setBounds(pt.x, pt.y, d.width, d.height)
-    layeredPane.add(resizer)
-    layeredPane.moveToFront(resizer)
-  }
+private val layeredPane = object : JLayeredPane() {
+  override fun isOptimizedDrawingEnabled() = false
+}
+private val toolbar = JToolBar("Resizable Components")
+private val pt = Point()
 
-  private fun createTable() {
-    val table = JTable(12, 3)
-    table.setPreferredScrollableViewportSize(Dimension(160, 160))
-    val c = JScrollPane(table)
-    val d = c.getPreferredSize()
-    val resizer = JResizer(BorderLayout())
-    resizer.add(c)
-    resizer.setBounds(pt.x, pt.y, d.width, d.height)
-    layeredPane.add(resizer)
-    layeredPane.moveToFront(resizer)
-  }
-
-  init {
-    val popup = object : JPopupMenu() {
-      override fun show(c: Component?, x: Int, y: Int) {
-        pt.setLocation(x, y)
-        super.show(c, x, y)
-      }
+fun makeUI(): Component {
+  val popup = object : JPopupMenu() {
+    override fun show(c: Component?, x: Int, y: Int) {
+      pt.setLocation(x, y)
+      super.show(c, x, y)
     }
-    popup.add("table").addActionListener { createTable() }
-    popup.add("tree").addActionListener { createTree() }
-    layeredPane.componentPopupMenu = popup
+  }
+  popup.add("table").addActionListener { createTable() }
+  popup.add("tree").addActionListener { createTree() }
+  layeredPane.componentPopupMenu = popup
 
-    add(layeredPane)
-    toolbar.add(object : AbstractAction("add table") {
-      override fun actionPerformed(e: ActionEvent) {
-        pt.setLocation(pt.x + 20, pt.y + 20)
-        createTable()
-      }
-    })
-    toolbar.addSeparator()
-    toolbar.add(object : AbstractAction("add tree") {
-      override fun actionPerformed(e: ActionEvent) {
-        pt.setLocation(pt.x + 20, pt.y + 20)
-        createTree()
-      }
-    })
-    add(toolbar, BorderLayout.NORTH)
-    setPreferredSize(Dimension(320, 240))
+  toolbar.add(object : AbstractAction("add table") {
+    override fun actionPerformed(e: ActionEvent) {
+      pt.setLocation(pt.x + 20, pt.y + 20)
+      createTable()
+    }
+  })
+  toolbar.addSeparator()
+  toolbar.add(object : AbstractAction("add tree") {
+    override fun actionPerformed(e: ActionEvent) {
+      pt.setLocation(pt.x + 20, pt.y + 20)
+      createTree()
+    }
+  })
+
+  return JPanel(BorderLayout()).also {
+    it.add(layeredPane)
+    it.add(toolbar, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class JResizer(layout: LayoutManager) : JPanel(layout) { // implements Serializable {
-  @Transient
-  private var resizeListener: MouseInputListener? = null
+private fun createTree() {
+  val tree = JTree()
+  tree.visibleRowCount = 8
+  val c = JScrollPane(tree)
+  val d = c.preferredSize
+  val resizer = JResizer(BorderLayout())
+  resizer.add(c)
+  resizer.setBounds(pt.x, pt.y, d.width, d.height)
+  layeredPane.add(resizer)
+  layeredPane.moveToFront(resizer)
+}
+
+private fun createTable() {
+  val table = JTable(12, 3)
+  table.preferredScrollableViewportSize = Dimension(160, 160)
+  val c = JScrollPane(table)
+  val d = c.preferredSize
+  val resizer = JResizer(BorderLayout())
+  resizer.add(c)
+  resizer.setBounds(pt.x, pt.y, d.width, d.height)
+  layeredPane.add(resizer)
+  layeredPane.moveToFront(resizer)
+}
+
+private class JResizer(layout: LayoutManager) : JPanel(layout) {
+  @Transient private var resizeListener: MouseInputListener? = null
 
   override fun updateUI() {
     removeMouseListener(resizeListener)
@@ -81,7 +82,7 @@ class JResizer(layout: LayoutManager) : JPanel(layout) { // implements Serializa
     resizeListener = ResizeMouseListener()
     addMouseListener(resizeListener)
     addMouseMotionListener(resizeListener)
-    setBorder(DefaultResizableBorder())
+    border = DefaultResizableBorder()
   }
 
   override fun setBorder(border: Border?) {
@@ -95,82 +96,58 @@ class JResizer(layout: LayoutManager) : JPanel(layout) { // implements Serializa
   }
 }
 
-interface ResizableBorder : Border {
+private interface ResizableBorder : Border {
   fun getResizeCursor(e: MouseEvent): Cursor
 }
 
-class DefaultResizableBorder : ResizableBorder, SwingConstants {
+private class DefaultResizableBorder : ResizableBorder, SwingConstants {
   private enum class Locations(cursor: Int, private val location: Function<Rectangle, Point>) {
     NORTH(
       Cursor.N_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x + r.width / 2 - SIZE / 2,
-          r.y
-        )
+        Point(r.x + r.width / 2 - SIZE / 2, r.y)
       }
     ),
     SOUTH(
       Cursor.S_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x + r.width / 2 - SIZE / 2,
-          r.y + r.height - SIZE
-        )
+        Point(r.x + r.width / 2 - SIZE / 2, r.y + r.height - SIZE)
       }
     ),
     WEST(
       Cursor.W_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x,
-          r.y + r.height / 2 - SIZE / 2
-        )
+        Point(r.x, r.y + r.height / 2 - SIZE / 2)
       }
     ),
     EAST(
       Cursor.E_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x + r.width - SIZE,
-          r.y + r.height / 2 - SIZE / 2
-        )
+        Point(r.x + r.width - SIZE, r.y + r.height / 2 - SIZE / 2)
       }
     ),
     NORTH_WEST(
       Cursor.NW_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x,
-          r.y
-        )
+        Point(r.x, r.y)
       }
     ),
     NORTH_EAST(
       Cursor.NE_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x + r.width - SIZE,
-          r.y
-        )
+        Point(r.x + r.width - SIZE, r.y)
       }
     ),
     SOUTH_WEST(
       Cursor.SW_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x,
-          r.y + r.height - SIZE
-        )
+        Point(r.x, r.y + r.height - SIZE)
       }
     ),
     SOUTH_EAST(
       Cursor.SE_RESIZE_CURSOR,
       Function { r ->
-        Point(
-          r.x + r.width - SIZE,
-          r.y + r.height - SIZE
-        )
+        Point(r.x + r.width - SIZE, r.y + r.height - SIZE)
       }
     );
 
@@ -191,24 +168,24 @@ class DefaultResizableBorder : ResizableBorder, SwingConstants {
     w: Int,
     h: Int
   ) {
-    g.setColor(Color.BLACK)
+    g.color = Color.BLACK
     g.drawRect(x + SIZE / 2, y + SIZE / 2, w - SIZE, h - SIZE)
     val rect = Rectangle(SIZE, SIZE)
     val r = Rectangle(x, y, w, h)
     for (loc in Locations.values()) {
-      rect.setLocation(loc.getPoint(r))
-      g.setColor(Color.WHITE)
+      rect.location = loc.getPoint(r)
+      g.color = Color.WHITE
       g.fillRect(rect.x, rect.y, rect.width - 1, rect.height - 1)
-      g.setColor(Color.BLACK)
+      g.color = Color.BLACK
       g.drawRect(rect.x, rect.y, rect.width - 1, rect.height - 1)
     }
   }
 
   override fun getResizeCursor(e: MouseEvent): Cursor {
-    val c = e.getComponent()
+    val c = e.component
     val w = c.width
     val h = c.height
-    val pt = e.getPoint()
+    val pt = e.point
     val bounds = Rectangle(w, h)
     val actualBounds = Rectangle(SIZE, SIZE, w - 2 * SIZE, h - 2 * SIZE)
     if (!bounds.contains(pt) || actualBounds.contains(pt)) {
@@ -218,7 +195,7 @@ class DefaultResizableBorder : ResizableBorder, SwingConstants {
     val r = Rectangle(0, 0, w, h)
     var cursor = Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR)
     for (loc in Locations.values()) {
-      rect.setLocation(loc.getPoint(r))
+      rect.location = loc.getPoint(r)
       if (rect.contains(pt)) {
         cursor = loc.cursor
       }
@@ -231,27 +208,27 @@ class DefaultResizableBorder : ResizableBorder, SwingConstants {
   }
 }
 
-class ResizeMouseListener : MouseInputAdapter() {
+private class ResizeMouseListener : MouseInputAdapter() {
   private var cursor = Cursor.getDefaultCursor()
   private val startPos = Point(-1, -1)
   private val startingBounds = Rectangle()
   override fun mouseMoved(e: MouseEvent) {
-    val c = e.getComponent() as? JComponent ?: return
-    (c.getBorder() as? ResizableBorder)?.also {
-      c.setCursor(it.getResizeCursor(e))
+    val c = e.component as? JComponent ?: return
+    (c.border as? ResizableBorder)?.also {
+      c.cursor = it.getResizeCursor(e)
     }
   }
 
   override fun mouseExited(e: MouseEvent) {
-    e.getComponent().setCursor(Cursor.getDefaultCursor())
+    e.component.cursor = Cursor.getDefaultCursor()
   }
 
   override fun mousePressed(e: MouseEvent) {
-    val c = e.getComponent()
+    val c = e.component
     val border = (c as? JComponent)?.border as? ResizableBorder ?: return
     cursor = border.getResizeCursor(e)
-    startPos.setLocation(SwingUtilities.convertPoint(c, e.x, e.y, null))
-    startingBounds.setBounds(c.getBounds())
+    startPos.location = SwingUtilities.convertPoint(c, e.x, e.y, null)
+    startingBounds.bounds = c.getBounds()
     (SwingUtilities.getAncestorOfClass(JLayeredPane::class.java, c) as? JLayeredPane)?.moveToFront(c)
   }
 
@@ -261,23 +238,23 @@ class ResizeMouseListener : MouseInputAdapter() {
 
   // @see %JAVA_HOME%/src/javax/swing/plaf/basic/BasicInternalFrameUI.java
   override fun mouseDragged(e: MouseEvent) {
-    if (startingBounds.isEmpty()) {
+    if (startingBounds.isEmpty) {
       return
     }
-    val c = e.getComponent()
+    val c = e.component
     val p = SwingUtilities.convertPoint(c, e.x, e.y, null)
     val deltaX = startPos.x - p.x
     val deltaY = startPos.y - p.y
     val parent = SwingUtilities.getUnwrappedParent(c)
-    Directions.getByCursorType(cursor.getType())?.also {
-      val delta = it.getLimitedDelta(startingBounds, parent.getBounds(), deltaX, deltaY)
-      c.setBounds(it.getBounds(startingBounds, delta))
+    Directions.getByCursorType(cursor.type)?.also {
+      val delta = it.getLimitedDelta(startingBounds, parent.bounds, deltaX, deltaY)
+      c.bounds = it.getBounds(startingBounds, delta)
     }
     parent.revalidate()
   }
 }
 
-enum class Directions(private val cursor: Int) {
+private enum class Directions(private val cursor: Int) {
   NORTH(Cursor.N_RESIZE_CURSOR) {
     override fun getLimitedDelta(
       startingBounds: Rectangle,
@@ -285,6 +262,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(0, getDeltaY(deltaY, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y - delta.y,
@@ -299,6 +277,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(0, getDeltaY(deltaY, parentBounds, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y,
@@ -313,6 +292,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, startingBounds), 0)
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x - delta.x,
       rect.y,
@@ -327,6 +307,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, parentBounds, startingBounds), 0)
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y,
@@ -341,6 +322,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, startingBounds), getDeltaY(deltaY, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x - delta.x,
       rect.y - delta.y,
@@ -355,6 +337,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, parentBounds, startingBounds), getDeltaY(deltaY, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y - delta.y,
@@ -369,6 +352,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, startingBounds), getDeltaY(deltaY, parentBounds, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y,
@@ -383,6 +367,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(getDeltaX(deltaX, parentBounds, startingBounds), getDeltaY(deltaY, parentBounds, startingBounds))
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x,
       rect.y,
@@ -397,6 +382,7 @@ enum class Directions(private val cursor: Int) {
       deltaX: Int,
       deltaY: Int
     ) = Point(deltaX, deltaY)
+
     override fun getBounds(rect: Rectangle, delta: Point) = Rectangle(
       rect.x - delta.x,
       rect.y - delta.y,
@@ -465,7 +451,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
