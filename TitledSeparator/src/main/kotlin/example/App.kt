99cbafd5b1a8d6a5e3ca3b3613552dd94a8446c2
aterai <aterai@outlook.com>
2020-07-14 00:57:02 +0900

    refactor: use of setter method instead of property access syntax

--- TitledSeparator/src/main/kotlin/example/App.kt
+++ TitledSeparator/src/main/kotlin/example/App.kt
@@ -5,28 +5,29 @@ import java.awt.geom.Point2D
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.border.TitledBorder
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val box = Box.createVerticalBox()
-    box.add(TitledSeparator("TitledBorder", 2, TitledBorder.DEFAULT_POSITION))
-    box.add(JCheckBox("JCheckBox 0"))
-    box.add(JCheckBox("JCheckBox 1"))
-    box.add(Box.createVerticalStrut(10))
-    box.add(TitledSeparator("TitledBorder ABOVE TOP", Color(0x64_B4_C8), 2, TitledBorder.ABOVE_TOP))
-    box.add(JCheckBox("JCheckBox 2"))
-    box.add(JCheckBox("JCheckBox 3"))
-    box.add(Box.createVerticalStrut(10))
-    box.add(JSeparator())
-    box.add(JCheckBox("JCheckBox 4"))
-    box.add(JCheckBox("JCheckBox 5"))
-    // box.add(Box.createVerticalStrut(8))
-    add(box, BorderLayout.NORTH)
-    setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    setPreferredSize(Dimension(320, 240))
+fun makeUI(): Component {
+  val box = Box.createVerticalBox()
+  box.add(TitledSeparator("TitledBorder", 2, TitledBorder.DEFAULT_POSITION))
+  box.add(JCheckBox("JCheckBox 0"))
+  box.add(JCheckBox("JCheckBox 1"))
+  box.add(Box.createVerticalStrut(10))
+  box.add(TitledSeparator("TitledBorder ABOVE TOP", Color(0x64_B4_C8), 2, TitledBorder.ABOVE_TOP))
+  box.add(JCheckBox("JCheckBox 2"))
+  box.add(JCheckBox("JCheckBox 3"))
+  box.add(Box.createVerticalStrut(10))
+  box.add(JSeparator())
+  box.add(JCheckBox("JCheckBox 4"))
+  box.add(JCheckBox("JCheckBox 5"))
+  // box.add(Box.createVerticalStrut(8))
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class TitledSeparator(
+private class TitledSeparator(
   private val title: String,
   private val target: Color?,
   private val separatorHeight: Int,
@@ -45,7 +46,7 @@ class TitledSeparator(
       BorderFactory.createMatteBorder(separatorHeight, 0, 0, 0, icon),
       title, TitledBorder.DEFAULT_JUSTIFICATION, titlePosition
     )
-    setBorder(b)
+    border = b
   }
 
   override fun getMaximumSize() = Dimension(Short.MAX_VALUE.toInt(), super.getPreferredSize().height)
@@ -66,7 +67,7 @@ class TitledSeparator(
         val start = Point2D.Float()
         val end = Point2D.Float(width.toFloat(), 0f)
         val dist = floatArrayOf(0f, 1f)
-        val ec = getBackground() ?: UIManager.getColor("Panel.background")
+        val ec = background ?: UIManager.getColor("Panel.background")
         val sc = target ?: ec
         painter1 = LinearGradientPaint(start, end, dist, arrayOf(sc.darker(), ec))
         painter2 = LinearGradientPaint(start, end, dist, arrayOf(sc.brighter(), ec))
@@ -74,9 +75,9 @@ class TitledSeparator(
       val h = iconHeight / 2
       val g2 = g.create() as? Graphics2D ?: return
       // XXX: g2.translate(x, y)
-      g2.setPaint(painter1)
+      g2.paint = painter1
       g2.fillRect(x, y, width, iconHeight)
-      g2.setPaint(painter2)
+      g2.paint = painter2
       g2.fillRect(x, y + h, width, iconHeight - h)
       g2.dispose()
     }
@@ -101,7 +102,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
