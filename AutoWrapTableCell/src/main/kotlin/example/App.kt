9de3287c0129bd8113775c3a154c810f7720f529
aterai <aterai@outlook.com>
2020-07-16 18:53:06 +0900

    detekt: MatchingDeclarationName

--- AutoWrapTableCell/src/main/kotlin/example/App.kt
+++ AutoWrapTableCell/src/main/kotlin/example/App.kt
@@ -5,51 +5,50 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("Default", "AutoWrap")
-    val data = arrayOf(
-      arrayOf<Any>("123456789012345678901234567890", "123456789012345678901234567890"),
-      arrayOf<Any>("1111", "22222222222222222222222222222222222222222222222222222222"),
-      arrayOf<Any>("3333333", "----------------------------------------------0"),
-      arrayOf<Any>("4444444444444444444", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|")
-    )
-    val model = DefaultTableModel(data, columnNames)
-    val table = object : JTable(model) {
-      private val evenColor = Color(0xE6_F0_FF)
-      override fun prepareRenderer(tcr: TableCellRenderer, row: Int, column: Int): Component {
-        val c = super.prepareRenderer(tcr, row, column)
-        if (isRowSelected(row)) {
-          c.foreground = getSelectionForeground()
-          c.background = getSelectionBackground()
-        } else {
-          c.foreground = foreground
-          c.background = if (row % 2 == 0) evenColor else background
-        }
-        return c
-      }
+private const val AUTO_WRAP_COLUMN = 1
 
-      override fun updateUI() {
-        getColumnModel().getColumn(AUTO_WRAP_COLUMN).cellRenderer = null
-        super.updateUI()
-        isEnabled = false
-        setShowGrid(false)
-        getColumnModel().getColumn(AUTO_WRAP_COLUMN).cellRenderer = TextAreaCellRenderer()
+fun makeUI(): Component {
+  val columnNames = arrayOf("Default", "AutoWrap")
+  val data = arrayOf(
+    arrayOf<Any>("123456789012345678901234567890", "123456789012345678901234567890"),
+    arrayOf<Any>("1111", "22222222222222222222222222222222222222222222222222222222"),
+    arrayOf<Any>("3333333", "----------------------------------------------0"),
+    arrayOf<Any>("4444444444444444444", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>|")
+  )
+  val model = DefaultTableModel(data, columnNames)
+  val table = object : JTable(model) {
+    private val evenColor = Color(0xE6_F0_FF)
+    override fun prepareRenderer(tcr: TableCellRenderer, row: Int, column: Int): Component {
+      val c = super.prepareRenderer(tcr, row, column)
+      if (isRowSelected(row)) {
+        c.foreground = getSelectionForeground()
+        c.background = getSelectionBackground()
+      } else {
+        c.foreground = foreground
+        c.background = if (row % 2 == 0) evenColor else background
       }
+      return c
     }
-    val scroll = JScrollPane(table)
-    scroll.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
-    scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-    add(scroll)
-    preferredSize = Dimension(320, 240)
-  }
 
-  companion object {
-    private const val AUTO_WRAP_COLUMN = 1
+    override fun updateUI() {
+      getColumnModel().getColumn(AUTO_WRAP_COLUMN).cellRenderer = null
+      super.updateUI()
+      isEnabled = false
+      setShowGrid(false)
+      getColumnModel().getColumn(AUTO_WRAP_COLUMN).cellRenderer = TextAreaCellRenderer()
+    }
+  }
+  val scroll = JScrollPane(table)
+  scroll.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+  scroll.horizontalScrollBarPolicy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-class TextAreaCellRenderer : TableCellRenderer {
+private class TextAreaCellRenderer : TableCellRenderer {
   private val renderer = JTextArea()
   private val rowAndCellHeights: MutableList<MutableList<Int>> = ArrayList()
 
@@ -74,16 +73,7 @@ class TextAreaCellRenderer : TableCellRenderer {
     row: Int,
     column: Int
   ) {
-    // The trick for this to work properly is to set the width of the column to the
-    // text area. The reason for this is that getPreferredSize(), without a width tries
-    // to place all the text in one line. By setting the size with the width of the column,
-    // getPreferredSize() returns the proper height which the row should have in
-    // order to make room for the text.
-    // int cWidth = table.getTableHeader().getColumnModel().getColumn(column).getWidth()
-    // int cWidth = table.getCellRect(row, column, false).width // Ignore IntercellSpacing
-    // renderer.setSize(new Dimension(cWidth, 1000))
     renderer.bounds = table.getCellRect(row, column, false)
-    // renderer.doLayout()
     val preferredHeight = renderer.preferredSize.height
     while (rowAndCellHeights.size <= row) {
       rowAndCellHeights.add(createMutableList(column))
@@ -118,7 +108,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
