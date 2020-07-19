c32d0fa70623456e4daeac18a3adea5b3c8b2343
aterai <aterai@outlook.com>
2020-07-20 03:01:59 +0900

    add: ReverseDirectionSpinner

---
+++ ReverseDirectionSpinner/src/main/kotlin/example/App.kt
@@ -0,0 +1,62 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val box = Box.createVerticalBox()
+
+  val spinner0 = JSpinner(SpinnerNumberModel(5, 0, 10, 1))
+  box.add(makeTitledPanel("stepSize: 1", spinner0))
+  box.add(Box.createVerticalStrut(10))
+
+  val spinner1 = JSpinner(SpinnerNumberModel(5, 0, 10, -1))
+  box.add(makeTitledPanel("stepSize: -1", spinner1))
+  box.add(Box.createVerticalStrut(10))
+
+  val scale = arrayOf(
+    "AAA", "AA+", "AA", "AA-", "A+", "A", "A-",
+    "BBB+", "BBB", "BBB-", "BB+", "BB", "BB-", "B+", "B", "B-",
+    "CCC+", "CCC", "CCC-", "CC", "R", "D"
+  )
+  val spinner2 = JSpinner(SpinnerListModel(scale))
+  box.add(makeTitledPanel("SpinnerListModel", spinner2))
+  box.add(Box.createVerticalStrut(10))
+
+  val spinner3 = JSpinner(object : SpinnerListModel(scale) {
+    override fun getNextValue() = super.getPreviousValue()
+
+    override fun getPreviousValue() = super.getNextValue()
+  })
+  box.add(makeTitledPanel("Reverse direction SpinnerListModel", spinner3))
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c)
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
