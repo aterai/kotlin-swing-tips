657e76e074510d740bec0348a2d508b9c6ad45ed
aterai <aterai@outlook.com>
2020-07-29 03:54:05 +0900

    refactor: use of setter method instead of property access syntax

--- ButtonToolBarBorder/src/main/kotlin/example/App.kt
+++ ButtonToolBarBorder/src/main/kotlin/example/App.kt
@@ -11,17 +11,17 @@ fun makeUI(): Component {
   val toolbar = JToolBar()
 
   val tg1 = JToggleButton("Tg1")
-  tg1.setEnabled(false)
+  tg1.isEnabled = false
   val tg2 = JToggleButton("Tg2")
   val tg3 = JToggleButton("Tg3")
-  tg3.setBorder(BorderFactory.createLineBorder(Color.BLUE))
+  tg3.border = BorderFactory.createLineBorder(Color.BLUE)
   val tg4 = JToggleButton("Tg4", true)
   val tg5 = JToggleButton("Tg5")
 
   val dim = Dimension(5, 5)
   val bg = ButtonGroup()
   listOf(tg1, tg2, tg3, tg4, tg5).forEach {
-    it.setFocusPainted(false)
+    it.isFocusPainted = false
     toolbar.add(it)
     toolbar.add(Box.createRigidArea(dim))
     bg.add(it)
@@ -32,7 +32,7 @@ fun makeUI(): Component {
   return JPanel(BorderLayout()).also {
     it.add(toolbar, BorderLayout.NORTH)
     it.add(JScrollPane(JTree()))
-    it.setPreferredSize(Dimension(320, 240))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
