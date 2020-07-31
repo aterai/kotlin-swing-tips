137bdf0014c9393f133fb8aaa7331e236a75d683
aterai <aterai@outlook.com>
2020-08-01 05:08:46 +0900

    refactor: use of setter method instead of property access syntax

--- VisibilityOfTab/src/main/kotlin/example/App.kt
+++ VisibilityOfTab/src/main/kotlin/example/App.kt
@@ -8,10 +8,10 @@ fun makeUI(): Component {
 
   val tabbedPane1 = object : JTabbedPane() {
     override fun removeTabAt(index: Int) {
-      if (getTabCount() > 0) {
-        setSelectedIndex(0)
+      if (tabCount > 0) {
+        selectedIndex = 0
         super.removeTabAt(index)
-        setSelectedIndex(index - 1)
+        selectedIndex = index - 1
       } else {
         super.removeTabAt(index)
       }
@@ -21,8 +21,8 @@ fun makeUI(): Component {
   val tabbedPane2 = object : JTabbedPane() {
     private fun getScrollableViewport(): Component? {
       var cmp: Component? = null
-      for (c in getComponents()) {
-        if ("TabbedPane.scrollableViewport" == c.getName()) {
+      for (c in components) {
+        if ("TabbedPane.scrollableViewport" == c.name) {
           cmp = c
           break
         }
@@ -31,15 +31,15 @@ fun makeUI(): Component {
     }
 
     private fun resetViewportPosition(idx: Int) {
-      if (getTabCount() <= 0) {
+      if (tabCount <= 0) {
         return
       }
       val viewport = getScrollableViewport() as? JViewport ?: return
-      (viewport.getView() as? JComponent)?.scrollRectToVisible(getBoundsAt(idx))
+      (viewport.view as? JComponent)?.scrollRectToVisible(getBoundsAt(idx))
     }
 
     override fun removeTabAt(index: Int) {
-      if (getTabCount() > 0) {
+      if (tabCount > 0) {
         resetViewportPosition(0)
         super.removeTabAt(index)
         resetViewportPosition(index - 1)
@@ -51,7 +51,7 @@ fun makeUI(): Component {
 
   val list = listOf(JTabbedPane(), tabbedPane1, tabbedPane2)
   list.forEach {
-    it.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT)
+    it.tabLayoutPolicy = JTabbedPane.SCROLL_TAB_LAYOUT
     it.addTab("00000000", JLabel("0"))
     it.addTab("11111111", JLabel("1"))
     it.addTab("22222222", JLabel("2"))
@@ -62,22 +62,22 @@ fun makeUI(): Component {
     it.addTab("77777777", JLabel("7"))
     it.addTab("88888888", JLabel("8"))
     it.addTab("99999999", JLabel("9"))
-    it.setSelectedIndex(it.getTabCount() - 1)
+    it.selectedIndex = it.tabCount - 1
     // TEST: EventQueue.invokeLater { it.setSelectedIndex(tabs.getTabCount() - 1) };
     p.add(it)
   }
 
   val button = JButton("Remove")
   button.addActionListener {
-    list.filter { it.getTabCount() > 0 }.forEach {
-      it.removeTabAt(it.getTabCount() - 1)
+    list.filter { it.tabCount > 0 }.forEach {
+      it.removeTabAt(it.tabCount - 1)
     }
   }
 
   return JPanel(BorderLayout()).also {
     it.add(p)
     it.add(button, BorderLayout.SOUTH)
-    it.setPreferredSize(Dimension(320, 240))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
