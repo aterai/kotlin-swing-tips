7cc909c84daeb0cb0f9104773794795f0aa1f33b
aterai <aterai@outlook.com>
2020-08-04 05:27:25 +0900

    refactor: use of setter method instead of property access syntax

--- TableHeaderHeight/src/main/kotlin/example/App.kt
+++ TableHeaderHeight/src/main/kotlin/example/App.kt
@@ -12,29 +12,29 @@ fun makeUI(): Component {
 
   val table1 = makeTable()
   // Bad: >>>>
-  val header = table1.getTableHeader()
+  val header = table1.tableHeader
   // Dimension d = header.getPreferredSize()
   // d.height = HEADER_HEIGHT
-  // header.setPreferredSize(d) // addColumn case test
-  header.setPreferredSize(Dimension(100, HEADER_HEIGHT))
+  // header.preferredSize = d // addColumn case test
+  header.preferredSize = Dimension(100, HEADER_HEIGHT)
   p.add(makeTitledPanel("Bad: JTableHeader#setPreferredSize(...)", JScrollPane(table1)))
   // <<<<
 
   val table2 = makeTable()
   val scroll = JScrollPane(table2)
-  scroll.setColumnHeader(object : JViewport() {
+  scroll.columnHeader = object : JViewport() {
     override fun getPreferredSize() = super.getPreferredSize()?.also {
       it.height = HEADER_HEIGHT
     }
-  })
+  }
   p.add(makeTitledPanel("Override getPreferredSize()", scroll))
 
   val button = JButton("addColumn")
   button.addActionListener {
     listOf(table1, table2).forEach {
-      it.getColumnModel().addColumn(TableColumn())
-      val h = it.getTableHeader()
-      val d = h.getPreferredSize()
+      it.columnModel.addColumn(TableColumn())
+      val h = it.tableHeader
+      val d = h.preferredSize
       println(d)
     }
   }
@@ -42,17 +42,17 @@ fun makeUI(): Component {
   return JPanel(BorderLayout()).also {
     it.add(p)
     it.add(button, BorderLayout.SOUTH)
-    it.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
-    it.setPreferredSize(Dimension(320, 240))
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-fun makeTable() = JTable(DefaultTableModel(2, 20)).also {
-  it.setAutoResizeMode(JTable.AUTO_RESIZE_OFF)
+private fun makeTable() = JTable(DefaultTableModel(2, 20)).also {
+  it.autoResizeMode = JTable.AUTO_RESIZE_OFF
 }
 
-fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
-  it.setBorder(BorderFactory.createTitledBorder(title))
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
   it.add(c)
 }
 
