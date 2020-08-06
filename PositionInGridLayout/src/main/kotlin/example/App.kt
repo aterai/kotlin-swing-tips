5f8f6c7b492c6bd29b86d17b42178a5fe23fe707
aterai <aterai@outlook.com>
2020-08-07 03:34:38 +0900

    refactor: use of setter method instead of property access syntax

--- PositionInGridLayout/src/main/kotlin/example/App.kt
+++ PositionInGridLayout/src/main/kotlin/example/App.kt
@@ -7,22 +7,22 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 fun makeUI(): Component {
   val gl = GridLayout(5, 7, 5, 5)
   val p = JPanel(gl)
-  p.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5))
+  p.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
 
   val log = JTextArea(3, 0)
   val al = ActionListener { e ->
-    val idx = p.getComponentZOrder(e.getSource() as? Component)
-    val row = idx / gl.getColumns()
-    val col = idx % gl.getColumns()
+    val idx = p.getComponentZOrder(e.source as? Component)
+    val row = idx / gl.columns
+    val col = idx % gl.columns
     log.append("Row: ${row + 1}, Column: ${col + 1}\n")
   }
-  List(gl.getRows() * gl.getColumns()) { JButton() }
+  List(gl.rows * gl.columns) { JButton() }
     .forEach { p.add(it.also { it.addActionListener(al) }) }
 
   return JPanel(BorderLayout()).also {
     it.add(p)
     it.add(JScrollPane(log), BorderLayout.SOUTH)
-    it.setPreferredSize(Dimension(320, 240))
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
