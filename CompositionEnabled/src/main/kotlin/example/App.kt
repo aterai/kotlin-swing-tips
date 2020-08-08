fe73b9a0ca513228ff1c6a094889a9d30e051736
aterai <aterai@outlook.com>
2020-08-09 04:11:19 +0900

    add: CompositionEnabled

---
+++ CompositionEnabled/src/main/kotlin/example/App.kt
@@ -0,0 +1,60 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.KeyEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private val check = JCheckBox("一時ウィンドウ(入力モード)->enterでセル編集開始")
+
+fun makeUI(): Component {
+  val table = object : JTable(4, 3) {
+    override fun processKeyBinding(
+      ks: KeyStroke,
+      e: KeyEvent,
+      condition: Int,
+      pressed: Boolean
+    ): Boolean {
+      if (!check.isSelected || isTabOrEnterKey(ks)) {
+        println("tab or enter typed")
+        return super.processKeyBinding(ks, e, condition, pressed)
+      }
+      if (inputContext.isCompositionEnabled && !isEditing && !pressed && !ks.isOnKeyRelease) {
+        val selectedRow = selectedRow
+        val selectedColumn = selectedColumn
+        if (selectedRow != -1 && selectedColumn != -1) {
+          val b = editCellAt(selectedRow, selectedColumn)
+          println("editCellAt: $b")
+        }
+      }
+      return super.processKeyBinding(ks, e, condition, pressed)
+    }
+
+    protected fun isTabOrEnterKey(ks: KeyStroke) =
+      KeyStroke.getKeyStroke('\t') == ks || KeyStroke.getKeyStroke('\n') == ks
+  }
+  table.putClientProperty("terminateEditOnFocusLost", true)
+
+  return JPanel(BorderLayout()).also {
+    it.add(check, BorderLayout.NORTH)
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
