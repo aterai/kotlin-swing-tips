3b9dfdd4336c41561c20fe0a748bcf6b7eae9a51
aterai <aterai@outlook.com>
2020-08-13 02:04:17 +0900

    detekt: MatchingDeclarationName

--- EditableComboCellEditor/src/main/kotlin/example/App.kt
+++ EditableComboCellEditor/src/main/kotlin/example/App.kt
@@ -6,35 +6,37 @@ import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("Column1", "Column2")
-    val data = arrayOf(
-      arrayOf("colors", makeModel("blue", "violet", "red", "yellow")),
-      arrayOf("sports", makeModel("basketball", "soccer", "football", "hockey")),
-      arrayOf("food", makeModel("hot dogs", "pizza", "ravioli", "bananas"))
-    )
-    val model = object : DefaultTableModel(data, columnNames) {
-      override fun getColumnClass(column: Int) =
-        if (column == 1) DefaultComboBoxModel::class.java else String::class.java
-    }
-    val table = JTable(model)
-    table.rowHeight = 24
-    table.autoCreateRowSorter = true
-    val col = table.columnModel.getColumn(1)
-    col.cellRenderer = ComboCellRenderer()
-    col.cellEditor = ComboCellEditor()
-    add(JScrollPane(table))
-    preferredSize = Dimension(320, 240)
+fun makeUI(): Component {
+  val columnNames = arrayOf("Column1", "Column2")
+  val data = arrayOf(
+    arrayOf("colors", makeModel("blue", "violet", "red", "yellow")),
+    arrayOf("sports", makeModel("basketball", "soccer", "football", "hockey")),
+    arrayOf("food", makeModel("hot dogs", "pizza", "ravioli", "bananas"))
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) =
+      if (column == 1) DefaultComboBoxModel::class.java else String::class.java
   }
+  val table = JTable(model)
+  table.rowHeight = 24
+  table.autoCreateRowSorter = true
 
-  private fun makeModel(vararg items: String) =
-    object : DefaultComboBoxModel<String>(items) {
-      override fun toString() = selectedItem?.toString() ?: ""
-    }
+  val col = table.columnModel.getColumn(1)
+  col.cellRenderer = ComboCellRenderer()
+  col.cellEditor = ComboCellEditor()
+
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
 }
 
-class ComboCellRenderer : TableCellRenderer {
+private fun makeModel(vararg items: String) =
+  object : DefaultComboBoxModel<String>(items) {
+    override fun toString() = selectedItem?.toString() ?: ""
+  }
+
+private class ComboCellRenderer : TableCellRenderer {
   private val combo = JComboBox<String>()
   override fun getTableCellRendererComponent(
     table: JTable,
@@ -52,7 +54,7 @@ class ComboCellRenderer : TableCellRenderer {
   }
 }
 
-class ComboCellEditor : AbstractCellEditor(), TableCellEditor {
+private class ComboCellEditor : AbstractCellEditor(), TableCellEditor {
   private val combo = JComboBox<String>()
   override fun getTableCellEditorComponent(
     table: JTable,
@@ -60,7 +62,7 @@ class ComboCellEditor : AbstractCellEditor(), TableCellEditor {
     isSelected: Boolean,
     row: Int,
     column: Int
-  ): Component { // combo.setBackground(table.getSelectionBackground());
+  ): Component {
     if (value is ComboBoxModel<*>) {
       @Suppress("UNCHECKED_CAST")
       combo.model = value as? ComboBoxModel<String>
@@ -97,7 +99,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
