f53071361354b0a04411dce47ded519ac9bcc70f
aterai <aterai@outlook.com>
2020-08-28 17:30:05 +0900

    ktlint: unexpected indentation

--- DividerDraggingColor/src/main/kotlin/example/App.kt
+++ DividerDraggingColor/src/main/kotlin/example/App.kt
@@ -4,11 +4,11 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-    UIManager.put("SplitPaneDivider.draggingColor", Color(0x64_FF_64_64, true))
-    return JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
-      it.topComponent = JScrollPane(JTable(5, 3))
-      it.bottomComponent = JScrollPane(JTree())
-      it.dividerSize = 24
+  UIManager.put("SplitPaneDivider.draggingColor", Color(0x64_FF_64_64, true))
+  return JSplitPane(JSplitPane.VERTICAL_SPLIT).also {
+    it.topComponent = JScrollPane(JTable(5, 3))
+    it.bottomComponent = JScrollPane(JTree())
+    it.dividerSize = 24
     // it.isContinuousLayout = false
     it.preferredSize = Dimension(320, 240)
   }
