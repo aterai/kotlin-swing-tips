b14009ff45ef04805de044c34b1843c7bb7492af
aterai <aterai@outlook.com>
2020-08-30 02:51:22 +0900

    add: DragEnabledTextComponent

---
+++ DragEnabledTextComponent/src/main/kotlin/example/App.kt
@@ -0,0 +1,68 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val textField0 = JTextField("Initially has BasicTextUI\$TextTransferHandler")
+  textField0.name = "default"
+
+  val textField1 = JTextField("setEditable(false)")
+  textField1.isEditable = false
+
+  val textField2 = JTextField("setEnabled(false)")
+  textField2.isEnabled = false
+
+  val textField3 = JTextField("setTransferHandler(null)")
+  textField3.transferHandler = null
+
+  val textField4 = JTextField("setDropTarget(null)")
+  textField4.dropTarget = null
+
+  val textField5 = JTextField("TransferHandler#canImport(...): false")
+  textField5.transferHandler = object : TransferHandler() {
+    override fun canImport(info: TransferSupport) = false
+  }
+
+  EventQueue.invokeLater {
+    val c = textField0.topLevelAncestor
+    if (c is JFrame) {
+      c.transferHandler = object : TransferHandler() {
+        override fun canImport(info: TransferSupport) = true
+
+        override fun importData(support: TransferSupport) = true
+      }
+    }
+  }
+
+  val box = Box.createVerticalBox()
+  box.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+  listOf(textField0, textField1, textField2, textField3, textField4, textField5).forEach {
+    it.dragEnabled = true
+    box.add(it)
+    box.add(Box.createVerticalStrut(10))
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(box, BorderLayout.NORTH)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
