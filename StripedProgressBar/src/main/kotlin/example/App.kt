67914c5e5883812e26d8f1d2b43587372ee8de55
aterai <aterai@outlook.com>
2020-08-31 00:38:31 +0900

    refactor: HierarchyListener() can be replaced with lambda

--- StripedProgressBar/src/main/kotlin/example/App.kt
+++ StripedProgressBar/src/main/kotlin/example/App.kt
@@ -2,7 +2,6 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.event.HierarchyEvent
-import java.awt.event.HierarchyListener
 import java.awt.geom.AffineTransform
 import java.awt.geom.GeneralPath
 import java.beans.PropertyChangeEvent
@@ -10,11 +9,56 @@ import java.beans.PropertyChangeListener
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.plaf.basic.BasicProgressBarUI
 
-class MainPanel : JPanel(BorderLayout()), HierarchyListener {
-  @Transient
-  private var worker: SwingWorker<String, Void>? = null
+fun makeUI(): Component {
+  UIManager.put("ProgressBar.cycleTime", 1000)
+  UIManager.put("ProgressBar.repaintInterval", 10)
 
-  override fun hierarchyChanged(e: HierarchyEvent) {
+  val model = DefaultBoundedRangeModel()
+
+  val progress1 = JProgressBar(model)
+  progress1.ui = StripedProgressBarUI(dir = true, slope = true)
+
+  val progress2 = JProgressBar(model)
+  progress2.ui = StripedProgressBarUI(dir = true, slope = false)
+
+  val progress3 = JProgressBar(model)
+  progress3.ui = StripedProgressBarUI(dir = false, slope = true)
+
+  val progress4 = JProgressBar(model)
+  progress4.ui = StripedProgressBarUI(dir = false, slope = false)
+
+  val list = listOf(
+    JProgressBar(model),
+    progress1,
+    progress2,
+    progress3,
+    progress4
+  )
+
+  val p = JPanel(GridLayout(5, 1))
+  list.forEach {
+    p.add(makePanel(it))
+  }
+
+  var worker: SwingWorker<String, Void>? = null
+  val button = JButton("Test start")
+  button.addActionListener {
+    worker?.takeUnless { it.isDone }?.cancel(true)
+    worker = BackgroundTask().also { w ->
+      list.forEach { bar ->
+        bar.isIndeterminate = true
+        w.addPropertyChangeListener(ProgressListener(bar))
+      }
+      w.execute()
+    }
+  }
+  val box = Box.createHorizontalBox()
+  box.add(Box.createHorizontalGlue())
+  box.add(button)
+  box.add(Box.createHorizontalStrut(5))
+
+  val panel = JPanel(BorderLayout())
+  panel.addHierarchyListener { e ->
     val isDisplayableChanged = e.changeFlags and HierarchyEvent.DISPLAYABILITY_CHANGED.toLong() != 0L
     if (isDisplayableChanged && !e.component.isDisplayable && worker != null) {
       println("DISPOSE_ON_CLOSE")
@@ -22,72 +66,24 @@ class MainPanel : JPanel(BorderLayout()), HierarchyListener {
       worker = null
     }
   }
-
-  init {
-    UIManager.put("ProgressBar.cycleTime", 1000)
-    UIManager.put("ProgressBar.repaintInterval", 10)
-
-    val model = DefaultBoundedRangeModel()
-
-    val progress1 = JProgressBar(model)
-    progress1.ui = StripedProgressBarUI(dir = true, slope = true)
-
-    val progress2 = JProgressBar(model)
-    progress2.ui = StripedProgressBarUI(dir = true, slope = false)
-
-    val progress3 = JProgressBar(model)
-    progress3.ui = StripedProgressBarUI(dir = false, slope = true)
-
-    val progress4 = JProgressBar(model)
-    progress4.ui = StripedProgressBarUI(dir = false, slope = false)
-
-    val list = listOf(
-      JProgressBar(model),
-      progress1,
-      progress2,
-      progress3,
-      progress4
-    )
-
-    val p = JPanel(GridLayout(5, 1))
-    list.forEach {
-      p.add(makePanel(it))
-    }
-
-    val button = JButton("Test start")
-    button.addActionListener {
-      worker?.takeUnless { it.isDone }?.cancel(true)
-      worker = BackgroundTask().also { w ->
-        list.forEach { bar ->
-          bar.isIndeterminate = true
-          w.addPropertyChangeListener(ProgressListener(bar))
-        }
-        w.execute()
-      }
-    }
-    val box = Box.createHorizontalBox()
-    box.add(Box.createHorizontalGlue())
-    box.add(button)
-    box.add(Box.createHorizontalStrut(5))
-    addHierarchyListener(this)
-    add(p)
-    add(box, BorderLayout.SOUTH)
-    border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
-    preferredSize = Dimension(320, 240)
-  }
-
-  private fun makePanel(cmp: Component): Component {
-    val c = GridBagConstraints()
-    c.fill = GridBagConstraints.HORIZONTAL
-    c.insets = Insets(5, 5, 5, 5)
-    c.weightx = 1.0
-    val p = JPanel(GridBagLayout())
-    p.add(cmp, c)
-    return p
-  }
+  panel.add(p)
+  panel.add(box, BorderLayout.SOUTH)
+  panel.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+  panel.preferredSize = Dimension(320, 240)
+  return panel
 }
 
-class StripedProgressBarUI(private val dir: Boolean, private val slope: Boolean) : BasicProgressBarUI() {
+private fun makePanel(cmp: Component): Component {
+  val c = GridBagConstraints()
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.insets = Insets(5, 5, 5, 5)
+  c.weightx = 1.0
+  val p = JPanel(GridBagLayout())
+  p.add(cmp, c)
+  return p
+}
+
+private class StripedProgressBarUI(private val dir: Boolean, private val slope: Boolean) : BasicProgressBarUI() {
   override fun getBoxLength(availableLength: Int, otherDimension: Int) = availableLength
   // (availableLength / 6.0).roundToInt()
 
@@ -139,7 +135,7 @@ class StripedProgressBarUI(private val dir: Boolean, private val slope: Boolean)
   }
 }
 
-class BackgroundTask : SwingWorker<String, Void>() {
+private class BackgroundTask : SwingWorker<String, Void>() {
   @Throws(InterruptedException::class)
   public override fun doInBackground(): String {
     Thread.sleep(5000) // dummy task 1
@@ -154,7 +150,7 @@ class BackgroundTask : SwingWorker<String, Void>() {
   }
 }
 
-class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
+private class ProgressListener(private val progressBar: JProgressBar) : PropertyChangeListener {
   override fun propertyChange(e: PropertyChangeEvent) {
     val strPropertyName = e.propertyName
     if ("progress" == strPropertyName) {
@@ -178,7 +174,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
