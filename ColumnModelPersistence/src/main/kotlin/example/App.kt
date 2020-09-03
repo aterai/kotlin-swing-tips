2ed1fa0c16a76eb0a44e5210c23eb71d588a8090
aterai <aterai@outlook.com>
2020-09-03 20:09:27 +0900

    detekt: MatchingDeclarationName

--- ColumnModelPersistence/src/main/kotlin/example/App.kt
+++ ColumnModelPersistence/src/main/kotlin/example/App.kt
@@ -19,78 +19,81 @@ import javax.swing.table.DefaultTableColumnModel
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 
-class MainPanel : JPanel(BorderLayout()) {
-  init {
-    val columnNames = arrayOf("A", "B")
-    val data = arrayOf(arrayOf("aaa", "ccc ccc"), arrayOf("bbb", "☀☁☂☃"))
-    val table = JTable(DefaultTableModel(data, columnNames))
-    table.autoCreateRowSorter = true
-    table.tableHeader.componentPopupMenu = TableHeaderPopupMenu()
-    val textArea = JTextArea()
-    val sp = JSplitPane(JSplitPane.VERTICAL_SPLIT)
-    sp.resizeWeight = .5
-    sp.topComponent = JScrollPane(table)
-    sp.bottomComponent = JScrollPane(textArea)
-    val encButton = JButton("XMLEncoder")
-    encButton.addActionListener {
-      runCatching {
-        val file = File.createTempFile("output", ".xml")
-        XMLEncoder(BufferedOutputStream(Files.newOutputStream(file.toPath()))).use { xe ->
-          val constructorPropertyNames = arrayOf("column", "sortOrder")
-          xe.setPersistenceDelegate(RowSorter.SortKey::class.java, DefaultPersistenceDelegate(constructorPropertyNames))
-          xe.writeObject(table.rowSorter.sortKeys)
-          xe.setPersistenceDelegate(DefaultTableModel::class.java, DefaultTableModelPersistenceDelegate())
-          xe.writeObject(table.model)
-          xe.setPersistenceDelegate(DefaultTableColumnModel::class.java, DefaultTableColumnModelPersistenceDelegate())
-          xe.writeObject(table.columnModel)
-        }
-        Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8)
-          .use { r -> textArea.read(r, "temp") }
-      }.onFailure {
-        it.printStackTrace()
-        textArea.text = it.message
+fun makeUI(): Component {
+  val columnNames = arrayOf("A", "B")
+  val data = arrayOf(arrayOf("aaa", "ccc ccc"), arrayOf("bbb", "☀☁☂☃"))
+  val table = JTable(DefaultTableModel(data, columnNames))
+  table.autoCreateRowSorter = true
+  table.tableHeader.componentPopupMenu = TableHeaderPopupMenu()
+
+  val textArea = JTextArea()
+
+  val sp = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+  sp.resizeWeight = .5
+  sp.topComponent = JScrollPane(table)
+  sp.bottomComponent = JScrollPane(textArea)
+
+  val encButton = JButton("XMLEncoder")
+  encButton.addActionListener {
+    runCatching {
+      val file = File.createTempFile("output", ".xml")
+      XMLEncoder(BufferedOutputStream(Files.newOutputStream(file.toPath()))).use { xe ->
+        val constructorPropertyNames = arrayOf("column", "sortOrder")
+        xe.setPersistenceDelegate(RowSorter.SortKey::class.java, DefaultPersistenceDelegate(constructorPropertyNames))
+        xe.writeObject(table.rowSorter.sortKeys)
+        xe.setPersistenceDelegate(DefaultTableModel::class.java, DefaultTableModelPersistenceDelegate())
+        xe.writeObject(table.model)
+        xe.setPersistenceDelegate(DefaultTableColumnModel::class.java, DefaultTableColumnModelPersistenceDelegate())
+        xe.writeObject(table.columnModel)
+      }
+      Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8).use { r -> textArea.read(r, "temp") }
+    }.onFailure {
+      it.printStackTrace()
+      textArea.text = it.message
+    }
+  }
+
+  val decButton = JButton("XMLDecoder")
+  decButton.addActionListener {
+    val text = textArea.text
+    if (text.isNotEmpty()) {
+      val bytes = text.toByteArray(StandardCharsets.UTF_8)
+      XMLDecoder(BufferedInputStream(ByteArrayInputStream(bytes))).use { xd ->
+        @Suppress("UNCHECKED_CAST")
+        val keys = xd.readObject() as List<RowSorter.SortKey>
+        val model = xd.readObject() as DefaultTableModel
+        table.model = model
+        table.autoCreateRowSorter = true
+        table.rowSorter.sortKeys = keys
+        val cm = xd.readObject() as DefaultTableColumnModel
+        table.columnModel = cm
       }
     }
-    val decButton = JButton("XMLDecoder")
-    decButton.addActionListener {
-      val text = textArea.text
-      if (text.isNotEmpty()) {
-        val bytes = text.toByteArray(StandardCharsets.UTF_8)
-        XMLDecoder(BufferedInputStream(ByteArrayInputStream(bytes))).use { xd ->
-          @Suppress("UNCHECKED_CAST")
-          val keys = xd.readObject() as List<RowSorter.SortKey>
-          val model = xd.readObject() as DefaultTableModel
-          table.model = model
-          table.autoCreateRowSorter = true
-          table.rowSorter.sortKeys = keys
-          val cm = xd.readObject() as DefaultTableColumnModel
-          table.columnModel = cm
-        }
-      }
-    }
-    val clearButton = JButton("clear")
-    clearButton.addActionListener {
-      table.model = DefaultTableModel()
-    }
-    val p = JPanel()
-    p.add(encButton)
-    p.add(decButton)
-    p.add(clearButton)
-    add(sp)
-    add(p, BorderLayout.SOUTH)
-    preferredSize = Dimension(320, 240)
+  }
+
+  val clearButton = JButton("clear")
+  clearButton.addActionListener {
+    table.model = DefaultTableModel()
+  }
+
+  val p = JPanel()
+  p.add(encButton)
+  p.add(decButton)
+  p.add(clearButton)
+
+  return JPanel(BorderLayout()).also {
+    it.add(sp)
+    it.add(p, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
   }
 }
 
-// http://web.archive.org/web/20090806075316/http://java.sun.com/products/jfc/tsc/articles/persistence4/
-// http://www.oracle.com/technetwork/java/persistence4-140124.html
-// https://ateraimemo.com/Swing/PersistenceDelegate.html
-class DefaultTableModelPersistenceDelegate : DefaultPersistenceDelegate() {
+private class DefaultTableModelPersistenceDelegate : DefaultPersistenceDelegate() {
   override fun initialize(
-    type: Class<*>,
-    oldInstance: Any,
-    newInstance: Any,
-    encoder: Encoder
+      type: Class<*>,
+      oldInstance: Any,
+      newInstance: Any,
+      encoder: Encoder
   ) {
     super.initialize(type, oldInstance, newInstance, encoder)
     (oldInstance as? DefaultTableModel)?.also { m ->
@@ -104,12 +107,12 @@ class DefaultTableModelPersistenceDelegate : DefaultPersistenceDelegate() {
   }
 }
 
-class DefaultTableColumnModelPersistenceDelegate : DefaultPersistenceDelegate() {
+private class DefaultTableColumnModelPersistenceDelegate : DefaultPersistenceDelegate() {
   override fun initialize(
-    type: Class<*>,
-    oldInstance: Any,
-    newInstance: Any,
-    encoder: Encoder
+      type: Class<*>,
+      oldInstance: Any,
+      newInstance: Any,
+      encoder: Encoder
   ) {
     super.initialize(type, oldInstance, newInstance, encoder)
     (oldInstance as? DefaultTableColumnModel)?.also { m ->
@@ -121,7 +124,7 @@ class DefaultTableColumnModelPersistenceDelegate : DefaultPersistenceDelegate() 
   }
 }
 
-class TableHeaderPopupMenu : JPopupMenu() {
+private class TableHeaderPopupMenu : JPopupMenu() {
   private var index = -1
   override fun show(c: Component, x: Int, y: Int) {
     (c as? JTableHeader)?.also { header ->
@@ -145,11 +148,11 @@ class TableHeaderPopupMenu : JPopupMenu() {
         textField.text = name
         val p = it.rootPane
         val ret = JOptionPane.showConfirmDialog(
-          p,
-          textField,
-          "edit",
-          JOptionPane.OK_CANCEL_OPTION,
-          JOptionPane.PLAIN_MESSAGE
+            p,
+            textField,
+            "edit",
+            JOptionPane.OK_CANCEL_OPTION,
+            JOptionPane.PLAIN_MESSAGE
         )
         if (ret == JOptionPane.OK_OPTION) {
           val str = textField.text.trim()
@@ -163,7 +166,7 @@ class TableHeaderPopupMenu : JPopupMenu() {
   }
 }
 
-class FocusAncestorListener : AncestorListener {
+private class FocusAncestorListener : AncestorListener {
   override fun ancestorAdded(e: AncestorEvent) {
     e.component.requestFocusInWindow()
   }
@@ -187,7 +190,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
