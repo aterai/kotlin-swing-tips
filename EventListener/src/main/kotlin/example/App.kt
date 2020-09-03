3933251ee07238d1c69990719084f74c0759f50f
aterai <aterai@outlook.com>
2020-09-03 20:19:36 +0900

    refactor: use a lambda expression with Functional (SAM) interfaces

--- EventListener/src/main/kotlin/example/App.kt
+++ EventListener/src/main/kotlin/example/App.kt
@@ -5,16 +5,19 @@ import java.util.EventListener
 import java.util.EventObject
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
-class MainPanel : JPanel(BorderLayout()) {
-  private fun addFontChangeListener(l: FontChangeListener?) {
+private val FONT12 = Font(Font.SANS_SERIF, Font.PLAIN, 12)
+private val FONT32 = Font(Font.SANS_SERIF, Font.PLAIN, 32)
+
+private val p = object : JPanel(BorderLayout()) {
+  fun addFontChangeListener(l: FontChangeListener?) {
     listenerList.add(FontChangeListener::class.java, l)
   }
 
-  // private fun removeFontChangeListener(l: FontChangeListener?) {
+  // fun removeFontChangeListener(l: FontChangeListener?) {
   //   listenerList.remove(FontChangeListener::class.java, l)
   // }
 
-  private fun fireFontChangeEvent(font: Font) {
+  fun fireFontChangeEvent(font: Font) {
     val listeners = listenerList.listenerList
     val e = FontChangeEvent(this, font)
     var i = listeners.size - 2
@@ -25,49 +28,44 @@ class MainPanel : JPanel(BorderLayout()) {
       i -= 2
     }
   }
-
-  init {
-    val button = MyButton("dummy")
-    button.font = FONT12
-    addFontChangeListener(button)
-
-    val label = MyLabel("test")
-    label.font = FONT12
-    addFontChangeListener(label)
-
-    val combo = MyComboBox()
-    combo.model = DefaultComboBoxModel(arrayOf("item1", "item2"))
-    combo.font = FONT12
-    addFontChangeListener(combo)
-
-    val menu = JMenu("Font")
-    menu.toolTipText = "Select font size"
-    menu.add("32pt").addActionListener {
-      fireFontChangeEvent(FONT32)
-    }
-    menu.add("12pt").addActionListener {
-      fireFontChangeEvent(FONT12)
-    }
-
-    val menuBar = JMenuBar()
-    menuBar.add(menu)
-
-    val panel = JPanel()
-    panel.add(label)
-    panel.add(combo)
-    panel.add(button)
-    add(menuBar, BorderLayout.NORTH)
-    add(panel)
-    preferredSize = Dimension(320, 240)
-  }
-
-  companion object {
-    private val FONT12 = Font(Font.SANS_SERIF, Font.PLAIN, 12)
-    private val FONT32 = Font(Font.SANS_SERIF, Font.PLAIN, 32)
-  }
 }
 
-interface FontChangeListener : EventListener {
+fun makeUI(): Component {
+  val button = JButton("JButton")
+  button.font = FONT12
+  p.addFontChangeListener { e -> button.font = e.font }
+
+  val label = JLabel("JLabel")
+  label.font = FONT12
+  p.addFontChangeListener { e -> button.font = e.font }
+
+  val combo = JComboBox(DefaultComboBoxModel(arrayOf("item1", "item2")))
+  combo.font = FONT12
+  p.addFontChangeListener { e -> button.font = e.font }
+
+  val menu = JMenu("Font")
+  menu.toolTipText = "Select font size"
+  menu.add("32pt").addActionListener {
+    p.fireFontChangeEvent(FONT32)
+  }
+  menu.add("12pt").addActionListener {
+    p.fireFontChangeEvent(FONT12)
+  }
+
+  val menuBar = JMenuBar()
+  menuBar.add(menu)
+
+  val panel = JPanel()
+  panel.add(label)
+  panel.add(combo)
+  panel.add(button)
+  p.add(menuBar, BorderLayout.NORTH)
+  p.add(panel)
+  p.preferredSize = Dimension(320, 240)
+  return p
+}
+
+fun interface FontChangeListener : EventListener {
   fun fontStateChanged(e: FontChangeEvent)
 }
 
@@ -77,23 +75,23 @@ class FontChangeEvent(source: Any?, val font: Font?) : EventObject(source) {
   }
 }
 
-private class MyComboBox : JComboBox<String>(), FontChangeListener {
-  override fun fontStateChanged(e: FontChangeEvent) {
-    font = e.font
-  }
-}
-
-private class MyLabel(str: String) : JLabel(str), FontChangeListener {
-  override fun fontStateChanged(e: FontChangeEvent) {
-    font = e.font
-  }
-}
-
-private class MyButton(str: String) : JButton(str), FontChangeListener {
-  override fun fontStateChanged(e: FontChangeEvent) {
-    font = e.font
-  }
-}
+// private class MyComboBox : JComboBox<String>(), FontChangeListener {
+//   override fun fontStateChanged(e: FontChangeEvent) {
+//     font = e.font
+//   }
+// }
+//
+// private class MyLabel(str: String) : JLabel(str), FontChangeListener {
+//   override fun fontStateChanged(e: FontChangeEvent) {
+//     font = e.font
+//   }
+// }
+//
+// private class MyButton(str: String) : JButton(str), FontChangeListener {
+//   override fun fontStateChanged(e: FontChangeEvent) {
+//     font = e.font
+//   }
+// }
 
 fun main() {
   EventQueue.invokeLater {
@@ -105,7 +103,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(MainPanel())
+      contentPane.add(makeUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
