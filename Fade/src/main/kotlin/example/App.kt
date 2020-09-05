f6793ec4af1183b664e669ccf632f069c200d31e
aterai <aterai@outlook.com>
2020-09-06 03:24:53 +0900

    add: Fade

---
+++ Fade/src/main/kotlin/example/App.kt
@@ -0,0 +1,71 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private var mode = Fade.IN
+
+fun makeUI(): Component {
+  val animator = Timer(25, null)
+  val alpha = AtomicInteger(10)
+  val cl = Thread.currentThread().contextClassLoader
+  val icon = ImageIcon(cl.getResource("example/test.png"))
+  val fade = object : JComponent() {
+    override fun paintComponent(g: Graphics) {
+      val g2 = g.create() as? Graphics2D ?: return
+      g2.paint = Color.BLACK
+      g2.fillRect(0, 0, width, height)
+      g2.composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha.get() * .1f)
+      icon.paintIcon(this, g2, 0, 0)
+      g2.dispose()
+    }
+  }
+  animator.addActionListener {
+    if (mode == Fade.IN && alpha.get() < 10) {
+      alpha.incrementAndGet()
+    } else if (mode == Fade.OUT && alpha.get() > 0) {
+      alpha.decrementAndGet()
+    } else {
+      animator.stop()
+    }
+    fade.repaint()
+  }
+
+  val button = JButton("Fade In/Out")
+  button.addActionListener {
+    mode = mode.toggle()
+    animator.start()
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(fade)
+    it.add(button, BorderLayout.SOUTH)
+    it.isOpaque = false
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private enum class Fade {
+  IN, OUT;
+
+  fun toggle() = if (this == IN) OUT else IN
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
