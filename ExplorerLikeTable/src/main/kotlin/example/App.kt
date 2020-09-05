30c314545f43ef2ec0ae16992a741ad6596cae22
aterai <aterai@outlook.com>
2020-09-06 04:17:03 +0900

    add: ExplorerLikeTable

---
+++ ExplorerLikeTable/src/main/kotlin/example/App.kt
@@ -0,0 +1,213 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
+import java.awt.event.InputEvent
+import java.awt.event.KeyEvent
+import java.awt.image.FilteredImageSource
+import java.awt.image.ImageProducer
+import java.awt.image.RGBImageFilter
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.border.Border
+import javax.swing.plaf.ColorUIResource
+import javax.swing.table.DefaultTableCellRenderer
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellRenderer
+import javax.swing.table.TableModel
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("Name", "Comment")
+  val data = arrayOf(
+    arrayOf("test1.jpg", "11111"),
+    arrayOf("test1234.jpg", "  "),
+    arrayOf("test15354.gif", "22222"),
+    arrayOf("t.png", "comment"),
+    arrayOf("33333.jpg", "123"),
+    arrayOf("4444444444444444.mpg", "test"),
+    arrayOf("5555555555555", ""),
+    arrayOf("test1.jpg", "")
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+
+    override fun isCellEditable(row: Int, column: Int) = false
+  }
+  val table = FileListTable(model)
+  val scroll = JScrollPane(table)
+  val tab = KeyStroke.getKeyStroke(KeyEvent.VK_TAB, 0)
+  val stab = KeyStroke.getKeyStroke(KeyEvent.VK_TAB, InputEvent.SHIFT_DOWN_MASK)
+  val im = table.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+  im.put(tab, im[KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0)])
+  im.put(stab, im[KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.SHIFT_DOWN_MASK)])
+  val orgColor = table.selectionBackground
+  val tflColor = scroll.background
+  table.addFocusListener(object : FocusListener {
+    override fun focusGained(e: FocusEvent) {
+      table.selectionForeground = Color.WHITE
+      table.selectionBackground = orgColor
+    }
+
+    override fun focusLost(e: FocusEvent) {
+      table.selectionForeground = Color.BLACK
+      table.selectionBackground = tflColor
+    }
+  })
+  table.componentPopupMenu = TablePopupMenu()
+
+  return JPanel(BorderLayout()).also {
+    it.add(scroll)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private class SelectedImageFilter : RGBImageFilter() {
+  override fun filterRGB(x: Int, y: Int, argb: Int): Int {
+    val r = argb shr 16 and 0xFF
+    val g = argb shr 8 and 0xFF
+    return argb and -0xffff01 or (r shr 1 shl 16) or (g shr 1 shl 8)
+    // return (argb & 0xFF_FF_FF_00) | ((argb & 0xFF) >> 1);
+  }
+}
+
+private class FileNameRenderer(table: JTable) : TableCellRenderer {
+  val dim = Dimension()
+  private val renderer = JPanel(BorderLayout())
+  private val textLabel = JLabel(" ")
+  private val iconLabel: JLabel
+  private val focusBorder = UIManager.getBorder("Table.focusCellHighlightBorder")
+  private val noFocusBorder: Border
+  private val icon: ImageIcon
+  private val selectedIcon: ImageIcon
+  override fun getTableCellRendererComponent(
+    table: JTable,
+    value: Any?,
+    isSelected: Boolean,
+    hasFocus: Boolean,
+    row: Int,
+    column: Int
+  ): Component {
+    textLabel.font = table.font
+    textLabel.text = value?.toString() ?: ""
+    textLabel.border = if (hasFocus) focusBorder else noFocusBorder
+    val fm = table.getFontMetrics(table.font)
+    val i = textLabel.insets
+    val width = iconLabel.preferredSize.width + fm.stringWidth(textLabel.text) + i.left + i.right
+    val colWidth = table.columnModel.getColumn(column).width
+    dim.width = width.coerceAtMost(colWidth)
+    if (isSelected) {
+      textLabel.isOpaque = true
+      textLabel.foreground = table.selectionForeground
+      textLabel.background = table.selectionBackground
+      iconLabel.icon = selectedIcon
+    } else {
+      textLabel.isOpaque = false
+      textLabel.foreground = table.foreground
+      textLabel.background = table.background
+      iconLabel.icon = icon
+    }
+    return renderer
+  }
+
+  init {
+    noFocusBorder = UIManager.getBorder("Table.noFocusBorder") ?: focusBorder.getBorderInsets(textLabel).let {
+      BorderFactory.createEmptyBorder(it.top, it.left, it.bottom, it.right)
+    }
+    val p = object : JPanel(BorderLayout()) {
+      override fun getPreferredSize() = dim
+    }
+    p.isOpaque = false
+    renderer.isOpaque = false
+
+    // [XP Style Icons - Download](https://xp-style-icons.en.softonic.com/)
+    val cl = Thread.currentThread().contextClassLoader
+    icon = ImageIcon(cl.getResource("example/wi0063-16.png"))
+    val ip: ImageProducer = FilteredImageSource(icon.image.source, SelectedImageFilter())
+    selectedIcon = ImageIcon(p.createImage(ip))
+    iconLabel = JLabel(icon)
+    iconLabel.border = BorderFactory.createEmptyBorder()
+    p.add(iconLabel, BorderLayout.WEST)
+    p.add(textLabel)
+    renderer.add(p, BorderLayout.WEST)
+    val d = iconLabel.preferredSize
+    dim.size = d
+    table.rowHeight = d.height
+  }
+}
+
+private class FileListTable(model: TableModel) : JTable(model) {
+  override fun updateUI() {
+    setSelectionForeground(ColorUIResource(Color.RED))
+    setSelectionBackground(ColorUIResource(Color.RED))
+    setDefaultRenderer(Any::class.java, null)
+    super.updateUI()
+    putClientProperty("Table.isFileList", true)
+    setCellSelectionEnabled(true)
+    intercellSpacing = Dimension()
+    setShowGrid(false)
+    autoCreateRowSorter = true
+    fillsViewportHeight = true
+    val r = DefaultTableCellRenderer()
+    setDefaultRenderer(Any::class.java) { table, value, _, _, row, column ->
+      r.getTableCellRendererComponent(table, value, false, false, row, column)
+    }
+    getColumnModel().getColumn(0).also {
+      it.cellRenderer = FileNameRenderer(this)
+      it.preferredWidth = 200
+    }
+    getColumnModel().getColumn(1).preferredWidth = 300
+  }
+}
+
+private class TablePopupMenu : JPopupMenu() {
+  private val delete: JMenuItem
+  override fun show(c: Component, x: Int, y: Int) {
+    if (c is JTable) {
+      delete.isEnabled = c.selectedRowCount > 0
+      super.show(c, x, y)
+    }
+  }
+
+  init {
+    add("add").addActionListener {
+      val table = invoker as? JTable
+      val model = table?.model
+      if (model is DefaultTableModel) {
+        model.addRow(arrayOf("New row", model.rowCount, false))
+        val r = table.getCellRect(model.rowCount - 1, 0, true)
+        table.scrollRectToVisible(r)
+      }
+    }
+    add("clearSelection").addActionListener { (invoker as? JTable)?.clearSelection() }
+    addSeparator()
+    delete = add("delete")
+    delete.addActionListener {
+      val table = invoker as? JTable
+      val model = table?.model
+      if (model is DefaultTableModel) {
+        val selection = table.selectedRows
+        for (i in selection.indices.reversed()) {
+          model.removeRow(table.convertRowIndexToModel(selection[i]))
+        }
+      }
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
