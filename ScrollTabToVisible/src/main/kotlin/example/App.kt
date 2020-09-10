4a716bbdb74b89712a2d3a8ba7c9fbdcda54b397
aterai <aterai@outlook.com>
2020-09-10 20:02:49 +0900

    refactor: remove explicit lambda parameter types

--- ScrollTabToVisible/src/main/kotlin/example/App.kt
+++ ScrollTabToVisible/src/main/kotlin/example/App.kt
@@ -2,30 +2,31 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.event.ChangeEvent
-
-private val check = JCheckBox("setSelectedIndex").also {
-  it.horizontalAlignment = SwingConstants.RIGHT
-}
 
 fun makeUI(): Component {
-  val jtp = JTabbedPane()
-  jtp.tabLayoutPolicy = JTabbedPane.SCROLL_TAB_LAYOUT
+  val tabbedPane = JTabbedPane()
+  tabbedPane.tabLayoutPolicy = JTabbedPane.SCROLL_TAB_LAYOUT
   for (i in 0 until 100) {
-    jtp.addTab("title$i", JLabel("label$i"))
+    tabbedPane.addTab("title$i", JLabel("label$i"))
   }
 
-  val slider = JSlider(0, jtp.tabCount - 1, 50)
+  val check = JCheckBox("setSelectedIndex")
+  check.horizontalAlignment = SwingConstants.RIGHT
+
+  val slider = JSlider(0, tabbedPane.tabCount - 1, 50)
   slider.majorTickSpacing = 10
   slider.minorTickSpacing = 5
   slider.paintTicks = true
   slider.paintLabels = true
-  slider.addChangeListener { e: ChangeEvent ->
-    val i = (e.source as JSlider).value
-    if (check.isSelected) {
-      jtp.selectedIndex = i
+  slider.addChangeListener { e ->
+    val s = e.source
+    if (s is JSlider) {
+      val i = s.value
+      if (check.isSelected) {
+        tabbedPane.selectedIndex = i
+      }
+      scrollTabAt(tabbedPane, i)
     }
-    scrollTabAt(jtp, i)
   }
 
   val p = JPanel(BorderLayout())
@@ -35,25 +36,25 @@ fun makeUI(): Component {
 
   return JPanel(BorderLayout()).also {
     it.add(p, BorderLayout.NORTH)
-    it.add(jtp)
+    it.add(tabbedPane)
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun scrollTabAt(tp: JTabbedPane, index: Int) {
+private fun scrollTabAt(tabbedPane: JTabbedPane, index: Int) {
   var cmp: Component? = null
-  for (c in tp.components) {
+  for (c in tabbedPane.components) {
     if ("TabbedPane.scrollableViewport" == c.name) {
       cmp = c
       break
     }
   }
   if (cmp is JViewport) {
-    for (i in 0 until tp.tabCount) {
-      tp.setForegroundAt(i, if (i == index) Color.RED else Color.BLACK)
+    for (i in 0 until tabbedPane.tabCount) {
+      tabbedPane.setForegroundAt(i, if (i == index) Color.RED else Color.BLACK)
     }
-    val d = tp.size
-    val r = tp.getBoundsAt(index)
+    val d = tabbedPane.size
+    val r = tabbedPane.getBoundsAt(index)
     val gw = (d.width - r.width) / 2
     r.grow(gw, 0)
     cmp.scrollRectToVisible(r)
