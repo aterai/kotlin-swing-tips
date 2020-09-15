abd833a9d11b0a76561123a9e1bb9e7bebd3fc47
aterai <aterai@outlook.com>
2020-09-16 01:31:57 +0900

    add: LayeredPane

---
+++ LayeredPane/src/main/kotlin/example/App.kt
@@ -0,0 +1,86 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.InputEvent
+import java.awt.event.KeyEvent
+import java.util.concurrent.atomic.AtomicInteger
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+private val desktop = JDesktopPane()
+
+fun makeUI(): Component {
+  EventQueue.invokeLater { desktop.rootPane.jMenuBar = createMenuBar() }
+  val frame = JInternalFrame("AlwaysOnTop", true, false, true, true)
+  frame.setSize(180, 180)
+  val layer = JLayeredPane.MODAL_LAYER + 1
+  val position = 0
+  desktop.add(frame, layer, position)
+  frame.isVisible = true
+
+  return JPanel(BorderLayout()).also {
+    it.add(desktop)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun createMenuBar(): JMenuBar {
+  val menuBar = JMenuBar()
+  val menu = JMenu("Document")
+  menu.mnemonic = KeyEvent.VK_D
+  menuBar.add(menu)
+  var menuItem = menu.add("New")
+  menuItem.mnemonic = KeyEvent.VK_N
+  menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.ALT_DOWN_MASK)
+  menuItem.actionCommand = "new"
+  menuItem.addActionListener {
+    val frame = MyInternalFrame()
+    desktop.add(frame)
+    frame.isVisible = true
+  }
+  menu.add(menuItem)
+  menuItem = menu.add("Quit")
+  menuItem.mnemonic = KeyEvent.VK_Q
+  menuItem.accelerator = KeyStroke.getKeyStroke(KeyEvent.VK_Q, InputEvent.ALT_DOWN_MASK)
+  menuItem.actionCommand = "quit"
+  menuItem.addActionListener {
+    SwingUtilities.getWindowAncestor(desktop)?.dispose()
+  }
+  menu.add(menuItem)
+  return menuBar
+}
+
+private class MyInternalFrame : JInternalFrame(
+  "Document #%s".format(OPEN_FRAME_COUNT.getAndIncrement()),
+  true,
+  true,
+  true,
+  true
+) {
+  companion object {
+    private const val OFFSET = 30
+    private val OPEN_FRAME_COUNT = AtomicInteger()
+  }
+
+  init {
+    setSize(180, 100)
+    setLocation(OFFSET * OPEN_FRAME_COUNT.toInt(), OFFSET * OPEN_FRAME_COUNT.toInt())
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
