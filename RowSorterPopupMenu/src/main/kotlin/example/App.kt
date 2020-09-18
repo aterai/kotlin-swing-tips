01b7bd64a245d3a6fbb2e63c7ebdbbc2c74c936d
aterai <aterai@outlook.com>
2020-09-18 18:06:02 +0900

    add: RowSorterPopupMenu

---
+++ RowSorterPopupMenu/src/main/kotlin/example/App.kt
@@ -0,0 +1,94 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.JTableHeader
+import javax.swing.table.TableModel
+import javax.swing.table.TableRowSorter
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  table.rowSorter = object : TableRowSorter<TableModel>(model) {
+    override fun toggleSortOrder(column: Int) {
+      /* Disable header click sorting */
+    }
+  }
+  table.rowSorter.sortKeys = listOf(RowSorter.SortKey(1, SortOrder.DESCENDING))
+  table.tableHeader.componentPopupMenu = TableHeaderPopupMenu()
+  table.columnModel.getColumn(0).also {
+    it.minWidth = 80
+    it.maxWidth = 80
+    it.resizable = false
+  }
+
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private class TableHeaderPopupMenu : JPopupMenu() {
+  private val actions = listOf(
+    SortAction(SortOrder.ASCENDING),
+    SortAction(SortOrder.DESCENDING)
+  )
+
+  private inner class SortAction(private val dir: SortOrder) : AbstractAction(dir.toString()) {
+    private var index = -1
+    fun setIndex(index: Int) {
+      this.index = index
+    }
+
+    override fun actionPerformed(e: ActionEvent) {
+      (invoker as? JTableHeader)?.table?.rowSorter?.sortKeys = listOf(RowSorter.SortKey(index, dir))
+    }
+  }
+
+  init {
+    actions.forEach { this.add(it) }
+  }
+
+  override fun show(c: Component, x: Int, y: Int) {
+    if (c is JTableHeader) {
+      val table = c.table
+      c.draggedColumn = null
+      c.repaint()
+      table.repaint()
+      val i = table.convertColumnIndexToModel(c.columnAtPoint(Point(x, y)))
+      if (i >= 0) {
+        actions.forEach { it.setIndex(i) }
+        super.show(c, x, y)
+      }
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
