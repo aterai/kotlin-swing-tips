208ca32f11de5585faafebeaa31b0ea8d67418ad
aterai <aterai@outlook.com>
2020-09-28 00:45:41 +0900

    add: PasswordFieldCutCopyAllowed

---
+++ PasswordFieldCutCopyAllowed/src/main/kotlin/example/App.kt
@@ -0,0 +1,49 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val password = JPasswordField()
+  password.putClientProperty("JPasswordField.cutCopyAllowed", true)
+
+  val p = JPanel(GridLayout(2, 1))
+  p.add(makeTitledPanel("JPasswordField.cutCopyAllowed: false(default)", JPasswordField()))
+  p.add(makeTitledPanel("JPasswordField.cutCopyAllowed: true", password))
+
+  return JPanel(BorderLayout(10, 10)).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(JTextArea()))
+    it.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTitledPanel(title: String, cmp: Component): Component {
+  val p = JPanel(GridBagLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  val c = GridBagConstraints()
+  c.weightx = 1.0
+  c.fill = GridBagConstraints.HORIZONTAL
+  c.insets = Insets(5, 5, 5, 5)
+  p.add(cmp, c)
+  return p
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
