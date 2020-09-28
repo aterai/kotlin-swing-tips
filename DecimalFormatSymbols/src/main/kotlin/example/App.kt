8c9d966c99173f4acbf055021928b1b66941b796
aterai <aterai@outlook.com>
2020-09-29 02:39:49 +0900

    add: DecimalFormatSymbols

--- DecimalFormatSymbols/src/main/kotlin/example/App.kt
+++ DecimalFormatSymbols/src/main/kotlin/example/App.kt
@@ -1,63 +1,100 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.FocusAdapter
-import java.awt.event.FocusEvent
-import java.util.Calendar
-import java.util.Date
+import java.awt.event.ActionEvent
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.JSpinner.DateEditor
+import javax.swing.JSpinner.DefaultEditor
+import javax.swing.text.DefaultFormatterFactory
+import javax.swing.text.NumberFormatter
 
 fun makeUI(): Component {
-  val dateFormat = "yyyy/MM/dd"
-  val date = Date()
-  val spinner1 = JSpinner(SpinnerDateModel(date, date, null, Calendar.DAY_OF_MONTH))
-  spinner1.editor = DateEditor(spinner1, dateFormat)
+  val s0 = JSpinner(makeSpinnerNumberModel())
+  val s1 = makeSpinner1(makeSpinnerNumberModel())
+  val s2 = makeSpinner2(makeSpinnerNumberModel())
+  val s3 = makeSpinner3(makeSpinnerNumberModel())
 
-  val today = Calendar.getInstance()
-  today.clear(Calendar.MILLISECOND)
-  today.clear(Calendar.SECOND)
-  today.clear(Calendar.MINUTE)
-  today[Calendar.HOUR_OF_DAY] = 0
-
-  val start = today.time
-  println(date)
-  println(start)
-
-  val spinner2 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  spinner2.editor = DateEditor(spinner2, dateFormat)
-
-  val spinner3 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  val editor = DateEditor(spinner3, dateFormat)
-  spinner3.editor = editor
-  val fl3 = object : FocusAdapter() {
-    override fun focusGained(e: FocusEvent) {
-      EventQueue.invokeLater {
-        val i = dateFormat.lastIndexOf("dd")
-        editor.textField.select(i, i + 2)
+  val act = object : AbstractAction("setEnabled") {
+    @Transient
+    private var old: Any? = null
+    override fun actionPerformed(e: ActionEvent) {
+      val flg = (e.source as? JCheckBox)?.isSelected == true
+      listOf(s0, s1, s2, s3).forEach { it.isEnabled = flg }
+      if (flg) {
+        listOf(s2, s3).forEach { it.value = old }
+      } else {
+        old = s2.value
+        listOf(s2, s3).forEach { it.value = Double.NaN }
       }
     }
   }
-  editor.textField.addFocusListener(fl3)
 
-  return JPanel(GridLayout(3, 1)).also {
-    it.add(makeTitledPanel("Calendar.DAY_OF_MONTH", spinner1))
-    it.add(makeTitledPanel("min: set(Calendar.HOUR_OF_DAY, 0)", spinner2))
-    it.add(makeTitledPanel("JSpinner.DateEditor + FocusListener", spinner3))
-    it.border = BorderFactory.createEmptyBorder(10, 5, 10, 5)
+  val cbx = JCheckBox(act)
+  cbx.isSelected = true
+
+  val box = Box.createVerticalBox()
+  box.border = BorderFactory.createEmptyBorder(5, 15, 5, 5)
+  listOf(s0, s1, s2, s3).forEach { c ->
+    c.isEnabled = true
+    c.alignmentX = Component.LEFT_ALIGNMENT
+    val d = c.preferredSize
+    d.width = Int.MAX_VALUE
+    c.maximumSize = d
+    box.add(c)
+    box.add(Box.createVerticalStrut(5))
+  }
+  box.add(Box.createVerticalGlue())
+
+  return JPanel(BorderLayout()).also {
+    it.add(cbx, BorderLayout.NORTH)
+    it.add(box)
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun makeTitledPanel(title: String, cmp: Component): Component {
-  val p = JPanel(GridBagLayout())
-  p.border = BorderFactory.createTitledBorder(title)
-  val c = GridBagConstraints()
-  c.weightx = 1.0
-  c.fill = GridBagConstraints.HORIZONTAL
-  c.insets = Insets(5, 5, 5, 5)
-  p.add(cmp, c)
-  return p
+private fun makeSpinnerNumberModel() = SpinnerNumberModel(
+  java.lang.Double.valueOf(10.0),
+  java.lang.Double.valueOf(0.0),
+  java.lang.Double.valueOf(100.0),
+  java.lang.Double.valueOf(1.0)
+)
+
+private fun makeSpinner1(m: SpinnerNumberModel) = JSpinner(m).also {
+  getJFormattedTextField(it)?.also { ftf ->
+    val dfs = DecimalFormatSymbols()
+    ftf.formatterFactory = makeFFactory(dfs)
+    ftf.disabledTextColor = UIManager.getColor("TextField.disabledColor")
+  }
+}
+
+private fun makeSpinner2(m: SpinnerNumberModel) = JSpinner(m).also {
+  getJFormattedTextField(it)?.also { ftf ->
+    val dfs = DecimalFormatSymbols()
+    dfs.naN = " "
+    ftf.formatterFactory = makeFFactory(dfs)
+  }
+}
+
+private fun makeSpinner3(m: SpinnerNumberModel) = JSpinner(m).also {
+  getJFormattedTextField(it)?.also { ftf ->
+    val dfs = DecimalFormatSymbols()
+    dfs.naN = "----"
+    ftf.formatterFactory = makeFFactory(dfs)
+  }
+}
+
+private fun getJFormattedTextField(s: JSpinner) = (s.editor as? DefaultEditor)?.let {
+  it.textField.also { ftf -> ftf.columns = 8 }
+}
+
+private fun makeFFactory(dfs: DecimalFormatSymbols): DefaultFormatterFactory {
+  val format = DecimalFormat("0.00", dfs)
+  val displayFormatter = NumberFormatter(format)
+  displayFormatter.valueClass = Double::class.java
+  val editFormatter = NumberFormatter(format)
+  editFormatter.valueClass = Double::class.java
+  return DefaultFormatterFactory(displayFormatter, displayFormatter, editFormatter)
 }
 
 fun main() {
