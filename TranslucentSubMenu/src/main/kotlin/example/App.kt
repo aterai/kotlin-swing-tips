58b5b9e64073fb89dd82a9dbce0f1e1a2d79ed95
aterai <aterai@outlook.com>
2020-10-16 19:12:44 +0900

    add: TranslucentSubMenu

---
+++ TranslucentSubMenu/src/main/kotlin/example/App.kt
@@ -0,0 +1,217 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.BorderUIResource
+
+fun makeUI(): Component {
+  val tree = JTree()
+  tree.componentPopupMenu = makePopupMenu()
+
+  return JPanel(BorderLayout()).also {
+    EventQueue.invokeLater { it.rootPane.jMenuBar = createMenuBar() }
+    it.add(JScrollPane(tree))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun createMenuBar(): JMenuBar {
+  val mb = JMenuBar()
+  val menuKeys = arrayOf("File", "Edit", "Help")
+  for (key in menuKeys) {
+    val m = createMenu(key)
+    mb.add(m)
+  }
+  return mb
+}
+
+private fun makePopupMenu(): JPopupMenu {
+  val menu: JMenu = TransparentMenu("Test")
+  menu.add("Undo")
+  menu.add("Redo")
+  val popup: JPopupMenu = TranslucentPopupMenu()
+  popup.add(menu)
+  popup.addSeparator()
+  popup.add("Cut")
+  popup.add("Copy")
+  popup.add("Paste")
+  popup.add("Delete")
+  return popup
+}
+
+private fun createMenu(key: String): JMenu {
+  val menu: JMenu = TransparentMenu(key)
+  menu.isOpaque = false // Motif lnf
+  val sub: JMenu = TransparentMenu("Submenu")
+  sub.add("JMenuItem")
+  sub.add("Looooooooooooooooooooong")
+  menu.add(sub)
+  menu.add("dummy1")
+  menu.add("dummy2")
+  return menu
+}
+
+private class TranslucentPopupMenu : JPopupMenu() {
+  override fun isOpaque() = false
+
+  override fun updateUI() {
+    super.updateUI()
+    if (UIManager.getBorder("PopupMenu.border") == null) {
+      border = BorderUIResource(BorderFactory.createLineBorder(Color.GRAY))
+    }
+  }
+
+  override fun add(c: Component): Component {
+    (c as? JComponent)?.isOpaque = false
+    return c
+  }
+
+  override fun add(menuItem: JMenuItem): JMenuItem {
+    menuItem.isOpaque = false
+    return super.add(menuItem)
+  }
+
+  override fun show(c: Component, x: Int, y: Int) {
+    EventQueue.invokeLater {
+      val p = topLevelAncestor
+      if (p is JWindow) {
+        println("Heavy weight")
+        p.setBackground(ALPHA_ZERO)
+      } else {
+        println("Light weight")
+      }
+    }
+    super.show(c, x, y)
+  }
+
+  override fun paintComponent(g: Graphics) {
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.paint = POPUP_LEFT
+    g2.fillRect(0, 0, LEFT_WIDTH, height)
+    g2.paint = POPUP_BACK
+    g2.fillRect(LEFT_WIDTH, 0, width, height)
+    g2.dispose()
+  }
+
+  companion object {
+    private val ALPHA_ZERO = Color(0x0, true)
+    private val POPUP_BACK: Paint = Color(250, 250, 250, 200)
+    private val POPUP_LEFT: Paint = Color(230, 230, 230, 200)
+    private const val LEFT_WIDTH = 24
+  }
+}
+
+private class TransparentMenu(title: String?) : JMenu(title) {
+  private var popupMenu: JPopupMenu? = null
+
+  private fun ensurePopupMenuCreated2(): JPopupMenu {
+    val popup = popupMenu ?: TranslucentPopupMenu().also {
+      it.invoker = this
+    }
+    popupListener = createWinListener(popup)
+    popupMenu = popup
+    return popup
+  }
+
+  override fun getPopupMenu() = popupMenu ?: ensurePopupMenuCreated2()
+
+  override fun add(menuItem: JMenuItem): JMenuItem {
+    menuItem.isOpaque = false
+    val popup = ensurePopupMenuCreated2()
+    return popup.add(menuItem)
+  }
+
+  override fun add(c: Component): Component {
+    (c as? JComponent)?.isOpaque = false
+    val popup = ensurePopupMenuCreated2()
+    popup.add(c)
+    return c
+  }
+
+  override fun addSeparator() {
+    ensurePopupMenuCreated2().addSeparator()
+  }
+
+  private fun checkIndex(pos: Int) {
+    require(pos >= 0) { "index less than zero." }
+  }
+
+  override fun insert(s: String, pos: Int) {
+    checkIndex(pos)
+    val popup = ensurePopupMenuCreated2()
+    popup.insert(JMenuItem(s), pos)
+  }
+
+  override fun insert(mi: JMenuItem, pos: Int): JMenuItem {
+    checkIndex(pos)
+    val popup = ensurePopupMenuCreated2()
+    popup.insert(mi, pos)
+    return mi
+  }
+
+  override fun insertSeparator(index: Int) {
+    checkIndex(index)
+    val popup = ensurePopupMenuCreated2()
+    popup.insert(JPopupMenu.Separator(), index)
+  }
+
+  override fun isPopupMenuVisible(): Boolean {
+    val popup = ensurePopupMenuCreated2()
+    return popup.isVisible
+  }
+}
+
+private class TranslucentPopupFactory : PopupFactory() {
+  override fun getPopup(
+    owner: Component,
+    contents: Component,
+    x: Int,
+    y: Int
+  ) = TranslucentPopup(owner, contents, x, y)
+}
+
+private class TranslucentPopup(
+  owner: Component?,
+  contents: Component,
+  ownerX: Int,
+  ownerY: Int
+) : Popup(owner, contents, ownerX, ownerY) {
+  private val popupWindow = JWindow()
+
+  init {
+    popupWindow.background = Color(0x0, true)
+    popupWindow.setLocation(ownerX, ownerY)
+    popupWindow.contentPane.add(contents)
+    contents.invalidate()
+  }
+
+  override fun show() {
+    popupWindow.isVisible = true
+    popupWindow.pack()
+  }
+
+  override fun hide() {
+    popupWindow.isVisible = false
+    popupWindow.removeAll()
+    popupWindow.dispose()
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+      PopupFactory.setSharedInstance(TranslucentPopupFactory())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
