0b1d2c5eb4a8a0a73130b6534f6920bd236dc74b
aterai <aterai@outlook.com>
2020-10-31 16:43:24 +0900

    add: FunctionKeyStartEditing

---
+++ FunctionKeyStartEditing/src/main/kotlin/example/App.kt
@@ -0,0 +1,77 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
+import java.awt.event.KeyEvent
+import java.util.EventObject
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+
+fun makeUI(): Component {
+  val check = JCheckBox("ignore: F1,F4-F7,F9-", true)
+
+  val textarea = JTextArea("F2: startEditing\nF8: focusHeader\nF3: beep")
+  textarea.isEditable = false
+
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = object : JTable(model) {
+    override fun editCellAt(row: Int, column: Int, e: EventObject) =
+      if (check.isSelected && e is KeyEvent && checkIgnoreKeyCode(e.keyCode)) {
+        false
+      } else {
+        super.editCellAt(row, column, e)
+      }
+  }
+  table.autoCreateRowSorter = true
+
+  val act = object : AbstractAction() {
+    override fun actionPerformed(e: ActionEvent) {
+      // println("F3")
+      Toolkit.getDefaultToolkit().beep()
+    }
+  }
+  table.actionMap.put("beep", act)
+
+  val im = table.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
+  im.put(KeyStroke.getKeyStroke(KeyEvent.VK_F3, 0), "beep")
+
+  val p = JPanel(BorderLayout())
+  p.add(check, BorderLayout.NORTH)
+  p.add(JScrollPane(textarea))
+
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun checkIgnoreKeyCode(code: Int) = KeyEvent.VK_F1 <= code && code <= KeyEvent.VK_F21
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
