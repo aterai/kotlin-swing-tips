10de2d8b61dc4db0a0efecefba5aa2af6dbe38ce
aterai <aterai@outlook.com>
2020-11-09 03:38:41 +0900

    add: HeaderFont

---
+++ HeaderFont/src/main/kotlin/example/App.kt
@@ -0,0 +1,52 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableCellRenderer
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("CCC", 92, true),
+    arrayOf("DDD", 0, false)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
+  val tableColumn = table.columnModel.getColumn(0)
+  val font = Font(Font.SANS_SERIF, Font.PLAIN, 32)
+  tableColumn.headerRenderer =
+    TableCellRenderer { t, v, selected, focus, row, col ->
+      t.tableHeader.defaultRenderer.getTableCellRendererComponent(t, v, selected, focus, row, col)?.also {
+        it.font = font
+      }
+    }
+
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
