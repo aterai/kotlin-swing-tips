e417ba4d8164072f584b70195fe5f377d08c9b68
aterai <aterai@outlook.com>
2020-11-19 17:47:52 +0900

    add: TabbedPaneWithText

---
+++ TabbedPaneWithText/src/main/kotlin/example/App.kt
@@ -0,0 +1,46 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+const val TEXT = "<--1234567890"
+
+fun makeUI(): Component {
+  val tabs = object : JTabbedPane() {
+    override fun paintComponent(g: Graphics) {
+      super.paintComponent(g)
+      val fm = g.fontMetrics
+      val stringWidth = fm.stringWidth(TEXT) + 10
+      val x = size.width - stringWidth
+      val lastTab = getBoundsAt(tabCount - 1)
+      val tabEnd = lastTab.x + lastTab.width
+      val xx = x.coerceAtLeast(tabEnd) + 5
+      g.drawString(TEXT, xx, 18)
+    }
+  }
+  tabs.addTab("title1", JLabel("tab1"))
+  tabs.addTab("title2", JLabel("tab2"))
+
+  return JPanel(BorderLayout()).also {
+    it.add(tabs)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
