7afc96d0ff062a8ad90b087b2d0dd6347feb5ae4
aterai <aterai@outlook.com>
2020-11-22 16:19:29 +0900

    add: SystemIcon

---
+++ SystemIcon/src/main/kotlin/example/App.kt
@@ -0,0 +1,97 @@
+package example
+
+import sun.awt.shell.ShellFolder
+import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.datatransfer.DataFlavor
+import java.awt.dnd.*
+import java.io.File
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.filechooser.FileSystemView
+
+fun makeUI(): Component {
+  val smallLabel = object : JLabel() {
+    override fun getPreferredSize() = Dimension(16 + 1, 16 + 1)
+
+    override fun getMaximumSize() = preferredSize
+
+    override fun updateUI() {
+      super.updateUI()
+      border = BorderFactory.createLineBorder(Color.GRAY, 1)
+      alignmentY = BOTTOM_ALIGNMENT
+    }
+  }
+  val largeLabel = object : JLabel() {
+    override fun getPreferredSize() = Dimension(32 + 1, 32 + 1)
+
+    override fun getMaximumSize() = preferredSize
+
+    override fun updateUI() {
+      super.updateUI()
+      border = BorderFactory.createLineBorder(Color.GRAY, 1)
+      alignmentY = BOTTOM_ALIGNMENT
+    }
+  }
+
+  val box = Box.createHorizontalBox()
+  box.border = BorderFactory.createTitledBorder("drop File")
+  box.add(smallLabel)
+  box.add(Box.createHorizontalStrut(5))
+  box.add(largeLabel)
+
+  val dtl = object : DropTargetAdapter() {
+    override fun dragOver(e: DropTargetDragEvent) {
+      if (e.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+        e.acceptDrag(DnDConstants.ACTION_COPY)
+      } else {
+        e.rejectDrag()
+      }
+    }
+
+    override fun drop(e: DropTargetDropEvent) {
+      runCatching {
+        if (e.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+          e.acceptDrop(DnDConstants.ACTION_COPY)
+          (e.transferable.getTransferData(DataFlavor.javaFileListFlavor) as? List<*>)?.also {
+            val file = it[0]
+            if (file is File) {
+              smallLabel.icon = FileSystemView.getFileSystemView().getSystemIcon(file)
+              largeLabel.icon = ImageIcon(ShellFolder.getShellFolder(file).getIcon(true))
+            }
+            e.dropComplete(true)
+          } ?: e.rejectDrop()
+        } else {
+          e.rejectDrop()
+        }
+      }.onFailure {
+        e.rejectDrop()
+      }
+    }
+  }
+
+  return JPanel().also {
+    it.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
+    it.add(box)
+    val msg = "<html>warning: ShellFolder is internal proprietary API<br>and may be removed in a future release"
+    it.add(JLabel(msg))
+    it.dropTarget = DropTarget(it, DnDConstants.ACTION_COPY, dtl, true)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
