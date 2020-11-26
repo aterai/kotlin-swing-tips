2e7a62b52650f853faad5bc27c47ea6dfcae3885
aterai <aterai@outlook.com>
2020-11-27 01:39:03 +0900

    add: NonEditableLine

---
+++ NonEditableLine/src/main/kotlin/example/App.kt
@@ -0,0 +1,69 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.text.AbstractDocument
+import javax.swing.text.AttributeSet
+import javax.swing.text.BadLocationException
+import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter
+import javax.swing.text.DocumentFilter
+import javax.swing.text.Highlighter.HighlightPainter
+
+fun makeUI(): Component {
+  val maskRange = 2
+  val highlightPainter: HighlightPainter = DefaultHighlightPainter(Color.GRAY)
+  val textArea = JTextArea()
+  textArea.text = "1234567890987654321\naaa bbb ccc ddd eee\n1234567890\n1234567890987654321"
+  (textArea.document as? AbstractDocument)?.documentFilter = NonEditableLineDocumentFilter(maskRange)
+  runCatching {
+    val highlighter = textArea.highlighter
+    val root = textArea.document.defaultRootElement
+    for (i in 0 until maskRange) {
+      val elem = root.getElement(i)
+      highlighter.addHighlight(elem.startOffset, elem.endOffset - 1, highlightPainter)
+    }
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(textArea))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private class NonEditableLineDocumentFilter(private val maskRange: Int) : DocumentFilter() {
+  @Throws(BadLocationException::class)
+  override fun insertString(fb: FilterBypass, offset: Int, text: String?, attr: AttributeSet?) {
+    if (text != null) {
+      replace(fb, offset, 0, text, attr)
+    }
+  }
+
+  @Throws(BadLocationException::class)
+  override fun remove(fb: FilterBypass, offset: Int, length: Int) {
+    replace(fb, offset, length, "", null)
+  }
+
+  @Throws(BadLocationException::class)
+  override fun replace(fb: FilterBypass, offset: Int, length: Int, text: String, attrs: AttributeSet?) {
+    if (fb.document.defaultRootElement.getElementIndex(offset) >= maskRange) {
+      fb.replace(offset, length, text, attrs)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
