17da6fb271ead5264f5a4a5a5b1849cf16ef8c18
aterai <aterai@outlook.com>
2020-12-28 04:36:33 +0900

    add: IconScrollBar

--- IconScrollBar/src/main/kotlin/example/App.kt
+++ IconScrollBar/src/main/kotlin/example/App.kt
@@ -1,63 +1,98 @@
 package example
 
+import com.sun.java.swing.plaf.windows.WindowsScrollBarUI
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.FocusAdapter
-import java.awt.event.FocusEvent
-import java.util.Calendar
-import java.util.Date
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.JSpinner.DateEditor
+import javax.swing.plaf.basic.BasicScrollBarUI
 
 fun makeUI(): Component {
-  val dateFormat = "yyyy/MM/dd"
-  val date = Date()
-  val spinner1 = JSpinner(SpinnerDateModel(date, date, null, Calendar.DAY_OF_MONTH))
-  spinner1.editor = DateEditor(spinner1, dateFormat)
-
-  val today = Calendar.getInstance()
-  today.clear(Calendar.MILLISECOND)
-  today.clear(Calendar.SECOND)
-  today.clear(Calendar.MINUTE)
-  today[Calendar.HOUR_OF_DAY] = 0
-
-  val start = today.time
-  println(date)
-  println(start)
-
-  val spinner2 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  spinner2.editor = DateEditor(spinner2, dateFormat)
-
-  val spinner3 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  val editor = DateEditor(spinner3, dateFormat)
-  spinner3.editor = editor
-  val fl3 = object : FocusAdapter() {
-    override fun focusGained(e: FocusEvent) {
-      EventQueue.invokeLater {
-        val i = dateFormat.lastIndexOf("dd")
-        editor.textField.select(i, i + 2)
-      }
-    }
+  val l = object : JLabel("1234567890") {
+    override fun getPreferredSize() = Dimension(1000, 1000)
   }
-  editor.textField.addFocusListener(fl3)
-
-  return JPanel(GridLayout(3, 1)).also {
-    it.add(makeTitledPanel("Calendar.DAY_OF_MONTH", spinner1))
-    it.add(makeTitledPanel("min: set(Calendar.HOUR_OF_DAY, 0)", spinner2))
-    it.add(makeTitledPanel("JSpinner.DateEditor + FocusListener", spinner3))
-    it.border = BorderFactory.createEmptyBorder(10, 5, 10, 5)
+  val scrollPane = JScrollPane(l)
+  if (scrollPane.verticalScrollBar.ui is WindowsScrollBarUI) {
+    scrollPane.verticalScrollBar.ui = BasicIconScrollBarUI()
+  } else {
+    scrollPane.verticalScrollBar.ui = WindowsIconScrollBarUI()
+  }
+  return JPanel(BorderLayout()).also {
+    it.add(scrollPane)
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun makeTitledPanel(title: String, cmp: Component): Component {
-  val p = JPanel(GridBagLayout())
-  p.border = BorderFactory.createTitledBorder(title)
-  val c = GridBagConstraints()
-  c.weightx = 1.0
-  c.fill = GridBagConstraints.HORIZONTAL
-  c.insets = Insets(5, 5, 5, 5)
-  p.add(cmp, c)
-  return p
+private class WindowsIconScrollBarUI : WindowsScrollBarUI() {
+  override fun paintThumb(g: Graphics, c: JComponent, thumbBounds: Rectangle) {
+    super.paintThumb(g, c, thumbBounds)
+    val sb = c as? JScrollBar
+    if (sb?.isEnabled != true || thumbBounds.width > thumbBounds.height) {
+      return
+    }
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    val oc: Color
+    val ic: Color
+    when {
+      isDragging -> {
+        oc = SystemColor.activeCaption.darker()
+        ic = SystemColor.inactiveCaptionText.darker()
+      }
+      isThumbRollover -> {
+        oc = SystemColor.activeCaption.brighter()
+        ic = SystemColor.inactiveCaptionText.brighter()
+      }
+      else -> {
+        oc = SystemColor.activeCaption
+        ic = SystemColor.inactiveCaptionText
+      }
+    }
+    paintCircle(g2, thumbBounds, 6, oc)
+    paintCircle(g2, thumbBounds, 10, ic)
+    g2.dispose()
+  }
+
+  private fun paintCircle(g2: Graphics2D, thumbBounds: Rectangle, w: Int, color: Color) {
+    g2.paint = color
+    val ww = thumbBounds.width - w
+    g2.fillOval(thumbBounds.x + w / 2, thumbBounds.y + (thumbBounds.height - ww) / 2, ww, ww)
+  }
+}
+
+private class BasicIconScrollBarUI : BasicScrollBarUI() {
+  override fun paintThumb(g: Graphics, c: JComponent, thumbBounds: Rectangle) {
+    super.paintThumb(g, c, thumbBounds)
+    val sb = c as? JScrollBar
+    if (sb?.isEnabled != true || thumbBounds.width > thumbBounds.height) {
+      return
+    }
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    val oc: Color
+    val ic: Color
+    when {
+      isDragging -> {
+        oc = SystemColor.activeCaption.darker()
+        ic = SystemColor.inactiveCaptionText.darker()
+      }
+      isThumbRollover -> {
+        oc = SystemColor.activeCaption.brighter()
+        ic = SystemColor.inactiveCaptionText.brighter()
+      }
+      else -> {
+        oc = SystemColor.activeCaption
+        ic = SystemColor.inactiveCaptionText
+      }
+    }
+    paintCircle(g2, thumbBounds, 6, oc)
+    paintCircle(g2, thumbBounds, 10, ic)
+    g2.dispose()
+  }
+
+  private fun paintCircle(g2: Graphics2D, thumbBounds: Rectangle, w: Int, color: Color) {
+    g2.paint = color
+    val ww = thumbBounds.width - w
+    g2.fillOval(thumbBounds.x + w / 2, thumbBounds.y + (thumbBounds.height - ww) / 2, ww, ww)
+  }
 }
 
 fun main() {
