6f0e5cb1b355b1f9eddf56c6464d92398f0d63fd
aterai <aterai@outlook.com>
2021-03-21 02:18:17 +0900

    chore: detekt.yml

--- CheckBoxNodeEditor/src/main/kotlin/example/CheckBoxNode.kt
+++ CheckBoxNodeEditor/src/main/kotlin/example/CheckBoxNode.kt
@@ -1,5 +1,5 @@
 package example
 
-protected data class CheckBoxNode(val label: String = "", val status: Status = Status.INDETERMINATE) {
+data class CheckBoxNode(val label: String = "", val status: Status = Status.INDETERMINATE) {
   override fun toString() = label
 }
