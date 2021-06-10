2d3a9bf4c639006da626e78c469511799e0d013f
aterai <aterai@outlook.com>
2021-06-11 00:50:14 +0900

    refactor: if the image does not exist, MissingIcon is used instead

--- ZoomingAndPanning/src/main/kotlin/example/App.kt
+++ ZoomingAndPanning/src/main/kotlin/example/App.kt
@@ -5,11 +5,15 @@ import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.event.MouseWheelEvent
 import java.awt.geom.AffineTransform
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-  val cl = Thread.currentThread().contextClassLoader
-  val icon = ImageIcon(cl.getResource("example/CRW_3857_JFR.jpg"))
+  val path = "example/CRW_3857_JFR.jpg"
+  val url = Thread.currentThread().contextClassLoader.getResource(path)
+  val img = url?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
+  val icon = ImageIcon(img)
   val p = object : JPanel(BorderLayout()) {
     var zoomAndPanHandler: ZoomAndPanHandler? = null
     override fun updateUI() {
@@ -28,7 +32,7 @@ fun makeUI(): Component {
     override fun paintComponent(g: Graphics) {
       super.paintComponent(g)
       val g2 = g.create() as? Graphics2D ?: return
-      g2.transform = zoomAndPanHandler?.coordAndZoomTransform
+      g2.transform = zoomAndPanHandler?.coordAndZoomAtf
       icon.paintIcon(this, g2, 0, 0)
       g2.dispose()
     }
@@ -37,9 +41,20 @@ fun makeUI(): Component {
   return p
 }
 
+private fun makeMissingImage(): Image {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
 private class ZoomAndPanHandler : MouseAdapter() {
   private val zoomRange = DefaultBoundedRangeModel(0, EXTENT, MIN_ZOOM, MAX_ZOOM + EXTENT)
-  val coordAndZoomTransform = AffineTransform()
+  val coordAndZoomAtf = AffineTransform()
   private val dragStartPoint = Point()
 
   override fun mousePressed(e: MouseEvent) {
@@ -50,7 +65,7 @@ private class ZoomAndPanHandler : MouseAdapter() {
     val dragEndPoint = e.point
     val dragStart = transformPoint(dragStartPoint)
     val dragEnd = transformPoint(dragEndPoint)
-    coordAndZoomTransform.translate(dragEnd.getX() - dragStart.getX(), dragEnd.getY() - dragStart.getY())
+    coordAndZoomAtf.translate(dragEnd.getX() - dragStart.getX(), dragEnd.getY() - dragStart.getY())
     dragStartPoint.location = dragEndPoint
     e.component.repaint()
   }
@@ -68,17 +83,17 @@ private class ZoomAndPanHandler : MouseAdapter() {
     val p = Point(r.x + r.width / 2, r.y + r.height / 2)
     val p1 = transformPoint(p)
     val scale = if (dir > 0) 1 / ZOOM_FACTOR else ZOOM_FACTOR
-    coordAndZoomTransform.scale(scale, scale)
+    coordAndZoomAtf.scale(scale, scale)
     val p2 = transformPoint(p)
-    coordAndZoomTransform.translate(p2.getX() - p1.getX(), p2.getY() - p1.getY())
+    coordAndZoomAtf.translate(p2.getX() - p1.getX(), p2.getY() - p1.getY())
     c.repaint()
   }
 
   // https://community.oracle.com/thread/1263955
   // How to implement Zoom & Pan in Java using Graphics2D
   private fun transformPoint(p1: Point): Point {
-    val inverse = runCatching { coordAndZoomTransform.createInverse() }
-      .getOrNull() ?: coordAndZoomTransform
+    val inverse = runCatching { coordAndZoomAtf.createInverse() }
+        .getOrNull() ?: coordAndZoomAtf
     val p2 = Point()
     inverse.transform(p1, p2)
     return p2
@@ -90,6 +105,31 @@ private class ZoomAndPanHandler : MouseAdapter() {
     private const val MAX_ZOOM = 10
     private const val EXTENT = 1
   }
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(
+      c: Component?,
+      g: Graphics,
+      x: Int,
+      y: Int
+  ) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.paint = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.paint = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 1000
+
+  override fun getIconHeight() = 1000
 }
 
 fun main() {
