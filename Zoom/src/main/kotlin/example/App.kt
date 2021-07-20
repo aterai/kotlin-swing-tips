6a49379521b2692ad2e1f053bc0307a29d2b0694
aterai <aterai@outlook.com>
2021-07-21 07:06:57 +0900

    refactor: argument 'getClass().getResource(...)' might be null

--- Zoom/src/main/kotlin/example/App.kt
+++ Zoom/src/main/kotlin/example/App.kt
@@ -2,12 +2,15 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.event.MouseWheelListener
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
+  val path = "example/test.png"
   val cl = Thread.currentThread().contextClassLoader
-  val icon = ImageIcon(cl.getResource("example/test.png"))
-  val zoom = ZoomImage(icon.image)
+  val img = cl.getResource(path)?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
+  val zoom = ZoomImage(img)
 
   val button1 = JButton("Zoom In")
   button1.addActionListener { zoom.changeScale(-5) }
@@ -18,11 +21,12 @@ fun makeUI(): Component {
   val button3 = JButton("Original size")
   button3.addActionListener { zoom.initScale() }
 
-  val box = Box.createHorizontalBox()
-  box.add(Box.createHorizontalGlue())
-  box.add(button1)
-  box.add(button2)
-  box.add(button3)
+  val box = Box.createHorizontalBox().also {
+    it.add(Box.createHorizontalGlue())
+    it.add(button1)
+    it.add(button2)
+    it.add(button3)
+  }
 
   return JPanel(BorderLayout()).also {
     it.add(zoom)
@@ -31,8 +35,38 @@ fun makeUI(): Component {
   }
 }
 
-private class ZoomImage(@field:Transient private val image: Image) : JPanel() {
-  @Transient
+private fun makeMissingImage(): Image {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.color = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.color = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 320
+
+  override fun getIconHeight() = 240
+}
+
+private class ZoomImage(private val image: Image) : JPanel() {
   private var handler: MouseWheelListener? = null
   private val iw = image.getWidth(this)
   private val ih = image.getHeight(this)
@@ -41,7 +75,7 @@ private class ZoomImage(@field:Transient private val image: Image) : JPanel() {
   override fun updateUI() {
     removeMouseWheelListener(handler)
     super.updateUI()
-    handler = MouseWheelListener { e -> changeScale(e.wheelRotation) }
+    handler = MouseWheelListener { changeScale(it.wheelRotation) }
     addMouseWheelListener(handler)
   }
 
