d64f58da3da5a17ed5b02f48f042abdbb798c468
aterai <aterai@outlook.com>
2021-07-27 02:45:38 +0900

    refactor: argument 'getClass().getResource(...)' might be null

--- XORMode/src/main/kotlin/example/App.kt
+++ XORMode/src/main/kotlin/example/App.kt
@@ -4,18 +4,32 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.geom.Path2D
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-  val cl = Thread.currentThread().contextClassLoader
-  val icon = ImageIcon(cl.getResource("example/test.png"))
+  val path = "example/test.png"
+  val url = Thread.currentThread().contextClassLoader.getResource(path)
+  val img = url?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
   return JPanel(BorderLayout()).also {
-    it.add(ImageIconPanel(icon))
+    it.add(ImagePanel(img))
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private class ImageIconPanel(private val icon: ImageIcon) : JPanel() {
+private fun makeMissingImage(): Image {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class ImagePanel(private val image: Image) : JPanel() {
   private var rbl: RubberBandingListener? = null
   private val stroke = BasicStroke(2f)
   private val rubberBand: Path2D = Path2D.Double()
@@ -32,12 +46,12 @@ private class ImageIconPanel(private val icon: ImageIcon) : JPanel() {
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
     val g2 = g.create() as? Graphics2D ?: return
-    val iw = icon.iconWidth
-    val ih = icon.iconHeight
+    val iw = image.getWidth(this)
+    val ih = image.getHeight(this)
     val dim = size
     val x = (dim.width - iw) / 2
     val y = (dim.height - ih) / 2
-    g.drawImage(icon.image, x, y, iw, ih, this)
+    g.drawImage(image, x, y, iw, ih, this)
 
     g2.paint = Color.RED
     g2.fillOval(10, 10, 32, 32)
@@ -91,6 +105,26 @@ private class ImageIconPanel(private val icon: ImageIcon) : JPanel() {
       e.component.repaint()
     }
   }
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.paint = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.paint = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 320
+
+  override fun getIconHeight() = 240
 }
 
 fun main() {
