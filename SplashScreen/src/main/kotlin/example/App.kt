ee20ff9113551f53ebb34278adbec6a6aff841d6
aterai <aterai@outlook.com>
2021-08-18 12:26:34 +0900

    refactor: argument 'getClass().getResource(...)' might be null

--- SplashScreen/src/main/kotlin/example/App.kt
+++ SplashScreen/src/main/kotlin/example/App.kt
@@ -1,6 +1,8 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
@@ -11,6 +13,37 @@ fun makeUI(): Component {
     it.add(JScrollPane(JTree()))
     it.preferredSize = Dimension(320, 240)
   }
+}
+
+private fun makeMissingImage(): Image {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.paint = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.paint = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 320
+
+  override fun getIconHeight() = 240
 }
 
 fun main() {
@@ -25,8 +58,9 @@ fun main() {
   EventQueue.invokeLater {
     println("splashScreen show start / EDT: " + EventQueue.isDispatchThread())
     val cl = Thread.currentThread().contextClassLoader
-    val img = ImageIcon(cl.getResource("example/splash.png"))
-    splashScreen.contentPane.add(JLabel(img))
+    val url = cl.getResource("example/splash.png")
+    val img = url?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
+    splashScreen.contentPane.add(JLabel(ImageIcon(img)))
     splashScreen.pack()
     splashScreen.setLocationRelativeTo(null)
     splashScreen.isVisible = true
