9c9734749ead057d7ccc3dea7f693c9fcd88670a
aterai <aterai@outlook.com>
2021-09-19 10:47:52 +0900

    add: AuxiliaryLookAndFeel

---
+++ AuxiliaryLookAndFeel/src/main/kotlin/example/App.kt
@@ -0,0 +1,133 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val combo = makeComboBox()
+  UIManager.put("ComboBox.font", combo.font)
+
+  val check = JCheckBox("<html>addAuxiliaryLookAndFeel<br>(Disable Right Click)")
+
+  val auxLookAndFeel = AuxiliaryWindowsLookAndFeel()
+  UIManager.addPropertyChangeListener { e ->
+    if ("lookAndFeel" == e.propertyName) {
+      val lnf = e.newValue.toString()
+      if (lnf.contains("Windows")) {
+        if (check.isSelected) {
+          UIManager.addAuxiliaryLookAndFeel(auxLookAndFeel)
+        }
+        check.isEnabled = true
+      } else {
+        UIManager.removeAuxiliaryLookAndFeel(auxLookAndFeel)
+        check.isEnabled = false
+      }
+    }
+  }
+  check.addActionListener { e ->
+    val lnf = UIManager.getLookAndFeel().name
+    if ((e.source as? JCheckBox)?.isSelected == true && lnf.contains("Windows")) {
+      UIManager.addAuxiliaryLookAndFeel(auxLookAndFeel)
+    } else {
+      UIManager.removeAuxiliaryLookAndFeel(auxLookAndFeel)
+    }
+    SwingUtilities.updateComponentTreeUI(check.rootPane)
+  }
+
+  combo.isEditable = true
+
+  val box = Box.createVerticalBox()
+  box.add(check)
+  box.add(Box.createVerticalStrut(5))
+  box.add(combo)
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeComboBox())
+  box.border = BorderFactory.createEmptyBorder(15, 15, 15, 15)
+
+  val mb = JMenuBar()
+  mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+
+  return JPanel(BorderLayout()).also {
+    EventQueue.invokeLater { it.rootPane.jMenuBar = mb }
+    it.add(box, BorderLayout.NORTH)
+    it.add(JScrollPane(JTree()))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeComboBox(): JComboBox<String> {
+  val model = DefaultComboBoxModel<String>().also {
+    it.addElement("aaa aaa")
+    it.addElement("aaa ab bb")
+    it.addElement("aaa ab bb cc")
+    it.addElement("1354123451234513512")
+    it.addElement("bbb1")
+    it.addElement("bbb12")
+  }
+  return JComboBox(model)
+}
+
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(lafName: String, lafClassName: String, lafGroup: ButtonGroup): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.actionCommand = lafClassName
+    lafItem.hideActionText = true
+    lafItem.addActionListener { e ->
+      val m = lafGroup.selection
+      runCatching {
+        setLookAndFeel(m.actionCommand)
+      }.onFailure {
+        UIManager.getLookAndFeel().provideErrorFeedback(e.source as? Component)
+      }
+    }
+    lafGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
---
+++ AuxiliaryLookAndFeel/src/main/kotlin/example/AuxiliaryWindowsLookAndFeel.kt
@@ -0,0 +1,26 @@
+package example
+
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+class AuxiliaryWindowsLookAndFeel : LookAndFeel() {
+  override fun getName() = "AuxiliaryWindows"
+
+  override fun getID() = "Not well known"
+
+  override fun getDescription() = "Auxiliary Windows Look and Feel"
+
+  override fun isSupportedLookAndFeel() = true
+
+  override fun isNativeLookAndFeel() = false
+
+  override fun getDefaults(): UIDefaults {
+    val table = object : UIDefaults() {
+      override fun getUIError(msg: String) {
+        /* not needed */
+      }
+    }
+    val uiDefaults = arrayOf<Any>("ComboBoxUI", "example.AuxiliaryWindowsComboBoxUI")
+    table.putDefaults(uiDefaults)
+    return table
+  }
+}
