a61480670d97c437581080de6f6b88da99b52765
aterai <aterai@outlook.com>
2021-09-28 12:00:38 +0900

    refactor: use requireNotNull(...) instead of Objects.requireNonNull(...)

--- Crossfade/src/main/kotlin/example/App.kt
+++ Crossfade/src/main/kotlin/example/App.kt
@@ -1,21 +1,20 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.util.Objects
 import java.util.concurrent.atomic.AtomicInteger
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.Timer
 
-private var mode = Crossfade.IN
+private var mode = CrossFade.IN
 
 fun makeUI(): Component {
   val cl = Thread.currentThread().contextClassLoader
-  val check = JCheckBox("Crossfade Type?", true)
-  val icon1 = ImageIcon(Objects.requireNonNull(cl.getResource("example/test.png")))
-  val icon2 = ImageIcon(Objects.requireNonNull(cl.getResource("example/test.jpg")))
+  val check = JCheckBox("CrossFade Type?", true)
+  val icon1 = ImageIcon(requireNotNull(cl.getResource("example/test.png")))
+  val icon2 = ImageIcon(requireNotNull(cl.getResource("example/test.jpg")))
   val button = JButton("change")
   val alpha = AtomicInteger(10)
-  val crossfade = object : JComponent() {
+  val fade = object : JComponent() {
     override fun paintComponent(g: Graphics) {
       val g2 = g.create() as? Graphics2D ?: return
       g2.paint = background
@@ -33,11 +32,11 @@ fun makeUI(): Component {
   val animator = Timer(50, null)
   animator.addActionListener {
     when {
-      mode == Crossfade.IN && alpha.get() < 10 -> alpha.incrementAndGet()
-      mode == Crossfade.OUT && alpha.get() > 0 -> alpha.decrementAndGet()
+      mode == CrossFade.IN && alpha.get() < 10 -> alpha.incrementAndGet()
+      mode == CrossFade.OUT && alpha.get() > 0 -> alpha.decrementAndGet()
       else -> animator.stop()
     }
-    crossfade.repaint()
+    fade.repaint()
   }
 
   button.addActionListener {
@@ -46,14 +45,14 @@ fun makeUI(): Component {
   }
 
   return JPanel(BorderLayout()).also {
-    it.add(crossfade)
+    it.add(fade)
     it.add(button, BorderLayout.NORTH)
     it.add(check, BorderLayout.SOUTH)
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private enum class Crossfade {
+private enum class CrossFade {
   IN, OUT;
 
   fun toggle() = if (this == IN) OUT else IN
