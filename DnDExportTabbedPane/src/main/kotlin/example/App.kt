109f0d793e95306f6ea5fc4a718cfa50d188744a
aterai <aterai@outlook.com>
2021-10-19 00:53:21 +0900

    ktlint: unexpected indentation

--- DnDExportTabbedPane/src/main/kotlin/example/App.kt
+++ DnDExportTabbedPane/src/main/kotlin/example/App.kt
@@ -258,9 +258,10 @@ class DnDTabbedPane : JTabbedPane() {
         val th = src.transferHandler
         val idx = src.indexAtLocation(tabPt.x, tabPt.y)
         val selIdx = src.selectedIndex
-        val isTabRunsRotated = src.ui !is MetalTabbedPaneUI &&
-            src.tabLayoutPolicy == WRAP_TAB_LAYOUT && idx != selIdx
-        dragTabIndex = if (isTabRunsRotated) selIdx else idx
+        val isWrapTabLayout = src.tabLayoutPolicy == WRAP_TAB_LAYOUT
+        val isNotMetal = src.ui !is MetalTabbedPaneUI
+        val isTabRunsRotated = isNotMetal && isWrapTabLayout && idx != selIdx
+        dragTabIndex = if (isTabRunsRotated && idx != selIdx) selIdx else idx
         th.exportAsDrag(src, e, TransferHandler.MOVE)
         RECT_LINE.setBounds(0, 0, 0, 0)
         src.rootPane.glassPane.isVisible = true
