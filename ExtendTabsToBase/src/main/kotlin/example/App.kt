f160c8907f6917d51080d5169bde82439bb21bf7
aterai <aterai@outlook.com>
2021-11-29 07:14:13 +0900

    add: ExtendTabsToBase

---
+++ ExtendTabsToBase/src/main/kotlin/example/App.kt
@@ -0,0 +1,48 @@
+package example
+
+import java.awt.*
+import javax.swing.*
+
+fun makeUI(): Component {
+  val tabbedPane = makeTabbedPane()
+  val d = UIManager.getLookAndFeelDefaults()
+  d["TabbedPane.extendTabsToBase"] = false
+  tabbedPane.putClientProperty("Nimbus.Overrides", d)
+  tabbedPane.putClientProperty("Nimbus.Overrides.InheritDefaults", false)
+  return JPanel(GridLayout(2, 1)).also {
+    it.add(makeTitledPanel("TabbedPane.extendTabsToBase: true", makeTabbedPane()))
+    it.add(makeTitledPanel("TabbedPane.extendTabsToBase: false", tabbedPane))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTabbedPane() = JTabbedPane().also {
+  it.addTab("JTable", JScrollPane(JTable(8, 3)))
+  it.addTab("JTree", JScrollPane(JTree()))
+  it.addTab("JLabel", JLabel("label"))
+  it.addTab("JButton", JButton("button"))
+  it.addTab("JSplitPane", JSplitPane())
+}
+
+private fun makeTitledPanel(title: String, c: Component) = JPanel(BorderLayout()).also {
+  it.border = BorderFactory.createTitledBorder(title)
+  it.add(c)
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel")
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
