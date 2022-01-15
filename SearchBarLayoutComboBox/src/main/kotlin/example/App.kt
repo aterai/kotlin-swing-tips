f2c75f3df24765127ef8bab93b45596387821c37
aterai <aterai@outlook.com>
2022-01-16 06:01:29 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- SearchBarLayoutComboBox/src/main/kotlin/example/App.kt
+++ SearchBarLayoutComboBox/src/main/kotlin/example/App.kt
@@ -1,6 +1,8 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
@@ -9,11 +11,10 @@ fun makeUI(): Component {
   p.add(JComboBox(arrayOf("Google", "Yahoo!", "Bing")))
   p.add(JLabel("SearchBar JComboBox"))
 
-  val cl = Thread.currentThread().contextClassLoader
   val m = SearchEngineComboBoxModel<example.SearchEngine>()
-  m.addElement(SearchEngine("Google", "http://www.google.com/", ImageIcon(cl.getResource("example/google.png"))))
-  m.addElement(SearchEngine("Yahoo!", "http://www.yahoo.com/", ImageIcon(cl.getResource("example/yahoo.png"))))
-  m.addElement(SearchEngine("Bing", "http://www.bing.com/", ImageIcon(cl.getResource("example/bing.png"))))
+  m.addElement(SearchEngine("Google", "http://www.google.com/", makeIcon("example/google.png")))
+  m.addElement(SearchEngine("Yahoo!", "http://www.yahoo.com/", makeIcon("example/yahoo.png")))
+  m.addElement(SearchEngine("Bing", "http://www.bing.com/", makeIcon("example/bing.png")))
 
   val combo = JSearchBar(m)
   combo.getEditor().setItem("java swing")
@@ -25,6 +26,23 @@ fun makeUI(): Component {
     it.border = BorderFactory.createEmptyBorder(5, 20, 5, 20)
     it.preferredSize = Dimension(320, 240)
   }
+}
+
+private fun makeIcon(path: String): ImageIcon {
+  val url = Thread.currentThread().contextClassLoader.getResource(path)
+  val img = url?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
+  return ImageIcon(img)
+}
+
+private fun makeMissingImage(): BufferedImage {
+  val missingIcon = UIManager.getIcon("html.missingImage")
+  val iw = missingIcon.iconWidth
+  val ih = missingIcon.iconHeight
+  val bi = BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, (16 - iw) / 2, (16 - ih) / 2)
+  g2.dispose()
+  return bi
 }
 
 fun main() {
