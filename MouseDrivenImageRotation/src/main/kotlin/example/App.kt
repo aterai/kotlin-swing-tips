e220223267b6961a52d34f5d907e4b4671a8876a
aterai <aterai@outlook.com>
2022-01-30 01:35:25 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- MouseDrivenImageRotation/src/main/kotlin/example/App.kt
+++ MouseDrivenImageRotation/src/main/kotlin/example/App.kt
@@ -9,17 +9,23 @@ import java.awt.geom.Ellipse2D
 import java.awt.geom.Point2D
 import java.awt.geom.Rectangle2D
 import java.awt.geom.RoundRectangle2D
+import java.awt.image.BufferedImage
 import java.awt.image.ImageObserver
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import kotlin.math.atan2
 
 fun makeUI(): Component {
+  val path = "example/test.png"
   val cl = Thread.currentThread().contextClassLoader
-  val di = DraggableImageMouseListener(ImageIcon(cl.getResource("example/test.png")))
+  val img = cl.getResource(path)?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
+  val di = DraggableImageMouseListener(ImageIcon(img))
   val p = object : JPanel() {
     override fun paintComponent(g: Graphics) {
       val g2 = g.create() as? Graphics2D ?: return
-      g2.paint = GradientPaint(50f, 0f, Color.GRAY, width.toFloat(), height.toFloat(), Color.DARK_GRAY, true)
+      val w = width.toFloat()
+      val h = height.toFloat()
+      g2.paint = GradientPaint(50f, 0f, Color.GRAY, w, h, Color.DARK_GRAY, true)
       g2.fillRect(0, 0, width, height)
       g2.dispose()
       di.paint(g, this)
@@ -144,6 +150,37 @@ private class DraggableImageMouseListener(ii: ImageIcon) : MouseAdapter() {
     private const val IR = 40.0
     private const val OR = IR * 3.0
   }
+}
+
+private fun makeMissingImage(): Image {
+  val missingIcon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.color = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.color = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 240
+
+  override fun getIconHeight() = 160
 }
 
 fun main() {
