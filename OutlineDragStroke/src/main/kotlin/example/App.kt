84bf67c4199827015f16ba3c8f5c15dcf4696cf0
aterai <aterai@outlook.com>
2022-02-01 00:24:13 +0900

    Fix: reset outline when DesktopManager#endResizingFrame(...) executes

--- OutlineDragStroke/src/main/kotlin/example/App.kt
+++ OutlineDragStroke/src/main/kotlin/example/App.kt
@@ -28,6 +28,7 @@ fun makeUI(): Component {
       (f as? JInternalFrame)?.desktopPane?.dragMode = JDesktopPane.LIVE_DRAG_MODE
       if (!rubberBand.isEmpty) {
         super.resizeFrame(f, rubberBand.x, rubberBand.y, rubberBand.width, rubberBand.height)
+        rubberBand.setBounds(0, 0, 0, 0)
       }
       super.endResizingFrame(f)
     }
