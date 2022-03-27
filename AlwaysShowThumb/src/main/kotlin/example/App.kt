14f2fef4df6bfa4886cfb61d6185d74bc693d9fd
aterai <aterai@outlook.com>
2022-03-28 08:06:55 +0900

    add: AlwaysShowThumb

---
+++ AlwaysShowThumb/src/main/kotlin/example/App.kt
@@ -0,0 +1,93 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  val scroll = JScrollPane(JTree())
+  scroll.verticalScrollBarPolicy = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS
+
+  val key = "ScrollBar.alwaysShowThumb"
+  val check = object : JCheckBox(key) {
+    override fun updateUI() {
+      super.updateUI()
+      EventQueue.invokeLater { isSelected = UIManager.getLookAndFeelDefaults().getBoolean(key) }
+    }
+  }
+  check.addActionListener {
+    UIManager.getLookAndFeelDefaults()[key] = check.isSelected
+    SwingUtilities.updateComponentTreeUI(scroll)
+  }
+
+  return JPanel(BorderLayout()).also {
+    val mb = JMenuBar()
+    mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+    EventQueue.invokeLater { it.rootPane.jMenuBar = mb }
+    it.add(check, BorderLayout.NORTH)
+    it.add(scroll)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(
+    lafName: String,
+    lafClassName: String,
+    lafGroup: ButtonGroup
+  ): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.actionCommand = lafClassName
+    lafItem.hideActionText = true
+    lafItem.addActionListener { e ->
+      val m = lafGroup.selection
+      runCatching {
+        setLookAndFeel(m.actionCommand)
+      }.onFailure {
+        UIManager.getLookAndFeel().provideErrorFeedback(e.source as? Component)
+      }
+    }
+    lafGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
