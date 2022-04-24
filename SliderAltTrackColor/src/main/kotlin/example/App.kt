379179e566df4bd02f9b47bb29137ff122384139
aterai <aterai@outlook.com>
2022-04-25 00:15:01 +0900

    add: SliderAltTrackColor

---
+++ SliderAltTrackColor/src/main/kotlin/example/App.kt
@@ -0,0 +1,103 @@
+package example
+
+import java.awt.*
+import javax.swing.*
+
+fun makeUI(): Component {
+  UIManager.put("Slider.altTrackColor", Color.RED)
+  val slider1 = JSlider()
+  slider1.inverted = true
+
+  val slider2 = JSlider()
+  slider2.putClientProperty("JSlider.isFilled", false)
+
+  val txt = "Slider.altTrackColor: RED"
+  val box = Box.createVerticalBox()
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel(txt, JSlider()))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("$txt + Slider.setInverted: true", slider1))
+  box.add(Box.createVerticalStrut(5))
+  box.add(makeTitledPanel("JSlider.isFilled: false", slider2))
+  box.add(Box.createVerticalGlue())
+
+  val mb = JMenuBar()
+  mb.add(LookAndFeelUtil.createLookAndFeelMenu())
+
+  return JPanel(BorderLayout()).also {
+    EventQueue.invokeLater { it.rootPane.jMenuBar = mb }
+    it.add(box)
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeTitledPanel(title: String, c: Component): Component {
+  val p = JPanel(BorderLayout())
+  p.border = BorderFactory.createTitledBorder(title)
+  p.add(c, BorderLayout.NORTH)
+  return p
+}
+
+private object LookAndFeelUtil {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+  fun createLookAndFeelMenu() = JMenu("LookAndFeel").also {
+    val lafRadioGroup = ButtonGroup()
+    for (lafInfo in UIManager.getInstalledLookAndFeels()) {
+      it.add(createLookAndFeelItem(lafInfo.name, lafInfo.className, lafRadioGroup))
+    }
+  }
+
+  private fun createLookAndFeelItem(
+    lafName: String,
+    lafClassName: String,
+    lafGroup: ButtonGroup
+  ): JMenuItem {
+    val lafItem = JRadioButtonMenuItem(lafName, lafClassName == lookAndFeel)
+    lafItem.actionCommand = lafClassName
+    lafItem.hideActionText = true
+    lafItem.addActionListener { e ->
+      val m = lafGroup.selection
+      runCatching {
+        setLookAndFeel(m.actionCommand)
+      }.onFailure {
+        UIManager.getLookAndFeel().provideErrorFeedback(e.source as? Component)
+      }
+    }
+    lafGroup.add(lafItem)
+    return lafItem
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(lookAndFeel: String) {
+    val oldLookAndFeel = LookAndFeelUtil.lookAndFeel
+    if (oldLookAndFeel != lookAndFeel) {
+      UIManager.setLookAndFeel(lookAndFeel)
+      LookAndFeelUtil.lookAndFeel = lookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
