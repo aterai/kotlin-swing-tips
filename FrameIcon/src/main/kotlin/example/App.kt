03241d9fb3e4f7fe5814ad5005483bd410c257dc
aterai <aterai@outlook.com>
2022-05-22 09:57:33 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- FrameIcon/src/main/kotlin/example/App.kt
+++ FrameIcon/src/main/kotlin/example/App.kt
@@ -1,15 +1,18 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
   val cl = Thread.currentThread().contextClassLoader
-  val icon = Toolkit.getDefaultToolkit().createImage(cl.getResource("example/16x16.png"))
+  val url = cl.getResource("example/16x16.png")
+  val image = url?.openStream()?.use(ImageIO::read) ?: makeMissingImage()
   val check = JCheckBox("setIconImage")
   check.addActionListener { e ->
     (e.source as? JCheckBox)?.also {
-      (it.topLevelAncestor as? Window)?.setIconImage(if (it.isSelected) icon else null)
+      (it.topLevelAncestor as? Window)?.setIconImage(if (it.isSelected) image else null)
     }
   }
 
@@ -22,6 +25,17 @@ fun makeUI(): Component {
     it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
     it.preferredSize = Dimension(320, 240)
   }
+}
+
+private fun makeMissingImage(): Image {
+  val missingIcon = UIManager.getIcon("html.missingImage")
+  val iw = missingIcon.iconWidth
+  val ih = missingIcon.iconHeight
+  val bi = BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, (16 - iw) / 2, (16 - ih) / 2)
+  g2.dispose()
+  return bi
 }
 
 fun main() {
