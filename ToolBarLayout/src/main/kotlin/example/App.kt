bd1c1e7cd99b46979b0ceb9931084eb27be686c6
aterai <aterai@outlook.com>
2022-06-04 12:49:13 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- ToolBarLayout/src/main/kotlin/example/App.kt
+++ ToolBarLayout/src/main/kotlin/example/App.kt
@@ -1,31 +1,26 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.net.URL
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-  val cl = Thread.currentThread().contextClassLoader
-  var url1 = cl.getResource("toolBarButtonGraphics/general/Copy24.gif")
-  var url2 = cl.getResource("toolBarButtonGraphics/general/Cut24.gif")
-  var url3 = cl.getResource("toolBarButtonGraphics/general/Help24.gif")
-  if (url1 == null) {
-    url1 = cl.getResource("example/Copy24.gif")
-    url2 = cl.getResource("example/Cut24.gif")
-    url3 = cl.getResource("example/Help24.gif")
-  }
+  val icon1 = makeIcon("toolbarButtonGraphics/general/Copy24.gif")
+  val icon2 = makeIcon("toolbarButtonGraphics/general/Cut24.gif")
+  val icon3 = makeIcon("toolbarButtonGraphics/general/Help24.gif")
 
   val toolBar1 = JToolBar("ToolBarButton")
-  toolBar1.add(JButton(ImageIcon(url1)))
-  toolBar1.add(JButton(ImageIcon(url2)))
+  toolBar1.add(JButton(icon1))
+  toolBar1.add(JButton(icon2))
   toolBar1.add(Box.createGlue())
-  toolBar1.add(JButton(ImageIcon(url3)))
+  toolBar1.add(JButton(icon3))
 
   val toolBar2 = JToolBar("JButton")
-  toolBar2.add(createToolBarButton(url1))
-  toolBar2.add(createToolBarButton(url2))
+  toolBar2.add(createToolBarButton(icon1))
+  toolBar2.add(createToolBarButton(icon2))
   toolBar2.add(Box.createGlue())
-  toolBar2.add(createToolBarButton(url3))
+  toolBar2.add(createToolBarButton(icon3))
 
   val p = JPanel(BorderLayout())
   p.add(toolBar1, BorderLayout.NORTH)
@@ -35,10 +30,27 @@ fun makeUI(): Component {
   return p
 }
 
-private fun createToolBarButton(url: URL?): JButton {
-  val b = JButton(ImageIcon(url))
+private fun createToolBarButton(icon: Icon): JButton {
+  val b = JButton(icon)
   b.isRequestFocusEnabled = false
   return b
+}
+
+private fun makeIcon(path: String): Icon {
+  val cl = Thread.currentThread().contextClassLoader
+  return cl.getResource(path)?.openStream()?.use { ImageIcon(ImageIO.read(it)) }
+    ?: makeMissingIcon()
+}
+
+private fun makeMissingIcon(): Icon {
+  val missingIcon = UIManager.getIcon("html.missingImage")
+  val iw = missingIcon.iconWidth
+  val ih = missingIcon.iconHeight
+  val bi = BufferedImage(24, 24, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, (24 - iw) / 2, (24 - ih) / 2)
+  g2.dispose()
+  return ImageIcon(bi)
 }
 
 fun main() {
