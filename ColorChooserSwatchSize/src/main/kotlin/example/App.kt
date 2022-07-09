7dd6cdd71292862512f0f3ec5e77f8ad064ab366
aterai <aterai@outlook.com>
2022-07-09 12:29:47 +0900

    refactor: use JTextArea.append(...) instead of println(...)

--- ColorChooserSwatchSize/src/main/kotlin/example/App.kt
+++ ColorChooserSwatchSize/src/main/kotlin/example/App.kt
@@ -4,15 +4,19 @@ import java.awt.* // ktlint-disable no-wildcard-imports
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-  // println(UIManager.getDimension("ColorChooser.swatchesRecentSwatchSize"))
-  // println(UIManager.getDimension("ColorChooser.swatchesSwatchSize"))
-  UIManager.put("ColorChooser.swatchesRecentSwatchSize", Dimension(10, 8))
-  UIManager.put("ColorChooser.swatchesSwatchSize", Dimension(6, 10))
+  val log = JTextArea()
+  val key1 = "ColorChooser.swatchesRecentSwatchSize"
+  log.append("$key1: ${UIManager.getDimension(key1)}\n")
+  val key2 = "ColorChooser.swatchesSwatchSize"
+  log.append("$key2: ${UIManager.getDimension(key2)}\n")
+
+  UIManager.put(key1, Dimension(10, 8))
+  UIManager.put(key2, Dimension(6, 10))
 
   val button1 = JButton("JColorChooser.showDialog(...)")
   button1.addActionListener {
     val color = JColorChooser.showDialog(null, "JColorChooser", null)
-    println(color)
+    log.append("color: $color\n")
   }
 
   val cc = JColorChooser()
@@ -21,13 +25,12 @@ fun makeUI(): Component {
     "JST ColorChooserSwatchSize",
     true,
     cc,
-    { println("ok") }
-  ) { println("cancel") }
+    { log.append("ok\n") }
+  ) { log.append("cancel\n") }
   val button2 = JButton("JColorChooser.createDialog(...).setVisible(true)")
   button2.addActionListener {
     dialog.isVisible = true
-    val color = cc.color
-    println(color)
+    log.append("color: ${cc.color}\n")
   }
 
   val p = JPanel()
