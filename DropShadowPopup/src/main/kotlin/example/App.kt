379a300982b4a8b0ef4ca2cfa427cf09a0c6b03b
aterai <aterai@outlook.com>
2022-07-26 19:53:37 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- DropShadowPopup/src/main/kotlin/example/App.kt
+++ DropShadowPopup/src/main/kotlin/example/App.kt
@@ -2,6 +2,7 @@ package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
 import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
@@ -11,7 +12,9 @@ fun makeUI(): Component {
   initPopupMenu(popup1)
 
   val cl = Thread.currentThread().contextClassLoader
-  val label = JLabel(ImageIcon(cl.getResource("example/test.png")))
+  val url = cl.getResource("example/test.png")
+  val icon = url?.openStream()?.use(ImageIO::read)?.let { ImageIcon(it) } ?: MissingIcon()
+  val label = JLabel(icon)
   label.componentPopupMenu = popup1
 
   val check = JCheckBox("Paint Shadow", true)
@@ -84,8 +87,10 @@ private class DropShadowPopupMenu : JPopupMenu() {
       }
     }
     EventQueue.invokeLater {
-      val top = topLevelAncestor
-      (top as? JWindow)?.background = Color(0x0, true)
+      val top = SwingUtilities.getWindowAncestor(this)
+      if (top?.type == Window.Type.POPUP) {
+        top.background = Color(0x0, true)
+      }
     }
     super.show(c, x, y)
   }
@@ -93,6 +98,26 @@ private class DropShadowPopupMenu : JPopupMenu() {
   companion object {
     private const val OFFSET = 4
   }
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component?, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.color = Color.WHITE
+    g2.fillRect(x, y, w, h)
+    g2.color = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(x + gap, y + gap, x + w - gap, y + h - gap)
+    g2.drawLine(x + gap, y + h - gap, x + w - gap, y + gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 320
+
+  override fun getIconHeight() = 240
 }
 
 fun main() {
