454d2766253ba0be359a7816eacb9405ab539fbc
aterai <aterai@outlook.com>
2022-09-20 09:29:53 +0900

    PMD: SystemPrintln

--- YesLastOptionPane/src/main/kotlin/example/App.kt
+++ YesLastOptionPane/src/main/kotlin/example/App.kt
@@ -6,36 +6,40 @@ import javax.swing.* // ktlint-disable no-wildcard-imports
 private const val KEY = "OptionPane.isYesLast"
 
 fun makeUI(): Component {
-  val log = JTextArea()
+  val log = object : JTextArea() {
+    override fun updateUI() {
+      UIManager.put(KEY, null)
+      super.updateUI()
+      val b = UIManager.getLookAndFeelDefaults().getBoolean(KEY)
+      EventQueue.invokeLater { text = "$KEY: $b" }
+    }
+  }
+
   val defaultButton = JButton("$KEY: false(default)")
   defaultButton.addActionListener {
     UIManager.put(KEY, false)
     val str = JOptionPane.showInputDialog(log.rootPane, "$KEY: false")
     log.text = str
   }
+
   val yesLastButton = JButton("$KEY: true")
   yesLastButton.addActionListener {
     UIManager.put(KEY, true)
     val str = JOptionPane.showInputDialog(log.rootPane, "$KEY: true")
     log.text = str
   }
+
   val p = JPanel(GridLayout(0, 1, 5, 5))
   p.border = BorderFactory.createTitledBorder("JOptionPane")
   p.add(defaultButton)
   p.add(yesLastButton)
 
-  val panel = object : JPanel(BorderLayout()) {
-    override fun updateUI() {
-      UIManager.put(KEY, null)
-      super.updateUI()
-      val b = UIManager.getLookAndFeelDefaults().getBoolean(KEY)
-      println("$KEY: $b")
-    }
+  return JPanel(BorderLayout()).also {
+    it.add(p, BorderLayout.NORTH)
+    it.add(JScrollPane(log))
+    it.border = BorderFactory.createEmptyBorder(5, 5, 5, 5)
+    it.preferredSize = Dimension(320, 240)
   }
-  panel.add(p, BorderLayout.NORTH)
-  panel.add(JScrollPane(log))
-  panel.preferredSize = Dimension(320, 240)
-  return panel
 }
 
 fun main() {
