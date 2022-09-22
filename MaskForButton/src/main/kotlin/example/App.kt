219518454c4925463192a7da866379f9126e2006
aterai <aterai@outlook.com>
2022-09-22 11:03:09 +0900

    add: MaskForButton

--- MaskForButton/src/main/kotlin/example/App.kt
+++ MaskForButton/src/main/kotlin/example/App.kt
@@ -1,63 +1,105 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.FocusAdapter
-import java.awt.event.FocusEvent
-import java.util.Calendar
-import java.util.Date
+import java.awt.event.InputEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.JSpinner.DateEditor
 
 fun makeUI(): Component {
-  val dateFormat = "yyyy/MM/dd"
-  val date = Date()
-  val spinner1 = JSpinner(SpinnerDateModel(date, date, null, Calendar.DAY_OF_MONTH))
-  spinner1.editor = DateEditor(spinner1, dateFormat)
+  val log = JTextArea()
+  log.isEditable = false
+  log.append("MouseInfo.getNumberOfButtons: ${MouseInfo.getNumberOfButtons()}\n")
 
-  val today = Calendar.getInstance()
-  today.clear(Calendar.MILLISECOND)
-  today.clear(Calendar.SECOND)
-  today.clear(Calendar.MINUTE)
-  today[Calendar.HOUR_OF_DAY] = 0
-
-  val start = today.time
-  println(date)
-  println(start)
-
-  val spinner2 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  spinner2.editor = DateEditor(spinner2, dateFormat)
-
-  val spinner3 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  val editor = DateEditor(spinner3, dateFormat)
-  spinner3.editor = editor
-  val fl3 = object : FocusAdapter() {
-    override fun focusGained(e: FocusEvent) {
-      EventQueue.invokeLater {
-        val i = dateFormat.lastIndexOf("dd")
-        editor.textField.select(i, i + 2)
+  val tabbedPane = JTabbedPane()
+  tabbedPane.componentPopupMenu = TabbedPanePopupMenu()
+  tabbedPane.addTab("Title a", JLabel("Close a tab by the middle mouse button clicking."))
+  tabbedPane.addTab("Title b", JLabel("JLabel b"))
+  tabbedPane.addTab("Title c", JLabel("JLabel c"))
+  tabbedPane.addMouseListener(object : MouseAdapter() {
+    override fun mouseClicked(e: MouseEvent) {
+      val button = e.button
+      val isB2Clicked = e.modifiersEx and InputEvent.getMaskForButton(2) != 0
+      val mask = if (button == 0) "NOBUTTON" else "BUTTON$button"
+      log.append("$mask\n")
+      log.append("BUTTON2 mouseClicked: $isB2Clicked\n")
+      val isB1Double = e.clickCount == 2 && button == 1
+      val isB2Down = MouseInfo.getNumberOfButtons() > 2 && button == 2
+      (e.component as? JTabbedPane)?.also {
+        val idx = it.indexAtLocation(e.x, e.y)
+        if (idx >= 0 && (isB2Down || isB1Double)) {
+          it.remove(idx)
+        }
       }
     }
-  }
-  editor.textField.addFocusListener(fl3)
 
-  return JPanel(GridLayout(3, 1)).also {
-    it.add(makeTitledPanel("Calendar.DAY_OF_MONTH", spinner1))
-    it.add(makeTitledPanel("min: set(Calendar.HOUR_OF_DAY, 0)", spinner2))
-    it.add(makeTitledPanel("JSpinner.DateEditor + FocusListener", spinner3))
-    it.border = BorderFactory.createEmptyBorder(10, 5, 10, 5)
+    override fun mousePressed(e: MouseEvent) {
+      val mousePressed = e.modifiersEx and InputEvent.getMaskForButton(2) != 0
+      log.append("BUTTON2 mousePressed: $mousePressed\n")
+    }
+
+    override fun mouseReleased(e: MouseEvent) {
+      val mouseReleased = e.modifiersEx and InputEvent.getMaskForButton(2) != 0
+      log.append("BUTTON2 mouseReleased: $mouseReleased\n")
+    }
+  })
+
+  return JPanel(GridLayout(2, 1)).also {
+    it.add(tabbedPane)
+    it.add(JScrollPane(log))
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun makeTitledPanel(title: String, cmp: Component): Component {
-  val p = JPanel(GridBagLayout())
-  p.border = BorderFactory.createTitledBorder(title)
-  val c = GridBagConstraints()
-  c.weightx = 1.0
-  c.fill = GridBagConstraints.HORIZONTAL
-  c.insets = Insets(5, 5, 5, 5)
-  p.add(cmp, c)
-  return p
+private class TabbedPanePopupMenu : JPopupMenu() {
+  private var count = 0
+  private val closePage: JMenuItem
+  private val closeAll: JMenuItem
+  private val closeAllButActive: JMenuItem
+
+  init {
+    add("New tab").addActionListener {
+      (invoker as? JTabbedPane)?.also {
+        it.addTab("Title: $count", JLabel("Tab: $count"))
+        it.selectedIndex = it.tabCount - 1
+        count++
+      }
+    }
+    addSeparator()
+
+    closePage = add("Close")
+    closePage.addActionListener {
+      (invoker as? JTabbedPane)?.also {
+        it.remove(it.selectedIndex)
+      }
+    }
+    addSeparator()
+
+    closeAll = add("Close all")
+    closeAll.addActionListener {
+      (invoker as? JTabbedPane)?.removeAll()
+    }
+
+    closeAllButActive = add("Close all bat active")
+    closeAllButActive.addActionListener {
+      (invoker as? JTabbedPane)?.also {
+        val idx = it.selectedIndex
+        val title = it.getTitleAt(idx)
+        val cmp = it.getComponentAt(idx)
+        it.removeAll()
+        it.addTab(title, cmp)
+      }
+    }
+  }
+
+  override fun show(c: Component, x: Int, y: Int) {
+    if (c is JTabbedPane) {
+      closePage.isEnabled = c.indexAtLocation(x, y) >= 0
+      closeAll.isEnabled = c.tabCount > 0
+      closeAllButActive.isEnabled = c.tabCount > 0
+      super.show(c, x, y)
+    }
+  }
 }
 
 fun main() {
