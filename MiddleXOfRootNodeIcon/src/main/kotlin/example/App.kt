dcfc1d03465da41f6755d5b2c941028810a9dfce
aterai <aterai@outlook.com>
2022-09-23 00:09:18 +0900

    add: MiddleXOfRootNodeIcon

--- MiddleXOfRootNodeIcon/src/main/kotlin/example/App.kt
+++ MiddleXOfRootNodeIcon/src/main/kotlin/example/App.kt
@@ -1,63 +1,107 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.FocusAdapter
-import java.awt.event.FocusEvent
-import java.util.Calendar
-import java.util.Date
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.JSpinner.DateEditor
+import javax.swing.plaf.LayerUI
 
 fun makeUI(): Component {
-  val dateFormat = "yyyy/MM/dd"
-  val date = Date()
-  val spinner1 = JSpinner(SpinnerDateModel(date, date, null, Calendar.DAY_OF_MONTH))
-  spinner1.editor = DateEditor(spinner1, dateFormat)
-
-  val today = Calendar.getInstance()
-  today.clear(Calendar.MILLISECOND)
-  today.clear(Calendar.SECOND)
-  today.clear(Calendar.MINUTE)
-  today[Calendar.HOUR_OF_DAY] = 0
-
-  val start = today.time
-  println(date)
-  println(start)
-
-  val spinner2 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  spinner2.editor = DateEditor(spinner2, dateFormat)
-
-  val spinner3 = JSpinner(SpinnerDateModel(date, start, null, Calendar.DAY_OF_MONTH))
-  val editor = DateEditor(spinner3, dateFormat)
-  spinner3.editor = editor
-  val fl3 = object : FocusAdapter() {
-    override fun focusGained(e: FocusEvent) {
-      EventQueue.invokeLater {
-        val i = dateFormat.lastIndexOf("dd")
-        editor.textField.select(i, i + 2)
+  val ow = UIManager.getIcon("Tree.openIcon").iconWidth
+  val iw = 32
+  val ih = 24
+  val icon0 = ColorIcon(Color.GREEN, Dimension(iw, ih))
+  val tree0 = object : JTree() {
+    override fun updateUI() {
+      setCellRenderer(null)
+      super.updateUI()
+      setRowHeight(0)
+      val r = getCellRenderer()
+      setCellRenderer {
+          tree: JTree,
+          value: Any?,
+          selected: Boolean,
+          expanded: Boolean,
+          leaf: Boolean,
+          row: Int,
+          hasFocus: Boolean ->
+        val c = r.getTreeCellRendererComponent(
+          tree,
+          value,
+          selected,
+          expanded,
+          leaf,
+          row,
+          hasFocus
+        )
+        if (c is JLabel && value == tree.model.root) {
+          c.icon = icon0
+        }
+        c
       }
     }
   }
-  editor.textField.addFocusListener(fl3)
+  tree0.border = BorderFactory.createEmptyBorder(1, 1, 1, 1)
+  val icon1 = ColorIcon(Color.GREEN, Dimension(ow, ih))
+  val icon2 = ColorIcon(Color(0x55_00_00_AA, true), Dimension(iw, ih))
+  val tree = object : JTree() {
+    override fun updateUI() {
+      setCellRenderer(null)
+      super.updateUI()
+      setRowHeight(0)
+      val r = getCellRenderer()
+      setCellRenderer {
+          tree: JTree,
+          value: Any?,
+          selected: Boolean,
+          expanded: Boolean,
+          leaf: Boolean,
+          row: Int,
+          hasFocus: Boolean ->
+        val c = r.getTreeCellRendererComponent(
+          tree,
+          value,
+          selected,
+          expanded,
+          leaf,
+          row,
+          hasFocus
+        )
+        if (c is JLabel && value == tree.model.root) {
+          c.icon = icon1
+          c.iconTextGap = 2 + (iw - icon1.iconWidth) / 2
+        }
+        c
+      }
+    }
+  }
+  tree.border = BorderFactory.createEmptyBorder(1, 1 + (iw - ow) / 2, 1, 1)
+  val layerUI = object : LayerUI<JTree>() {
+    override fun paint(g: Graphics, c: JComponent) {
+      super.paint(g, c)
+      val g2 = g.create() as? Graphics2D ?: return
+      icon2.paintIcon(c, g2, 1, 1)
+      g2.dispose()
+    }
+  }
 
-  return JPanel(GridLayout(3, 1)).also {
-    it.add(makeTitledPanel("Calendar.DAY_OF_MONTH", spinner1))
-    it.add(makeTitledPanel("min: set(Calendar.HOUR_OF_DAY, 0)", spinner2))
-    it.add(makeTitledPanel("JSpinner.DateEditor + FocusListener", spinner3))
-    it.border = BorderFactory.createEmptyBorder(10, 5, 10, 5)
+  return JPanel(GridLayout(1, 2)).also {
+    it.add(JScrollPane(tree0))
+    it.add(JScrollPane(JLayer(tree, layerUI)))
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun makeTitledPanel(title: String, cmp: Component): Component {
-  val p = JPanel(GridBagLayout())
-  p.border = BorderFactory.createTitledBorder(title)
-  val c = GridBagConstraints()
-  c.weightx = 1.0
-  c.fill = GridBagConstraints.HORIZONTAL
-  c.insets = Insets(5, 5, 5, 5)
-  p.add(cmp, c)
-  return p
+private class ColorIcon(private val color: Color, private val dim: Dimension) : Icon {
+  override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.translate(x, y)
+    g2.color = color
+    g2.fillRect(1, 1, dim.width - 2, dim.height - 2)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = dim.width
+
+  override fun getIconHeight() = dim.height
 }
 
 fun main() {
