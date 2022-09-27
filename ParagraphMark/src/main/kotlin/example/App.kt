fa08e4ed502834a35aef5f334b3420c0e8a0b97f
aterai <aterai@outlook.com>
2022-09-28 00:19:57 +0900

    refactor: remove redundant import

--- ParagraphMark/src/main/kotlin/example/App.kt
+++ ParagraphMark/src/main/kotlin/example/App.kt
@@ -1,12 +1,8 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.InputEvent
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.text.AbstractDocument
-import javax.swing.text.BadLocationException
 import javax.swing.text.BoxView
 import javax.swing.text.ComponentView
 import javax.swing.text.Element
--- SortIconLayoutHeaderRenderer/src/main/kotlin/example/App.kt
+++ SortIconLayoutHeaderRenderer/src/main/kotlin/example/App.kt
@@ -1,9 +1,6 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.event.ActionEvent
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
