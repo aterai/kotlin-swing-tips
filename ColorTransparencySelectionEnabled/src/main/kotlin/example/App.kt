e016705ed6d6acf377c1f3a033a763fdd2dd84f4
aterai <aterai@outlook.com>
2022-11-15 12:13:13 +0900

    fix: ArrayIndexOutOfBoundsException in JColorChooser of GTKLookAndFeel

--- ColorTransparencySelectionEnabled/src/main/kotlin/example/App.kt
+++ ColorTransparencySelectionEnabled/src/main/kotlin/example/App.kt
@@ -7,6 +7,7 @@ import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.colorchooser.AbstractColorChooserPanel
+import javax.swing.plaf.basic.BasicColorChooserUI
 
 private val defaultRadio = JRadioButton("Default")
 private val enabledRadio = JRadioButton("setEnabled(false)", true)
@@ -15,50 +16,54 @@ fun makeUI(): Component {
   val label = JLabel()
   label.isOpaque = true
   label.background = Color.WHITE
-  val box = Box.createVerticalBox()
-  box.border = BorderFactory.createTitledBorder("ColorTransparencySelectionEnabled")
-  val bg = ButtonGroup()
-  val visibleRadio = JRadioButton("setVisible(false)")
-  for (r in listOf(defaultRadio, enabledRadio, visibleRadio)) {
-    bg.add(r)
-    box.add(r)
-  }
+
   val button = JButton("open JColorChooser")
   button.addActionListener {
-    val rgbName = UIManager.getString("ColorChooser.rgbNameText", button.locale)
-    val cc = JColorChooser()
-    for (ccPanel in cc.chooserPanels) {
-      // Java 9: ccPanel.setColorTransparencySelectionEnabled(colorTransparency)
-      if (rgbName == ccPanel.displayName) {
-        if (!defaultRadio.isSelected) {
-          EventQueue.invokeLater { setTransparencySelectionEnabled(ccPanel) }
-        }
-      } else {
-        cc.removeChooserPanel(ccPanel)
-      }
-    }
-    val rp = button.rootPane
-    val ok = ColorTracker(cc)
-    val dialog = JColorChooser.createDialog(rp, "title", true, cc, ok, null)
-    dialog.addComponentListener(object : ComponentAdapter() {
-      override fun componentHidden(e: ComponentEvent) {
-        (e.component as? Window)?.dispose()
-      }
-    })
-    dialog.isVisible = true // blocks until user brings dialog down...
-    val color = ok.color
+    val color = getColor(button.rootPane)
     if (color != null) {
       label.background = color
     }
   }
 
   return JPanel(BorderLayout(10, 10)).also {
-    it.add(box, BorderLayout.NORTH)
+    it.add(makeBox(), BorderLayout.NORTH)
     it.add(label)
     it.add(button, BorderLayout.SOUTH)
     it.border = BorderFactory.createEmptyBorder(10, 10, 10, 10)
     it.preferredSize = Dimension(320, 240)
   }
+}
+
+private fun getColor(parent: Container): Color? {
+  val rgbName = UIManager.getString("ColorChooser.rgbNameText", parent.locale)
+  val cc = object : JColorChooser() {
+    override fun updateUI() {
+      super.updateUI()
+      if ("GTK".equals(UIManager.getLookAndFeel().id)) {
+        setUI(BasicColorChooserUI())
+      }
+    }
+  }
+  for (ccPanel in cc.chooserPanels) {
+    // Java 9: ccPanel.setColorTransparencySelectionEnabled(colorTransparency)
+    if (rgbName == ccPanel.displayName) {
+      if (!defaultRadio.isSelected) {
+        EventQueue.invokeLater { setTransparencySelectionEnabled(ccPanel) }
+      }
+    } else {
+      cc.removeChooserPanel(ccPanel)
+    }
+  }
+  val ok = ColorTracker(cc)
+  val dialog = JColorChooser.createDialog(parent, "title", true, cc, ok, null)
+  dialog.addComponentListener(object : ComponentAdapter() {
+    override fun componentHidden(e: ComponentEvent) {
+      (e.component as? Window)?.dispose()
+    }
+  })
+  dialog.isVisible = true // blocks until user brings dialog down...
+  val color = ok.color
+  return color
 }
 
 private fun setTransparencySelectionEnabled(p: AbstractColorChooserPanel) {
@@ -80,6 +85,18 @@ private fun setEnabledOrVisible(c: Component) {
   } else {
     c.isVisible = false
   }
+}
+
+private fun makeBox(): Box {
+  val box = Box.createVerticalBox()
+  box.border = BorderFactory.createTitledBorder("ColorTransparencySelectionEnabled")
+  val bg = ButtonGroup()
+  val visibleRadio = JRadioButton("setVisible(false)")
+  for (r in listOf(defaultRadio, enabledRadio, visibleRadio)) {
+    bg.add(r)
+    box.add(r)
+  }
+  return box
 }
 
 private class ColorTracker(private val chooser: JColorChooser) : ActionListener {
