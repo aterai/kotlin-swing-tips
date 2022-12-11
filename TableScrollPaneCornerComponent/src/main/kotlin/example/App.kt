14ebeb8351662e1f4954dea756e2292110197c1f
aterai <aterai@outlook.com>
2022-12-12 02:21:13 +0900

    add: TableScrollPaneCornerComponent

---
+++ TableScrollPaneCornerComponent/src/main/kotlin/example/App.kt
@@ -0,0 +1,43 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+
+fun makeUI(): Component {
+  // val o = UIManager.get("Table.scrollPaneCornerComponent")
+  val table = object : JTable(15, 3) {
+    override fun getScrollableTracksViewportWidth(): Boolean {
+      val c = SwingUtilities.getAncestorOfClass(JScrollPane::class.java, this)
+      (c as? JScrollPane)?.also {
+        it.getCorner(ScrollPaneConstants.UPPER_RIGHT_CORNER)?.isVisible =
+          preferredSize.width >= it.viewport.width
+      }
+      return super.getScrollableTracksViewportWidth()
+    }
+  }
+  return JPanel(GridLayout(0, 1)).also {
+    listOf(JTable(15, 3), table).forEach { t ->
+      t.autoResizeMode = JTable.AUTO_RESIZE_OFF
+      it.add(JScrollPane(t))
+    }
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel")
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
