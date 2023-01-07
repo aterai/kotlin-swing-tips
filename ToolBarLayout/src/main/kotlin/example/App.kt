65aeef64a8d8dbe84ab5665261ff92975678ebb7
aterai <aterai@outlook.com>
2023-01-08 03:43:51 +0900

    refactor: use ColorIcon instead of ImageIcon generated from gif file

--- ToolBarLayout/src/main/kotlin/example/App.kt
+++ ToolBarLayout/src/main/kotlin/example/App.kt
@@ -1,56 +1,52 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
-import java.awt.image.BufferedImage
-import javax.imageio.ImageIO
 import javax.swing.* // ktlint-disable no-wildcard-imports
 
 fun makeUI(): Component {
-  val icon1 = makeIcon("toolbarButtonGraphics/general/Copy24.gif")
-  val icon2 = makeIcon("toolbarButtonGraphics/general/Cut24.gif")
-  val icon3 = makeIcon("toolbarButtonGraphics/general/Help24.gif")
+  val icon1 = ColorIcon(Color.RED)
+  val icon2 = ColorIcon(Color.GREEN)
+  val icon3 = ColorIcon(Color.BLUE)
 
-  val toolBar1 = JToolBar("ToolBarButton")
-  toolBar1.add(JButton(icon1))
-  toolBar1.add(JButton(icon2))
-  toolBar1.add(Box.createGlue())
-  toolBar1.add(JButton(icon3))
+  val toolBar1 = JToolBar("ToolBarButton").also {
+    it.add(JButton(icon1))
+    it.add(JButton(icon2))
+    it.add(Box.createGlue())
+    it.add(JButton(icon3))
+  }
 
-  val toolBar2 = JToolBar("JButton")
-  toolBar2.add(createToolBarButton(icon1))
-  toolBar2.add(createToolBarButton(icon2))
-  toolBar2.add(Box.createGlue())
-  toolBar2.add(createToolBarButton(icon3))
+  val toolBar2 = JToolBar("JButton").also {
+    it.add(createToolBarButton(icon1))
+    it.add(createToolBarButton(icon2))
+    it.add(Box.createGlue())
+    it.add(createToolBarButton(icon3))
+  }
 
-  val p = JPanel(BorderLayout())
-  p.add(toolBar1, BorderLayout.NORTH)
-  p.add(JScrollPane(JTextArea()))
-  p.add(toolBar2, BorderLayout.SOUTH)
-  p.preferredSize = Dimension(320, 240)
-  return p
+  return JPanel(BorderLayout()).also {
+    it.add(toolBar1, BorderLayout.NORTH)
+    it.add(JScrollPane(JTextArea()))
+    it.add(toolBar2, BorderLayout.SOUTH)
+    it.preferredSize = Dimension(320, 240)
+  }
 }
 
-private fun createToolBarButton(icon: Icon): JButton {
-  val b = JButton(icon)
-  b.isRequestFocusEnabled = false
-  return b
+private fun createToolBarButton(icon: Icon) = JButton(icon).also {
+  it.isRequestFocusEnabled = false
 }
 
-private fun makeIcon(path: String): Icon {
-  val cl = Thread.currentThread().contextClassLoader
-  return cl.getResource(path)?.openStream()?.use { ImageIcon(ImageIO.read(it)) }
-    ?: makeMissingIcon()
-}
+private class ColorIcon(private val color: Color) : Icon {
+  override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as? Graphics2D ?: return
+    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    g2.translate(x, y)
+    g2.paint = color
+    g2.fillOval(1, 1, iconWidth - 2, iconHeight - 2)
+    g2.dispose()
+  }
 
-private fun makeMissingIcon(): Icon {
-  val missingIcon = UIManager.getIcon("html.missingImage")
-  val iw = missingIcon.iconWidth
-  val ih = missingIcon.iconHeight
-  val bi = BufferedImage(24, 24, BufferedImage.TYPE_INT_ARGB)
-  val g2 = bi.createGraphics()
-  missingIcon.paintIcon(null, g2, (24 - iw) / 2, (24 - ih) / 2)
-  g2.dispose()
-  return ImageIcon(bi)
+  override fun getIconWidth() = 24
+
+  override fun getIconHeight() = 24
 }
 
 fun main() {
