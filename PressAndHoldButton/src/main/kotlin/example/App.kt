df4776d4823f8bd40aebc0f24b2068b6abae67f7
aterai <aterai@outlook.com>
2023-01-21 08:39:52 +0900

    PMD: SystemPrintln

--- PressAndHoldButton/src/main/kotlin/example/App.kt
+++ PressAndHoldButton/src/main/kotlin/example/App.kt
@@ -5,136 +5,140 @@ import java.awt.event.ActionEvent
 import java.awt.event.MouseEvent
 import java.awt.event.MouseListener
 import javax.swing.* // ktlint-disable no-wildcard-imports
-import javax.swing.Timer
 
-fun makeUI() = JPanel(BorderLayout()).also {
-  val cl = Thread.currentThread().contextClassLoader
+fun makeUI(): Component {
+  val log = JTextArea("press and hold the button for 1000 milliseconds\n")
+  log.isEditable = false
+
+  val popupMenu = JPopupMenu()
+  popupMenu.layout = GridLayout(0, 3, 5, 5)
+  val bg = ButtonGroup()
+  makeMenuList()
+    .map { makeMenuButton(it) }
+    .forEach {
+      it.addActionListener {
+        val cmd = bg.selection?.actionCommand ?: "null"
+        log.append("Selected JRadioButton command: $cmd\n")
+        popupMenu.isVisible = false
+      }
+      popupMenu.add(it)
+      bg.add(it)
+    }
+
+  val icon = UIManager.getIcon("FileChooser.detailsViewIcon")
+  val button = PressAndHoldButton(icon, popupMenu)
+  button.addActionListener {
+    val cmd = bg.selection?.actionCommand ?: "null"
+    log.append("Selected action command: $cmd\n")
+  }
+
   val toolBar = JToolBar()
-  toolBar.add(PressAndHoldButton(ImageIcon(cl.getResource("example/ei0021-16.png"))))
-  it.add(toolBar, BorderLayout.NORTH)
-  it.add(JLabel("press and hold the button for 1000 milliseconds"))
-  it.preferredSize = Dimension(320, 240)
+  toolBar.add(button)
+
+  return JPanel(BorderLayout(5, 5)).also {
+    it.add(toolBar, BorderLayout.NORTH)
+    it.add(JScrollPane(log))
+    it.preferredSize = Dimension(320, 240)
+  }
 }
 
-private class PressAndHoldButton(icon: Icon?) : JButton(icon) {
+private fun makeMenuList() = listOf(
+  MenuContext("BLACK", Color.BLACK),
+  MenuContext("BLUE", Color.BLUE),
+  MenuContext("CYAN", Color.CYAN),
+  MenuContext("GREEN", Color.GREEN),
+  MenuContext("MAGENTA", Color.MAGENTA),
+  MenuContext("ORANGE", Color.ORANGE),
+  MenuContext("PINK", Color.PINK),
+  MenuContext("RED", Color.RED),
+  MenuContext("YELLOW", Color.YELLOW)
+)
+
+private fun makeMenuButton(m: MenuContext): AbstractButton {
+  val b = JRadioButton(m.command)
+  b.actionCommand = m.command
+  b.foreground = m.color
+  b.border = BorderFactory.createEmptyBorder()
+  return b
+}
+
+private class PressAndHoldButton(icon: Icon?, popupMenu: JPopupMenu?) : JButton(icon) {
   private var handler: PressAndHoldHandler? = null
+  private val popupMenu: JPopupMenu?
 
   init {
     action.putValue(Action.SMALL_ICON, icon)
+    this.popupMenu = popupMenu
   }
 
   override fun updateUI() {
     removeMouseListener(handler)
     super.updateUI()
-    handler = PressAndHoldHandler().also {
-      SwingUtilities.updateComponentTreeUI(it.pop)
-      action = it
-      addMouseListener(it)
+    if (popupMenu != null) {
+      SwingUtilities.updateComponentTreeUI(popupMenu)
     }
+    handler = PressAndHoldHandler()
+    action = handler
+    addMouseListener(handler)
     isFocusable = false
-    border = BorderFactory.createEmptyBorder(4, 4, 4, 4 + ARROW_ICON.iconWidth)
+    border = BorderFactory.createEmptyBorder(4, 4, 4, 4 + Companion.ARROW_ICON.iconWidth)
   }
 
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
     val r = SwingUtilities.calculateInnerArea(this, null)
-    ARROW_ICON.paintIcon(this, g, r.x + r.width, r.y + (r.height - ARROW_ICON.iconHeight) / 2)
+    val cy = (r.height - Companion.ARROW_ICON.iconHeight) / 2
+    Companion.ARROW_ICON.paintIcon(this, g, r.x + r.width, r.y + cy)
+  }
+
+  private inner class PressAndHoldHandler: AbstractAction(), MouseListener {
+    private val holdTimer = Timer(1000) { e ->
+      val timer = e.source as Timer
+      if (popupMenu != null && getModel().isPressed && timer.isRunning) {
+        timer.stop()
+        popupMenu.show(this@PressAndHoldButton, 0, height)
+        popupMenu.requestFocusInWindow()
+      }
+    }
+
+    init {
+      holdTimer.initialDelay = 1000
+    }
+
+    override fun actionPerformed(e: ActionEvent) {
+      if (holdTimer.isRunning) {
+        holdTimer.stop()
+      }
+    }
+
+    override fun mousePressed(e: MouseEvent) {
+      val c = e.component
+      if (SwingUtilities.isLeftMouseButton(e) && c.isEnabled) {
+        holdTimer.start()
+      }
+    }
+
+    override fun mouseReleased(e: MouseEvent) {
+      holdTimer.stop()
+    }
+
+    override fun mouseExited(e: MouseEvent) {
+      if (holdTimer.isRunning) {
+        holdTimer.stop()
+      }
+    }
+
+    override fun mouseEntered(e: MouseEvent) {
+      /* not needed */
+    }
+
+    override fun mouseClicked(e: MouseEvent) {
+      /* not needed */
+    }
   }
 
   companion object {
     private val ARROW_ICON = MenuArrowIcon()
-  }
-}
-
-private class PressAndHoldHandler : AbstractAction(), MouseListener {
-  val pop = JPopupMenu()
-  private val bg = ButtonGroup()
-  private var arrowButton: AbstractButton? = null
-  private val holdTimer = Timer(1000) { e ->
-    println("InitialDelay(1000)")
-    arrowButton?.also {
-      val timer = e.source as? Timer
-      if (it.model.isPressed && timer?.isRunning == true) {
-        timer.stop()
-        pop.show(it, 0, it.height)
-        pop.requestFocusInWindow()
-      }
-    }
-  }
-
-  init {
-    holdTimer.initialDelay = 1000
-    pop.layout = GridLayout(0, 3, 5, 5)
-    makeMenuList()
-      .map { makeMenuButton(it) }
-      .forEach {
-        it.addActionListener {
-          println(bg.selection.actionCommand)
-          pop.isVisible = false
-        }
-        pop.add(it)
-        bg.add(it)
-      }
-  }
-
-  private fun makeMenuList() = listOf(
-    MenuContext("BLACK", Color.BLACK),
-    MenuContext("BLUE", Color.BLUE),
-    MenuContext("CYAN", Color.CYAN),
-    MenuContext("GREEN", Color.GREEN),
-    MenuContext("MAGENTA", Color.MAGENTA),
-    MenuContext("ORANGE", Color.ORANGE),
-    MenuContext("PINK", Color.PINK),
-    MenuContext("RED", Color.RED),
-    MenuContext("YELLOW", Color.YELLOW)
-  )
-
-  override fun actionPerformed(e: ActionEvent) {
-    println("actionPerformed")
-    if (holdTimer.isRunning) {
-      val model = bg.selection
-      if (model != null) {
-        println(model.actionCommand)
-      }
-      holdTimer.stop()
-    }
-  }
-
-  override fun mousePressed(e: MouseEvent) {
-    println("mousePressed")
-    val c = e.component
-    if (SwingUtilities.isLeftMouseButton(e) && c is AbstractButton && c.isEnabled) {
-      arrowButton = c
-      holdTimer.start()
-    }
-  }
-
-  override fun mouseReleased(e: MouseEvent) {
-    holdTimer.stop()
-  }
-
-  override fun mouseExited(e: MouseEvent) {
-    if (holdTimer.isRunning) {
-      holdTimer.stop()
-    }
-  }
-
-  override fun mouseEntered(e: MouseEvent) {
-    /* not needed */
-  }
-
-  override fun mouseClicked(e: MouseEvent) {
-    /* not needed */
-  }
-
-  companion object {
-    private fun makeMenuButton(m: MenuContext): AbstractButton {
-      val b = JRadioButton(m.command)
-      b.actionCommand = m.command
-      b.foreground = m.color
-      b.border = BorderFactory.createEmptyBorder()
-      return b
-    }
   }
 }
 
