e31c852bc36cc21543baabd5a779365016d94fbc
aterai <aterai@outlook.com>
2023-01-22 09:19:27 +0900

    PMD: SystemPrintln

--- DesktopProperty/src/main/kotlin/example/App.kt
+++ DesktopProperty/src/main/kotlin/example/App.kt
@@ -3,16 +3,37 @@ package example
 import java.awt.* // ktlint-disable no-wildcard-imports
 import java.beans.PropertyChangeEvent
 import java.beans.PropertyChangeListener
+import java.lang.invoke.MethodHandles
+import java.util.logging.Logger
 import javax.swing.* // ktlint-disable no-wildcard-imports
 import javax.swing.table.DefaultTableModel
 
-private val columnNames = arrayOf("Name", "Class", "Value")
-private val model = DefaultTableModel(null, columnNames)
+private val logger = Logger.getLogger(MethodHandles.lookup().lookupClass().name)
 
-private fun initModel(e: PropertyChangeEvent?) {
+fun makeUI(): Component {
+  val columnNames = arrayOf("Name", "Class", "Value")
+  val model = DefaultTableModel(null, columnNames)
+  val table = object : JTable(model) {
+    override fun isCellEditable(row: Int, column: Int) = false
+  }
+  table.autoCreateRowSorter = true
+  val l = PropertyChangeListener { e -> updateModel(model, e) }
+  Toolkit.getDefaultToolkit().addPropertyChangeListener("win.xpstyle.colorName", l)
+  Toolkit.getDefaultToolkit().addPropertyChangeListener("awt.multiClickInterval", l)
+  updateModel(model, null)
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun updateModel(model: DefaultTableModel, e: PropertyChangeEvent?) {
   if (e != null) {
-    println("----\n${e.propertyName}\n")
-    println(Toolkit.getDefaultToolkit().getDesktopProperty(e.propertyName))
+    logger.info {
+      val n = e.propertyName
+      val p = Toolkit.getDefaultToolkit().getDesktopProperty(n)
+      "$n: $p\n"
+    }
   }
   model.rowCount = 0
   val tk = Toolkit.getDefaultToolkit()
@@ -20,21 +41,6 @@ private fun initModel(e: PropertyChangeEvent?) {
     val o = tk.getDesktopProperty(it.toString())
     val row = arrayOf(it, o.javaClass, o)
     model.addRow(row)
-  }
-}
-
-fun makeUI(): Component {
-  val table = object : JTable(model) {
-    override fun isCellEditable(row: Int, column: Int) = false
-  }
-  table.autoCreateRowSorter = true
-  val l = PropertyChangeListener { e -> initModel(e) }
-  Toolkit.getDefaultToolkit().addPropertyChangeListener("win.xpstyle.colorName", l)
-  Toolkit.getDefaultToolkit().addPropertyChangeListener("awt.multiClickInterval", l)
-  initModel(null)
-  return JPanel(BorderLayout()).also {
-    it.add(JScrollPane(table))
-    it.preferredSize = Dimension(320, 240)
   }
 }
 
