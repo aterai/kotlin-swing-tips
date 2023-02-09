c6a57e67b441f9851a4e46fd008081dea6cd015a
aterai <aterai@outlook.com>
2023-02-10 00:30:46 +0900

    refactor: add example to blur JButton with JLayer

--- BlurButton/src/main/kotlin/example/App.kt
+++ BlurButton/src/main/kotlin/example/App.kt
@@ -1,42 +1,45 @@
 package example
 
 import java.awt.* // ktlint-disable no-wildcard-imports
+import java.awt.event.ActionEvent
 import java.awt.image.BufferedImage
 import java.awt.image.ConvolveOp
 import java.awt.image.Kernel
 import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.plaf.LayerUI
 
 fun makeUI(): Component {
-  val p0 = JPanel()
-  p0.border = BorderFactory.createTitledBorder("Default JButton")
-  val b0 = JButton("Default JButton")
-  p0.add(b0)
+  val p = JPanel()
+  p.border = BorderFactory.createTitledBorder("Apply a blur effect if disabled")
 
-  val p1 = JPanel()
-  p1.border = BorderFactory.createTitledBorder("Blurred JButton1")
-  val b1 = BlurredButton("Blurred JButton1")
-  p1.add(b1)
+  val b0 = JButton("<html>Default <font color='red'>JButton")
+  p.add(b0)
 
-  val p2 = JPanel()
-  p2.border = BorderFactory.createTitledBorder("Blurred JButton(ConvolveOp.EDGE_NO_OP)")
-  val b2 = BlurButton("Blurred JButton2")
-  p2.add(b2)
+  val b1 = BlurredButton("Blurred JButton")
+  p.add(b1)
 
-  val box = Box.createVerticalBox()
-  listOf(p0, p1, p2).forEach {
-    box.add(it)
-    box.add(Box.createVerticalStrut(10))
-  }
+  val b2 = BlurButton("Blurred JButton(ConvolveOp.EDGE_NO_OP)")
+  p.add(b2)
 
-  val button = JToggleButton("setEnabled(false)")
+  val b3 = JButton("<html>Blurred <font color='blue'>JLayer")
+  p.add(JLayer(b3, BlurLayerUI()))
+
+  val button = JCheckBox("setEnabled", true)
   button.addActionListener { e ->
     val f = (e.source as? AbstractButton)?.isSelected ?: false
-    listOf(b0, b1, b2).forEach { it.isEnabled = !f }
+    listOf(b0, b1, b2, b3).forEach { it.isEnabled = !f }
   }
 
-  return JPanel(BorderLayout()).also {
-    it.add(box, BorderLayout.NORTH)
-    it.add(button, BorderLayout.SOUTH)
+  val box = Box.createHorizontalBox()
+  box.add(Box.createHorizontalGlue())
+  box.add(button)
+
+  return JPanel(BorderLayout(10, 10)).also {
+    val mb = JMenuBar()
+    mb.add(LookAndFeelUtils.createLookAndFeelMenu())
+    EventQueue.invokeLater { it.rootPane.jMenuBar = mb }
+    it.add(p)
+    it.add(box, BorderLayout.SOUTH)
     it.preferredSize = Dimension(320, 240)
   }
 }
@@ -100,6 +103,84 @@ private class BlurButton(label: String) : JButton(label) {
       .05f, .05f, .05f
     )
     private val CONVOLVE_OP = ConvolveOp(Kernel(3, 3, DATA), ConvolveOp.EDGE_NO_OP, null)
+  }
+}
+
+private class BlurLayerUI<V : AbstractButton?> : LayerUI<V>() {
+  private var buf: BufferedImage? = null
+  override fun paint(g: Graphics, c: JComponent) {
+    if (c is JLayer<*>) {
+      val view = c.view
+      if (view.isEnabled) {
+        view.paint(g)
+      } else {
+        val d = view.size
+        val img = buf?.takeIf { it.width == d.width && it.height == d.height }
+          ?: BufferedImage(d.width, d.height, BufferedImage.TYPE_INT_ARGB)
+        val g2 = img.createGraphics()
+        view.paint(g2)
+        g2.dispose()
+        g.drawImage(CONVOLVE_OP.filter(buf, null), 0, 0, c)
+        buf = img
+      }
+    }
+  }
+
+  companion object {
+    private val CONVOLVE_OP = ConvolveOp(
+      Kernel(
+        3, 3, floatArrayOf(
+          .05f, .05f, .05f,
+          .05f, .60f, .05f,
+          .05f, .05f, .05f
+        )
+      ), ConvolveOp.EDGE_NO_OP, null
+    )
+  }
+}
+
+private object LookAndFeelUtils {
+  private var lookAndFeel = UIManager.getLookAndFeel().javaClass.name
+
+  fun createLookAndFeelMenu(): JMenu {
+    val menu = JMenu("LookAndFeel")
+    val buttonGroup = ButtonGroup()
+    for (info in UIManager.getInstalledLookAndFeels()) {
+      val b = JRadioButtonMenuItem(info.name, info.className == lookAndFeel)
+      initLookAndFeelAction(info, b)
+      menu.add(b)
+      buttonGroup.add(b)
+    }
+    return menu
+  }
+
+  fun initLookAndFeelAction(info: UIManager.LookAndFeelInfo, b: AbstractButton) {
+    val cmd = info.className
+    b.text = info.name
+    b.actionCommand = cmd
+    b.hideActionText = true
+    b.addActionListener { setLookAndFeel(cmd) }
+  }
+
+  @Throws(
+    ClassNotFoundException::class,
+    InstantiationException::class,
+    IllegalAccessException::class,
+    UnsupportedLookAndFeelException::class
+  )
+  private fun setLookAndFeel(newLookAndFeel: String) {
+    val oldLookAndFeel = lookAndFeel
+    if (oldLookAndFeel != newLookAndFeel) {
+      UIManager.setLookAndFeel(newLookAndFeel)
+      lookAndFeel = newLookAndFeel
+      updateLookAndFeel()
+    }
+  }
+
+  private fun updateLookAndFeel() {
+    for (window in Window.getWindows()) {
+      SwingUtilities.updateComponentTreeUI(window)
+    }
   }
 }
 
