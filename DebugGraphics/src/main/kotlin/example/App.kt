c39ffc1182e868a00a68614710d6571f25ab08a4
aterai <aterai@outlook.com>
2023-02-20 01:33:42 +0900

    add: DebugGraphics

---
+++ DebugGraphics/src/main/kotlin/example/App.kt
@@ -0,0 +1,47 @@
+package example
+
+import java.awt.* // ktlint-disable no-wildcard-imports
+import javax.swing.* // ktlint-disable no-wildcard-imports
+import javax.swing.table.DefaultTableModel
+
+fun makeUI(): Component {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf("aaa", 12, true),
+    arrayOf("bbb", 5, false),
+    arrayOf("ccc", 92, true)
+  )
+  val model = object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
+  }
+  val table = JTable(model)
+  table.autoCreateRowSorter = true
+
+  val repaintManager = RepaintManager.currentManager(table)
+  repaintManager.isDoubleBufferingEnabled = false
+  table.debugGraphicsOptions =
+    DebugGraphics.BUFFERED_OPTION or DebugGraphics.FLASH_OPTION or DebugGraphics.LOG_OPTION
+
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+fun main() {
+  EventQueue.invokeLater {
+    runCatching {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel")
+    }.onFailure {
+      it.printStackTrace()
+      Toolkit.getDefaultToolkit().beep()
+    }
+    JFrame().apply {
+      defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+      contentPane.add(makeUI())
+      pack()
+      setLocationRelativeTo(null)
+      isVisible = true
+    }
+  }
+}
