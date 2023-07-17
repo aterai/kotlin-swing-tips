98dde537fd0aa73c018a946b365920ef5d4e141c
aterai <aterai@outlook.com>
2023-07-18 08:03:59 +0900

    detekt: ReturnCount

--- SingleThreadExecutor/src/main/kotlin/example/App.kt
+++ SingleThreadExecutor/src/main/kotlin/example/App.kt
@@ -55,15 +55,12 @@ private fun addActionPerformed() {
   val key = model.rowCount
   val worker = object : BackgroundTask() {
     override fun process(c: List<Int>) {
-      if (isCancelled) {
-        return
-      }
-      if (!table.isDisplayable) {
+      if (table.isDisplayable && !isCancelled) {
+        c.forEach { model.setValueAt(it, key, 2) }
+      } else {
         cancel(true)
         executor.shutdown()
-        return
       }
-      c.forEach { model.setValueAt(it, key, 2) }
     }
 
     override fun done() {
