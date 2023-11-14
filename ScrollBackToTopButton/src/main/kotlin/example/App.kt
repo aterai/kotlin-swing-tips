f23a18238d29bc5474adb606bfb8c0f766d78208
aterai <aterai@outlook.com>
2023-11-15 07:05:31 +0900

    fix: Expecting a class body

--- ScrollBackToTopButton/src/main/kotlin/example/App.kt
+++ ScrollBackToTopButton/src/main/kotlin/example/App.kt
@@ -17,7 +17,7 @@ fun makeUI(): Component {
   scroll1.setRowHeaderView(LineNumberView(textArea))
   textArea.border = BorderFactory.createEmptyBorder(0, 2, 0, 0)
 
-  val table = object : JTable(500, 3)
+  val table = JTable(500, 3)
   val scroll2 = JScrollPane(table)
   SwingUtilities.invokeLater { table.scrollRectToVisible(table.getCellRect(500, 0, true)) }
 
