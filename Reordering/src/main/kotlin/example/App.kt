dc158a32805380a80e0f6bc9a9e2aebf9b3b0513
TERAI Atsuhiro <aterai@outlook.com>
2024-09-19 14:33:16 +0900

    refactor: simplify the example

--- Reordering/src/main/kotlin/example/App.kt
+++ Reordering/src/main/kotlin/example/App.kt
@@ -2,23 +2,12 @@ package example
 
 import java.awt.*
 import javax.swing.*
-import javax.swing.table.DefaultTableModel
 
 fun makeUI(): Component {
-  val columnNames = arrayOf("String", "Integer", "Boolean")
-  val data = arrayOf(
-    arrayOf("aaa", 12, true),
-    arrayOf("bbb", 5, false),
-    arrayOf("CCC", 92, true),
-    arrayOf("DDD", 0, false),
-  )
-  val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  val table = JTable(model)
+  val table = JTable(6, 3)
   val header = table.tableHeader
   header.reorderingAllowed = false
-  val checkBox = JCheckBox("disable column dragging", true)
+  val checkBox = JCheckBox("Disable reordering by dragging columns", true)
   checkBox.addActionListener {
     header.reorderingAllowed = (it.source as? JCheckBox)?.isSelected != true
   }
