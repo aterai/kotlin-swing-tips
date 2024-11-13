4ca220be5f2d9f3a119a675112120578d238c73f
TERAI Atsuhiro <aterai@outlook.com>
2024-11-14 03:06:45 +0900

    fix: 'DefaultTableColumnModel.getColumn(-1)' throws 'ArrayIndexOutOfBoundsException'

--- HeaderTooltips/src/main/kotlin/example/App.kt
+++ HeaderTooltips/src/main/kotlin/example/App.kt
@@ -3,29 +3,22 @@ package example
 import java.awt.*
 import java.awt.event.MouseEvent
 import javax.swing.*
-import javax.swing.table.DefaultTableModel
 import javax.swing.table.JTableHeader
 
 fun makeUI(): Component {
-  val columnNames = arrayOf("String", "Integer", "Boolean")
-  val data = arrayOf(
-    arrayOf("aaa", 12, true),
-    arrayOf("bbb", 5, false),
-    arrayOf("CCC", 92, true),
-    arrayOf("DDD", 0, false),
-  )
-  val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  val table = JTable(model)
+  val table = JTable(5, 3)
+  table.autoResizeMode = JTable.AUTO_RESIZE_OFF
   table.tableHeader = object : JTableHeader(table.columnModel) {
     override fun getToolTipText(e: MouseEvent): String? {
-      // val name = getTable().getColumnName(columnAtPoint(e.point))
-      val c = columnModel.getColumn(columnAtPoint(e.point))
+      val column = columnAtPoint(e.point)
+      return if (column >= 0) getToolTipText(column) else null
+    }
+
+    private fun getToolTipText(column: Int): String {
+      val c = columnModel.getColumn(column)
       return "%s (width=%dpx)".format(c.headerValue, c.width)
     }
   }
-
   return JPanel(BorderLayout()).also {
     it.add(JScrollPane(table))
     it.preferredSize = Dimension(320, 240)
