de35a30c88fe85194042d46e2953601ee55c7379
TERAI Atsuhiro <aterai@outlook.com>
2024-11-21 03:27:06 +0900

    refactor: argument 'javaClass#getResource(...)' might be null

--- TabThumbnail/src/main/kotlin/example/App.kt
+++ TabThumbnail/src/main/kotlin/example/App.kt
@@ -4,31 +4,38 @@ import java.awt.*
 import java.awt.event.MouseEvent
 import java.awt.image.BufferedImage
 import java.io.File
-import java.net.URL
+import javax.imageio.ImageIO
 import javax.swing.*
 
 fun makeUI(): Component {
   ToolTipManager.sharedInstance().dismissDelay = Int.MAX_VALUE
   val cl = Thread.currentThread().contextClassLoader
-  val icon = ImageIcon(cl.getResource("example/wi0124-48.png"))
+  val icon = cl
+    .getResource("example/wi0124-48.png")
+    ?.openStream()
+    ?.use(ImageIO::read)
+    ?.let { ImageIcon(it) }
+    ?: MissingIcon()
   val tabbedPane = TabThumbnailTabbedPane()
   tabbedPane.addTab("wi0124-48.png", null, JLabel(icon), "wi0124-48")
-  addImageTab(tabbedPane, cl.getResource("example/GIANT_TCR1_2013.jpg"))
-  addImageTab(tabbedPane, cl.getResource("example/CRW_3857_JFR.jpg"))
-
+  addImageTab(tabbedPane, "example/GIANT_TCR1_2013.jpg")
+  addImageTab(tabbedPane, "example/CRW_3857_JFR.jpg")
   return JPanel(BorderLayout()).also {
     it.add(tabbedPane)
     it.preferredSize = Dimension(320, 240)
   }
 }
 
-private fun addImageTab(
-  tabbedPane: JTabbedPane,
-  url: URL?,
-) {
-  requireNotNull(url) { "Resource not found" }
-  val scroll = JScrollPane(JLabel(ImageIcon(url)))
-  val f = File(url.file)
+private fun addImageTab(tabbedPane: JTabbedPane, path: String) {
+  val cl = Thread.currentThread().contextClassLoader
+  val icon = cl
+    .getResource(path)
+    ?.openStream()
+    ?.use(ImageIO::read)
+    ?.let { ImageIcon(it) }
+    ?: MissingIcon()
+  val scroll = JScrollPane(JLabel(icon))
+  val f = File(path)
   tabbedPane.addTab(f.name, null, scroll, "tooltip")
 }
 
@@ -88,18 +95,50 @@ private class TabThumbnailTabbedPane : JTabbedPane() {
   }
 
   override fun getToolTipText(e: MouseEvent): String? {
-    var str: String? = null
-    val index = indexAtLocation(e.x, e.y)
-    if (current == index) {
-      str = super.getToolTipText(e)
+    val idx = indexAtLocation(e.x, e.y)
+    return if (current == idx) {
+      super.getToolTipText(e)
+    } else {
+      current = idx
+      null
     }
-    current = index
-    return str
   }
 
   companion object {
     private const val SCALE = .15
   }
+}
+
+private fun makeMissingImage(): Image {
+  val missingIcon: Icon = MissingIcon()
+  val w = missingIcon.iconWidth
+  val h = missingIcon.iconHeight
+  val bi = BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB)
+  val g2 = bi.createGraphics()
+  missingIcon.paintIcon(null, g2, 0, 0)
+  g2.dispose()
+  return bi
+}
+
+private class MissingIcon : Icon {
+  override fun paintIcon(c: Component, g: Graphics, x: Int, y: Int) {
+    val g2 = g.create() as Graphics2D
+    val w = iconWidth
+    val h = iconHeight
+    val gap = w / 5
+    g2.color = Color.WHITE
+    g2.translate(x, y)
+    g2.fillRect(0, 0, w, h)
+    g2.color = Color.RED
+    g2.stroke = BasicStroke(w / 8f)
+    g2.drawLine(gap, gap, w - gap, h - gap)
+    g2.drawLine(gap, h - gap, w - gap, gap)
+    g2.dispose()
+  }
+
+  override fun getIconWidth() = 1_000
+
+  override fun getIconHeight() = 1_000
 }
 
 fun main() {
