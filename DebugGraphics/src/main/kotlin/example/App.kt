c3f9365eb1a6204fd70a6536edfcd4857e99c022
TERAI Atsuhiro <aterai@outlook.com>
2025-01-28 10:24:44 +0900

    ktlint: Exceeded max line length

--- DebugGraphics/src/main/kotlin/example/App.kt
+++ DebugGraphics/src/main/kotlin/example/App.kt
@@ -2,29 +2,33 @@ package example
 
 import java.awt.*
 import javax.swing.*
+import javax.swing.DebugGraphics.BUFFERED_OPTION
+import javax.swing.DebugGraphics.FLASH_OPTION
+import javax.swing.DebugGraphics.LOG_OPTION
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableModel
 
 fun makeUI(): Component {
+  val table = JTable(makeModel())
+  table.autoCreateRowSorter = true
+  val repaintManager = RepaintManager.currentManager(table)
+  repaintManager.isDoubleBufferingEnabled = false
+  table.debugGraphicsOptions = BUFFERED_OPTION or FLASH_OPTION or LOG_OPTION
+  return JPanel(BorderLayout()).also {
+    it.add(JScrollPane(table))
+    it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeModel(): TableModel {
   val columnNames = arrayOf("String", "Integer", "Boolean")
   val data = arrayOf(
     arrayOf("aaa", 12, true),
     arrayOf("bbb", 5, false),
     arrayOf("ccc", 92, true),
   )
-  val model = object : DefaultTableModel(data, columnNames) {
+  return object : DefaultTableModel(data, columnNames) {
     override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
-  val table = JTable(model)
-  table.autoCreateRowSorter = true
-
-  val repaintManager = RepaintManager.currentManager(table)
-  repaintManager.isDoubleBufferingEnabled = false
-  table.debugGraphicsOptions =
-    DebugGraphics.BUFFERED_OPTION or DebugGraphics.FLASH_OPTION or DebugGraphics.LOG_OPTION
-
-  return JPanel(BorderLayout()).also {
-    it.add(JScrollPane(table))
-    it.preferredSize = Dimension(320, 240)
   }
 }
 
