af9940fef6ded9644e41a8705e999e0a41519ada
TERAI Atsuhiro <aterai@outlook.com>
2025-05-15 03:10:24 +0900

    refactor: use property access syntax instead of setter method

--- examples/TableRowSorter/src/main/kotlin/example/App.kt
+++ examples/TableRowSorter/src/main/kotlin/example/App.kt
@@ -4,30 +4,30 @@ import java.awt.*
 import javax.swing.*
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellRenderer
+import javax.swing.table.TableModel
 import javax.swing.table.TableRowSorter
 
 fun makeUI(): Component {
-  val columnNames = arrayOf("String", "Integer", "Boolean")
-  val data = arrayOf(
-    arrayOf("aaa", 12, true),
-    arrayOf("bbb", 5, false),
-    arrayOf("CCC", 92, true),
-    arrayOf("DDD", 0, false),
-  )
-  val model = object : DefaultTableModel(data, columnNames) {
-    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
-  }
+  val model = makeModel()
   val table = JTable(model)
   table.rowSorter = TableRowSorter(model)
   val renderer = TableCellRenderer { tbl, value, isSelected, hasFocus, row, column ->
-    val r = tbl.tableHeader.defaultRenderer
-    r.getTableCellRendererComponent(tbl, value, isSelected, hasFocus, row, column).also {
-      val rs = tbl.rowSorter
-      if (rs is DefaultRowSorter<*, *>) {
-        val cmi = tbl.convertColumnIndexToModel(column)
-        it.foreground = if (rs.isSortable(cmi)) Color.BLACK else Color.GRAY
+    val renderer = tbl.tableHeader.defaultRenderer
+    renderer
+      .getTableCellRendererComponent(
+        tbl,
+        value,
+        isSelected,
+        hasFocus,
+        row,
+        column,
+      ).also {
+        val rs = tbl.rowSorter
+        if (rs is DefaultRowSorter<*, *>) {
+          val cmi = tbl.convertColumnIndexToModel(column)
+          it.foreground = if (rs.isSortable(cmi)) Color.BLACK else Color.GRAY
+        }
       }
-    }
   }
   val columns = table.columnModel
   for (i in 0..<columns.columnCount) {
@@ -50,13 +50,26 @@ fun makeUI(): Component {
   }
   val button = JButton("clear SortKeys")
   button.addActionListener {
-    table.rowSorter.setSortKeys(null)
+    table.rowSorter.sortKeys = null
   }
   return JPanel(BorderLayout()).also {
     it.add(check, BorderLayout.NORTH)
     it.add(button, BorderLayout.SOUTH)
     it.add(JScrollPane(table))
     it.preferredSize = Dimension(320, 240)
+  }
+}
+
+private fun makeModel(): TableModel {
+  val columnNames = arrayOf("String", "Integer", "Boolean")
+  val data = arrayOf(
+    arrayOf<Any>("aaa", 12, true),
+    arrayOf<Any>("bbb", 5, false),
+    arrayOf<Any>("CCC", 92, true),
+    arrayOf<Any>("DDD", 0, false),
+  )
+  return object : DefaultTableModel(data, columnNames) {
+    override fun getColumnClass(column: Int) = getValueAt(0, column).javaClass
   }
 }
 
