62cf6d1957a3379d6394d7b1c1025d652f51ada9
TERAI Atsuhiro <aterai@outlook.com>
2026-04-12 02:15:34 +0900

    fix: In dark mode, instead of a fade-out effect, it appears as a hazy display

--- examples/ListOverflowWithFadeScroll/src/main/kotlin/example/App.kt
+++ examples/ListOverflowWithFadeScroll/src/main/kotlin/example/App.kt
@@ -23,33 +23,58 @@ private fun makeScrollPane(c: Component) = object : JScrollPane(c) {
 }
 
 private class FadeScrollLayerUI : LayerUI<JScrollPane>() {
-  override fun paint(g: Graphics, c: JComponent?) {
+  override fun paint(g: Graphics, c: JComponent) {
     super.paint(g, c)
-    val g2 = g.create() as? Graphics2D ?: return
-    g2.setRenderingHint(
-      RenderingHints.KEY_ANTIALIASING,
-      RenderingHints.VALUE_ANTIALIAS_ON,
-    )
-    g2.paint = Color(0x12_FF_FF_FF, true)
     if (c is JLayer<*>) {
       val scroll = c.getView() as? JScrollPane ?: return
       val r = scroll.getViewportBorderBounds()
       val m = scroll.getVerticalScrollBar().getModel()
-      g2.clip = r
+
+      // 1. Dynamically get background color of JList
+      val bgc = getBgColor(scroll)
+      val transparent = Color(bgc.rgb and 0x00_FF_FF_FF, true)
+
+      val g2 = g.create() as Graphics2D
+      g2.setRenderingHint(
+        RenderingHints.KEY_ANTIALIASING,
+        RenderingHints.VALUE_ANTIALIAS_ON,
+      )
+      // g2.clip = r
+
+      // 2. Top edge fade (background color Å® transparent)
       if (m.minimum < m.value) {
-        for (i in OVERFLOW downTo 1) {
-          g2.fillRect(0, r.y - i, r.width, OVERFLOW - i)
-        }
+        g2.paint = GradientPaint(
+          0f,
+          r.y.toFloat(),
+          bgc,
+          0f,
+          (r.y + OVERFLOW).toFloat(),
+          transparent
+        )
+        g2.fillRect(r.x, r.y, r.width, OVERFLOW)
       }
+
+      // 3. Bottom edge fade (transparent Å® background color)
       if (m.value + m.extent < m.maximum) {
-        g2.translate(r.x, r.y + r.height - OVERFLOW)
-        for (i in 0..<OVERFLOW) {
-          g2.fillRect(0, i, r.width, OVERFLOW - i)
-        }
+        val fadeTop = r.y + r.height - OVERFLOW
+        g2.paint = GradientPaint(
+          0f,
+          fadeTop.toFloat(),
+          transparent,
+          0f,
+          (r.y + r.height).toFloat(),
+          bgc
+        )
+        g2.fillRect(r.x, fadeTop, r.width, OVERFLOW)
       }
+      g2.dispose()
     }
-    g2.dispose()
   }
+
+  private fun getBgColor(scroll: JScrollPane) = scroll.getViewport().view
+    ?.getBackground()
+    ?: UIManager.getColor("List.background")
+    ?: Color.WHITE
 
   companion object {
     const val OVERFLOW = 32
