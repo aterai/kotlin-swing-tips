e290bbd27bd6f27818addaffa02eb5a94b2cc560
TERAI Atsuhiro <aterai@outlook.com>
2026-04-26 01:34:40 +0900

    refactor: better class names, method names, and variable names

--- examples/SelectableToolTip/src/main/kotlin/example/App.kt
+++ examples/SelectableToolTip/src/main/kotlin/example/App.kt
@@ -9,71 +9,36 @@ import javax.swing.text.html.HTML
 import javax.swing.text.html.HTMLEditorKit
 
 private const val HTML_TEXT = """
-  <html>
-    <body>
-      <a href='https://ateraimemo.com/Swing.html' title='Title: JST'>Java Swing Tips</a>
-    </body>
-  </html>
+<html>
+<body>
+<a href='https://ateraimemo.com/Swing.html' title='JST'>Java Swing Tips</a>
+</body>
+</html>
 """
 
-fun makeUI(): Component {
-  val hint = JEditorPane()
-  hint.editorKit = HTMLEditorKit()
-  hint.isEditable = false
-  hint.isOpaque = false
+fun createUI(): Component {
+  val hintEditor = JEditorPane()
+  hintEditor.editorKit = HTMLEditorKit()
+  hintEditor.isEditable = false
+  hintEditor.isOpaque = false
 
   val check = JCheckBox()
   check.isOpaque = false
 
-  val panel = JPanel(BorderLayout())
-  panel.add(hint)
-  panel.add(check, BorderLayout.EAST)
+  val tooltipContentPanel = JPanel(BorderLayout())
+  tooltipContentPanel.add(hintEditor)
+  tooltipContentPanel.add(check, BorderLayout.EAST)
 
   val popup = JPopupMenu()
-  popup.add(JScrollPane(panel))
-  popup.border = BorderFactory.createEmptyBorder()
+  popup.add(JScrollPane(tooltipContentPanel))
+  popup.setBorder(BorderFactory.createEmptyBorder())
 
-  val editor = object : JEditorPane() {
-    override fun createToolTip(): JToolTip {
-      val tip = super.createToolTip()
-      tip.addHierarchyListener { e ->
-        val b = e.changeFlags and HierarchyEvent.SHOWING_CHANGED.toLong() != 0L
-        if (b && e.component.isShowing) {
-          panel.background = tip.background
-          popup.show(tip, 0, 0)
-        }
-      }
-      return tip
-    }
-  }
+  val editor = RichToolTipEditorPane(popup, tooltipContentPanel)
   editor.editorKit = HTMLEditorKit()
   editor.text = HTML_TEXT
   editor.isEditable = false
   editor.addHyperlinkListener { e ->
-    when (e.eventType) {
-      HyperlinkEvent.EventType.ACTIVATED -> {
-        JOptionPane.showMessageDialog(
-          editor,
-          "You click the link with the URL " + e.url,
-        )
-      }
-
-      HyperlinkEvent.EventType.ENTERED -> {
-        e.sourceElement
-          ?.let { it.attributes.getAttribute(HTML.Tag.A) as? AttributeSet }
-          ?.also {
-            editor.toolTipText = ""
-            val title = it.getAttribute(HTML.Attribute.TITLE).toString()
-            val url = e.url.toString()
-            hint.text = "<html>$title: <a href='$url'>$url</a>"
-            popup.pack()
-          }
-      }
-
-      HyperlinkEvent.EventType.EXITED -> {
-        editor.toolTipText = null
-      }
-    }
+    handleHyperlinkEvent(e, editor, hintEditor)
   }
 
   val p = JPanel(GridLayout(2, 1))
@@ -81,6 +46,54 @@ fun makeUI(): Component {
   p.add(JScrollPane(JTextArea(HTML_TEXT)))
   p.preferredSize = Dimension(320, 240)
   return p
+}
+
+private fun handleHyperlinkEvent(
+  e: HyperlinkEvent,
+  editor: RichToolTipEditorPane,
+  hintEditor: JEditorPane
+) {
+  when (e.eventType) {
+    HyperlinkEvent.EventType.ACTIVATED -> {
+      JOptionPane.showMessageDialog(
+        editor,
+        "You click the link with the URL " + e.url,
+      )
+    }
+
+    HyperlinkEvent.EventType.ENTERED -> {
+      e.sourceElement
+        ?.let { it.attributes.getAttribute(HTML.Tag.A) as? AttributeSet }
+        ?.also {
+          editor.toolTipText = ""
+          val title = it.getAttribute(HTML.Attribute.TITLE).toString()
+          val url = e.url.toString()
+          hintEditor.text = "<html>$title: <a href='$url'>$url</a>"
+          SwingUtilities.getWindowAncestor(hintEditor)?.pack()
+        }
+    }
+
+    HyperlinkEvent.EventType.EXITED -> {
+      editor.toolTipText = null
+    }
+  }
+}
+
+private class RichToolTipEditorPane(
+  private val popup: JPopupMenu,
+  private val tooltipContentPanel: JPanel,
+) : JEditorPane() {
+  override fun createToolTip(): JToolTip {
+    val tip = super.createToolTip()
+    tip.addHierarchyListener { e ->
+      val b = e.changeFlags and HierarchyEvent.SHOWING_CHANGED.toLong() != 0L
+      if (b && e.component.isShowing) {
+        tooltipContentPanel.background = tip.background
+        popup.show(tip, 0, 0)
+      }
+    }
+    return tip
+  }
 }
 
 fun main() {
@@ -93,7 +106,7 @@ fun main() {
     }
     JFrame().apply {
       defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-      contentPane.add(makeUI())
+      contentPane.add(createUI())
       pack()
       setLocationRelativeTo(null)
       isVisible = true
