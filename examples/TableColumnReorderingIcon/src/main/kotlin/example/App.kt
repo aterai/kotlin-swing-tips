86ec73316f14f0d6b941334acb7c1aa2890d6502
TERAI Atsuhiro <aterai@outlook.com>
2026-08-16 23:37:01 +0900

    ktlint: Exceeded max line length

--- examples/TableColumnReorderingIcon/src/main/kotlin/example/App.kt
+++ examples/TableColumnReorderingIcon/src/main/kotlin/example/App.kt
@@ -31,7 +31,8 @@ private class ColumnDragLayerUI : LayerUI<JScrollPane>() {
   override fun installUI(c: JComponent) {
     super.installUI(c)
     if (c is JLayer<*>) {
-      c.layerEventMask = AWTEvent.MOUSE_EVENT_MASK or AWTEvent.MOUSE_MOTION_EVENT_MASK
+      c.layerEventMask =
+        AWTEvent.MOUSE_EVENT_MASK or AWTEvent.MOUSE_MOTION_EVENT_MASK
     }
   }
 
