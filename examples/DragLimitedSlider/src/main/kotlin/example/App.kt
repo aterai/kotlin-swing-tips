153efc5228ccde0090d66d603cf1ba12fe271a90
TERAI Atsuhiro <aterai@outlook.com>
2026-08-27 02:16:28 +0900

    refactor: implement DragLimitedSlider without subclassing SliderUI

--- examples/DragLimitedSlider/src/main/kotlin/example/App.kt
+++ examples/DragLimitedSlider/src/main/kotlin/example/App.kt
@@ -1,22 +1,24 @@
 package example
 
-import com.sun.java.swing.plaf.windows.WindowsSliderUI
 import java.awt.*
 import java.awt.event.MouseEvent
 import javax.swing.*
 import javax.swing.plaf.basic.BasicSliderUI
 
-private const val MAXI = 80
-private const val MINI = 40
+private const val LOWER_LIMIT = 40
+private const val UPPER_LIMIT = 80
 
 fun createUI(): Component {
-  val slider1 = makeSlider("ChangeListener")
-  val slider2 = makeSlider("TrackListener")
-  if (slider2.ui is WindowsSliderUI) {
-    slider2.setUI(WindowsDragLimitedSliderUI(slider2))
-  } else {
-    slider2.setUI(BasicDragLimitedSliderUI(slider2))
-  }
+  val slider1 = JSlider(0, 100, LOWER_LIMIT)
+  initSlider(slider1)
+  val title1 = "ChangeListener"
+  slider1.setBorder(BorderFactory.createTitledBorder(title1))
+
+  val slider2 = DragLimitedSlider(0, 100, LOWER_LIMIT, LOWER_LIMIT, UPPER_LIMIT)
+  initSlider(slider2)
+  val title2 = "ChangeListener + DragLimitedSlider"
+  slider2.setBorder(BorderFactory.createTitledBorder(title2))
+
   return JPanel(GridLayout(2, 1, 5, 5)).also {
     it.add(slider1)
     it.add(slider2)
@@ -24,64 +26,83 @@ fun createUI(): Component {
   }
 }
 
-private fun makeSlider(title: String): JSlider {
-  val slider = JSlider(0, 100, 40)
-  slider.border = BorderFactory.createTitledBorder(title)
+private fun initSlider(slider: JSlider) {
   slider.majorTickSpacing = 10
   slider.paintTicks = true
   slider.paintLabels = true
-  // slider.labelTable?.elements()?.toList()
-  //   ?.filterIsInstance<JLabel>()
-  //   ?.filterNot { it.text.toInt() in MINI..MAXI }
-  //   ?.forEach { it.foreground = Color.RED }
-  (slider.labelTable as? Map<*, *>)?.forEach { (_, value) ->
-    if (value is JLabel && value.text.toInt() !in MINI..MAXI) {
-      value.foreground = Color.RED
+  val labelTable = slider.labelTable
+  if (labelTable is MutableMap<*, *>) {
+    labelTable.forEach { (_: Any?, value: Any?) ->
+      if (value is JLabel) {
+        highlightOutOfRange(value)
+      }
     }
   }
   slider.model.addChangeListener { e ->
     (e.source as? BoundedRangeModel)?.also {
-      it.value = it.value.coerceIn(MINI, MAXI)
-    }
-  }
-  return slider
-}
-
-private class WindowsDragLimitedSliderUI(
-  slider: JSlider,
-) : WindowsSliderUI(slider) {
-  override fun createTrackListener(slider: JSlider) = object : TrackListener() {
-    override fun mouseDragged(e: MouseEvent) { // case HORIZONTAL:
-      val halfThumbWidth = thumbRect.width / 2
-      val thumbLeft = e.x - offset
-      val maxPos = xPositionForValue(MAXI) - halfThumbWidth
-      val minPos = xPositionForValue(MINI) - halfThumbWidth
-      if (thumbLeft > maxPos) {
-        e.translatePoint(maxPos + offset - e.x, 0)
-      } else if (thumbLeft < minPos) {
-        e.translatePoint(minPos + offset - e.x, 0)
-      }
-      super.mouseDragged(e)
+      it.value = it.value.coerceIn(LOWER_LIMIT, UPPER_LIMIT)
     }
   }
 }
 
-private class BasicDragLimitedSliderUI(
-  slider: JSlider,
-) : BasicSliderUI(slider) {
-  override fun createTrackListener(slider: JSlider) = object : TrackListener() {
-    override fun mouseDragged(e: MouseEvent) { // case HORIZONTAL:
-      val halfThumbWidth = thumbRect.width / 2
-      val thumbLeft = e.x - offset
-      val maxPos = xPositionForValue(MAXI) - halfThumbWidth
-      val minPos = xPositionForValue(MINI) - halfThumbWidth
-      if (thumbLeft > maxPos) {
-        e.translatePoint(maxPos + offset - e.x, 0)
-      } else if (thumbLeft < minPos) {
-        e.translatePoint(minPos + offset - e.x, 0)
+private fun highlightOutOfRange(label: JLabel) {
+  val value = label.text.toInt()
+  if (value !in LOWER_LIMIT..UPPER_LIMIT) {
+    label.setForeground(Color.RED)
+  }
+}
+
+private class DragLimitedSlider(
+  min: Int,
+  max: Int,
+  value: Int,
+  private val lowerDragLimit: Int,
+  private val upperDragLimit: Int,
+) : JSlider(min, max, value) {
+  override fun processMouseEvent(e: MouseEvent) {
+    val id = e.getID()
+    val isPressed = id == MouseEvent.MOUSE_PRESSED
+    val isReleased = id == MouseEvent.MOUSE_RELEASED
+    if ((isPressed || isReleased) && isThumbDragEvent(e)) {
+      if (isPressed) {
+        startThumbDrag(e)
+      } else {
+        setValueIsAdjusting(false)
       }
-      super.mouseDragged(e)
+    } else {
+      super.processMouseEvent(e)
     }
+  }
+
+  override fun processMouseMotionEvent(e: MouseEvent) {
+    if (e.getID() == MouseEvent.MOUSE_DRAGGED && isThumbDragEvent(e)) {
+      setValue(getLimitedValue(e.getPoint()))
+    } else {
+      super.processMouseMotionEvent(e)
+    }
+  }
+
+  private fun startThumbDrag(e: MouseEvent) {
+    if (isRequestFocusEnabled) {
+      requestFocusInWindow()
+    }
+    setValueIsAdjusting(true)
+    setValue(getLimitedValue(e.getPoint()))
+  }
+
+  private fun isThumbDragEvent(e: MouseEvent): Boolean =
+    isEnabled && getUI() is BasicSliderUI &&
+      SwingUtilities.isLeftMouseButton(e)
+
+  private fun getLimitedValue(pt: Point): Int {
+    val ui = getUI() as BasicSliderUI
+    val horizontal = getOrientation() == HORIZONTAL
+    val value = if (horizontal) {
+      ui.valueForXPosition(pt.x)
+    } else {
+      ui.valueForYPosition(pt.y)
+    }
+    return value.coerceIn(lowerDragLimit, upperDragLimit)
   }
 }
 
